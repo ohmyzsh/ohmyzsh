cbaffd4a1cbf8120638f2969b9915df6c0388fb7
Robby Russell <robby@planetargon.com>
2009-09-01 07:41:18 -0700

    Moving the loading of all .zsh files into a different file so that we can remove zshrc from the repository

---
+++ oh-my-zsh.sh
@@ -0,0 +1,5 @@
+# Initializes Oh My Zsh
+
+# Load all of the config files in ~/oh-my-zsh that end in .zsh
+# TIP: Add files you don't want in git to .gitignore
+for i in $ZSH/*.zsh; do source $i; done;
