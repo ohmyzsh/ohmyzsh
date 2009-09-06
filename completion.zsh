b0bdeede6116056d35681a5b70801e8aabcbfbb1
Jamie van Dyke <fearoffish@fearofpro.local>
2009-09-06 13:19:33 +0100

    added case-insensitive auto completion

--- completion.zsh
+++ completion.zsh
@@ -11,6 +11,9 @@ compinit
 
 zmodload -i zsh/complist
 
+## case-insensitive (all),partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' hosts $( sed 's/[, ].*$//' $HOME/.ssh/known_hosts )
 
