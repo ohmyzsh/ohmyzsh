9f0df188f4dd820d2f72f8095619709e62808ff5
Christopher Sexton <csexton@gmail.com>
2009-09-22 14:48:55 -0400

    Git 1.6 support
    `git-symbolic-ref` needs to be `git symbolic-ref`

--- git.zsh
+++ git.zsh
@@ -11,7 +11,7 @@ ZSH_THEME_GIT_PROMPT_CLEAN=""
 
 # get the name of the branch we are on
 function git_prompt_info() {
-  ref=$(git-symbolic-ref HEAD 2> /dev/null) || return
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
