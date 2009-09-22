8fe67686dba78a273b058f7f7e3d26c286d269f2
James Cox <james@imaj.es>
2009-09-22 23:36:25 +0100

    would rather store history in $HOME and double size

--- history.zsh
+++ history.zsh
@@ -7,9 +7,8 @@ setopt HIST_IGNORE_DUPS
 
 ## Command history configuration
 #
-HISTFILE=$ZSH/log/.zsh_history
-HISTSIZE=2500
-SAVEHIST=2500
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=5000
+SAVEHIST=5000
 setopt hist_ignore_dups # ignore duplication command history list
 setopt share_history # share command history data
-
