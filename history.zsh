556ee4198f26731c85bfdd40ee1a2edfee755cb5
James Cox <james@imaj.es>
2009-09-23 00:44:57 +0100

    tidy up history

--- history.zsh
+++ history.zsh
@@ -1,10 +1,3 @@
-# History stuff.
-setopt HIST_VERIFY
-setopt INC_APPEND_HISTORY
-setopt SHARE_HISTORY
-setopt EXTENDED_HISTORY
-setopt HIST_IGNORE_DUPS
-
 ## Command history configuration
 #
 HISTFILE=$HOME/.zsh_history
@@ -12,3 +5,8 @@ HISTSIZE=5000
 SAVEHIST=5000
 setopt hist_ignore_dups # ignore duplication command history list
 setopt share_history # share command history data
+
+setopt hist_verify
+setopt inc_append_history
+setopt extended_history
+setopt hist_expire_dups_first
