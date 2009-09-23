8debd097fbea09032c27b2c8d69d8430d7ea0551
Robby Russell <robby@planetargon.com>
2009-09-23 07:43:36 -0700

    Moving all zsh config options into a lib/ subdirectory to make way for some upcoming changes to directory structure and configuration options

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -2,4 +2,4 @@
 
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
-for config_file ($ZSH/*.zsh) source $config_file
+for config_file ($ZSH/lib/*.zsh) source $config_file
