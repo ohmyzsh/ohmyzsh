ed457cbee7a9f1b962848272fb7eda6b4af010c3
Robby Russell <robby@planetargon.com>
2009-09-23 17:12:19 -0700

    Oh My Zsh gets a weekly auto-updater... the future is now!

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,5 +1,5 @@
 current_path=`pwd`
 echo "Upgrading Oh My Zsh"
 ( cd $ZSH && git pull origin master )
-echo "Done."
+echo "Hooray! Oh My Zsh has been updated and/or is at the current version. \nAny new updates will be reflected when you start your next terminal session."
 cd $current_path
