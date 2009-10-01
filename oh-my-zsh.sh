eddab5034cb19caa99a1a0e381de4dcf63ef0fe0
Jon Evans <jon@springyweb.com>
2009-10-01 08:55:07 +0100

    run update check *after* running custom configuration scripts. This means we are running it after $PATH has potentially been set up, which means it's more likely that commands like "git" will be found

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -6,3 +6,11 @@ for config_file ($ZSH/lib/*.zsh) source $config_file
 
 # Load all of your custom configurations from custom/
 for config_file ($ZSH/custom/*.zsh) source $config_file
+
+# Check for updates on initial load...
+if [ "$DISABLE_AUTO_UPDATE" = "true" ]
+then
+  return
+else
+  /bin/sh $ZSH/tools/check_for_upgrade.sh
+fi
