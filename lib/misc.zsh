5e2ad9a8ed883d75afc18b9a8b17ff563e2bfee5
Jon Evans <jon@springyweb.com>
2009-10-01 08:55:07 +0100

    run update check *after* running custom configuration scripts. This means we are running it after $PATH has potentially been set up, which means it's more likely that commands like "git" will be found

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -7,11 +7,3 @@ bindkey "^[m" copy-prev-shell-word
 
 ## jobs
 setopt long_list_jobs
-
-# Check for updates on initial load...
-if [ "$DISABLE_AUTO_UPDATE" = "true" ]
-then
-  return
-else
-  /bin/sh $ZSH/tools/check_for_upgrade.sh
-fi
