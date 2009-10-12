6ef2e1f51c42a1e8b96f5ffc8a5b8f1e715d1738
To1ne <toon@tonotdo.com>
2009-10-12 21:17:36 +0200

    Added some comment

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,19 +1,23 @@
+#!/bin/zsh
+
+# Push and pop directories on directory stack
 alias pu='pushd'
 alias po='popd'
 
-alias sc='ruby script/console'
-alias sd='ruby script/server --debugger'
+# Ruby related
 alias ss='thin --stats "/thin/stats" start'
 
-alias mr='mate CHANGELOG app config db lib public script spec test'
+# Basic directory operations
 alias .='pwd'
 alias ...='cd ../..'
 
+# Super user
 alias _='sudo'
 alias ss='sudo su -'
 
 #alias g='grep -in'
 
+# Git related
 alias g='git'
 alias gst='git status'
 alias gl='git pull'
@@ -24,24 +28,36 @@ alias gca='git commit -v -a'
 alias gb='git branch'
 alias gba='git branch -a'
 
+# Show history
 alias history='fc -l 1'
 
-alias ls='ls -F'
+# List direcory contents
+#alias ls='ls -F' # This messes up colors on my linux system
+alias l='ls -la'
 alias ll='ls -alr'
-alias l='ls'
-alias ll='ls -l'
 alias sl=ls # often screw this up
 
 alias sgem='sudo gem'
 
+# Find ruby file
 alias rfind='find . -name *.rb | xargs grep -n'
 
+# Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
 
+# TextMate
 alias et='mate . &'
 alias ett='mate app config lib db public spec test Rakefile Capfile Todo &'
 alias etp='mate app config lib db public spec test vendor/plugins vendor/gems Rakefile Capfile Todo &'
 alias etts='mate app config lib db public script spec test vendor/plugins vendor/gems Rakefile Capfile Todo &'
+
+## Ruby related
+# Ruby scripts
+alias sc='ruby script/console'
+alias sd='ruby script/server --debugger'
+
+# Editor Ruby file in TextMate
+alias mr='mate CHANGELOG app config db lib public script spec test'
 
 
 
