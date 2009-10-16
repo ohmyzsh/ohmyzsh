1de3f62255d811778a15213ef0a2a4620b2b3bcd
Toon Claes <toon@tonotdo.com>
2009-10-16 23:03:50 +0200

    Removed useless else

--- tools/install.sh
+++ tools/install.sh
@@ -2,10 +2,10 @@ if [ -d ~/.oh-my-zsh ]
 then
   echo "You already have Oh My Zsh installed. You'll need to remove ~/.oh-my-zsh if you want to install"
   exit
-else
-  echo "Cloning Oh My Zsh..."
-  /usr/bin/env git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
+
+echo "Cloning Oh My Zsh..."
+/usr/bin/env git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "Looking for an existing zsh config..."
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
@@ -27,4 +27,3 @@ chsh -s /bin/zsh
 echo "Hooray! Oh My Zsh has been installed."
 /bin/zsh
 source ~/.zshrc
-
