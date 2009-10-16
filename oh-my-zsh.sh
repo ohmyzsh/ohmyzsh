11d0b3c2ac271f8d87629c73c9832f135558a6a2
Toon Claes <toon@tonotdo.com>
2009-10-16 22:12:31 +0200

    extended path variable, temporary

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -6,3 +6,6 @@ for config_file ($ZSH/lib/*.zsh) source $config_file
 
 # Load all of your custom configurations from custom/
 for config_file ($ZSH/custom/*.zsh) source $config_file
+
+
+export PATH=~/bin:/opt/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/local/sbin:/opt/local/lib/postgresql83/bin
