e2dba2c3e94115829e9172294410cae4aedc7e96
Toon Claes <toon@tonotdo.com>
2009-11-04 20:37:18 +0100

    Joined the appearance so the theme can apply LSCOLORS too

---
+++ lib/appearance.zsh
@@ -0,0 +1,34 @@
+# Colors
+autoload colors; colors;
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
+
+#setopt no_beep
+setopt auto_cd
+setopt multios
+setopt cdablevarS
+
+if [[ x$WINDOW != x ]]
+then
+    SCREEN_NO="%B$WINDOW%b "
+else
+    SCREEN_NO=""
+fi
+
+# Apply theming defaults
+PS1="%n@%m:%~%# "
+
+# git theming default: Variables for theming the git info prompt
+# ZSH_THEME_GIT_PROMPT_PREFIX - Prefix at the very beginning of the prompt, before the branch name
+ZSH_THEME_GIT_PROMPT_PREFIX="git:("
+# ZSH_THEME_GIT_PROMPT_SUFFIX - At the very end of the prompt
+ZSH_THEME_GIT_PROMPT_SUFFIX=")"
+# ZSH_THEME_GIT_PROMPT_DIRTY  - Text to display if the branch is dirty
+ZSH_THEME_GIT_PROMPT_DIRTY="*"
+# ZSH_THEME_GIT_PROMPT_CLEAN  - Text to display if the branch is clean
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+# Setup the prompt with pretty colors
+setopt prompt_subst
+
+# Load the theme
+source "$ZSH/themes/$ZSH_THEME.zsh-theme"
