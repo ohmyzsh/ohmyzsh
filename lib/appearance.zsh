9c0dcdaefc87f9af941e4239a8806b957414e6c6
Toon Claes <toon@tonotdo.com>
2009-11-05 20:48:08 +0100

    parse error fix

--- lib/appearance.zsh
+++ lib/appearance.zsh
@@ -4,7 +4,8 @@ export LSCOLORS="Gxfxcxdxbxegedabagacad"
 #export LS_COLORS
 
 # Enable ls colors
-if [ "$DISABLE_LS_COLORS" != "true" ] then
+if [ "$DISABLE_LS_COLORS" != "true" ]
+then
   # Find the option for using colors in ls, depending on the version: Linux or BSD
   ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
 fi
