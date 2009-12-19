0d00b8a63643e5f572ba3c62b36a4a790a1a783a
Martin Klepsch <martinklepsch@gmail.com>
2009-12-19 14:35:57 +0100

    Putting Git related aliases into git.zsh to have them in context.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -25,21 +25,6 @@ alias ss='sudo su -'
 
 #alias g='grep -in'
 
-# Git related
-alias g='git'
-alias gst='git status'
-alias gl='git pull'
-alias gup='git fetch && git rebase'
-alias gp='git push'
-alias gd='git diff | mate'
-alias gdv='git diff -w "$@" | vim -R -'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gb='git branch'
-alias gba='git branch -a'
-alias gcount='git shortlog -sn'
-alias gcp='git cherry-pick'
-
 # Show history
 alias history='fc -l 1'
 
