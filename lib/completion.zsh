db18f4361beac88fd8f31fe9ac67113fdef146e3
Ole Reifschneider <mail@ole-reifschneider.de>
2010-01-19 15:51:54 +0100

    lib/completion: Don't select the first item of completion list.
    Based on a commit by: Ole Reifschneider <mail@ole-reifschneider.de>

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -1,6 +1,5 @@
 ## fixme - the load process here seems a bit bizarre
 
-setopt noautomenu
 setopt complete_in_word
 setopt always_to_end
 
@@ -30,7 +29,7 @@ unsetopt MENU_COMPLETE
 # should this be in keybindings?
 bindkey -M menuselect '^o' accept-and-infer-next-history
 
-zstyle ':completion:*:*:*:*:*' menu yes select
+zstyle ':completion:*:*:*:*:*' menu select
 # zstyle ':completion:*:*:*:*:processes' force-list always
 
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
