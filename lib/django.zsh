a06ec3426a10c69d565c49f04e7e87957d90e15f
Steve Losh <steve@dwaiter.com>
2010-03-19 03:44:13 +0800

    Add some useful Django-related aliases.

---
+++ lib/django.zsh
@@ -0,0 +1,6 @@
+alias pmr='python manage.py runserver'
+alias pmrp='python manage.py runserver_plus'
+alias pmrpg='pmrp --adminmedia=`pwd`/media/admin'
+alias pmsdb='python manage.py syncdb'
+alias pms='python manage.py shell'
+alias pmlf='python manage.py loaddata fixtures/*'
