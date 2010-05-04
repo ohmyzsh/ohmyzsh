4ea95c3c166df3d7201fa44f3a67f12ca711c8ca
Steve Losh <steve@stevelosh.com>
2010-05-04 14:36:48 -0400

    Start a custom version of startapp.

--- lib/django.zsh
+++ lib/django.zsh
@@ -9,3 +9,11 @@ alias pmlf='python manage.py loaddata fixtures/*'
 alias gs='gunicorn_django'
 alias gk='kill `cat .gunicorn.pid`'
 alias gl='tail -f .gunicorn.log'
+
+function djapp() {
+    mkdir -p $1/templates/$1
+    touch $1/__init__.py
+    echo "from django.db import models\n\n" > $1/models.py
+    echo "from django.contrib import admin\nfrom $1.models import *\n\n" > $1/admin.py
+    echo "from django.conf.urls.defaults import *\n\n" > $1/urls.py
+}
