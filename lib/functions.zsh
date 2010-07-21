8b78c0135f0e01ad07526b6fdab440ed01840390
Steve Losh <steve@stevelosh.com>
2010-07-21 16:52:28 -0400

    Remove some unused aliases/functions.

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -32,15 +32,3 @@ function uninstall_oh_my_zsh() {
 function upgrade_oh_my_zsh() {
   /bin/sh $ZSH/tools/upgrade.sh
 }
-
-function tab() {
-  osascript 2>/dev/null <<EOF
-    tell application "System Events"
-      tell process "Terminal" to keystroke "t" using command down
-    end
-    tell application "Terminal"
-      activate
-      do script with command "cd \"$PWD\"; clear; $*" in window 1
-    end tell
-EOF
-}
