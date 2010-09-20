e3e284016c8de8889150fa6f59f78bff93b14e83
Matt Cable <wozz@wookie.net>
2010-09-19 19:43:49 -0600

    Added installation function

--- plugins/dirpersist.plugin.zsh
+++ plugins/dirpersist.plugin.zsh
@@ -4,6 +4,17 @@
 # 
 # Run dirpersiststore in ~/.zlogout
 
+dirpersistinstall () {
+    if grep -qL 'dirpersiststore' ~/.zlogout; then
+    else
+        if read -q \?"Would you like to set up your .zlogout file for use with dirspersist? (y/n) "; then
+            echo "# Store dirs stack\n# See ~/.oh-my-zsh/plugins/dirspersist.plugin.zsh\ndirpersiststore" >> ~/.zlogout
+        else
+            echo "If you don't want this message to appear, remove dirspersist from \$plugins"
+        fi
+    fi
+}
+
 dirpersiststore () {
     dirs -p | tail -r | perl -ne 'chomp;s/([& ])/\\$1/g ;print "if [ -d $_ ]; then pushd -q $_; fi\n"' > ~/.zdirstore
 }
@@ -16,6 +27,8 @@ dirpersistrestore () {
 
 DIRSTACKSIZE=10
 setopt autopushd pushdminus pushdsilent pushdtohome pushdignoredups
+
+dirpersistinstall
 dirpersistrestore
 
 # Make popd changes permanent without having to wait for logout
