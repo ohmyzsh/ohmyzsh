d3bf47d5401e23828bba67d3bdc742d69d554232
Jake Bell <jake.b.bell@gmail.com>
2010-10-01 16:01:04 -0500

    Adding ability to override plugins from the custom directory.

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -12,7 +12,13 @@ for config_file ($ZSH/custom/*.zsh) source $config_file
 
 # Load all of the plugins that were defined in ~/.zshrc
 plugin=${plugin:=()}
-for plugin ($plugins) source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+for plugin ($plugins) 
+  # First check in the custom/ directory.
+  if [ -d $ZSH/custom/plugins/$plugin ]; then
+    source $ZSH/custom/plugins/$plugin/$plugin.plugin.zsh;
+  else
+    source $ZSH/plugins/$plugin/$plugin.plugin.zsh;
+  fi
 
 # Check for updates on initial load...
 if [ "$DISABLE_AUTO_UPDATE" = "true" ]
