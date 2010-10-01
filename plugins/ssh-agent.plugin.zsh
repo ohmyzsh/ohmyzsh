d647a9b25c43b75eea6fee29f27affe5edc4b72e
Robby Russell <robby@planetargon.com>
2010-09-30 20:54:45 -0700

    Tidying up the spacing to bring in line with coding standards

--- plugins/ssh-agent.plugin.zsh
+++ plugins/ssh-agent.plugin.zsh
@@ -4,21 +4,20 @@
 local SSH_ENV=$HOME/.ssh/environment
 
 function start_agent {
-     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
-     chmod 600 ${SSH_ENV}
-     . ${SSH_ENV} > /dev/null
-     /usr/bin/ssh-add;
+  /usr/bin/env ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
+  chmod 600 ${SSH_ENV}
+  . ${SSH_ENV} > /dev/null
+  /usr/bin/ssh-add;
 }
 
 # Source SSH settings, if applicable
 
 if [ -f "${SSH_ENV}" ]; then
-     . ${SSH_ENV} > /dev/null
-     #ps ${SSH_AGENT_PID} doesn't work under cywgin
-     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-         start_agent;
-     }
+  . ${SSH_ENV} > /dev/null
+  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+    start_agent;
+  }
 else
-     start_agent;
+  start_agent;
 fi
 
