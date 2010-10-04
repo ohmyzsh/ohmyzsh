f92d4c20bc00608536c05c33f4eaf572c41d9101
MatToufoutu <mattoufootu@gmail.com>
2010-10-04 11:24:56 +0200

    added completion for pip

---
+++ lib/pip_completion.zsh
@@ -0,0 +1,10 @@
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
+compctl -K _pip_completion pip
+
