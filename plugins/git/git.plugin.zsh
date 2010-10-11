6bb0c1c9269025867e886b76bcbf737e5a54400d
Sven Lito <me@svenlito.com>
2010-10-11 02:43:51 +0100

    added git log with graph

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -27,6 +27,8 @@ alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
 alias glg='git log --stat --max-count=5'
 compdef _git glg=git-log
+alias glgg='git log --graph --max-count=5'
+compdef _git glgg=git-log
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
