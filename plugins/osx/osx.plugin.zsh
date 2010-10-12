2bbb2a6a4fbd5922f240ac3de3c36ae87f917d30
Sven Lito <me@svenlito.com>
2010-10-12 20:11:59 +0100

    added OS X finder function

--- plugins/osx/osx.plugin.zsh
+++ plugins/osx/osx.plugin.zsh
@@ -9,3 +9,17 @@ function tab() {
     end tell
 EOF
 }
+
+# Functions for opening/finding/selecting things in OS X Finder
+
+function finder() {
+    if [[ $# -eq 0 ]]; then
+        open $PWD
+    elif [[ $# -eq 1 ]]; then
+        open $1
+    else
+        for dir in $argv; do
+            open $dir
+        done
+    fi
+}
