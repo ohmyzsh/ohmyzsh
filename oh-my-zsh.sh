18eaede408378fd499f8c36a5a101507ab5f460f
Zifei Tong <chevalun@gmail.com>
2010-10-16 02:46:14 +0800

    Load all plugins from each plugin folder

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -12,7 +12,7 @@ for config_file ($ZSH/custom/*.zsh) source $config_file
 
 # Load all of the plugins that were defined in ~/.zshrc
 plugin=${plugin:=()}
-for plugin ($plugins) source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+for plugin ($plugins) for plugin_file ($ZSH/plugins/$plugin/*.plugin.zsh) source $plugin_file
 
 # Check for updates on initial load...
 if [ "$DISABLE_AUTO_UPDATE" = "true" ]
