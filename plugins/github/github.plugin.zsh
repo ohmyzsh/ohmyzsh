fd71de112d15cd7295ba5c311b2ade2bf6e028f8
Aziz Light <aziiz.light@gmail.com>
2010-11-07 04:21:29 +0800

    Wrapped git in hub, if hub is available.

--- plugins/github/github.plugin.zsh
+++ plugins/github/github.plugin.zsh
@@ -2,3 +2,14 @@
 fpath=($ZSH/plugins/github $fpath)
 autoload -U compinit
 compinit -i
+
+# git + hub = github
+# https://github.com/defunkt/hub
+function git()
+{
+	if [ -x $(which hub) ]; then
+		hub "$@"
+	else
+		git "$@"
+	fi
+}
