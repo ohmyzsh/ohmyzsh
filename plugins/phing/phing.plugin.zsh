bdaf872ce6e411f881e5e6138fc61a9c1645e67a
Claus Witt <claus@wittnezz.dk>
2010-11-17 11:17:03 +0100

    Removed the echo statement - no need for that.

--- plugins/phing/phing.plugin.zsh
+++ plugins/phing/phing.plugin.zsh
@@ -10,7 +10,6 @@ _phing_does_target_list_need_generating () {
 _phing () {
   if [ -f build.xml ]; then
     if _phing_does_target_list_need_generating; then
-      echo "\nGenerating .phing_targets..." > /dev/stderr
       phing -l |grep -v ":" |grep -v "^$"|grep -v "\-" > .phing_targets
     fi
     compadd `cat .phing_targets`
