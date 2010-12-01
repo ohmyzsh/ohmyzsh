2b61ddee5b80134268c925ad70fd6ad8c1e35393
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-12-01 17:09:15 +0100

    Allow to use another key Use $terminfo to get key codes

--- plugins/refresh/refresh.plugin.zsh
+++ plugins/refresh/refresh.plugin.zsh
@@ -1,8 +1,16 @@
 #!/usr/bin/env zsh
 # ------------------------------------------------------------------------------
 # Simple ZLE widget that binds F5 to "source ~/.zshrc".
+#
+# You can bind it to another ikey by setting the REFRESH_KEY
+# environment variable, for example to bind F4 instead of F5:
+#   export REFRESH_KEY=$terminfo[kf4]
 # ------------------------------------------------------------------------------
+
+# Default key is F5.
+DEFAULT_REFRESH_KEY=$terminfo[kf5]
 
 _refresh() { source ~/.zshrc }
 zle -N _refresh
-bindkey '^[[15~' _refresh
+bindkey ${REFRESH_KEY:-$DEFAULT_REFRESH_KEY} _refresh
+
