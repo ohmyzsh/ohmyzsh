60b4fff076ff42994899ff1f13fdb612f7c55b4d
Alek Rollyson <al3k@al3k.net>
2010-12-02 01:57:22 -0500

    added sudo to aliases for them to work

--- plugins/pacman/pacman.plugin.zsh
+++ plugins/pacman/pacman.plugin.zsh
@@ -1,7 +1,7 @@
 #aliases
-alias paci='pacman -Syu' #works for both update and install
-alias pacr='pacman -R'   #delete packages
-alias pacs='pacman -Ss'  #search packages
+alias paci='sudo pacman -Syu' #works for both update and install
+alias pacr='sudo pacman -R'   #delete packages
+alias pacs='sudo pacman -Ss'  #search packages
 
 #pacman completion
 zstyle ':completion:*:pacman:*' force-list always
