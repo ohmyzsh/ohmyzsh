0d59069136adc2066ef23faed52950f32ec239f8
Anton Stroganov <stroganov.a@gmail.com>
2010-12-04 16:43:03 -0800

    added missing rake completion plugin back in

---
+++ plugins/rake/rake.plugin.zsh
@@ -0,0 +1,20 @@
+function _rake_does_task_list_need_generating () {
+  if [ ! -f .rake_tasks ]; then return 0;
+  else
+    accurate=$(stat -f%m .rake_tasks)
+    changed=$(stat -f%m Rakefile)
+    return $(expr $accurate '>=' $changed)
+  fi
+}
+
+function _rake () {
+  if [ -f Rakefile ]; then
+    if _rake_does_task_list_need_generating; then
+      echo "\nGenerating .rake_tasks..." > /dev/stderr
+      rake --silent --tasks | cut -d " " -f 2 > .rake_tasks
+    fi
+    compadd `cat .rake_tasks`
+  fi
+}
+
+compdef _rake rake
