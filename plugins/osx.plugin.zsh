7ee5f7ab33d2abcb52318b09dcc75484967e632e
Rachel Ober <rachelober@gmail.com>
2010-12-06 17:32:43 -0500

    Adding aliases for git and my own OSX commands to their respective plugins

--- plugins/osx.plugin.zsh
+++ plugins/osx.plugin.zsh
@@ -9,3 +9,40 @@ function tab() {
     end tell
 EOF
 }
+
+# OSX doesn't have wget
+alias wget='curl -O'
+
+# Postgres for Mac OS X
+if [ -x /usr/local/pgsql ]; then
+  export PATH=/usr/local/pgsql/bin:$PATH
+fi
+ 
+# Macports for Mac OS X
+if [ -x /opt/local/bin/port ]; then
+  export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+  export MANPATH=/opt/local/share/man:$MANPATH
+  
+  alias Pg='sudo port -v install'
+  alias Ps='port search'
+  alias Pi='port info'
+  alias Pr='sudo port uninstall'
+  alias dsl='port installed | grep -i'
+ 
+  function InitMacports () {
+    cmds=(
+      cat chgrp chmod chown cp dd df diff du false head
+      ln ls mkdir mkfifo mv pwd rm rmdir sleep sort
+      stat tail tee true uniq who whoami yes
+    )
+
+    for cmd in $cmds; do
+      ln -vs /opt/local/bin/g$cmd /usr/local/bin/$cmd
+    done
+  }
+fi
+ 
+# Fink for Mac OS X
+if [ -x /sw/bin/init.sh ]; then
+  _append_to_path /sw/bin/init.sh
+fi
