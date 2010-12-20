14628eef371a79df478973731ce49fc3eb45de36
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-12-20 12:50:55 -0300

    Removed remote_console. It doesn't handle rvm, capistrano, and so on.

--- plugins/rails3/rails3.plugin.zsh
+++ plugins/rails3/rails3.plugin.zsh
@@ -6,7 +6,3 @@ alias rdbm='rake db:migrate db:test:clone'
 alias rc='ruby script/rails console'
 alias rd='ruby script/rais server --debugger'
 alias devlog='tail -f log/development.log'
-
-function remote_console() {
-  /usr/bin/env ssh $1 "( cd $2 && ruby script/rails console production )"
-}
