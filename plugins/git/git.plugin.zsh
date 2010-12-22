612dc5abfb9e2f46b727353869526aef6ed46574
MatToufoutu <mattoufootu@gmail.com>
2010-12-22 16:26:44 +0100

    resolved another conflict with upstream

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -13,10 +13,7 @@ alias gb='git branch'
 alias gba='git branch -a'
 alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
-<<<<<<< HEAD:plugins/git.plugin.zsh
-=======
 alias glg='git log --stat --max-count=5'
->>>>>>> upstream/master:plugins/git/git.plugin.zsh
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
