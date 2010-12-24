cd35d86fffc44f998fb2a3c9ba28ab1a2b21a99e
SuprDewd <suprdewd@gmail.com>
2010-12-24 22:20:57 +0000

    Added a function to extract various archives. Also an alias for the function.

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -37,3 +37,24 @@ function take() {
   mkdir -p $1
   cd $1
 }
+
+function extract() {
+  if [[ -f $1 ]]; then
+    case $1 in
+      *.tar.bz2) tar xvjf $1;;
+      *.tar.gz) tar xvzf $1;;
+      *.bz2) bunzip $1;;
+      *.rar) unrar $1;;
+      *.gz) gunzip $1;;
+      *.tar) tar xvf $1;;
+      *.tbz2) tar xvjf $1;;
+      *.tgz) tar xvzf $1;;
+      *.zip) unzip $1;;
+      *.Z) uncompress $1;;
+      *.7z) 7z x $1;;
+      *) echo "'$1' cannot be extracted via >extract<";;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
