e188ded31f23d13a5d376ae3c7f7db5dffd93de8
Robin Ramael <robin.ramael@gmail.com>
2011-01-07 15:03:14 +0100

    Added a python plugin, which adds, among others, command completion and editing commands in a texteditor to the python prompt
    the plugin sets a provided pythonrc file as the startupscript for the python cli.

---
+++ plugins/python/python.plugin.zsh
@@ -0,0 +1,17 @@
+# use the ./pythonrc.py file as the rc for the python
+# this enables among other things:
+#   - command completion on <tab>, 
+#   - prettyprinting of long variables 
+#   - a colored prompt 
+#   - editing commands in an editor with '\e'
+#       to change the editor: set the EDITOR env variable.
+
+PYTHONSTARTUP=$ZSH/plugins/python/pythonrc.py
+export PYTHONSTARTUP
+
+#aliases
+alias py=python
+alias pytohn=python
+alias pyton=python
+alias py=python
+alias py=python
