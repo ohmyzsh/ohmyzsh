f09d6bd00ffbc641700d159c687e0c7797f55aa6
Michael Komitee <mkomitee@me.com>
2011-01-07 14:34:20 -0500

    adding bindings

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -1,2 +1,6 @@
 bindkey -M viins "^r" vi-history-search-backward
 bindkey -M vicmd "^r" vi-history-search-backward
+bindkey -M vicmd "f" vi-find-next-char
+bindkey -M vicmd "F" vi-find-prev-char
+bindkey -M viins "^F" vi-find-next-char
+bindkey -M viins "^P" vi-find-prev-char
