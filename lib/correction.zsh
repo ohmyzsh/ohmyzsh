cdeb88ec7b94785b8ecea07568f540fc1e963f76
Michael Komitee <mkomitee@me.com>
2011-01-08 13:46:57 -0500

    not allowing autocorrect for rm or rmdir

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -2,6 +2,8 @@ setopt correct_all
 
 alias man='nocorrect man'
 alias mv='nocorrect mv'
+alias rm='nocorrect rm'
+alias rmdir='nocorrect rmdir'
 alias mysql='nocorrect mysql'
 alias mkdir='nocorrect mkdir'
 alias gist='nocorrect gist'
