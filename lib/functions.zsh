faac3ba8fcad6ad1742f82a7d508a37c0c70d809
Renaud (Nel) Morvan <nel@w3fu.com>
2011-01-30 08:21:49 +0100

    Refactor window and tab title in tty
    Term window title and tab title are now skinable
    Tab title can be different from window title (when supported by term)
    Default theme is optimized of usuability (no %u@%m: $~ in a 10 char tab)
    Cleanup code duplication and add comment for supported terms
    On osX it works great on iterm, and is decent once you tweak Terminal pref
    Tested under GNU screen, iTerm and Apple Terminal, need to be tested on linux
    TODO implement Konsole support (via dbus)

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -1,26 +1,3 @@
-## fixme, i duplicated this in xterms - oops
-function title {
-  if [[ $TERM == "screen" ]]; then
-    # Use these two for GNU Screen:
-    print -nR $'\033k'$1$'\033'\\\
-
-    print -nR $'\033]0;'$2$'\a'
-  elif [[ ($TERM =~ "^xterm") ]] || [[ ($TERM == "rxvt") ]]; then
-    # Use this one instead for XTerms:
-    print -nR $'\033]0;'$*$'\a'
-  fi
-}
-
-function precmd {
-  title zsh "$PWD"
-}
-
-function preexec {
-  emulate -L zsh
-  local -a cmd; cmd=(${(z)1})
-  title $cmd[1]:t "$cmd[2,-1]"
-}
-
 function zsh_stats() {
   history | awk '{print $2}' | sort | uniq -c | sort -rn | head
 }
