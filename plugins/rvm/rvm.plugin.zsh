cfdaf3c1e4d1bb53911a67fb2e2a4997ac63a815
Alex Ray <ajray@ncsu.edu>
2011-02-05 21:54:57 -0500

    rvm plugin to fix cwd error

---
+++ plugins/rvm/rvm.plugin.zsh
@@ -0,0 +1,5 @@
+# Prevents ~rvm_rvmrc_cwd from showing in the prompt
+# http://rvm.beginrescueend.com/integration/zsh/
+unsetopt auto_name_dirs
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
