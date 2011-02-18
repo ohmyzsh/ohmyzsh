6467e90efbb8a0064045ccd2c09f1b0513d782c3
Ben Langfeld <ben@langfeld.me>
2011-02-18 12:48:27 +0000

    Add iTerm version of tab function (itab)

--- plugins/osx/osx.plugin.zsh
+++ plugins/osx/osx.plugin.zsh
@@ -30,3 +30,28 @@ end do_submenu
 do_submenu("Terminal", "Shell", "New Tab", 1)
 EOF
 }
+
+function itab() {
+savepath
+osascript >/dev/null <<EOF
+on do_submenu(app_name, menu_name, menu_item)
+    -- bring the target application to the front
+    tell application app_name
+      activate
+    end tell
+    tell application "System Events"
+      tell process app_name
+        tell menu bar 1
+          tell menu bar item menu_name
+            tell menu menu_name
+              click menu item menu_item
+            end tell
+          end tell
+        end tell
+      end tell
+    end tell
+end do_submenu
+
+do_submenu("iTerm", "Shell", "New Tab")
+EOF
+}
