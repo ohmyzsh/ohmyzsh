2190f8771f9038efa3b9c89d46ff1ee865ba4686
Christopher Chow <soliah@gmail.com>
2011-02-22 23:33:58 +1100

    Clean up rails plugin so it will merge cleanly with upstream.

--- plugins/rails/rails.plugin.zsh
+++ plugins/rails/rails.plugin.zsh
@@ -2,21 +2,21 @@ fpath=($ZSH/plugins/rails $fpath)
 autoload -U compinit
 compinit -i
 
-function _rails () {
-  if [ -e script/server ]; then
+function _rails_command () {
+  if [ -e "script/server" ]; then
     ruby script/$@
   else
     ruby script/rails $@
   fi
 }
 
-alias rs='_rails server'
-alias rd='_rails server --debugger'
-alias rg='_rails generate'
-alias rd='_rails destroy'
-alias rp='_rails plugin'
-alias rc='_rails console'
-alias rdb='_rails dbconsole'
+alias rs='_rails_command server'
+alias rd='_rails_command server --debugger'
+alias rg='_rails_command generate'
+alias rd='_rails_command destroy'
+alias rp='_rails_command plugin'
+alias rc='_rails_command console'
+alias rdb='_rails_command dbconsole'
 alias rdbm='rake db:migrate db:test:clone'
 alias devlog='tail -f log/development.log'
 alias cuke='bundle exec cucumber'
