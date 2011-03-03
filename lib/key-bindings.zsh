667bd563a168534756a8ac892193bbaf1b82d0c5
Sankara Rameswaran <sankara@bankbazaar.com>
2011-03-03 18:24:49 +0530

    Adding a couple of more useful key bindings

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -40,3 +40,9 @@ bindkey '^[[Z' reverse-menu-complete
 ## Fix weird sequence that rxvt produces
 #bindkey -s '^[[Z' '\t'
 #
+
+#Completion in the middle of a line
+bindkey '^i' expand-or-complete-prefix
+##pushes current command on command stack and gives blank line, after that line runs command stack is popped
+bindkey "^t" push-line-or-edit
+
