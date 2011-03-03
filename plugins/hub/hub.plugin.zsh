2717e147b303beef0f84e6dfbb2db562c8fc6740
Theodore Robert Campbell Jr <trcjr@stupidfoot.com>
2011-03-02 20:00:40 -0500

    blueyed's ZSH-fu is much stronger than mine.

--- plugins/hub/hub.plugin.zsh
+++ plugins/hub/hub.plugin.zsh
@@ -1,5 +1,5 @@
 # hub alias from defunkt
 # https://github.com/defunkt/hub
-if [ $( which hub > /dev/null 2>&1 ; echo -n $? ) -lt 1 ]; then
-    eval $( hub alias -s zsh )
+if which hub > /dev/null; then
+    eval $(hub alias -s zsh)
 fi
