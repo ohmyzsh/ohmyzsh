7380c1b7833b32d9bd1ca638ceae5661898143e5
Theodore Robert Campbell Jr <trcjr@stupidfoot.com>
2011-03-03 21:38:56 -0500

    Whoops, this was supposed to be in the last commit!

--- plugins/github/github.plugin.zsh
+++ plugins/github/github.plugin.zsh
@@ -1,3 +1,9 @@
+# hub alias from defunkt
+# https://github.com/defunkt/hub
+if which hub > /dev/null; then
+    eval $(hub alias -s zsh)
+fi
+
 # add github completion function to path
 fpath=($ZSH/plugins/github $fpath)
 autoload -U compinit
