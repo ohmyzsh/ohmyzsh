b82d287b4b535fbbb16e2dbdd2d9c0e7c759df54
Kuba Serafinowski <zizzfizzix@gmail.com>
2011-03-05 13:26:46 +0100

    Make sure that new line will be inserted.

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -7,6 +7,6 @@ echo ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
 echo '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
 echo '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
 echo '                        /____/'
-echo "Hooray! Oh My Zsh has been updated and/or is at the current version. \nAny new updates will be reflected when you start your next terminal session."
+echo -e "Hooray! Oh My Zsh has been updated and/or is at the current version. \nAny new updates will be reflected when you start your next terminal session."
 echo "To keep up on the latest, be sure to follow Oh My Zsh on twitter: http://twitter.com/ohmyzsh"
 cd $current_path
