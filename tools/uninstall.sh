abe0472dcb88a2482132ce1213f40aac56ce450a
Kuba Serafinowski <zizzfizzix@gmail.com>
2011-03-06 09:38:18 +0100

    Tweak uninstall script: remove copy-paste error, some texts etc.

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,18 +1,18 @@
-echo "Removing ~/.oh-my-zsh"
+echo "Removing Oh My Zsh"
 if [[ -d ~/.oh-my-zsh ]]
 then
   rm -rf ~/.oh-my-zsh
 fi
 
-echo "Looking for an existing zsh config..."
+echo "Looking for a previous zsh config..."
 if [ -f ~/.zshrc.pre-oh-my-zsh ] || [ -h ~/.zshrc.pre-oh-my-zsh ]
 then
-  echo "Found ~/.zshrc. Backing up to ~/.zshrc.pre-oh-my-zsh";
+  echo "File found! Moving it back in place";
   rm ~/.zshrc;
-  cp ~/.zshrc.pre-oh-my-zsh ~/.zshrc;
+  mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc;
   source ~/.zshrc;
 else
-  echo "Switching back to bash"
+  echo "File not found. Switching back to bash"
   chsh -s /bin/bash
   source /etc/profile
 fi
