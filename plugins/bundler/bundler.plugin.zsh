c7c0a24804f0de7d959c84a2a1a9fc234ce96250
Ben Langfeld <ben@langfeld.me>
2011-03-29 18:33:14 +0100

    Update the bundler plugin to follow the "vendor everything" routine
    * As described here: http://ryan.mcgeary.org/2011/02/09/vendor-everything-still-applies/

--- plugins/bundler/bundler.plugin.zsh
+++ plugins/bundler/bundler.plugin.zsh
@@ -1,3 +1,5 @@
-alias be="bundle exec"
-alias bi="bundle install"
-alias bu="bundle update"
+alias b="bundle"
+alias bi="b install --path vendor"
+alias bu="b update"
+alias be="b exec"
+alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
