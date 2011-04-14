a183bcbc6d8d61bd9c1077b45d1c5b3239e96a71
Tristan Carel <tristan.carel@gmail.com>
2011-04-14 07:17:10 +0200

    More comments

--- plugins/emacs/emacsclient.sh
+++ plugins/emacs/emacsclient.sh
@@ -6,5 +6,6 @@ x=`emacsclient --alternate-editor '' --eval '(x-display-list)' 2>/dev/null`
 if [ -z "$x" ] ;then
     emacsclient --alternate-editor "" --create-frame $@
 else
+    # prevent creating another X frame if there is at least one present.
     emacsclient --alternate-editor "" $@
 fi
