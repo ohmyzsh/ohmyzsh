3d4dc048a0876519ab2d45e252665b75c98e568a
gwjo <gowen72@gmail.com>
2011-04-19 19:58:46 -0400

    Change gdv() to use "git branch".  Fixes #282

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -9,7 +9,7 @@ alias gup='git fetch && git rebase'
 compdef _git gup=git-fetch
 alias gp='git push'
 compdef _git gp=git-push
-gdv() { git-diff -w "$@" | view - }
+gdv() { git diff -w "$@" | view - }
 compdef _git gdv=git-diff
 alias gc='git commit -v'
 compdef _git gc=git-commit
