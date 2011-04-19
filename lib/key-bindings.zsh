c7c959de60add8371e08926d642b2bbb7406ce84
Arthur Kalmenson <arthur.kalm@gmail.com>
2011-04-19 19:01:18 -0400

    Make the delete key work correctly, instead of outputting a ~

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -22,6 +22,10 @@ bindkey ' ' magic-space    # also do history expansion on space
 
 bindkey '^[[Z' reverse-menu-complete
 
+# Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
+
 # consider emacs keybindings:
 
 #bindkey -e  ## emacs key bindings
