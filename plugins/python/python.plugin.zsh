653db8fbe99084e9e0d80857fab2dfc2b2e24584
Thiago Avelino <thiagoavelinoster@gmail.com>
2011-04-30 17:20:21 -0300

    Remove python compiled byte-code

--- plugins/python/python.plugin.zsh
+++ plugins/python/python.plugin.zsh
@@ -2,3 +2,6 @@ alias spip='sudo pip'
 
 # Find python file
 alias pyfind='find . -name *.py | xargs grep -n'
+
+# Remove python compiled byte-code (By diofeher)
+alias pycrm='rm `find . | grep -E "*.(pyc|pyo)$"`'
