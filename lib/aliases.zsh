63f9e36645157e8638af8c21ad1591c8aebdb33d
tinogomes <tinorj@gmail.com>
2011-05-12 08:26:32 -0300

    Some new aliases, for reload .zshrc and `netstat`

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -23,3 +23,9 @@ alias sl=ls # often screw this up
 alias afind='ack-grep -il'
 
 alias x=extract
+
+alias reload="source ~/.zshrc"
+
+alias netr='netstat -rn '
+alias nets='netstat -an -f inet '
+alias netl='netstat -an -f inet | grep LISTEN'
