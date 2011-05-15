f45b43a267c45bab9aa00c95ad7fa34cf3c1810d
Pat Regan <thehead@patshead.com>
2011-05-15 17:05:11 -0400

    Add a "go" command plugin

---
+++ plugins/go-command/go-command.plugin.zsh
@@ -0,0 +1,27 @@
+go() {
+  if [ -f "$1" ]; then
+    if [ -n "`file $1 | grep '\(text\|empty\|no magic\)'`" ]; then
+      $EDITOR "$1"
+    else
+      if [ -e "`which xdg-open`" ]; then
+        if [ -n "$DISPLAY" ]; then
+          xdg-open "$1" > /dev/null
+        else
+          echo "DISPLAY not set:  xdg-open requires X11"
+        fi
+      else
+        echo "xdg-open not found:  unable to open '$1'"
+      fi
+    fi
+  elif [ -d "$1" ]; then
+    cd "$1"
+  elif [ "" = "$1" ]; then
+    cd
+  elif [ -n "`declare -f | grep '^j ()'`" ]; then
+    j "$1"
+  else
+    echo "Ran out of things to do with '$1'"
+  fi
+}
+
+alias g=go
