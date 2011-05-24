5c1b341132119f08047a506e2c2b06fa0c0db52e
Casey Watson <watsoncj@gmail.com>
2011-05-24 12:15:58 -0600

    Fix to random theme selection
     - themes array is 1-based
    
     - theme files names are located in indicies 1 through N inclusive
    
     - this resolves an issue where you would occasionally see: "no such
       file or directory. Random theme '' loaded..."

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -31,7 +31,7 @@ if [ "$ZSH_THEME" = "random" ]
 then
   themes=($ZSH/themes/*zsh-theme)
   N=${#themes[@]}
-  ((N=RANDOM%N))
+  ((N=(RANDOM%N)+1))
   RANDOM_THEME=${themes[$N]}
   source "$RANDOM_THEME"
   echo "[oh-my-zsh] Random theme '$RANDOM_THEME' loaded..."
