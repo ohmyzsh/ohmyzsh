7b8290a4e1c9229ba66795897344114cc20d9e98
Andrew Hodges <betawaffle@gmail.com>
2011-05-28 10:50:06 -0400

    Gem List Helper
    Add helper function to list gems in a pretty way (only with rvm, for now). Add
    missng EOF newline and a todo to the ruby plugin.

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -38,3 +38,15 @@ function rvm-update {
 function rvm-link-completion {
 	ln -s "$rvm_path/scripts/zsh/Completion/_rvm" "$ZSH/plugins/rvm/_rvm.official"
 }
+
+# TODO: Make this usable w/o rvm.
+function gems {
+	local current_ruby=`rvm-prompt i v p`
+	local current_gemset=`rvm-prompt g`
+
+	gem list $@ | sed \
+		-Ee "s/\([0-9\.]+( .+)?\)/$fg[blue]&$reset_color/g" \
+		-Ee "s|$(echo $rvm_path)|$fg[magenta]\$rvm_path$reset_color|g" \
+		-Ee "s/$current_ruby@global/$fg[yellow]&$reset_color/g" \
+		-Ee "s/$current_ruby$current_gemset$/$fg[green]&$reset_color/g"
+}
