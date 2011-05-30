80ed1cbc5dd9a82dfa54e5381d75210280246f6f
Andrew Hodges <betawaffle@gmail.com>
2011-05-30 10:07:15 -0400

    Rollback of Pull #366
    sorin-ionescu had some good points.

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -1,6 +1,5 @@
-fpath=($ZSH/plugins/rvm $fpath)
-autoload -U compinit
-compinit -i
+# What does this really do?
+# fpath=($ZSH/plugins/rvm $fpath)
 
 alias rubies='rvm list rubies'
 alias gemsets='rvm gemset list'
@@ -33,10 +32,6 @@ compdef _rb19 rb19
 function rvm-update {
 	rvm get head
 	rvm reload # TODO: Reload rvm completion?
-}
-
-function rvm-link-completion {
-	ln -s "$rvm_path/scripts/zsh/Completion/_rvm" "$ZSH/plugins/rvm/_rvm.official"
 }
 
 # TODO: Make this usable w/o rvm.
