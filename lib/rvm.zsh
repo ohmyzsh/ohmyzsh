3df2fe787e9317bf4f4fba72b8de042890a64386
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-06-01 14:14:20 -0400

    Merged /lib/git.zsh and /lib/rvm.zsh into /plugins/.

--- lib/rvm.zsh
+++
@@ -1,8 +0,0 @@
-# Get the name of the current branch.
-function rvm_prompt_info() {
-  local ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null)
-  if [[ -n "$ruby_version" ]]; then
-    echo "($ruby_version)"
-  fi
-}
-
