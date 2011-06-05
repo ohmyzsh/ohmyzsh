35b879a17b965b0ea28009c4561962c9ed17ef1f
gwjo <gowen72@gmail.com>
2011-06-04 21:07:55 -0400

    Add support for agent forwarding

--- plugins/ssh-agent/ssh-agent.plugin.zsh
+++ plugins/ssh-agent/ssh-agent.plugin.zsh
@@ -1,23 +1,51 @@
-# Based on code from Joseph M. Reagle
-# http://www.cygwin.com/ml/cygwin/2001-06/msg00537.html
+#
+# INSTRUCTIONS
+#
+#   To enabled agent forwarding support add the following to
+#   your .zshrc file:
+#
+#     zstyle :omz:plugins:ssh-agent agent-forwarding on
+#
+#
+# CREDITS
+#
+#   Based on code from Joseph M. Reagle
+#   http://www.cygwin.com/ml/cygwin/2001-06/msg00537.html
+#
+#   Agent forwarding support based on ideas from
+#   Florent Thoumie and Jonas Pfenniger
+#
 
-local SSH_ENV=$HOME/.ssh/environment-$HOST
+local _plugin__ssh_env=$HOME/.ssh/environment-$HOST
+local _plugin__forwarding
 
-function start_agent {
-  /usr/bin/env ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
-  chmod 600 ${SSH_ENV}
-  . ${SSH_ENV} > /dev/null
+function _plugin__start_agent()
+{
+  /usr/bin/env ssh-agent | sed 's/^echo/#echo/' > ${_plugin__ssh_env}
+  chmod 600 ${_plugin__ssh_env}
+  . ${_plugin__ssh_env} > /dev/null
   /usr/bin/ssh-add;
 }
 
-# Source SSH settings, if applicable
+# test if agent-forwarding is enabled
+zstyle -b :omz:plugins:ssh-agent agent-forwarding _plugin__forwarding
+if [[ ${_plugin__forwarding} == "yes" && -z $SSH_AGENT_PID && -n "$SSH_AUTH_SOCK" ]]; then
+  # No PID but a AUTH_SOCK means agent forwarding is enabled
+  # Add a nifty symlink for screen/tmux 
+  [[ -L $SSH_AUTH_SOCK ]] || ln -sf "$SSH_AUTH_SOCK" /tmp/ssh-agent-$USER-screen
 
-if [ -f "${SSH_ENV}" ]; then
-  . ${SSH_ENV} > /dev/null
+elif [ -f "${_plugin__ssh_env}" ]; then
+  # Source SSH settings, if applicable
+  . ${_plugin__ssh_env} > /dev/null
   ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-    start_agent;
+    _plugin__start_agent;
   }
 else
-  start_agent;
+  _plugin__start_agent;
 fi
+
+# tidy up after ourselves
+unfunction _plugin__start_agent
+unset _plugin__forwarding
+unset _plugin__ssh_env
 
