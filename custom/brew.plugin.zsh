ca92b660638ffa6938e6baf7f4c109ca378b51a2
Locojay <locojay@dev.dev>
2011-06-06 20:32:56 -0400

    brew plugin fix

--- custom/brew.plugin.zsh
+++ custom/brew.plugin.zsh
@@ -1,3 +1,10 @@
 #python path ==> brew easy_install and pip are used
 export PATH='/usr/local/share/python':$PATH
 export HOMEBREW_EDITOR=vim
+
+export PATH=`echo $PATH | sed -e 's|/usr/local/bin||' -e 's|::|:|g'` # Remove /usr/local/bin
+export PATH=`echo $PATH | sed -e 's|/usr/bin|/usr/local/bin:&|'`     # Add it in front of /usr/bin
+export PATH=`echo $PATH | sed -e 's|/usr/bin|/usr/local/sbin:&|'`    # Add /usr/local/sbin
+function brew-link-completion {
+  ln -s "$(brew --prefix)/Library/Contributions/brew_zsh_completion.zsh" "$ZSH/plugins/brew/_brew.official"
+}
