c5e67bcdc09b6605183f3ed0e9036c30c42b105a
Bodo Tasche <bodo@wannawork.de>
2011-06-12 23:16:47 +0200

    Moved opening braces

--- plugins/bundler-exec/bundler-exec.plugin.zsh
+++ plugins/bundler-exec/bundler-exec.plugin.zsh
@@ -1,17 +1,15 @@
 # This plugin is based on https://github.com/gma/bundler-exec
-# modify the BUNDLED_COMMANDS if needed
+# modify the bundled_commands if needed
 
 bundled_commands=(cucumber heroku rackup rails rake rspec ruby shotgun spec spork)
 
 ## Functions
 
-_bundler-installed()
-{
+_bundler-installed() {
     which bundle > /dev/null 2>&1
 }
 
-_within-bundled-project()
-{
+_within-bundled-project() {
     local check_dir=$PWD
     while [ "$(dirname $check_dir)" != "/" ]; do
         [ -f "$check_dir/Gemfile" ] && return
@@ -20,8 +18,7 @@ _within-bundled-project()
     false
 }
 
-_run-with-bundler()
-{
+_run-with-bundler() {
     local command="$1"
     shift
     if _bundler-installed && _within-bundled-project; then
