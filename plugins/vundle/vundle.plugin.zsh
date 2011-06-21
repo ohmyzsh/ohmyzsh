a335e9df9f13a5caa2b83cad9ebcfaa25b748202
Max Gonzih <gonzih@gmail.com>
2011-06-21 13:50:39 +0300

    plugin for vundle (vim plugins managment system) that provide vundle, vundle-update and vundle-init aliases

---
+++ plugins/vundle/vundle.plugin.zsh
@@ -0,0 +1,3 @@
+alias vundle-init='git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle'
+alias vundle='vim -c "execute \"BundleInstall\" | q"'
+alias vundle-update='vim -c "execute \"BundleInstall!\" | q"'
