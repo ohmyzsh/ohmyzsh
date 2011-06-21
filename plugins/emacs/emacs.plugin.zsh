970266dba6e85cf706b05728060dbd24013725e4
Jochen Lillich <jochen@lillich.info>
2011-06-21 20:30:26 +0200

    New emacs plugin

---
+++ plugins/emacs/emacs.plugin.zsh
@@ -0,0 +1,16 @@
+export EMACS_APP="/Applications/Emacs.app"
+
+# open in background, eventually using running app
+export EDITOR="open -a $EMACS_APP"
+
+# always open new app, wait until closed
+export GIT_EDITOR="open -n -W -a $EMACS_APP"
+
+alias em="$EDITOR"
+
+# workaround b/c Emacs can only open already existing files
+function _emacs_touch_and_edit() {
+    touch $*
+    $EDITOR $*
+}
+alias emt=_emacs_touch_and_edit
