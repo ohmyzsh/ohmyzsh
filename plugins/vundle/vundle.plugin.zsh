499819ced108aa4b0d90dd0ac922a174c965d814
Max Gonzih <gonzih@gmail.com>
2011-06-29 10:26:42 +0300

    vundle plugin refactored first it checks existens of vundle plugin, if plugin dont exist run git clone

--- plugins/vundle/vundle.plugin.zsh
+++ plugins/vundle/vundle.plugin.zsh
@@ -1,3 +1,22 @@
-alias vundle-init='git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle'
-alias vundle='vim -c "execute \"BundleInstall\" | q"'
-alias vundle-update='vim -c "execute \"BundleInstall!\" | q"'
+function vundle-init () {
+  if [ ! -d ~/.vim/bundle/vundle/ ]
+  then
+    mkdir -p ~/.vim/bundle/vundle/
+  fi
+
+  if [ ! -d ~/.vim/bundle/vundle/.git/ ]
+  then
+    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  fi
+}
+
+function vundle () {
+  vundle-init
+  vim -c "execute \"BundleInstall\" | q"
+}
+
+
+function vundle-update () {
+  vundle-init
+  vim -c "execute \"BundleInstall!\" | q"
+}
