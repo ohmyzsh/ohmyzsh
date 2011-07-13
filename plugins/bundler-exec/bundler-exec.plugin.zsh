a77e6fb003cc80fbbff5d453a5ca3d38638d99ed
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-07-13 22:40:23 +0100

    fix indentation

--- plugins/bundler-exec/bundler-exec.plugin.zsh
+++ plugins/bundler-exec/bundler-exec.plugin.zsh
@@ -6,29 +6,29 @@ bundled_commands=(cap capify cucumber heroku rackup rails rake rspec ruby shotgu
 ## Functions
 
 _bundler-installed() {
-    which bundle > /dev/null 2>&1
+  which bundle > /dev/null 2>&1
 }
 
 _within-bundled-project() {
-    local check_dir=$PWD
-    while [ "$(dirname $check_dir)" != "/" ]; do
-        [ -f "$check_dir/Gemfile" ] && return
-        check_dir="$(dirname $check_dir)"
-    done
-    false
+  local check_dir=$PWD
+  while [ "$(dirname $check_dir)" != "/" ]; do
+    [ -f "$check_dir/Gemfile" ] && return
+    check_dir="$(dirname $check_dir)"
+  done
+  false
 }
 
 _run-with-bundler() {
-    local command="$1"
-    shift
-    if _bundler-installed && _within-bundled-project; then
-        bundle exec $command "$@"
-    else
-      $command "$@"
-    fi
+  local command="$1"
+  shift
+  if _bundler-installed && _within-bundled-project; then
+    bundle exec $command "$@"
+  else
+    $command "$@"
+  fi
 }
 
 ## Main program
 for cmd in $bundled_commands; do
-   alias $cmd="_run-with-bundler $cmd"
+  alias $cmd="_run-with-bundler $cmd"
 done
