37dbcf7a20c394b2237cc3bf4f9a0a7813875e8f
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-07-14 17:36:14 -0400

    Cleaned up and documented history.

--- functions/history.zsh
+++ functions/history.zsh
@@ -1,14 +1,19 @@
-## Command history configuration
-HISTFILE="$HOME/.zsh_history"
+HISTFILE="$HOME/.zhistory"
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt hist_verify
-setopt hist_expire_dups_first
-setopt hist_ignore_space
-setopt hist_ignore_dups
-setopt share_history
-setopt append_history
-setopt extended_history
-setopt inc_append_history
+setopt bang_hist                 # Treat the '!' character specially during expansion.
+setopt extended_history          # Write the history file in the ":start:elapsed;command" format.
+setopt append_history            # Append to the history file, don't replace it.
+setopt inc_append_history        # Write to the history file immediately, not when the shell exits.
+setopt share_history             # Share history between all sessions.
+setopt hist_expire_dups_first    # Expire duplicate entries first when trimming history.
+setopt hist_ignore_dups          # Don't record an entry that was just recorded again.
+setopt hist_ignore_all_dups      # Delete old recorded entry if new entry is a duplicate.
+setopt hist_find_no_dups         # Do not display a line previously found.
+setopt hist_ignore_space         # Don't record an entry starting with a space.
+setopt hist_save_no_dups         # Don't write duplicate entries in the history file.
+setopt hist_reduce_blanks        # Remove superfluous blanks before recording entry.
+setopt hist_verify               # Don't execute immediately upon history expansion.
+setopt hist_beep                 # Beep when accessing nonexistent history.
 
