1d17502aed5fc04cd1ca3eb35757b5ba2822414a
meh <meh@paranoici.org>
2011-07-16 07:18:14 +0200

    rsync: add rsync- prefixes and use aliases

--- plugins/rsync/rsync.plugin.zsh
+++ plugins/rsync/rsync.plugin.zsh
@@ -1,15 +1,4 @@
-function copy {
-    rsync -av --progress -h "$1" "$2"
-}
-
-function move {
-    rsync -av --progress -h --remove-source-files "$1" "$2"
-}
-
-function update {
-    rsync -avu --progress -h "$1" "$2"
-}
-
-function synchronize {
-    rsync -avu --delete --progress -h "$1" "$2"
-}
+alias rsync-copy="rsync -av --progress -h"
+alias rsync-move="rsync -av --progress -h --remove-source-files"
+alias rsync-update="rsync -avu --progress -h"
+alias rsync-synchronize="rsync -avu --delete --progress -h"
