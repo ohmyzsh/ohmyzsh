94656ce760bec0198fad61f079672ac4f0aab32b
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-07-16 21:43:51 -0400

    Fixed terminal window/tab title.

--- functions/terminal.zsh
+++ functions/terminal.zsh
@@ -13,34 +13,43 @@ fi
 # Fully supports GNU Screen, iTerm, and most modern xterm and rxvt terminals.
 # Partially supports Mac OS X Terminal since it can't set window and tab separately.
 # Usage: title "tab title" "window title"
-function title {
+function terminal-title {
   if [[ "$DISABLE_AUTO_TITLE" != 'true' ]]; then
     if [[ "$TERM" == screen* ]]; then
       # Set GNU Screen's hardstatus (usually truncated at 20 characters).
-      print -Pn "\ek$1:q\e\\"
+      printf "\ek%s\e\\" ${(V)1}
     elif [[ "$TERM" == xterm* ]] || [[ "$TERM" == rxvt* ]]; then
       # Set the window title.
-      print -Pn "\e]2;$2:q\a"
+      printf "\e]2;%s\a" ${(V)2}
       # Set the tab title (will override window title on a broken terminal).
-      print -Pn "\e]1;$1:q\a"
+      printf "\e]1;%s\a" ${(V)1}
     fi
   fi
 }
 
-# 15 character, left-truncated current working directory.
-ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<"
-ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
+# Don't override precmd/preexec, append to hook array.
+autoload -Uz add-zsh-hook
 
 # Set the tab and window titles before the prompt is displayed.
-function precmd {
-  title "$ZSH_THEME_TERM_TAB_TITLE_IDLE" "$ZSH_THEME_TERM_TITLE_IDLE"
+function terminal-title-precmd {
+  # 15 character, left-truncated current working directory.
+  local terminal_tab_title="${(%):-%15<...<%~%<<}"
+  local terminal_window_title="${(%):-%n@%m: %~}"
+  terminal-title "$terminal_tab_title" "$terminal_window_title"
 }
+add-zsh-hook precmd terminal-title-precmd
 
 # Set the tab and window titles before command execution.
-function preexec {
+function terminal-title-preexec {
   emulate -L zsh
   setopt extended_glob
   # Command name only, or if this is sudo or ssh, the next command.
   local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
-  title "$CMD" "%100>...>$2%<<"
+  local trimmed="${2[1,100]}"
+  if [[ "$2" != "$trimmed" ]]; then
+    trimmed="${trimmed}..."
+  fi
+  terminal-title "$CMD" "$trimmed"
 }
+add-zsh-hook preexec terminal-title-preexec
+
