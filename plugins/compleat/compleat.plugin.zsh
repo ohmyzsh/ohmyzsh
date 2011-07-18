af04d5794846da860754ea9382ba8621a7c62b37
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-07-17 20:28:50 -0400

    Check for bashcompinit availability before loading it; unset variables.

--- plugins/compleat/compleat.plugin.zsh
+++ plugins/compleat/compleat.plugin.zsh
@@ -1,20 +1,20 @@
 # ------------------------------------------------------------------------------
 #          FILE:  compleat.plugin.zsh
 #   DESCRIPTION:  oh-my-zsh plugin file.
-#        AUTHOR:  Sorin Ionescu (sorin.ionescu@gmail.com)
-#       VERSION:  1.0.0
+#        AUTHOR:  Sorin Ionescu <sorin.ionescu@gmail.com>
+#       VERSION:  1.0.1
 # ------------------------------------------------------------------------------
 
 if (( ${+commands[compleat]} )); then
-  local prefix="${commands[compleat]:h:h}"
-  local setup="${prefix}/share/compleat-1.0/compleat_setup"
+  compleat_setup="${commands[compleat]:h:h}/share/compleat-1.0/compleat_setup"
 
-  if [[ -f "$setup" ]]; then
+  if [[ -f "$compleat_setup" ]]; then
     if ! bashcompinit >/dev/null 2>&1; then
-      autoload -U bashcompinit
-      bashcompinit -i
+      autoload -Uz bashcompinit && bashcompinit
     fi
 
-    source "$setup"
+    source "$compleat_setup"
+    unset compleat_setup
   fi
 fi
+
