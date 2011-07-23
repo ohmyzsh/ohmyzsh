512f3678c07accd6d121e9c0d6d50f008c99708a
Benjamin Boudreau <dreurmail@gmail.com>
2011-07-23 11:22:39 -0400

    Remove sudo when using yaourt + do not rely on abs when not in path - In archlinux plugin

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -1,26 +1,30 @@
-# Archlinux zsh aliases and functions for zsh
-
-# Aliases ###################################################################
+# Archlinux zsh aliases and functions
+# Usage is also described at https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins
 
 # Look for yaourt, and add some useful functions if we have it.
 if [[ -x `which yaourt` ]]; then
   upgrade () {
-    yaourt -Syu -C
+    yaourt -Syu
   }
+  alias yaconf='yaourt -C'        # Fix all configuration files with vimdiff
   # Pacman - https://wiki.archlinux.org/index.php/Pacman_Tips
-  alias yaupg='sudo yaourt -Syu'        # Synchronize with repositories before upgrading packages that are out of date on the local system.
-  alias yain='sudo yaourt -S'           # Install specific package(s) from the repositories
-  alias yains='sudo yaourt -U'          # Install specific package not from the repositories but from a file 
-  alias yare='sudo yaourt -R'           # Remove the specified package(s), retaining its configuration(s) and required dependencies
-  alias yarem='sudo yaourt -Rns'        # Remove the specified package(s), its configuration(s) and unneeded dependencies
-  alias yarep='yaourt -Si'              # Display information about a given package in the repositories
-  alias yareps='yaourt -Ss'             # Search for package(s) in the repositories
-  alias yaloc='yaourt -Qi'              # Display information about a given package in the local database
-  alias yalocs='yaourt -Qs'             # Search for package(s) in the local database
+  alias yaupg='yaourt -Syu'        # Synchronize with repositories before upgrading packages that are out of date on the local system.
+  alias yain='yaourt -S'           # Install specific package(s) from the repositories
+  alias yains='yaourt -U'          # Install specific package not from the repositories but from a file 
+  alias yare='yaourt -R'           # Remove the specified package(s), retaining its configuration(s) and required dependencies
+  alias yarem='yaourt -Rns'        # Remove the specified package(s), its configuration(s) and unneeded dependencies
+  alias yarep='yaourt -Si'         # Display information about a given package in the repositories
+  alias yareps='yaourt -Ss'        # Search for package(s) in the repositories
+  alias yaloc='yaourt -Qi'         # Display information about a given package in the local database
+  alias yalocs='yaourt -Qs'        # Search for package(s) in the local database
   # Additional yaourt alias examples
-  alias yaupd='sudo yaourt -Sy && sudo abs'     # Update and refresh the local package and ABS databases against repositories
-  alias yainsd='sudo yaourt -S --asdeps'        # Install given package(s) as dependencies of another package
-  alias yamir='sudo yaourt -Syy'                # Force refresh of all package lists after updating /etc/pacman.d/mirrorlist
+  if [[ -x `which abs` ]]; then
+    alias yaupd='yaourt -Sy && sudo abs'   # Update and refresh the local package and ABS databases against repositories
+  else
+    alias yaupd='yaourt -Sy'               # Update and refresh the local package and ABS databases against repositories
+  fi
+  alias yainsd='yaourt -S --asdeps'        # Install given package(s) as dependencies of another package
+  alias yamir='yaourt -Syy'                # Force refresh of all package lists after updating /etc/pacman.d/mirrorlist
 else
  upgrade() {
    sudo pacman -Syu
@@ -38,7 +42,11 @@ alias pacreps='pacman -Ss'             # Search for package(s) in the repositori
 alias pacloc='pacman -Qi'              # Display information about a given package in the local database
 alias paclocs='pacman -Qs'             # Search for package(s) in the local database
 # Additional pacman alias examples
-alias pacupd='sudo pacman -Sy && sudo abs'     # Update and refresh the local package and ABS databases against repositories
+if [[ -x `which abs` ]]; then
+  alias pacupd='sudo pacman -Sy && sudo abs'     # Update and refresh the local package and ABS databases against repositories
+else
+  alias pacupd='sudo pacman -Sy'     # Update and refresh the local package and ABS databases against repositories
+fi
 alias pacinsd='sudo pacman -S --asdeps'        # Install given package(s) as dependencies of another package
 alias pacmir='sudo pacman -Syy'                # Force refresh of all package lists after updating /etc/pacman.d/mirrorlist
 
