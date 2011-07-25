559bbadba555e9b81501ddf0ebc1c3211a845568
Andrea <andrea.damico@gmail.com>
2011-07-25 15:17:32 +0200

    prepend /usr/local/rvm to rvm prompt script

--- lib/rvm.zsh
+++ lib/rvm.zsh
@@ -1,6 +1,6 @@
 # get the name of the branch we are on
 function rvm_prompt_info() {
-  ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null) || return
+  ruby_version=$($rvm_path/bin/rvm-prompt 2> /dev/null) || return
   echo "($ruby_version)"
 }
 
