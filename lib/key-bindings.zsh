a1c07b7554849f72bfdde85554e9d8027bcd047d
Randy Hancock <rhancock@genome.wustl.edu>
2011-08-01 10:10:42 -0500

    Fix edit-command-line binding
    This binding doesn't work when the edit-command-line.zsh file is loaded
    after the key-bindings.zsh file because 'bindkey -e' in key-bindings.zsh
    resets the binding. Moving the bindings to they key-bindings.zsh file
    and removing edit-command-line.zsh.

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -30,6 +30,11 @@ bindkey "^[[3~" delete-char
 bindkey "^[3;5~" delete-char
 bindkey "\e[3~" delete-char
 
+# Edit the current command line in $EDITOR
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '\C-x\C-e' edit-command-line
+
 # consider emacs keybindings:
 
 #bindkey -e  ## emacs key bindings
