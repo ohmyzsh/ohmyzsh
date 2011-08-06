749feb2720948a4ac9a981286654962e699d8d94
Daniel Bolton <dbb@9y.com>
2011-08-06 15:22:55 -0400

    Merge "deb" and "debian" plugins.

--- plugins/debian/debian.plugin.zsh
+++ plugins/debian/debian.plugin.zsh
@@ -1,4 +1,4 @@
-# https://github.com/dbbolton/
+# https://github.com/dbb/
 #
 # Debian-related zsh aliases and functions for zsh
 
@@ -6,14 +6,29 @@
 # Aliases ###################################################################
 
 # Some self-explanatory aliases
+alias acs="apt-cache search"
 alias afs='apt-file search --regexp'
 alias aps='aptitude search'
+alias as="aptitude -F \"* %p -> %d \n(%v/%V)\" \
+		--no-gui --disable-columns search"	# search package
 alias apsrc='apt-get source'
 alias apv='apt-cache policy'
 
+# aliases that use su -c ##############
 alias apdg='su -c "aptitude update && aptitude safe-upgrade"'
 alias apud='su -c "aptitude update"'
 alias apug='su -c "aptitude safe-upgrade"'
+# end aliases that use su -c ##########
+
+# aliases that use sudo ###############
+alias ad="sudo apt-get update"				# update packages lists
+alias au="sudo apt-get update && \
+		sudo apt-get dselect-upgrade"		# upgrade packages
+alias ai="sudo apt-get install"				# install package
+alias ar="sudo apt-get remove --purge && \
+		sudo apt-get autoremove --purge"	# remove package
+alias ac="sudo apt-get clean && sudo apt-get autoclean" # clean apt cache
+# end aliases that use sudo ###########
 
 # print all installed packages
 alias allpkgs='aptitude search -F "%p" --disable-columns ~i'
@@ -33,6 +48,13 @@ alias kclean='su -c '\''aptitude remove -P ?and(~i~nlinux-(ima|hea) ?not(~n`unam
 
 # Functions #################################################################
 
+# install packages without sudo
+apin() {
+    cmd="su -lc 'aptitude -P install $@' root"
+    print "$cmd"
+    eval "$cmd"
+}
+
 # create a simple script that can be used to 'duplicate' a system
 apt-copy() {
 	print '#!/bin/sh'"\n" > apt-copy.sh
@@ -46,8 +68,10 @@ apt-copy() {
 
 
 # Kernel-package building shortcut
-dbb-build () {
-	MAKEFLAGS=''		# temporarily unset MAKEFLAGS ( '-j3' will fail )
+kerndeb () {
+    # temporarily unset MAKEFLAGS ( '-j3' will fail )
+    MAKEFLAGS=$( print - $MAKEFLAGS | perl -pe 's/-j\s*[\d]+//g' )		
+    print '$MAKEFLAGS set to '"'$MAKEFLAGS'"
 	appendage='-custom' # this shows up in $ (uname -r )
     revision=$(date +"%Y%m%d") # this shows up in the .deb file name
 
