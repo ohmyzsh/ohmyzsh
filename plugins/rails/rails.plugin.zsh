dd2fbd01017e1aa4f97c1b1ba00c4bed58318d47
Chris Edwards <chris@leopipadesigns.co.uk>
2011-08-09 11:43:26 +0200

    Added some handy alias'

--- plugins/rails/rails.plugin.zsh
+++ plugins/rails/rails.plugin.zsh
@@ -1,4 +1,4 @@
-alias ss='thin --stats "/thin/stats" start'
+alias tss='thin --stats "/thin/stats" start'
 alias sg='ruby script/generate'
 alias sd='ruby script/destroy'
 alias sp='ruby script/plugin'
@@ -7,6 +7,11 @@ alias rdbm='rake db:migrate'
 alias sc='ruby script/console'
 alias sd='ruby script/server --debugger'
 alias devlog='tail -f log/development.log'
+alias ss='ruby script/server'
+alias gi='gem install'
+alias sgi='rvmsudo gem install'
+alias gu='gem uninstall'
+alias sgu='rvmsudo gem uninstall'
 
 function remote_console() {
   /usr/bin/env ssh $1 "( cd $2 && ruby script/console production )"
