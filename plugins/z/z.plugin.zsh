a4591962aa7baf661bb44390fac45909389d689a
JuanPablo <jpabloaj@gmail.com>
2011-08-16 17:34:05 -0400

    if exist brew add z

--- plugins/z/z.plugin.zsh
+++ plugins/z/z.plugin.zsh
@@ -1,6 +1,8 @@
-if [ -f `brew --prefix`/etc/profile.d/z.sh ]; then
-  . `brew --prefix`/etc/profile.d/z.sh
-  function precmd () {
-    z --add "$(pwd -P)"
-  }
+if [ $( which brew ) ]; then 
+	if [ -f `brew --prefix`/etc/profile.d/z.sh ]; then
+	  . `brew --prefix`/etc/profile.d/z.sh
+	  function precmd () {
+		z --add "$(pwd -P)"
+	  }
+	fi
 fi
