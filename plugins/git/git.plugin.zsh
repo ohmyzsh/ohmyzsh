bd6aba9c68db06758593e9ef707839c2f0271235
Mark Szymanski <mrman208@me.com>
2011-08-27 16:23:49 -0500

    Add an alias for ga --> git add, too

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -31,6 +31,8 @@ alias glgg='git log --graph --max-count=5'
 compdef _git glgg=git-log
 alias gss='git status -s'
 compdef _git gss=git-status
+alias ga='git add'
+compdef _git ga=git-add
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
