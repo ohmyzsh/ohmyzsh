1fb141cfb51f753fe997d17c12ac810ad321f36f
Mark Szymanski <mrman208@me.com>
2011-08-27 16:16:51 -0500

    Add gss alias for git status -s

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -29,6 +29,8 @@ alias glg='git log --stat --max-count=5'
 compdef _git glg=git-log
 alias glgg='git log --graph --max-count=5'
 compdef _git glgg=git-log
+alias gss='git status -s'
+compdef _git gss=git-status
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
