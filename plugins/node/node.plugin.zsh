aa00cab97e0334a6a907cd78617a5917bd32735c
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-08-27 21:02:46 -0400

    Merged npm plugin into node plugin.

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -1,5 +1,9 @@
+# Complete npm.
+eval "$(npm completion 2>/dev/null)"
+
 # Open the node api for your current version to the optional section.
-# TODO: Make the section part easier to use.
-function node-docs {
-	open "http://nodejs.org/docs/$(node --version)/api/all.html#$1"
+# TODO: Make the sections easier to use.
+function node-docs() {
+  open "http://nodejs.org/docs/$(node --version)/api/all.html#$1"
 }
+
