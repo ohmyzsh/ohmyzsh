b52adc8373e3696603d379d2fef48eff316cb967
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-08-27 21:03:50 -0400

    Added npm completion caching.

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -1,5 +1,13 @@
 # Complete npm.
-eval "$(npm completion 2>/dev/null)"
+cache_file="${0:h}/cache.zsh"
+if [[ ! -f "$cache_file" ]] && (( $+commands[npm] )); then
+  # npm is slow; cache its output.
+  npm completion >! "$cache_file" 2>/dev/null
+  source "$cache_file"
+else
+  source "$cache_file"
+fi
+unset cache_file
 
 # Open the node api for your current version to the optional section.
 # TODO: Make the sections easier to use.
