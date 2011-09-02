fe175661fddd04691cc03012f84490e729cf2bae
Max Gonzih <gonzih@gmail.com>
2011-09-02 16:06:32 +0300

    vundle call fixed for new vundle version

--- plugins/vundle/vundle.plugin.zsh
+++ plugins/vundle/vundle.plugin.zsh
@@ -12,11 +12,11 @@ function vundle-init () {
 
 function vundle () {
   vundle-init
-  vim -c "execute \"BundleInstall\" | q"
+  vim -c "execute \"BundleInstall\" | q | q"
 }
 
 
 function vundle-update () {
   vundle-init
-  vim -c "execute \"BundleInstall!\" | q"
+  vim -c "execute \"BundleInstall!\" | q | q"
 }
