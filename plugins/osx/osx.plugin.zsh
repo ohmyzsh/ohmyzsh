997ab00fcd82de50ea982c6ad5b69312e20a2c71
D. Rimron <darran@xalior.com>
2011-09-14 21:59:21 +0100

    Adding in a new command "tabhere"...
    ...can you guess what it does?
    
    -Dx

--- plugins/osx/osx.plugin.zsh
+++ plugins/osx/osx.plugin.zsh
@@ -37,6 +37,38 @@ do_submenu("Terminal", "Shell", "New Tab", 1)
 EOF
 }
 
+function tabhere() {
+savepath
+osascript >/dev/null <<EOF
+on do_submenu(app_name, menu_name, menu_item, submenu_item)
+    -- bring the target application to the front
+    tell application app_name
+      activate
+    end tell
+    tell application "System Events"
+      tell process app_name
+        tell menu bar 1
+          tell menu bar item menu_name
+            tell menu menu_name
+              tell menu item menu_item
+                tell menu menu_item
+                  click menu item submenu_item
+                end tell
+              end tell
+            end tell
+          end tell
+        end tell
+      end tell
+    end tell
+end do_submenu
+
+do_submenu("Terminal", "Shell", "New Tab", 1)
+                    tell application "System Events"
+                      keystroke "cd `cat ~/.current_path~`\n"
+                    end tell
+EOF
+}
+
 function itab() {
 savepath
 osascript >/dev/null <<EOF
