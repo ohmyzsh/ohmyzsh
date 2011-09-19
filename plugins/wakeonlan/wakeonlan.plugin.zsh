4b3318ab6ec5a62a07fa2195d263de11c3c4a662
Paul Gideon Dann <pdgiddie@gmail.com>
2011-09-19 12:40:29 +0100

    Tweaks to the wakeonlan plugin
    These were suggested by sorin-ionescu in pull request #343.

--- plugins/wakeonlan/wakeonlan.plugin.zsh
+++ plugins/wakeonlan/wakeonlan.plugin.zsh
@@ -1,15 +1,18 @@
 function wake() {
-  local config_file=~/.wakeonlan/$1
-  if [[ ! -f $config_file ]]; then
+  local config_file="$HOME/.wakeonlan/$1"
+  if [[ ! -f "$config_file" ]]; then
     echo "ERROR: There is no configuration file at \"$config_file\"."
-    return
+    return 1
   fi
 
-  which wakeonlan > /dev/null
-  if [[ ! $? == 0 ]]; then
+  if (( ! $+commands[wakeonlan] )); then
     echo "ERROR: Can't find \"wakeonlan\".  Are you sure it's installed?"
-    return
+    return 1
   fi
 
   wakeonlan -f $config_file
 }
+
+if (( $+functions[compdef] )); then
+  compdef "_arguments '1:device to wake:_files -W '\''$HOME/.wakeonlan'\'''" wake
+fi
