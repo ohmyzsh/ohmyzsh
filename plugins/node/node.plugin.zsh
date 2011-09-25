6231dbbd26e86376171c8dc43e52d55af37f69e4
Mark Robert Henderson <henderson.mark@gmail.com>
2011-09-25 01:46:39 -0500

    Added support in node-docs for versions like -pre and -rc. Accounts for the dash.

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -1,5 +1,12 @@
 # Open the node api for your current version to the optional section.
 # TODO: Make the section part easier to use.
 function node-docs {
-	open "http://nodejs.org/docs/$(node --version)/api/all.html#$1"
+  nodeversion=`node --version`
+  dashlocation=`echo $nodeversion | sed -n "s/-.*//p" | wc -c`;
+
+  if [[ $dashlocation -eq 0 ]]; then
+    open "http://nodejs.org/docs/${nodeversion}/api/all.html#$1";
+  else
+    open "http://nodejs.org/docs/${nodeversion:0:$dashlocation - 1}/api/all.html#$1";
+  fi
 }
