3dbc808b96e959d220b102f7ccdc6c69ad6b1a1c
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-09-27 18:51:56 -0400

    Removed completions managed by zsh-completions.

--- plugins/ruby/init.zsh
+++ plugins/ruby/init.zsh
@@ -15,27 +15,13 @@ if [[ "$OSTYPE" == darwin* ]]; then
 fi
 
 # Loads RVM into the shell session.
-completion_file="${0:h}/_rvm"
-if [[ ! -e "$completion_file" ]] && [[ -L "$completion_file" ]]; then
-  unlink "$completion_file" 2> /dev/null
-fi
-
 if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
   # Auto adding variable-stored paths to ~ list conflicts with RVM.
   unsetopt auto_name_dirs
 
   # Source RVM.
   source "$HOME/.rvm/scripts/rvm"
-
-  # Complete RVM.
-  if [[ ! -e "$completion_file" ]]; then
-    ln -s \
-      "$rvm_path/scripts/zsh/Completion/_rvm" \
-      "$completion_file" \
-      2> /dev/null
-  fi
 fi
-unset completion_file
 
 # Loads rbenv into the shell session.
 if [[ -s "$HOME/.rbenv/bin/rbenv" ]]; then
