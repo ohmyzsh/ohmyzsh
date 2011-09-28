b35b171c5da883146ed4a3daa21d4e4e1457926b
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-09-28 15:44:32 -0400

    Removed autojump in favour of z.

--- plugins/autojump/init.zsh
+++
@@ -1,8 +0,0 @@
-if [[ -f /etc/profile.d/autojump.zsh ]]; then
-  source /etc/profile.d/autojump.zsh
-elif [[ -f /opt/local/etc/profile.d/autojump.zsh ]]; then
-  source /opt/local/etc/profile.d/autojump.zsh
-elif [[ -f "$(brew --prefix 2> /dev/null)/etc/autojump.zsh" ]]; then
-  source "$(brew --prefix)/etc/autojump.zsh"
-fi
-
