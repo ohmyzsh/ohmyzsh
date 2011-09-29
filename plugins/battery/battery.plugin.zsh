9dede1e0fe5e78b778626bc2344c457ca7a8b2eb
Peter Hoeg <p.hoeg@northwind.sg>
2011-09-29 17:12:09 +0800

    add: battery plugin

---
+++ plugins/battery/battery.plugin.zsh
@@ -0,0 +1,9 @@
+if [[ $(acpi | grep -c ^Battery) -gt 0 ]] ; then
+  function battery_pct_remaining() { echo "$(acpi | cut -f2 -d ',' | tr -cd '[:digit:]')" }
+  function battery_time_remaining() { echo $(acpi | cut -f3 -d ',') }
+  function battery_pct_prompt() { echo "%{$fg[red]%}[$(battery_pct_remaining)]%{$reset_color%}" }
+else
+  error_msg='no battery'
+  function battery_pct_remaining() { echo $error_msg }
+  function battery_time_remaining() { echo $error_msg }
+fi
