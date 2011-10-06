3c5fb0ad8890d0553774d94230ce4155df2f3bd8
Robin Wenglewski <robin@wenglewski.de>
2011-10-06 22:42:02 +0200

    update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -7,14 +7,12 @@ alias e="$EDITOR"
 
 # git
 alias g='git'
-alias gl='git pull'
 alias gp='git push'
-alias gph='gp && gp heroku'
 alias gd='git diff'
 alias gc='git clone'
 alias gco='git checkout'
 alias gb='git branch'
-alias gs='git status'
+alias gs='git status -sb'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gca='grm; git add .;gs;read;git commit'
 alias git-authors='git shortlog -s -n'
