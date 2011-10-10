e50cc5162d08778881a7113482aceab1d5f660f0
Tristan Carel <tcarel@exalead.com>
2011-10-10 18:04:13 +0200

    Comment

--- plugins/emacs/emacsclient.sh
+++ plugins/emacs/emacsclient.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# Starts emacs daemon if not already started.
-
+# get list of available X windows.
 x=`emacsclient --alternate-editor '' --eval '(x-display-list)' 2>/dev/null`
+
 if [ -z "$x" ] ;then
+    # Create one if there is no X window yet.
     emacsclient --alternate-editor "" --create-frame "$@"
 else
     # prevent creating another X frame if there is at least one present.
