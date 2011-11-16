5a2ae86f6140e03169a61201832d60de6fb8dd5b
Russell Harmon <russ@eatnumber1.com>
2011-11-16 04:32:05 -0500

    Use printf rather than echo -e in update.sh
    update.sh is an "sh" script, not a zsh or bash script. On platforms
    which have real sh, echo does not have the -e option.

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,12 +1,12 @@
 current_path=`pwd`
-echo -e "\033[0;34mUpgrading Oh My Zsh\033[0m"
+printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 ( cd $ZSH && git pull origin master )
-echo -e "\033[0;32m"'         __                                     __   '"\033[0m"
-echo -e "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
-echo -e "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
-echo -e "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"\033[0m"
-echo -e "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m"
-echo -e "\033[0;32m"'                        /____/                       '"\033[0m"
-echo -e "\033[0;34mHooray! Oh My Zsh has been updated and/or is at the current version.\033[0m"
-echo -e "\033[0;34mTo keep up on the latest, be sure to follow Oh My Zsh on twitter: \033[1mhttp://twitter.com/ohmyzsh\033[0m"
+printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
+printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
+printf '\033[0;32m%s\033[0m\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
+printf '\033[0;32m%s\033[0m\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
+printf '\033[0;32m%s\033[0m\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
+printf '\033[0;32m%s\033[0m\n' '                        /____/                       '
+printf '\033[0;34m%s\033[0m\n' 'Hooray! Oh My Zsh has been updated and/or is at the current version.'
+printf '\033[0;34m%s\033[1m%s\033[0m\n' 'To keep up on the latest, be sure to follow Oh My Zsh on twitter: ' 'http://twitter.com/ohmyzsh'
 cd "$current_path"
