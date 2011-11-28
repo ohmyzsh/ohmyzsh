4fa59516046525cae2dc5b348ddf49ead9f67dcc
Simon Gomizelj <simongmzlj@gmail.com>
2011-11-28 14:33:26 -0500

    Useless, it just sources the (non-existent) ubuntu file

--- plugins/command-not-found/command-not-found.plugin.zsh
+++
@@ -1,5 +0,0 @@
-# Uses the command-not-found package zsh support
-# as seen in http://www.porcheron.info/command-not-found-for-zsh/
-# this is installed in Ubuntu
-
-source /etc/zsh_command_not_found
