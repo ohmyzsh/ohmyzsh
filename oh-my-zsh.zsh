e915a1111cf6757b29d02556d9252bcb90b65776
Evan LeCompte (Home) <evanlec@gmail.com>
2011-11-29 05:19:06 -0500

    Don't check for tools/check_for_upgrade.sh (its gone now)

--- oh-my-zsh.zsh
+++ oh-my-zsh.zsh
@@ -1,9 +1,3 @@
-# Check for updates on initial load...
-if [ "$DISABLE_AUTO_UPDATE" != "true" ]
-then
-  /usr/bin/env ZSH=$ZSH zsh $ZSH/tools/check_for_upgrade.sh
-fi
-
 # Initializes Oh My Zsh
 
 # add a function path
