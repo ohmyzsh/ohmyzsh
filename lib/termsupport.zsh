4b143ab382ff9de952acc14a79c7557f810670a4
praful mathur <praful.mathur@gmail.com>
2011-12-02 11:12:47 -0500

    setopt globalextended

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -23,7 +23,7 @@ function precmd {
 #Appears at the beginning of (and during) of command execution
 function preexec {
   emulate -L zsh
-  setopt extended_glob
+  setopt extendedglob
   local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
   title "$CMD" "%100>...>$2%<<"
 }
