15892d6e76ffb7a6a92d6cf574a8ae43a679d668
Simon Gomizelj <simongmzlj@gmail.com>
2011-12-02 12:54:42 -0500

    Try again. This works.

--- lib/theming.zsh
+++ lib/theming.zsh
@@ -1,14 +1,13 @@
 set_theme() {
-  local theme
-
-  theme=({$OMZ,$ZSH}/themes/$1-theme(N))
-  source $theme[1]
+  local themes
+  themes=({$OMZ,$ZSH}/themes/$1.zsh-theme(N))
+  source $themes[1]
 }
 
 random_theme() {
   local themes
-  themes=($ZSH/themes/*zsh-theme(N))
-  source "$themes[$RANDOM%$#themes+1]"
+  themes=({$OMZ,$ZSH}/themes/*.zsh-theme(N))
+  source $themes[$RANDOM%$#themes+1]
 }
 
 # compdef "_files -g '*.zsh'" set_theme
