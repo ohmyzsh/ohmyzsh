0939b88a8260ecb925b55d10dcd1ccc06bc855f9
James McGlashan <jamesm.sitegen@gmail.com>
2011-12-06 17:55:56 +1100

    tiny update with themeing

--- lib/theming.zsh
+++ lib/theming.zsh
@@ -1,7 +1,7 @@
 set_theme() {
   local themes
   themes=({$OMZ,$ZSH}/themes/$1.zsh-theme(N))
-  source $themes[1]
+  source $themes[1] || omz_log_msg "theme: $1 was not found. falling back to default." && source $ZSH/themes/default.zsh-theme
 }
 
 random_theme() {
