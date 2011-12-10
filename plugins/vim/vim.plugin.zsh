dd14b935cb0fb886495f8e099759844d5358ff58
James McGlashan <jamesm.sitegen@gmail.com>
2011-12-11 04:29:04 +1100

    plugin with aliases for vim. currenly only one. although can be expanded.

---
+++ plugins/vim/vim.plugin.zsh
@@ -0,0 +1,3 @@
+# Aliases to commands one may use with vim.
+# Colorize the source of a file and create a html file.
+alias code2html="vim omz -f +'syn on' +'set background=dark' +'colorscheme pablo' +'TOhtml' +'w' +'qa' &>/dev/null"
