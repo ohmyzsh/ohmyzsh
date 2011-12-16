6c9ad747b11acbb3cc430bd1948fb9162b0d02fc
James McGlashan <jamesm.sitegen@gmail.com>
2011-12-16 12:53:27 +1100

    added svim alias

--- plugins/vim/vim.plugin.zsh
+++ plugins/vim/vim.plugin.zsh
@@ -1,4 +1,7 @@
 # Vim helper plugin.
+# Aliases
+alias svim='sudo vim'
+
 # Colorize the source of a file and create a html file.
 code2html() {
   vim $@ +'syn on' +'set background=dark' +'colorscheme pablo' +'TOhtml' +'w' +'qa' &>/dev/null
