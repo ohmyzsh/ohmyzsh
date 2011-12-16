98578ec2412d5b5d2e105f3ddd0d41eb21c3de14
Daniel Hahler <git@thequod.de>
2011-12-16 14:21:45 +0100

    `setopt append_history` is not necessary.
    Especially given the inc_append_history option, it is not necessary to
    set the (default) append_history option.

--- lib/history.zsh
+++ lib/history.zsh
@@ -3,7 +3,6 @@ HISTFILE=$HOME/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
