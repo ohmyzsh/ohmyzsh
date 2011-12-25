908a6062243facdf0e96ccaa4e9512cb15eefd56
Jens Kleikamp <jens@codes-concepts.com>
2011-12-25 18:44:03 +0100

    added path for autojump on gentoo

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -1,5 +1,7 @@
 if [ -f /opt/local/etc/profile.d/autojump.sh ]; then
   . /opt/local/etc/profile.d/autojump.sh
+elif [ -f /etc/profile.d/autojump.zsh ]; then
+  . /etc/profile.d/autojump.zsh
 elif [ -f `brew --prefix`/etc/autojump ]; then
   . `brew --prefix`/etc/autojump
 fi
