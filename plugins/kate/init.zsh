cf9c37d2b4d7bb70f5460c22020df736bb6d4ca9
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-12-28 11:50:21 -0500

    Easily open files and directories in Kate.

--- plugins/kate/init.zsh
+++ plugins/kate/init.zsh
@@ -3,6 +3,10 @@ alias kate='kate >/dev/null 2>&1' # Silent start.
 
 # Functions
 function kt() {
-  cd "$1" && kate .
+  if [[ -z "$1" ]]; then
+    kate .
+  else
+    ( [[ -d "$1" ]] && cd "$1" && kate . )
+  fi
 }
 
