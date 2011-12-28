b308a94c91bad63afefa18341c6b31688a496ae8
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-12-28 11:49:26 -0500

    Removed useless aliases in TextMate plugin.

--- plugins/textmate/init.zsh
+++ plugins/textmate/init.zsh
@@ -1,12 +1,3 @@
-# Aliases
-alias et='mate .'
-alias ett='mate app config lib db public spec test Rakefile Capfile Todo'
-alias etp='mate app config features lib db public spec test Gemfile Rakefile Capfile Todo'
-alias etts='mate app config lib db public script spec test vendor/plugins vendor/gems Rakefile Capfile Todo'
-
-# Edit Ruby app in TextMate.
-alias mr='mate CHANGELOG app config db lib public script spec test'
-
 # Functions
 function tm() {
   if [[ -z "$1" ]]; then
