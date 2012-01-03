fda36d257d79aa8089d6d4aa32188ab6ef636ec8
Calogero Lo Leggio <kalos@nerdrug.org>
2012-01-03 18:03:53 +0100

    autojump plugin enhanced

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -1,5 +1,9 @@
-if [ -f /opt/local/etc/profile.d/autojump.sh ]; then
-  . /opt/local/etc/profile.d/autojump.sh
-elif [ -f `brew --prefix`/etc/autojump ]; then
-  . `brew --prefix`/etc/autojump
+if [ -f /usr/share/autojump/autojump.zsh ]; then # debian and ubuntu package
+  . /usr/share/autojump/autojump.zsh
+elif [ -f /etc/profile.d/autojump.zsh ]; then # manual installation
+  . /etc/profile.d/autojump.zsh
+elif (`command -v brew >/dev/null`); then # mac os x with brew
+  if [ -f `brew --prefix`/etc/autojump ]; then
+    . `brew --prefix`/etc/autojump
+  fi
 fi
