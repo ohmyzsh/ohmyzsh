b70159ede278ebaa84c9fc388019684ac7ba5ab2
Dieter Plaetinck <dieter@plaetinck.be>
2012-01-11 13:51:55 +0100

    quote correctly(?) to avoid bogus output from subshells fixes #770

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -7,7 +7,7 @@ function title {
   if [[ "$TERM" == screen* ]]; then
     print -Pn "\ek$1:q\e\\" #set screen hardstatus, usually truncated at 20 chars
   elif [[ "$TERM" == xterm* ]] || [[ $TERM == rxvt* ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
-    print -Pn "\e]2;$2:q\a" #set window name
+    print -Pn $'\e]0;$2\a' #set window name
     print -Pn "\e]1;$1:q\a" #set icon (=tab) name (will override window name on broken terminal)
   fi
 }
