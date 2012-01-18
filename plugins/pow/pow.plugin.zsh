c6347ceb88aa1b769df240b59c5c1e40f03ad224
John Antoni Griffiths <john@johnantoni.com>
2012-01-18 12:52:27 -0500

    fix for pow plugin to default to current dir

--- plugins/pow/pow.plugin.zsh
+++ plugins/pow/pow.plugin.zsh
@@ -1,10 +1,25 @@
-# Thanks to Christopher Sexton
+# Restart a rack app running under pow
+# http://pow.cx/
+#
+# Adds a kapow command that will restart an app
+#
+#   $ kapow myapp
+#   $ kapow # defaults to current directory
+#
+# Supports command completion.
+#
+# If you are not already using completion you might need to enable it with
+# 
+#    autoload -U compinit compinit
+#
+# Thanks also to Christopher Sexton
 # https://gist.github.com/965032
+#
 function kapow {
-	touch ~/.pow/$1/tmp/restart.txt
-	if [ $? -eq 0 ]; then
-		echo "$fg[yellow]Pow restarting $1...$reset_color"
-	fi
+	FOLDERNAME=$1
+	if [ -z "$FOLDERNAME" ]; then; FOLDERNAME=${PWD##*/}; fi
+	touch ~/.pow/$FOLDERNAME/tmp/restart.txt;
+	if [ $? -eq 0 ]; then; echo "pow: restarting $FOLDERNAME" ; fi
 }
 
 compctl -W ~/.pow -/ kapow
