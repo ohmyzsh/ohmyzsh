e672930c456f8f4e4811bdb125b6392d6e2908a1
Kyle West <kswest@gmail.com>
2012-01-30 22:51:58 -0500

    add keybinding documentation. New vim theme.

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -1,35 +1,38 @@
-# TODO: Explain what some of this does..
+# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
+# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
+# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
 
-bindkey -e
-bindkey '\ew' kill-region
-bindkey -s '\el' "ls\n"
-bindkey -s '\e.' "..\n"
-bindkey '^r' history-incremental-search-backward
-bindkey "^[[5~" up-line-or-history
-bindkey "^[[6~" down-line-or-history
+bindkey -e                                          # Use emacs key bindings
 
-# make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+bindkey '\ew' kill-region                           # [Esc-w] - Kill from the cursor to the mark
+bindkey -s '\el' 'ls\n'                             # [Esc-l] - run command: ls
+bindkey -s '\e.' '..\n'                             # [Esc-.] - run command: .. (up directory)
+bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+bindkey '^[[5~' up-line-or-history                  # [PageUp] - Up a line of history
+bindkey '^[[6~' down-line-or-history                # [PageDown] - Down a line of history
 
-bindkey "^[[H" beginning-of-line
-bindkey "^[[1~" beginning-of-line
-bindkey "^[OH" beginning-of-line
-bindkey "^[[F"  end-of-line
-bindkey "^[[4~" end-of-line
-bindkey "^[OF" end-of-line
-bindkey ' ' magic-space    # also do history expansion on space
+bindkey '^[[A' up-line-or-search                    # start typing + [Up-Arrow] - fuzzy find history forward
+bindkey '^[[B' down-line-or-search                  # start typing + [Down-Arrow] - fuzzy find history backward
 
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
+bindkey '^[[H' beginning-of-line                    # [Home] - Go to beginning of line
+bindkey '^[[1~' beginning-of-line                   # [Home] - Go to beginning of line
+bindkey '^[OH' beginning-of-line                    # [Home] - Go to beginning of line
+bindkey '^[[F'  end-of-line                         # [End] - Go to end of line
+bindkey '^[[4~' end-of-line                         # [End] - Go to end of line
+bindkey '^[OF' end-of-line                          # [End] - Go to end of line
 
-bindkey '^[[Z' reverse-menu-complete
+bindkey ' ' magic-space                             # [Space] - do history expansion
+
+bindkey '^[[1;5C' forward-word                      # [Ctrl-RightArrow] - move forward one word
+bindkey '^[[1;5D' backward-word                     # [Ctrl-LeftArrow] - move backward one word
+
+bindkey '^[[Z' reverse-menu-complete                # [TODO] - Perform menu completion, like menu-complete, except that if a menu completion is already in progress, move to the previous completion rather than the next.
 
 # Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
-bindkey '^?' backward-delete-char
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
-bindkey "\e[3~" delete-char
+bindkey '^?' backward-delete-char                   # [Delete] - delete backward
+bindkey '^[[3~' delete-char                         # [fn-Delete] - delete forward
+bindkey '^[3;5~' delete-char                        # [TODO] - delete forward
+bindkey '\e[3~' delete-char                         # [TODO] - delete forward
 
 # consider emacs keybindings:
 
