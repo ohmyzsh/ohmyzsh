37ba1cbc98b106eda950c2cc909a201ca4f556cd
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-02-07 18:30:15 -0500

    Added an alias to git stash include untracked.

--- plugins/git/alias.zsh
+++ plugins/git/alias.zsh
@@ -207,6 +207,8 @@ alias gsp='git stash pop'
 compdef _git gsp=git-stash
 alias gss='git stash save'
 compdef _git gss=git-stash
+alias gsS='git stash save --include-untracked'
+compdef _git gsS=git-stash
 alias gsS='git stash save --patch --no-keep-index'
 compdef _git gsS=git-stash
 
