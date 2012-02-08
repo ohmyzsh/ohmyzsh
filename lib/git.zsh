a9f6aed307f128a58176b151bc36854c9d3899f7
Aleksey Orekhov <ao2391@columbia.edu>
2012-02-08 15:30:58 -0500

    fixed introduced to parse_git_dirty

--- lib/git.zsh
+++ lib/git.zsh
@@ -8,7 +8,7 @@ function git_prompt_info() {
 # Checks if working tree is dirty
 parse_git_dirty() {
   local SUBMODULE_SYNTAX=''
-  if [[ POST_1_7_2_GIT -gt 0 ]]; then
+  if [[ $POST_1_7_2_GIT -gt 0 ]]; then
         SUBMODULE_SYNTAX="--ignore-submodules=dirty"
   fi
   if [[ -n $(git status -s ${SUBMODULE_SYNTAX}  2> /dev/null) ]]; then
