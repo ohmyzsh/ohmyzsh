63b666ba86ec30320945b1941b3543ba48c7dcfe
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-02-09 14:28:47 -0500

    Merged the bundler plugin into the ruby plugin.
    Gem management without bundler is a pain.
    Ruby and bundler are inseparable.

--- plugins/ruby/init.zsh
+++ plugins/ruby/init.zsh
@@ -24,3 +24,12 @@ else
   fi
 fi
 
+# Aliases
+alias b='bundle'
+alias be='b exec'
+alias bi='b install --path vendor/bundle'
+alias bl='b list'
+alias bo='b open'
+alias bp='b package'
+alias bu='b update'
+alias binit="bi && b package && print '\nvendor/ruby' >>! .gitignore"
