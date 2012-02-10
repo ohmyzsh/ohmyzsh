58874461bbd1bbb6bd4442745dec54dcfec57790
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-02-09 22:20:36 -0500

    Renamed binit to bI and fixed bundle .gitignore.

--- plugins/ruby/init.zsh
+++ plugins/ruby/init.zsh
@@ -32,4 +32,9 @@ alias bl='b list'
 alias bo='b open'
 alias bp='b package'
 alias bu='b update'
-alias binit="bi && b package && print '\nvendor/ruby' >>! .gitignore"
+alias bI='bi \
+  && b package \
+  && print .bundle       >>! .gitignore \
+  && print vendor/bundle >>! .gitignore \
+  && print vendor/cache  >>! .gitignore'
+
