b3277242f0ce64f9f8752615ee87a87d3c01f0f9
Simon <contact@saimon.org>
2012-02-23 14:02:13 +0100

    - move alias from git plugin to git-svn - use local variables for the get_svnversion function

--- plugins/git-svn/git-svn.plugin.zsh
+++ plugins/git-svn/git-svn.plugin.zsh
@@ -1,14 +1,23 @@
+# Git and svn mix
+alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
+compdef git-svn-dcommit-push=git
+
+alias gsr='git svn rebase'
+alias gsd='git svn dcommit'
+
 #
 # git equivalent of svnversion
 #
 get_svnversion()
 {
-    alias git_svnversion="git svn find-rev `git describe --always` 2>/dev/null"
-    LANG=C SVN_VERSION=`/usr/bin/svnversion`
-    if [ "X$SVN_VERSION" == "Xexported" -o "X$SVN_VERSION" == "X" ]; then
-        git_svnversion
+    local git_version=`git describe --always 2>/dev/null`
+    local git_svnversion=`git svn find-rev $git_version 2>/dev/null`
+    local svn_version
+    LANG=C svn_version=`/usr/bin/svnversion`
+    if [ "X$svn_version" == "Xexported" -o "X$svn_version" == "X" ]; then
+        echo $git_svnversion
     else
-        echo $SVN_VERSION
+        echo $svn_version
     fi
 }
 alias svnversion=get_svnversion
