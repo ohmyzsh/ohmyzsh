fcd318089de1817191b862ebc7075d0995eff38b
Simon <contact@saimon.org>
2012-02-23 13:37:58 +0100

    use git describe instead of git log

--- plugins/git-svn/git-svn.plugin.zsh
+++ plugins/git-svn/git-svn.plugin.zsh
@@ -3,7 +3,7 @@
 #
 get_svnversion()
 {
-    alias git_svnversion="git svn find-rev `git log -1 --pretty=format:%H 2>/dev/null` 2>/dev/null"
+    alias git_svnversion="git svn find-rev `git describe --always` 2>/dev/null"
     LANG=C SVN_VERSION=`/usr/bin/svnversion`
     if [ "X$SVN_VERSION" == "Xexported" -o "X$SVN_VERSION" == "X" ]; then
         git_svnversion
