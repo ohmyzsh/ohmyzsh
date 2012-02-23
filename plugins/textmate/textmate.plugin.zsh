1273db8adff541f47fc198cb98bccc590c609f67
tinogomes <tinorj@gmail.com>
2012-02-23 11:34:14 -0200

    renamed alias reload_bundles to reload-bundles at textmate plugin

--- plugins/textmate/textmate.plugin.zsh
+++ plugins/textmate/textmate.plugin.zsh
@@ -11,4 +11,4 @@ function tm() {
   mate $1
 }
 
-alias reload_bundles="osascript -e 'tell app \"TextMate\" to reload bundles'"
+alias reload-bundles="osascript -e 'tell app \"TextMate\" to reload bundles'"
