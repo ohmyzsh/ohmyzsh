e643795ac8cf249774693b1e7a91b4d6605cd99f
Mat Schaffer <mat@schaffer.me>
2012-03-17 22:03:01 -0300

    Don't clobber standard Esc+. behavior
    Esc+. works as "last arg" on both bash and zsh. Seems like a shame to introduce a new standard.
    
    Conflicts:
    	lib/key-bindings.zsh

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -6,7 +6,6 @@ bindkey -e                                          # Use emacs key bindings
 
 bindkey '\ew' kill-region                           # [Esc-w] - Kill from the cursor to the mark
 bindkey -s '\el' 'ls\n'                             # [Esc-l] - run command: ls
-bindkey -s '\e.' '..\n'                             # [Esc-.] - run command: .. (up directory)
 bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
 bindkey '^[[5~' up-line-or-history                  # [PageUp] - Up a line of history
 bindkey '^[[6~' down-line-or-history                # [PageDown] - Down a line of history
