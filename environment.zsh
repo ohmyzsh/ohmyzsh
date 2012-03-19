390c353f9762698045c543f9bbc9b1ffd9420675
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-03-19 14:06:41 -0400

    [Fix #35] Add a comment on enabling mouse scroll

--- environment.zsh
+++ environment.zsh
@@ -94,6 +94,9 @@ fi
 export LESSCHARSET="UTF-8"
 export LESSHISTFILE='-'
 export LESSEDIT='vim ?lm+%lm. %f'
+
+# Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
+# Remove -X and -F (exit if the content fits on one screen) to enable it.
 export LESS='-F -g -i -M -R -S -w -X -z-4'
 
 if (( $+commands[lesspipe.sh] )); then
