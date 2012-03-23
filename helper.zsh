ohmyzsh/ohmyzsh

commit cdb95f810f600eed2610189202aa532862d78728

[Fix #41] Do not truncate the commit id

helper.zsh
@@ -15,6 +15,15 @@ function is-true() {
   [[ -n "$1" && "$1" == (1|[Yy]([Ee][Ss]|)|[Tt]([Rr][Uu][Ee]|)|[Oo]([Nn]|)) ]]
 }
 
+# Prints the first non-empty string in the arguments array.
+function coalesce() {
+  for arg in "$argv[@]"; do
+    print "$arg"
+    return 0
+  done
+  return 1
+}
+
 # Trap signals were generated with 'kill -l'.
 # DEBUG, EXIT, and ZERR are Zsh signals.
 TRAP_SIGNALS=(

plugins/git/functions/git-info
@@ -140,7 +140,6 @@ function git-info() {
   local clean
   local clean_formatted
   local commit
-  local commit_short
   local commit_format
   local deleted=0
   local deleted_format
@@ -220,9 +219,8 @@ function git-info() {
   commit="$(git rev-parse HEAD 2>/dev/null)"
 
   # Format commit (short).
-  commit_short="$commit[1,7]"
   zstyle -s ':omz:plugin:git:prompt' commit 'commit_format'
-  zformat -f commit_formatted "$commit_format" "c:$commit_short"
+  zformat -f commit_formatted "$commit_format" "c:$commit"
 
   # Stashed
   if [[ -f "$(_git-dir)/refs/stash" ]]; then
@@ -238,9 +236,6 @@ function git-info() {
     (( line_number++ ))
 
     if (( line_number == 1 )) && [[ "$line" == *'(no branch)'* ]]; then
-      # Set branch to commit (short) when the branch is not found.
-      branch="$commit_short"
-
       # Get action.
       action="$(_git-action)"
       if [[ -n "$action" ]]; then

themes/sorin/prompt_sorin_setup
@@ -35,12 +35,12 @@ function prompt_sorin_setup() {
   zstyle ':omz:plugin:git:prompt' deleted ' %%B%F{red}✖%f%%b'
   zstyle ':omz:plugin:git:prompt' modified ' %%B%F{blue}✱%f%%b'
   zstyle ':omz:plugin:git:prompt' renamed ' %%B%F{magenta}➜%f%%b'
-  zstyle ':omz:plugin:git:prompt' commit '%c'
+  zstyle ':omz:plugin:git:prompt' commit '%.7c'
   zstyle ':omz:plugin:git:prompt' stashed ' %%B%F{cyan}✭%f%%b'
   zstyle ':omz:plugin:git:prompt' unmerged ' %%B%F{yellow}═%f%%b'
   zstyle ':omz:plugin:git:prompt' untracked ' %%B%F{white}◼%f%%b'
-  zstyle ':omz:plugin:git:prompt' prompt ' %F{blue}git%f%b%s'
   zstyle ':omz:plugin:git:prompt' rprompt '%A%B%S%a%d%m%r%U%u'
+  zstyle -e ':omz:plugin:git:prompt' prompt 'reply=("%F{blue}git%f$(coalesce "%b" "%c")%s")'
 
   PROMPT='%F{cyan}%1~%f${git_prompt_info} %(!.%B%F{red}#%f%b.%B%F{green}❯%f%b) '
   RPROMPT='%(?::%F{red}⏎%f)${VIM:+" %B%F{green}V%f%b"}${git_rprompt_info}'

commit 5e5a7803b6c9f1d7931087a6975755433ac4ac28

Fix a formatting bug introduced in cdb95f8

helper.zsh
@@ -17,7 +17,7 @@ function is-true() {
 
 # Prints the first non-empty string in the arguments array.
 function coalesce() {
-  for arg in "$argv[@]"; do
+  for arg in $argv; do
     print "$arg"
     return 0
   done

plugins/git/functions/git-info
@@ -218,9 +218,11 @@ function git-info() {
   # Get commit.
   commit="$(git rev-parse HEAD 2>/dev/null)"
 
-  # Format commit (short).
-  zstyle -s ':omz:plugin:git:prompt' commit 'commit_format'
-  zformat -f commit_formatted "$commit_format" "c:$commit"
+  # Format commit.
+  if [[ -n "$commit" ]]; then
+    zstyle -s ':omz:plugin:git:prompt' commit 'commit_format'
+    zformat -f commit_formatted "$commit_format" "c:$commit"
+  fi
 
   # Stashed
   if [[ -f "$(_git-dir)/refs/stash" ]]; then
@@ -238,6 +240,8 @@ function git-info() {
     if (( line_number == 1 )) && [[ "$line" == *'(no branch)'* ]]; then
       # Get action.
       action="$(_git-action)"
+
+      # Format action.
       if [[ -n "$action" ]]; then
         zstyle -s ':omz:plugin:git:prompt' action 'action_format'
         zformat -f action_formatted "$action_format" "s:$action"
@@ -287,11 +291,11 @@ function git-info() {
   done < <("${(z)status_cmd}" 2>/dev/null)
 
   # Format branch.
-  zstyle -s ':omz:plugin:git:prompt' branch 'branch_format'
-  zformat -f branch_formatted "$branch_format" "b:$branch"
+  if [[ -n "$branch" ]]; then
+    zstyle -s ':omz:plugin:git:prompt' branch 'branch_format'
+    zformat -f branch_formatted "$branch_format" "b:$branch"
 
-  # Format remote.
-  if [[ "$branch" != "$commit" ]]; then
+    # Format remote.
     if [[ -z "$remote" ]]; then
       remote="${$( \
         git rev-parse \

themes/sorin/prompt_sorin_setup
@@ -35,14 +35,14 @@ function prompt_sorin_setup() {
   zstyle ':omz:plugin:git:prompt' deleted ' %%B%F{red}✖%f%%b'
   zstyle ':omz:plugin:git:prompt' modified ' %%B%F{blue}✱%f%%b'
   zstyle ':omz:plugin:git:prompt' renamed ' %%B%F{magenta}➜%f%%b'
-  zstyle ':omz:plugin:git:prompt' commit '%.7c'
+  zstyle ':omz:plugin:git:prompt' commit ':%F{red}%.7c%f'
   zstyle ':omz:plugin:git:prompt' stashed ' %%B%F{cyan}✭%f%%b'
   zstyle ':omz:plugin:git:prompt' unmerged ' %%B%F{yellow}═%f%%b'
   zstyle ':omz:plugin:git:prompt' untracked ' %%B%F{white}◼%f%%b'
+  zstyle ':omz:plugin:git:prompt' prompt ' %F{blue}git%f$(coalesce "%b" "%c")%s'
   zstyle ':omz:plugin:git:prompt' rprompt '%A%B%S%a%d%m%r%U%u'
-  zstyle -e ':omz:plugin:git:prompt' prompt 'reply=(" %F{blue}git%f$(coalesce "%b" "%c")%s")'
 
-  PROMPT='%F{cyan}%1~%f${git_prompt_info} %(!.%B%F{red}#%f%b.%B%F{green}❯%f%b) '
+  PROMPT='%F{cyan}%1~%f${(e)git_prompt_info} %(!.%B%F{red}#%f%b.%B%F{green}❯%f%b) '
   RPROMPT='%(?::%F{red}⏎%f)${VIM:+" %B%F{green}V%f%b"}${git_rprompt_info}'
   SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
 }