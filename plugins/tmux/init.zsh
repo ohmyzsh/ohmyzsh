8cce8ee65d7f5b5eb841d1f2876b969656d50af6
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-03-26 18:29:06 -0400

    [#63] Replace $SHLVL with $TMUX

--- plugins/tmux/init.zsh
+++ plugins/tmux/init.zsh
@@ -20,9 +20,7 @@ alias ta="tmux attach-session"
 alias tl="tmux list-sessions"
 
 # Auto Start
-if (( $SHLVL == 1 )) && zstyle -t ':omz:plugin:tmux:auto' start; then
-  (( SHLVL += 1 )) && export SHLVL
-
+if [[ -z "$TMUX" ]] && zstyle -t ':omz:plugin:tmux:auto' start; then
   session="$(
     tmux list-sessions 2> /dev/null \
       | cut -d':' -f1 \
