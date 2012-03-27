4f4184b5643e9b9d563b86aceff4888f5b560fde
Moses Nakamura <nnythm@gmail.com>
2012-03-27 12:38:09 -0400

    added git grep shortcut--ggr

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -38,6 +38,8 @@ alias gm='git merge'
 compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
+alias ggr='git grep'
+compdef _git ggr='git grep'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
