04d7bda8fbfe82cc6f40090b83d1f3a500c4bb84
Joey Lomanto <joeylomanto@gmail.com>
2012-03-29 16:00:24 -0700

    Adding pretty log to git plugin (gplg).

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -30,6 +30,8 @@ alias glg='git log --stat --max-count=5'
 compdef _git glg=git-log
 alias glgg='git log --graph --max-count=5'
 compdef _git glgg=git-log
+alias gplg="git log --no-merges --graph --pretty=format:'%Cred%h%Creset - %C(green)%aN%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+compdef _git gplg=git-log
 alias gss='git status -s'
 compdef _git gss=git-status
 alias ga='git add'
