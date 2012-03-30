49e3e24e7c2edde55c20b15f5741a6389da4f8f1
Brian Mattern <bmattern@uw.edu>
2012-03-29 19:12:22 -0700

    include default completion matcher if CASE_SENSITIVE

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -12,7 +12,7 @@ zmodload -i zsh/complist
 
 ## case-insensitive (all),partial-word and then substring completion
 if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+  zstyle ':completion:*' matcher-list '' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
   unset CASE_SENSITIVE
 else
   zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
