30e7eb2d6ff40cbd9868361698c704c5e7d94df9
Jeffrey Horn <jrhorn424@gmail.com>
2012-04-01 04:33:19 -0400

    Modfiy local variables to latest ruby patches

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -1,8 +1,8 @@
 alias rubies='rvm list rubies'
 alias gemsets='rvm gemset list'
 
-local ruby18='ruby-1.8.7-p334'
-local ruby19='ruby-1.9.2-p180'
+local ruby18='ruby-1.8.7-p358'
+local ruby19='ruby-1.9.3-p125'
 
 function rb18 {
 	if [ -z "$1" ]; then
