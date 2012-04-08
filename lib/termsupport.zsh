d7cb18eb3d57e96821e8f7d2163463e65b07e6c8
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-04-08 13:04:31 +0200

    + added _show_colors function to debug current 16-color mappings

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -42,6 +42,25 @@ function xterm_color_reset() {
   echo -e "%{\033[0m%}"
 }
 
+function _show_colors() {
+ T='■'   # The test text
+ 
+ echo -e "\n           40m 41m 42m 43m 44m 45m 46m 47m"; 
+ 
+ for FGs in '    m' '   1m' '  30m' '1;30m' \
+            '  31m' '1;31m' '  32m' '1;32m' \
+            '  33m' '1;33m' '  34m' '1;34m' \
+            '  35m' '1;35m' '  36m' '1;36m' \
+            '  37m' '1;37m';
+   do FG=${FGs// /}
+   echo -en " $FGs \033[$FG $T "
+   for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+ 	do echo -en "$EINS \033[$FG\033[$BG $T \033[0m";
+   done
+   echo;
+ done
+}
+
 autoload -U add-zsh-hook
 add-zsh-hook precmd  omz_termsupport_precmd
 add-zsh-hook preexec omz_termsupport_preexec
