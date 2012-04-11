26d120f20332f9c59c925503477fad8d08c688ca
Jakub Zalewski <zalew7@gmail.com>
2012-04-11 21:46:24 +0200

    bugfix: now it works properly

--- lib/android.zsh
+++ lib/android.zsh
@@ -15,17 +15,18 @@ function android_sdk_version() {
   versions[13]='3.2 Honeycomb MR2'
   versions[14]='4.0.0-2 Ice Cream Sandwich'
   versions[15]='4.0.0-2 Ice Cream Sandwich MR1'
-  version=$versions[$sdk]
-  echo "${version}"
+  version="$versions[$sdk]"
+  echo "$version"
 }
 
 function android_package_name() {
   package=$(xmlstarlet sel -t -v "/manifest/@package" AndroidManifest.xml 2> /dev/null) || return
-  echo "${package}"
+  echo "$package"
 }
 
 function android_prompt_info() {
-    if (android_sdk_version) {
+    if [ -n "$(android_sdk_version)" ]; then
     echo "${ZSH_THEME_ANDROID_PROMPT_PREFIX}$(android_package_name) $(android_sdk_version)${ZSH_THEME_ANDROID_PROMPT_SUFFIX}"
-    }
+    fi
+    
 }
