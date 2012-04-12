ad7c53598bc8c5980c01c6466f9ee6078a22df27
ArcheyDevil <archeydevil@gmail.com>
2012-04-12 15:55:42 +1000

    If no theme if defined, don't log a message, still works if the theme is supplied but doesn't exist.

--- lib/theming.zsh
+++ lib/theming.zsh
@@ -2,7 +2,7 @@ set_theme() {
   local themes
   themes=({$OMZ,$ZSH}/themes/$1.zsh-theme(N))
   if ! source $themes[1] 2> /dev/null; then
-    omz_log_msg "theme: $1 was not found. falling back to default."
+    [[ "x$1" != "x" ]] && omz_log_msg "theme: $1 was not found. falling back to default."
     source $ZSH/themes/default.zsh-theme
   fi
 }
