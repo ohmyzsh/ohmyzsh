5809e8da87bcfb2d2fa34a00b2cbc32e9ca01de9
mapc <musikmichael@web.de>
2012-05-05 10:52:46 +0200

    Add cat (+write, +append), enhance formatting

--- plugins/singlechar/singlechar.plugin.zsh
+++ plugins/singlechar/singlechar.plugin.zsh
@@ -11,14 +11,35 @@ default ROOT sudo
 ###########################
 # Alias
 
+# CAT, GREP
+
 alias y='"$GREP" -i'
 alias n='"$GREP" -vi'
 
+alias c='cat'
+alias w='cat >'
+alias a='cat >>'
+
+# XARGS
+
 alias x='xargs'
+
 alias xy='xargs "$GREP" -i'
 alias xn='xargs "$GREP" -iv'
 
+alias xc='xargs cat'
+alias xw='xargs cat >'
+alias xa='xargs cat >>'
+
+# SUDO
+
 alias s='"$ROOT"'
+
 alias sx='"$ROOT" xargs'
+
 alias sxy='"$ROOT" xargs "$GREP" -i'
 alias sxn='"$ROOT" xargs "$GREP" -iv'
+
+alias sxc='"$ROOT" xargs cat'
+alias sxw='"$ROOT" xargs cat >'
+alias sxa='"$ROOT" xargs cat >>'
