2a07aa39b333ecf87d4ba5f9083544988f886dac
Carlos Cardoso <cadusk@yahoo.com.br>
2012-05-15 10:30:54 -0300

    Atualizado script de override vim para Mac.

--- plugins/vim-override/vim-override.plugin.zsh
+++ plugins/vim-override/vim-override.plugin.zsh
@@ -1,10 +1,7 @@
 # try to replace OSX's default vim by MacVim's version
-RECENTVIM=`find /usr/local/Cellar/macvim -name Vim`
+RECENTVIM=`test -e /usr/local/Cellar/macvim && find /usr/local/Cellar/macvim -name Vim`
 
 # if mode indicator wasn't setup by theme, define default
 if [[ "$OSTYPE" == darwin* && -e $RECENTVIM ]]; then
-	echo "Redirecionando vim..."
   alias vim="$RECENTVIM"
-else
-	echo "deu zica criando alias"
 fi
