8923694fb42393d063487203e803199b394bbb47
Varun Vijayaraghavan <varun.vijayaraghavan@visualrevenue.com>
2012-05-24 11:33:19 -0400

    Fixed a bug in checking the platform

--- plugins/sublime/sublime.plugin.zsh
+++ plugins/sublime/sublime.plugin.zsh
@@ -1,8 +1,9 @@
 # Sublime Text 2 Aliases
 #unamestr = 'uname'
-if [[ uname == 'Linux' ]]; then
-	alias st='open -a /usr/bin/sublime_text'
-elif  [[ uname == 'Darwin' ]]; then
-	alias st='open -a "/Applications/Sublime Text 2.app'
+
+if [[ $('uname') == 'Linux' ]]; then
+	alias st='/usr/bin/sublime_text&'
+elif  [[ $('uname') == 'Darwin' ]]; then
+	alias st='open -a /Applications/Sublime Text 2.app'
 fi
 alias stt='st .'
