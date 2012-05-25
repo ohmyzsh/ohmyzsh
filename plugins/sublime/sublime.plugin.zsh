3cbc829321876bdee4872fccf77ab3a177b107ca
Nick Kugaevsky <nick@kugaevsky.ru>
2012-05-25 17:18:18 +0400

    Sublime text aliases for Darwin and Linux.

--- plugins/sublime/sublime.plugin.zsh
+++ plugins/sublime/sublime.plugin.zsh
@@ -1,3 +1,11 @@
 # Sublime Text 2 Aliases
-alias st='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
-alias stt='st .'
+
+unamestr=`uname`
+
+if [[ "$unamestr" == 'Darwin' ]]; then
+        alias st='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+        alias stt='st .'
+elif [[ "$unamestr" == 'Linux' ]]; then
+        alias st='/usr/bin/sublime-text-2'
+        alias stt='/usr/bin/sublime-text-2 .'
+fi
