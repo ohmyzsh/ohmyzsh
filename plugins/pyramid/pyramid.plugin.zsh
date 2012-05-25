a9bcabc2cc39b806112265d3bd1bee23906956a8
Dewayne Richardson <dewayne_richardson@cable.comcast.com>
2012-05-25 11:12:00 -0600

    added more aliases for pyramid

--- plugins/pyramid/pyramid.plugin.zsh
+++ plugins/pyramid/pyramid.plugin.zsh
@@ -1,2 +1,13 @@
 alias pdr="pserve development.ini --reload""
 alias ppr="pserve production.ini --reload""
+
+# Scaffolding Aliases
+#Available scaffolds:
+# alchemy:          Pyramid SQLAlchemy project using url dispatch
+alias pca="pcreate -s alchemy $*"
+# pyramid_mongodb:  pyramid MongoDB project
+alias pcm="pcreate -s pyramid_mongodb $*"
+# starter:          Pyramid starter project
+alias pcs="pcreate -s starter $*"
+# zodb:             Pyramid ZODB project using traversal
+alias pcz="pcreate -s zodb $*"
