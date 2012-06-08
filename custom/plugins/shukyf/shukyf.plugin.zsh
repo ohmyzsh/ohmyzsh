4e9da6c1679fd09ecea87b49ff26ea480cc7504b
Shuky Dvir <shuky.dvir@biodata.com>
2012-06-08 16:11:28 +0300

    adding more plugin

--- custom/plugins/shukyf/shukyf.plugin.zsh
+++ custom/plugins/shukyf/shukyf.plugin.zsh
@@ -10,3 +10,15 @@ b() { cd ~/code/biokm/$1;  }
 
 _b() { _files -W ~/code/biokm -/; }
 compdef _b b
+
+
+
+
+p() { cd ~/private_projects/$1;  }
+
+_p() { _files -W ~/private_projects -/; }
+compdef _p p
+
+
+alias gmf='git merge --no-ff'
+compdef _git gmf=git-merge--no-ff
