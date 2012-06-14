b9a2da61c73b0838f18d0f360e222cb153fbe707
nXqd <nxqd.inbox@gmail.com>
2012-06-14 08:06:36 +0300

    avoid VCS folders

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -8,6 +8,5 @@ GREP_OPTIONS=
 for PATTERN in .cvs .git .hg .svn; do
     GREP_OPTIONS+="--exclude-dir=$PATTERN "
 done
-GREP_OPTIONS+="--color=auto"
-export GREP_OPTIONS="$GREP_OPTIONS"
+export GREP_OPTIONS+='--color=auto '
 export GREP_COLOR='1;32'
