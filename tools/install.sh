d81a06e9f96cbcd0beb47713d03310fb30a13d60
staticsafe <staticsafe@entropynet.net>
2012-06-16 15:12:23 -0400

    Use hash instead of which in install script See - http://stackoverflow.com/a/677212/1403956

--- tools/install.sh
+++ tools/install.sh
@@ -5,7 +5,7 @@ then
 fi
 
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-which git > /dev/null
+hash git &>/dev/null
 if [[ $? -eq 0 ]]; then
   /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 else
