63c637726ee176c6ab3af40f39689ebb5c2b3222
Daniel Dixon <code@danieldixon.com>
2012-06-19 19:43:47 -0500

    Customizing for me.

--- tools/install.sh
+++ tools/install.sh
@@ -7,7 +7,7 @@ fi
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
 which git > /dev/null
 if [[ $? -eq 0 ]]; then
-  /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  /usr/bin/env git clone https://github.com/onedanshow/oh-my-zsh.git ~/.oh-my-zsh
 else
   echo "git not installed"
   exit
