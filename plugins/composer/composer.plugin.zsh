97e97cb864776aaf8fc6c021568b88d227551e06
Natrim <maly@kinet.cz>
2012-06-21 09:45:19 +0200

    tabs

--- plugins/composer/composer.plugin.zsh
+++ plugins/composer/composer.plugin.zsh
@@ -1,11 +1,11 @@
 #alias
 if which composer.phar &> /dev/null; then
-    if which composer &> /dev/null; then
-    else
-	alias composer="composer.phar"
-    fi
+  if which composer &> /dev/null; then
+  else
+    alias composer="composer.phar"
+  fi
 else
-    if which composer &> /dev/null; then
-	alias composer.phar="composer"
-    fi    
+  if which composer &> /dev/null; then
+    alias composer.phar="composer"
+  fi
 fi
