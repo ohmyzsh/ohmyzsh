a636ada19825722146f1c08733bb6eabf4cbf7c3
Natrim <maly@kinet.cz>
2012-06-21 09:43:28 +0200

    move the aliases a little

--- plugins/composer/composer.plugin.zsh
+++ plugins/composer/composer.plugin.zsh
@@ -1,2 +1,11 @@
 #alias
-alias composer="composer.phar"
+if which composer.phar &> /dev/null; then
+    if which composer &> /dev/null; then
+    else
+	alias composer="composer.phar"
+    fi
+else
+    if which composer &> /dev/null; then
+	alias composer.phar="composer"
+    fi    
+fi
