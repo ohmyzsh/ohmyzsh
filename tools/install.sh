5a2d82d7809333af86f0a3ec2fe7f0a19bd5521d
Nicolas McCurdy <thenickperson@gmail.com>
2012-06-22 11:03:13 -0400

    when installing, add original oh-my-zsh repo as upstream

--- tools/install.sh
+++ tools/install.sh
@@ -6,6 +6,7 @@ fi
 
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
 /usr/bin/env git clone https://github.com/thenickperson/oh-my-zsh.git ~/.oh-my-zsh
+/usr/bin/env git remote add upstream https://github.com/robbyrussell/oh-my-zsh.git
 
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
