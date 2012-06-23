6347656b6fef9d592232dcb0019be42b14f052a0
Pavel Puchkin <neoascetic@gmail.com>
2012-06-24 10:47:31 +1100

    Some fixes about local variables

--- plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
+++ plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
@@ -10,16 +10,16 @@ for wrapsource in "/usr/local/bin/virtualenvwrapper.sh" "/etc/bash_completion.d/
       # by placing a .venv file in the project root with a virtualenv name in it
       function _workon_cwd {
         # Check that this is a Git repo and get its root
-        REPO_ROOT=`git_get_root`
-        if [[ -n "$REPO_ROOT" ]]; then
-          ENV_NAME=`basename "$REPO_ROOT"`
-          if [[ -f "$REPO_ROOT/.venv" ]]; then
-            ENV_NAME=`cat "$REPO_ROOT/.venv"`
+        local repo_root=`git_get_root`
+        if [[ -n "$repo_root" ]]; then
+          local env_name=`basename "$repo_root"`
+          if [[ -f "$repo_root/.venv" ]]; then
+            env_name=`cat "$repo_root/.venv"`
           fi
           # Activate the environment only if it is not already active
-          if [[ "$VIRTUAL_ENV" != "$WORKON_HOME/$ENV_NAME" ]]; then
-            if [[ -e "$WORKON_HOME/$ENV_NAME/bin/activate" ]]; then
-              workon "$ENV_NAME" && export CD_VIRTUAL_ENV="$ENV_NAME"
+          if [[ "$VIRTUAL_ENV" != "$WORKON_HOME/$env_name" ]]; then
+            if [[ -e "$WORKON_HOME/$env_name/bin/activate" ]]; then
+              workon "$env_name" && export CD_VIRTUAL_ENV="$env_name"
             else
               _deactivate
             fi
@@ -29,8 +29,6 @@ for wrapsource in "/usr/local/bin/virtualenvwrapper.sh" "/etc/bash_completion.d/
           # Note: this only happens if the virtualenv was activated automatically
           _deactivate
         fi
-        unset REPO_ROOT
-        unset ENV_NAME
       }
 
       function _deactivate() {
