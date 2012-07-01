b643d50f5f4be0ac5e7d4fb5d1b78e0ce058aa1e
Nuno Cardoso <nunopcardoso@gmail.com>
2012-07-01 14:13:13 +0100

    Modified lib/zsh.sh for better prefix/suffix support

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,7 +1,12 @@
 # get the name of the branch we are on
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  if parse_git_dirty; then
+	INFO="$ZSH_THEME_GIT_PROMPT_DIRTY_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_DIRTY"
+  else
+	INFO="$ZSH_THEME_GIT_PROMPT_CLEAN_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_CLEAN"
+  fi
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX$INFO$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
 
@@ -11,11 +16,7 @@ parse_git_dirty() {
   if [[ $POST_1_7_2_GIT -gt 0 ]]; then
         SUBMODULE_SYNTAX="--ignore-submodules=dirty"
   fi
-  if [[ -n $(git status -s ${SUBMODULE_SYNTAX}  2> /dev/null) ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
-  else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
-  fi
+  [[ -n $(git status -s ${SUBMODULE_SYNTAX}  2> /dev/null) ]] && return 0 || return 1
 }
 
 
