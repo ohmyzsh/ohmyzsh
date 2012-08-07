74faabbcabf6dc9a60737bfd54ba1b3a341c547d
Sean M. Collins <sean@coreitpro.com>
2012-08-07 14:38:48 -0400

    Add shortcut for showing log of all branches

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -30,6 +30,8 @@ alias glg='git log --stat --max-count=5'
 compdef _git glg=git-log
 alias glgg='git log --graph --max-count=5'
 compdef _git glgg=git-log
+alias glgga='git log --graph --decorate --all' 
+compdef _git glgga=git-log
 alias gss='git status -s'
 compdef _git gss=git-status
 alias ga='git add'
