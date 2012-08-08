1347cb7bb065a56d80b57b1949779c441a6049b4
TonySeek <tonyseek@gmail.com>
2012-08-08 14:48:54 +0800

    Added the virtualenv_prompt_info function.

---
+++ plugins/virtualenv/virtualenv.plugin.zsh
@@ -0,0 +1,17 @@
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
+ZSH_THEME_VIRTUAL_ENV_PROMPT_PREFIX="("
+ZSH_THEME_VIRTUAL_ENV_PROMPT_SUFFIX=")"
+
+function virtualenv_prompt_info() {
+    if [ -n "$VIRTUAL_ENV" ]; then
+        if [ -f "$VIRTUAL_ENV/__name__" ]; then
+            local name=`cat $VIRTUAL_ENV/__name__`
+        elif [ `basename $VIRTUAL_ENV` = "__" ]; then
+            local name=$(basename $(dirname $VIRTUAL_ENV))
+        else
+            local name=$(basename $VIRTUAL_ENV)
+        fi
+        echo "$ZSH_THEME_VIRTUAL_ENV_PROMPT_PREFIX$name$ZSH_THEME_VIRTUAL_ENV_PROMPT_SUFFIX"
+    fi
+}
