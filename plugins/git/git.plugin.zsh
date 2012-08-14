2e60d4b09f39564bd7c68b82178f832ee93e20d4
Marcelo Eden <edendroid@gmail.com>
2012-08-14 10:23:54 -0300

    do not correct git commands

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -38,6 +38,8 @@ alias gm='git merge'
 compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
+alias gt="git tag"
+compdef _git gt=git-tag
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
