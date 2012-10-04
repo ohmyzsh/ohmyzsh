cb341924223d9195e9c0172204b0764a0f9fcb4c
quiwamu <quiwamu@gmail.com>
2012-10-04 11:24:14 +0900

    modify git_prompt_status
    modified test expression because $(echo | grep foo) returns 'true' in
    some zsh ( for example 4.2.6 )

--- lib/git.zsh
+++ lib/git.zsh
@@ -40,30 +40,30 @@ function git_prompt_long_sha() {
 git_prompt_status() {
   INDEX=$(git status --porcelain 2> /dev/null)
   STATUS=""
-  if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
+  if echo "$INDEX" | grep '^?? ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^A  ' &> /dev/null); then
+  if echo "$INDEX" | grep '^A  ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  elif $(echo "$INDEX" | grep '^M  ' &> /dev/null); then
+  elif echo "$INDEX" | grep '^M  ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
+  if echo "$INDEX" | grep '^ M ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^AM ' &> /dev/null); then
+  elif echo "$INDEX" | grep '^AM ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^ T ' &> /dev/null); then
+  elif echo "$INDEX" | grep '^ T ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^R  ' &> /dev/null); then
+  if echo "$INDEX" | grep '^R  ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
+  if echo "$INDEX" | grep '^ D ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  elif $(echo "$INDEX" | grep '^AD ' &> /dev/null); then
+  elif echo "$INDEX" | grep '^AD ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
+  if echo "$INDEX" | grep '^UU ' &> /dev/null; then
     STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
   fi
   echo $STATUS
