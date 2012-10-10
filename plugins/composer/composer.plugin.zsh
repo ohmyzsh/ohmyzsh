06ac1dd526a10d22b587e693d61da7de45768564
natrim <natrim_cz@yahoo.com>
2012-10-10 09:43:27 +0300

    Update plugins/composer/composer.plugin.zsh
    add more aliases

--- plugins/composer/composer.plugin.zsh
+++ plugins/composer/composer.plugin.zsh
@@ -1,4 +1,4 @@
-#alias
+# main alias
 if which composer.phar &> /dev/null; then
   if which composer &> /dev/null; then
   else
@@ -9,3 +9,13 @@ else
     alias composer.phar="composer"
   fi
 fi
+
+# other aliases                                                                                                                                                                                                                                                                       
+alias c='composer'                                                                                                                                                                                                                                                              
+alias csu='composer self-update'                                                                                                                                                                                                                                                
+alias cu='composer update'                                                                                                                                                                                                                                                      
+alias ci='composer install'                                                                                                                                                                                                                                                     
+alias ccp='composer create-project'                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                
+# install composer in the current directory                                                                                                                                                                                                                                     
+alias cget='curl -s https://getcomposer.org/installer | php'
