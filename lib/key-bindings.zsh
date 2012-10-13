9f2dd08ad5465d42136f778e134430a2db80f076
Felix Dreissig <f30@f30.me>
2012-10-14 00:07:47 +0200

    Make sure the terminal is always in application mode when zle is active.

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -2,6 +2,19 @@
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
 
+# Make sure that the terminal is in application mode when zle is active, since
+# only then values from $terminfo are valid
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+  function zle-line-init() {
+    echoti smkx
+  }
+  function zle-line-finish() {
+    echoti rmkx
+  }
+  zle -N zle-line-init
+  zle -N zle-line-finish
+fi
+
 bindkey -e                                          # Use emacs key bindings
 
 bindkey '\ew' kill-region                           # [Esc-w] - Kill from the cursor to the mark
