c071030b6835d86c09c2ef393c5917649d4abde8
Carlos Cardoso <cadusk@yahoo.com.br>
2012-10-25 21:22:32 -0200

    Added tmux plugin.

---
+++ plugins/tmux/tmux.plugin.zsh
@@ -0,0 +1,1 @@
+alias tmux="tmux -u2"
--- templates/zshrc-mac.zsh
+++ templates/zshrc-mac.zsh
@@ -32,7 +32,7 @@ DISABLE_VENV_CD="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git ssh-agent python pythonbrew vim-override virtualenvwrapper)
+plugins=(git tmux ssh-agent python pythonbrew vim-override virtualenvwrapper)
 
 source $ZSH/oh-my-zsh.sh
 # source `which virtualenvwrapper.sh`
