5c983257726dfc8ac97d5a5e259dcaa9540b808b
Hauke Stange <hauke@jimdo.com>
2012-11-28 10:47:31 +0100

    fixed autojump: autocompletion works with homebrew again

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -1,4 +1,4 @@
-if [ $commands[autojump] ]; then # check if autojump is
+if [ $commands[autojump] ]; then # check if autojump is installed
   if [ -f /usr/share/autojump/autojump.zsh ]; then # debian and ubuntu package
     . /usr/share/autojump/autojump.zsh
   elif [ -f /etc/profile.d/autojump.zsh ]; then # manual installation
