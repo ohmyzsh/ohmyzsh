d83c6116f560d55cf80af8b4cd9e1de7f3572c7d
Sean Jones <neuralsandwich@gmail.com>
2012-12-05 16:45:54 +0000

    Added check for zsh installation (@TomDiddle commit:f80b2727)

--- tools/install.sh
+++ tools/install.sh
@@ -1,3 +1,11 @@
+which zsh>/dev/null
+RES=$?
+if [ $RES -ne 0 ]
+then
+  echo "\033[0;33mYou do not have zsh installed.\033[0m You'll need to install zsh first if you want to install oh-my-zsh"
+  exit
+fi
+
 if [ -d ~/.oh-my-zsh ]
 then
   echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove ~/.oh-my-zsh if you want to install"
