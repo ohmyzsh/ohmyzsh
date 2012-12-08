de36883b9756a58bf505f3c5828ec9cdd7dbca4d
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2012-12-08 19:24:44 +0100

    Use $commands instead of which command.

--- plugins/fasd/fasd.plugin.zsh
+++ plugins/fasd/fasd.plugin.zsh
@@ -2,7 +2,7 @@ if [ $commands[fasd] ]; then # check if fasd is installed
   eval "$(fasd --init auto)"
   alias v='f -e vim'
 
-  if [[ $('uname') == 'Linux' ]] && [[ -e $( which xdg-open 2>&1 ) ]]; then
+  if [[ $('uname') == 'Linux' ]] && [ $commands[xdg-open] ]; then
     alias o='a -e xdg-open'
   elif [[ $('uname') == 'Darwin' ]]; then
     alias o='a -e open'
