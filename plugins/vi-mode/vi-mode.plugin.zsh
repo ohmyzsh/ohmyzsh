003dd8f4b68062b7fd5e4e1725264189ee56e28a
Greg Berenfield <gberenfield@berenfield.com>
2013-01-02 16:20:22 -0500

    fix for Issue 1479

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -7,11 +7,11 @@ function zle-keymap-select zle-line-init zle-line-finish {
     case "$0" in
       (zle-line-init)
         # Enable terminal application mode.
-        echoti smkx
+        printf '%s' ${terminfo[smkx]}
       ;;
       (zle-line-finish)
         # Disable terminal application mode.
-        echoti rmkx
+        printf '%s' ${terminfo[rmkx]}
       ;;
     esac
   fi
