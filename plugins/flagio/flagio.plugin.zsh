2cc3c5bf37b6ecd13629f0849d4a96508b3af04b
ushi <ushi@porkbox.net>
2013-01-02 20:47:22 +0100

    Renamed $path into $file

--- plugins/flagio/flagio.plugin.zsh
+++ plugins/flagio/flagio.plugin.zsh
@@ -1,15 +1,17 @@
 # Upload files to flag.io
 upload() {
+  local file
+
   if [ $# -lt 1 ]; then
     printf 'usage: upload <file> [<file> ...]\n'
     return 1
   fi
 
-  for path in $@; do
-    if [ -f "$path" ]; then
-      curl -sT "$path" flag.io | egrep -v '^#'
+  for file in $@; do
+    if [ -f "$file" ]; then
+      curl -sT "$file" flag.io | egrep -v '^#'
     else
-      printf 'File not found: %s\n' "$path"
+      printf 'File not found: %s\n' "$file"
     fi
   done
 }
