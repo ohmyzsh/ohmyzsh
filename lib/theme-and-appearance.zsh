e5736d39103c59b115270dcf91d31419d3f08130
Cosmin Lu?ă <q4break@gmail.com>
2013-01-03 14:44:17 +0200

    Fixed coding style

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -7,13 +7,12 @@ export LSCOLORS="Gxfxcxdxbxegedabagacad"
 if [ "$DISABLE_LS_COLORS" != "true" ]
 then
   # Find the option for using colors in ls, depending on the version: Linux or BSD
-  if [[ "$(uname -s)" == "NetBSD" ]]
-  then
-      # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
-      # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
-      gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
+  if [[ "$(uname -s)" == "NetBSD" ]]; then
+    # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
+    # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
+    gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
   else
-      ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+    ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
   fi
 fi
 
