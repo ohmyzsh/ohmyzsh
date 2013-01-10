f8d725bad2481cd818ca374d0ae2166b9bf26740
tinogomes <tinorj@gmail.com>
2013-01-10 14:09:55 -0200

    Added encoding_file function for ruby plugin

--- plugins/ruby/ruby.plugin.zsh
+++ plugins/ruby/ruby.plugin.zsh
@@ -4,3 +4,9 @@ alias sgem='sudo gem'
 
 # Find ruby file
 alias rfind='find . -name "*.rb" | xargs grep -n'
+
+# Add first line "#encoding: UTF-8" into a file (for Ruby 1.9)
+encoding_file = function() {
+	sed '1i#encoding: UTF-8' $1 > /tmp/new_file.tmp
+	mv /tmp/new_file.tmp $1
+}
