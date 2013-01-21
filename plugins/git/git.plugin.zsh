209ed16c6984585c6b9d9366ab478ff3d7215118
Ellison Leão <ellisonleao@gmail.com>
2013-01-21 10:41:28 -0200

    added fetch -p alias on git plugin

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -38,6 +38,8 @@ alias gm='git merge'
 compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
+alias gfp='git fetch -p'
+
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
