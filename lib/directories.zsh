4c91f6d13e32d7389cae299d96f6778451a45fc8
Serdar Dalgıç <sd@serdardalgic.org>
2013-01-25 12:58:55 +0200

    Add autoenv plugin, which adopts using Kenneth Reitz's autoenv into oh-my-zsh.

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -29,6 +29,9 @@ cd () {
     cd ../../../..
   elif [[ "x$*" == "x......" ]]; then
     cd ../../../../..
+  elif [ -d ~/.autoenv ]; then
+    source ~/.autoenv/activate.sh
+    autoenv_cd "$@"
   else
     builtin cd "$@"
   fi
