40b7eb9963e056c7f52560a3cb09bd2cef50a1c6
Douglas Creager <douglas.creager@redjack.com>
2013-01-29 09:21:49 -0500

    Use upstream git completion but oh-my-zsh prompt
    The gitfast plugin lets you use the faster upstream zsh completion
    macros.  Before, it would also force you into using the upstream prompt
    expressions.  I want to be able to choose which of the two upstream bits
    to use — I want upstream completion but oh-my-zsh prompts, for instance.

--- plugins/gitfast/gitfast.plugin.zsh
+++ plugins/gitfast/gitfast.plugin.zsh
@@ -1,7 +1,10 @@
 dir=$(dirname $0)
 source $dir/../git/git.plugin.zsh
-source $dir/git-prompt.sh
 
-function git_prompt_info() {
-  __git_ps1 "${ZSH_THEME_GIT_PROMPT_PREFIX//\%/%%}%s${ZSH_THEME_GIT_PROMPT_SUFFIX//\%/%%}"
-}
+if [ -z "$GITFAST_USE_OHMYZSH_PROMPT" ]; then
+    source $dir/git-prompt.sh
+
+    function git_prompt_info() {
+      __git_ps1 "${ZSH_THEME_GIT_PROMPT_PREFIX//\%/%%}%s${ZSH_THEME_GIT_PROMPT_SUFFIX//\%/%%}"
+    }
+fi
