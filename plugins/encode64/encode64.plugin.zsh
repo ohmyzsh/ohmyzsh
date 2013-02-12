f84b2eddeb6e539891bb9c6cd505f4ae5db81c3d
Gaetan SEMET <gsemet@tldlab231.tl.intel.com>
2013-02-12 14:50:46 +0100

    Creating new completion scheme for buildbot and repo tools

--- plugins/encode64/encode64.plugin.zsh
+++ plugins/encode64/encode64.plugin.zsh
@@ -1,4 +1,4 @@
 encode64(){ echo -n $1 | base64 }
-decode64(){ echo -n $1 | base64 -D }
+decode64(){ echo -n $1 | base64 -d }
 alias e64=encode64
 alias d64=decode64
---
+++ plugins/repo/repo.plugin.zsh
@@ -0,0 +1,36 @@
+# Aliases
+#alias r='repo'
+
+##
+#function listRepoCompletions {
+#     reply=(
+#	abandon
+	#branch
+#	branches
+#	checkout
+#	checkout-manifest
+#	cherry-pick
+#	diff
+	#download
+#	format-patch
+#	forall
+#	grep
+#	help
+#	init
+#	list
+	#overview
+#	prune
+#	push
+#	rebase
+#	smartsync
+#	stage
+#	start
+#	status
+	#sync
+#	tag-manifest
+#	upload
+#    );
+#}
+
+###compctl -K listRepoCompletions repo
+
