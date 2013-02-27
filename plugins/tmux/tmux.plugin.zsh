7b15627851983e63a5756f0b3633938c44ed7a7d
Josh Matthews <jmatth@nbcs.rutgers.edu>
2013-02-26 21:47:11 -0500

    Fixing typos, logic, and gremlins in tmux plugin.

--- plugins/tmux/tmux.plugin.zsh
+++ plugins/tmux/tmux.plugin.zsh
@@ -6,7 +6,7 @@
 # Automatically close the terminal when tmux exits
 [[ -n "$ZSH_TMUX_AUTOQUIT" ]] || ZSH_TMUX_AUTOQUIT=$ZSH_TMUX_AUTOSTART
 # Set term to screen or screen-256color based on current terminal support
-[[ -n "$ZSH_TMUX_FIXTERM" ]] || ZSH_TMUX_AUTOCONNECT=true
+[[ -n "$ZSH_TMUX_FIXTERM" ]] || ZSH_TMUX_FIXTERM=true
 
 # Get the absolute path to the current directory
 local zsh_tmux_plugin_path="$(cd "$(dirname "$0")" && pwd)"
@@ -14,49 +14,50 @@ local zsh_tmux_plugin_path="$(cd "$(dirname "$0")" && pwd)"
 # Determine if the terminal supports 256 colors
 if [[ `tput colors` == "256" ]]
 then
-	export $ZSH_TMUX_TERM="screen-256"
+	export ZSH_TMUX_TERM="screen-256color"
 else
-	export $ZSH_TMUX_TERM="screen"
+	export ZSH_TMUX_TERM="screen"
 fi
 
 # Local variable to store the local config file to use, if any.
 local fixed_config=""
 
-# Set the correct local config file to use
+# Set the correct local config file to use.
 if [[ "$ZSH_TMUX_FIXTERM" == "true" ]]
 then
 	if [[ -f $HOME/.tmux.conf || -h $HOME/.tmux.conf ]]
 	then
 		#use this when they have a ~/.tmux.conf
-		fixed_config=$zsh_tmux_plugin_path/tmux.extra.conf
+		fixed_config="$zsh_tmux_plugin_path/tmux.extra.conf"
 	else
 		#use this when they don't have a ~/.tmux.conf
-		fixed_config=$zsh_tmux_plugin_path/tmux.only.conf
+		fixed_config="$zsh_tmux_plugin_path/tmux.only.conf"
 	fi
 fi
 
-# Override tmux with our function
-function zsh_tmux_plugin_start()
+# Wrapper function for tmux.
+function zsh_tmux_plugin_run()
 {
 	# We have other arguments, just run them
-	if [[ ! -n "$@" ]]
+	if [[ -n "$@" ]]
 	then
 		\tmux $@
 	# Try to connect to an existing session.
 	elif [[ "$ZSH_TMUX_AUTOCONNECT" == "true" ]]
 	then
-		\tmux attach || tmux -f $fixed_config new-session
+		\tmux attach || \tmux `[[ -n "$fixed_config" ]] && echo '-f ' $fixed_config`  new-session
 		[[ "$ZSH_TMUX_AUTOQUIT" == "true" ]] && exit
-	# Just try to fix the TERM variable.
+	# Just run tmux, fixing the TERM variable if requested.
 	else
-		\tmux -f $fixed_config
+		\tmux `[[ -n "$fixed_config" ]] && echo '-f ' $fixed_config`
 		[[ "$ZSH_TMUX_AUTOQUIT" == "true" ]] && exit
 	fi
 }
 
+# Alias tmux to our wrapper function.
 alias tmux=zsh_tmux_plugin_start
 
 if [[ "$ZSH_TMUX_AUTOSTART" == "true" ]]
 then
-	zsh_tmux_plugin_start
+	zsh_tmux_plugin_run
 fi
