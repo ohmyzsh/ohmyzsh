bdf4f5a347789069dda438e64467344b551fff00
Spencer Rinehart <anubis@overthemonkey.com>
2013-03-12 13:15:05 -0400

    Allow ":" and "-" characters in phing tasks.
    Tasks that included hyphens or colons were being excluded from
    completion.  This improves the usage for this.

--- plugins/phing/phing.plugin.zsh
+++ plugins/phing/phing.plugin.zsh
@@ -7,7 +7,7 @@ _phing_does_target_list_need_generating () {
 _phing () {
   if [ -f build.xml ]; then
     if _phing_does_target_list_need_generating; then
-      phing -l |grep -v ":" |grep -v "^$"|grep -v "\-" > .phing_targets
+      phing -l |grep -v ":$" |grep -v "^-*$" > .phing_targets
     fi
     compadd `cat .phing_targets`
   fi
