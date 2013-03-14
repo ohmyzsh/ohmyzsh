83ecaf4154fa54fc62c486d84a1ee1c03be804f7
Steven Lu <stevenlu443@gmail.com>
2013-03-14 18:36:26 +0000

    turning off history setting

--- lib/history.zsh
+++ lib/history.zsh
@@ -10,4 +10,4 @@ setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
-setopt share_history # share command history data
+#setopt share_history # share command history data
