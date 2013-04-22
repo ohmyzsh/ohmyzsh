e41714d72c6c5629efe632b1052b590f5f8905c5
Jeremy Attali <jeremy.attali@gmail.com>
2013-04-22 10:59:08 +0200

    Added option to allow untracked files as non dirty
    In this commit, the option only works for git but it should not be to hard for
    someone who knows svn to so the same.
    This commit is largely inspired by @yoavweiss, I only added an option to use
    it.

--- lib/git.zsh
+++ lib/git.zsh
@@ -15,7 +15,11 @@ parse_git_dirty() {
     if [[ $POST_1_7_2_GIT -gt 0 ]]; then
           SUBMODULE_SYNTAX="--ignore-submodules=dirty"
     fi  
-    GIT_STATUS=$(git status -s ${SUBMODULE_SYNTAX} 2> /dev/null | tail -n1)
+    if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" != "true" ]]; then
+        GIT_STATUS=$(git status -s ${SUBMODULE_SYNTAX} 2> /dev/null | tail -n1)
+    else
+        GIT_STATUS=$(git status -s ${SUBMODULE_SYNTAX} -uno 2> /dev/null | tail -n1)
+    fi
     if [[ -n $GIT_STATUS && "$GIT_STATUS" != "$CLEAN_MESSAGE" ]]; then
       echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
     else
