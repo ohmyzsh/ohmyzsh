c35e31a2785c90497b9ac2fdfb2b1d65dbc6b73b
Pavel Puchkin <neoascetic@gmail.com>
2013-04-27 01:11:20 -0300

    Use local variables.

--- plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
+++ plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
@@ -9,17 +9,17 @@ if [[ -f "$wrapsource" ]]; then
     # by placing a .venv file in the project root with a virtualenv name in it
     function _workon_cwd {
         # Check that this is a Git repo
-        PROJECT_ROOT=`git_get_root`
-        if [[ -n "$PROJECT_ROOT" ]]; then
+        local repo_root=`git_get_root`
+        if [[ -n "$repo_root" ]]; then
             # Check for virtualenv name override
-            ENV_NAME=`basename "$PROJECT_ROOT"`
-            if [[ -f "$PROJECT_ROOT/.venv" ]]; then
-                ENV_NAME=`cat "$PROJECT_ROOT/.venv"`
+            local env_name=`basename "$repo_root"`
+            if [[ -f "$repo_root/.venv" ]]; then
+                env_name=`cat "$repo_root/.venv"`
             fi
             # Activate the environment only if it is not already active
-            if [[ "$VIRTUAL_ENV" != "$WORKON_HOME/$ENV_NAME" ]]; then
-                if [[ -e "$WORKON_HOME/$ENV_NAME/bin/activate" ]]; then
-                    workon "$ENV_NAME" && export CD_VIRTUAL_ENV="$ENV_NAME"
+            if [[ "$VIRTUAL_ENV" != "$WORKON_HOME/$env_name" ]]; then
+                if [[ -e "$WORKON_HOME/$env_name/bin/activate" ]]; then
+                    workon "$env_name" && export CD_VIRTUAL_ENV="$env_name"
                 else
                   _deactivate
                 fi
@@ -29,8 +29,6 @@ if [[ -f "$wrapsource" ]]; then
             # Note: this only happens if the virtualenv was activated automatically
             _deactivate
         fi
-        unset PROJECT_ROOT
-        unset ENV_NAME
     }
 
     function _deactivate() {
