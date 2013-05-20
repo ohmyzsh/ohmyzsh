3dc8d07d6a4cfba9fc549783f8f9db2dc15e3fde
Hong Xu <hong@topbug.net>
2013-05-20 15:23:57 -0700

    Improve the `ls --color` detection on OS X

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -7,8 +7,8 @@ export LSCOLORS="Gxfxcxdxbxegedabagacad"
 if [ "$DISABLE_LS_COLORS" != "true" ]
 then
   # Find the option for using colors in ls, depending on the version: Linux or BSD
-  if [[ "$(uname -s)" == "NetBSD" ]]; then
-    # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
+  if [[ "$(uname -s)" =~ "(NetBSD|Darwin)" ]]; then
+    # On NetBSD and OS X, test if "gls" (GNU ls) is installed (this one supports colors); 
     # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
     gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
   else
