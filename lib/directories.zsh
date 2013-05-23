63efab72ff42acc7d0a98278a6be4b72f217060d
Josh Matthews <josh@jmatthews.us>
2013-05-23 11:54:37 -0400

    Removing cd override from directories lib.

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -21,20 +21,6 @@ alias 7='cd -7'
 alias 8='cd -8'
 alias 9='cd -9'
 
-cd () {
-  if   [[ "x$*" == "x..." ]]; then
-    cd ../..
-  elif [[ "x$*" == "x...." ]]; then
-    cd ../../..
-  elif [[ "x$*" == "x....." ]]; then
-    cd ../../../..
-  elif [[ "x$*" == "x......" ]]; then
-    cd ../../../../..
-  else
-    builtin cd "$@"
-  fi
-}
-
 alias md='mkdir -p'
 alias rd=rmdir
 alias d='dirs -v | head -10'
