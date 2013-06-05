3ae99a386a7334fe59d57f9cf0edfad2752a0770
Gaetan Semet <gaetan@xeberon.net>
2013-06-05 11:02:27 +0200

    Updated launch_script completion (not all feature supported)
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- custom/plugins/launch_trial/_launch_trial.sh
+++ custom/plugins/launch_trial/_launch_trial.sh
@@ -6,11 +6,28 @@ __test_programs()
   _describe -t tests 'Select test program:' tests
 }
 
+__test_classes()
+{
+  testprogram="$words[2]"
+  #echo "EXECUTING ./launch_trial.sh --list-test-classes $testprogram"
+  classes=($(./launch_trial.sh --list-test-classes $testprogram))
+  # echo "CLASSES= $classes"
+  _describe -t classes 'Select class:' classes
+}
+
+__test_cases()
+{
+  testprogram="$words[2]"
+  testclass="$words[3]"
+  cases=($(./launch_trial.sh --list-test-cases $testprogram.$testclass))
+  _describe -t cases 'Select test case:' cases
+}
+
 __test_program_and_class()
 {
   _arguments -C : \
     '2: :__test_programs' \
-    '3: :__test_programs' 
+    '3: :__test_programs'
 }
 
 _launch_trial()
@@ -25,8 +42,10 @@ _launch_trial()
     '(- 1 *)--htmlcov[generate html coverage report in htmlcov surbdirectory]' \
     '(- 1 *)--list-test-programs[list all test programs]' \
     '(- 1 *)--list-test-classes[list test classes for a given test program]:programs:__test_programs' \
-    '(- 1 *)--list-test-methods[list all test method of a given test class]:class:__test_program_and_class' \
-    ':projects:__test_programs' \
+    '(- 1 *)--list-test-cases[list all test cases of a given test class]:class:__test_program_and_class' \
+    ':project:__test_programs' \
+    ':class:__test_classes' \
+    ':testcase:__test_cases' \
     && ret=0
 
   return $ret
