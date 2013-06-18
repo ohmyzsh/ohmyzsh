e6fc584464231c7f2ecbd10cb6c8220db4cfa5f7
Gaetan Semet <gaetan@xeberon.net>
2013-06-18 11:49:55 +0200

    compdef for git plugin

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -95,10 +95,15 @@ alias gmt='git mergetool --no-prompt'
 compdef _git gmt=git-mergetool
 
 alias gg='git gui citool'
+compdef _git gg='git gui'
 alias gga='git gui citool --amend'
+compdef _git gga='git gui citool --amend'
 alias gk='gitk --all --branches'
+compdef _git gk='gitk'
 alias gss='git stash show --text'
+compdef _git gss='git stash show'
 alias gitk-entier-history='gitk --all $(git log -g --pretty=format:%h)' # show complete history, with dangling commits
+compdef _git gitk='gitk'
 # Note: if the commit has been cleaned my 'git gc', the dangling commits older than 2 weeks may have been deleted
 #
 alias gitconfig="vim ~/.gitconfig"
