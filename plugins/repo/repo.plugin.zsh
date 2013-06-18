946546282874ca242f9cb7b7a93feb2c068f8e85
Gaetan Semet <gaetan@xeberon.net>
2013-06-18 11:28:42 +0200

    New aliases for repo
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- plugins/repo/repo.plugin.zsh
+++ plugins/repo/repo.plugin.zsh
@@ -2,4 +2,8 @@
 alias r='repo'
 compdef _repo r=repo
 alias rra='repo rebase --auto-stash'
-compdef _repo r=rra='repo rebase --auto-stash'
+compdef _repo rra='repo rebase --auto-stash'
+alias rs='repo sync'
+compdef _repo rs='repo sync'
+alias rsrra='repo sync ; repo rebase --auto-stash'
+compdef _repo rsrra='repo sync ; repo rebase --auto-stash'
