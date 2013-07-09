329e94bf141f18dab011810a36b793a1651e4540
Christophe Bliard <christophe.bliard@trux.info>
2013-07-09 13:51:52 +0200

    Update colorize.plugin.zsh
    correctly detect when pygmentize is not installed
    do not exit shell if when pygmentize is not installed

--- plugins/colorize/colorize.plugin.zsh
+++ plugins/colorize/colorize.plugin.zsh
@@ -6,9 +6,9 @@
 alias colorize='colorize_via_pygmentize'
 
 colorize_via_pygmentize() {
-    if [ ! -x $(which pygmentize) ]; then
-        echo package \'pygmentize\' is not installed!
-        exit -1
+    if [ ! -x "$(which pygmentize)" ]; then
+        echo "package \'pygmentize\' is not installed!"
+        return -1
     fi
 
     if [ $# -eq 0 ]; then
