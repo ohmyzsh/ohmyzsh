7d4f24877773ecfefb83c42def511dc2eb8c1f18
Fabio Fernandes <im.fabiofl@gmail.com>
2013-07-14 16:51:11 -0300

    Fixed Regex.

--- plugins/droplr/droplr.rb
+++ plugins/droplr/droplr.rb
@@ -18,7 +18,7 @@ if ARGV[0].nil?
    exit!(1)
 end
 
-if ARGV[0][%r{^http?://|https://}i]
+if ARGV[0][%r{^http[|s]://}i]
 	`osascript -e 'tell app "Droplr" to shorten "#{ARGV[0]}"'`
 else
 	`open -ga /Applications/Droplr.app "#{ARGV[0]}"`
