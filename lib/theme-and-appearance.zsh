f0857e652e38a5159369dcbd168bf1fda4f8a026
Rob Dudley <rob.dudley@gmail.com>
2013-08-13 09:03:57 +0100

    BSD fix for unsupported ls -G alias and added fallback supprt for colorls as per Issue #836

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -8,9 +8,7 @@ if [ "$DISABLE_LS_COLORS" != "true" ]
 then
 
   # Find the option for using colors in ls, depending on the version: Linux or BSD
-  if [[ $(uname -s) =~ ".*BSD$" ]]; then
-
-    print "BSD"
+  if [[ $(uname -s) =~ ".*win$" ]]; then
     # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
     # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
     gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty' || colorls -G . &>/dev/null 2>&1 && alias ls='colorls -G'
