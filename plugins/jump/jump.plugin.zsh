e458ed6ba53d9edd95e516ab70b995179297893a
Jeroen Janssens <jeroen.janssens@visualrevenue.com>
2013-08-18 14:16:26 -0400

    Add tab completion for jump plugin

--- plugins/jump/jump.plugin.zsh
+++ plugins/jump/jump.plugin.zsh
@@ -8,14 +8,21 @@
 #
 export MARKPATH=$HOME/.marks
 function jump { 
-	cd -P $MARKPATH/$1 2>/dev/null || echo "No such mark: $1"
+	cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
 }
 function mark { 
-	mkdir -p $MARKPATH; ln -s $(pwd) $MARKPATH/$1
+	mkdir -p "$MARKPATH"; ln -s "$(pwd)" $MARKPATH/$1
 }
 function unmark { 
-	rm -i $MARKPATH/$1 
+	rm -i "$MARKPATH/$1"
 }
 function marks {
-	ls -l $MARKPATH | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+	ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
 }
+
+function _completemarks {
+  reply=($(ls $MARKPATH))
+}
+
+compctl -K _completemarks jump
+compctl -K _completemarks unmark
