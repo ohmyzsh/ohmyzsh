dad3c5411dc91acd32f67e80935db379e163434c
Hamish Downer <hamish@aptivate.org>
2013-08-20 18:14:56 +0100

    make git_prompt_status more accurate
    The git_prompt_status function only matched some of the possible output
    from `git status --porcelain --branch` - this commit should match pretty
    much the whole range, defined by the git status man page as:
    
        X          Y     Meaning
        -------------------------------------------------
                  [MD]   not updated
        M        [ MD]   updated in index
        A        [ MD]   added to index
        D         [ M]   deleted from index
        R        [ MD]   renamed in index
        C        [ MD]   copied in index
        [MARC]           index and work tree matches
        [ MARC]     M    work tree changed since index
        [ MARC]     D    deleted in work tree
        -------------------------------------------------
        D           D    unmerged, both deleted
        A           U    unmerged, added by us
        U           D    unmerged, deleted by them
        U           A    unmerged, added by them
        D           U    unmerged, deleted by us
        A           A    unmerged, both added
        U           U    unmerged, both modified
        -------------------------------------------------
        ?           ?    untracked
        !           !    ignored
        -------------------------------------------------
    
    Note that with the current variables it doesn't really distinguish
    between changes in the staging area and changes in the working directory
    - to do that would require a lot more ZSH_THEME_GIT_PROMPT_XYZ variables.

--- lib/git.zsh
+++ lib/git.zsh
@@ -74,32 +74,39 @@ git_prompt_status() {
   if $(echo "$INDEX" | grep -E '^\?\? ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^A  ' &> /dev/null); then
+  if $(echo "$INDEX" | grep '^A[ MDAU] ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  elif $(echo "$INDEX" | grep '^M  ' &> /dev/null); then
+  elif $(echo "$INDEX" | grep '^UA  ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
+  if $(echo "$INDEX" | grep '^C[ MD]  ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_COPIED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^M[ MD] ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^AM ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^ T ' &> /dev/null); then
+  elif $(echo "$INDEX" | grep '^[ MARC]M ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^R  ' &> /dev/null); then
+  if $(echo "$INDEX" | grep '^R[ MD]  ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
   fi
-  if $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
+  if $(echo "$INDEX" | grep '^[MARCDU ]D ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  elif $(echo "$INDEX" | grep '^D  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  elif $(echo "$INDEX" | grep '^AD ' &> /dev/null); then
+  elif $(echo "$INDEX" | grep '^D[ UM] ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
   fi
   if $(command git rev-parse --verify refs/stash >/dev/null 2>&1); then
     STATUS="$ZSH_THEME_GIT_PROMPT_STASHED$STATUS"
   fi
   if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
+  elif $(echo "$INDEX" | grep '^AA ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
+  elif $(echo "$INDEX" | grep '^DD ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
+  elif $(echo "$INDEX" | grep '^U[DA] ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
+  elif $(echo "$INDEX" | grep '^[DA]U ' &> /dev/null); then
     STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
   fi
   if $(echo "$INDEX" | grep '^## .*ahead' &> /dev/null); then
