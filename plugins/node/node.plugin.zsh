300118ec05e6a88d8331c007d076ec1fbf9c5e9c
dchusovitin <dchusovitin@gmail.com>
2013-08-24 13:12:03 +0400

    Fixed opening documentation on Linux (node)

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -1,5 +1,13 @@
 # Open the node api for your current version to the optional section.
 # TODO: Make the section part easier to use.
 function node-docs {
-	open "http://nodejs.org/docs/$(node --version)/api/all.html#all_$1"
+  # get the open command
+  local open_cmd
+  if [[ $(uname -s) == 'Darwin' ]]; then
+    open_cmd='open'
+  else
+    open_cmd='xdg-open'
+  fi
+
+  $open_cmd "http://nodejs.org/docs/$(node --version)/api/all.html#all_$1"
 }
