1b0346fa1763ba057703bed10ca4524ad00226d2
Alexander Gronemann <ag@highcode.dk>
2013-08-29 16:20:40 +0200

    Update bundler.plugin.zsh
    Added taps to bundled_commands

--- plugins/bundler/bundler.plugin.zsh
+++ plugins/bundler/bundler.plugin.zsh
@@ -14,7 +14,7 @@ eval "alias bi='bundle install --jobs=$cores_num'"
 
 # The following is based on https://github.com/gma/bundler-exec
 
-bundled_commands=(annotate berks cap capify cucumber foodcritic foreman guard jekyll kitchen knife middleman nanoc rackup rainbows rake rspec ruby shotgun spec spin spork strainer tailor thin thor unicorn unicorn_rails puma)
+bundled_commands=(annotate berks cap capify cucumber foodcritic foreman guard jekyll kitchen knife middleman nanoc rackup rainbows rake rspec ruby shotgun spec spin spork strainer tailor taps thin thor unicorn unicorn_rails puma)
 
 ## Functions
 
