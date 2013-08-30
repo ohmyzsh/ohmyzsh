8777dd25def8f06c0a281e8e997bd4c09175b1ed
Gaetan Semet <gaetan@xeberon.net>
2013-08-30 14:04:25 +0200

    Improvement of the jump plugin
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- plugins/jump/jump.plugin.zsh
+++ plugins/jump/jump.plugin.zsh
@@ -7,7 +7,19 @@
 # marks: lists all marks
 #
 export MARKPATH=$HOME/.marks
-function jump { 
+function jump {
+  if [[ -z $1 ]]; then
+    echo "Error: no mark required"
+    return
+  fi
+  if [[ $1 == "--help" ]]; then
+    echo "Jump plugin:"
+    echo "  'jump FOO': jump to a mark named FOO"
+    echo "  'mark FOO': create a mark named FOO"
+    echo "  'unmark FOO': delete a mark"
+    echo "  'marks': lists all marks"
+    return
+  fi
 	cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
 }
 function mark { 
@@ -17,7 +29,11 @@ function unmark {
 	rm -i "$MARKPATH/$1"
 }
 function marks {
-	ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+	if [[ -d $MARKPATH ]]; then
+    ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+  else
+    echo "No mark directory"
+  fi
 }
 
 function _completemarks {
@@ -26,3 +42,7 @@ function _completemarks {
 
 compctl -K _completemarks jump
 compctl -K _completemarks unmark
+
+alias j='jump'
+compdef _jump j=jump
+
