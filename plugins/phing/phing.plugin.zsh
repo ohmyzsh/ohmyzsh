a7c88c988a4be02b8883c06e57c6eb290c8fcb21
Fräntz Miccoli <frantz.miccoli@fakedomain.com>
2013-09-15 23:16:11 +0200

    Removed comments and other elements that might appear in the phing -l output. The current version doesn't work at all on my environment (OSX 10.7)

--- plugins/phing/phing.plugin.zsh
+++ plugins/phing/phing.plugin.zsh
@@ -7,7 +7,7 @@ _phing_does_target_list_need_generating () {
 _phing () {
   if [ -f build.xml ]; then
     if _phing_does_target_list_need_generating; then
-      phing -l |grep -v ":$" |grep -v "^-*$" > .phing_targets
+      phing -l|grep -v "\[property\]"|grep -v "Buildfile"|sed 1d|grep -v ":$" |grep -v "^\-*$"|awk '{print $1}' > .phing_targets
     fi
     compadd `cat .phing_targets`
   fi
