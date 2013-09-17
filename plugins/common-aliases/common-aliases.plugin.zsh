efb5c7f6c887ff6be25cd37b730a8137dad7ac9a
Stibbons <gaetan@xeberon.net>
2013-09-18 00:10:37 +0200

    ls --color is not recognized under mac
    Signed-off-by: Stibbons <gaetan@xeberon.net>

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -3,12 +3,12 @@
 #
 
 # ls, the common ones I use a lot shortened for rapid fire usage
-alias ls='ls --color' #I like color
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
+alias ls='ls -G'      # I like color
+alias l='ls -lFh'     # size,show type,human readable
+alias la='ls -lAFh'   # long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   # sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   # long list,sorted by date,show type,human readable
+alias ll='ls -l'      # long list
 alias ldot='ls -ld .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'
