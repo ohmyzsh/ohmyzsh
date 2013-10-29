9ce2284550c91f87a76ba5c4779a99845eaa7862
Gaetan Semet <gaetan@xeberon.net>
2013-10-29 16:17:39 +0100

    new alias for _repo: ru
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- plugins/repo/repo.plugin.zsh
+++ plugins/repo/repo.plugin.zsh
@@ -10,3 +10,6 @@ compdef _repo rs='repo sync'
 
 alias rsrra='repo sync ; repo rebase --auto-stash'
 compdef _repo rsrra='repo sync ; repo rebase --auto-stash'
+
+alias ru='repo upload'
+compdef _repo ru='repo upload'
