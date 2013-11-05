05bf6a58a2d4867afc910fc5fbac2d9ad3c3e584
Chris Martin <christopher.martin2@nike.com>
2013-11-05 08:37:34 -0800

    first pass at some useful xcode stuff

---
+++ custom/xcode.plugin.zsh
@@ -0,0 +1,13 @@
+function xc {
+  xcode_proj=`find . -name "*.xc*" -d 1 | sort -r | head -1`
+  if [[ `echo -n $xcode_proj | wc -m` == 0 ]]
+  then
+    echo "No xcworkspace/xcodeproj file found in the current directory."
+  else
+    echo "Found $xcode_proj" 
+    open "$xcode_proj" 
+  fi
+}
+
+alias xcb='xcodebuild'
+alias xcs='xcode-select -p'
