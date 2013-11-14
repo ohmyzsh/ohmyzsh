bed736e74f518e09b674bed6f5cce8f5de35df69
Petter Abrahamsson <petter@jebus.nu>
2013-11-14 07:06:54 -0500

    Add support for colored ls output on OpenBSD

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -11,6 +11,10 @@ then
     # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
     # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
     gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
+  elif [[ "$(uname -s)" == "OpenBSD" ]]; then
+    # On OpenBSD, test if "colorls" is installed (this one supports colors);
+    # otherwise, leave ls as is, because OpenBSD's ls doesn't support -G
+    colorls -G -d . &>/dev/null 2>&1 && alias ls='colorls -G'
   else
     ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
   fi
