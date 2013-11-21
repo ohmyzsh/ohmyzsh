89ae627540b88d53a5ee4c83af0a1632f940aaad
Valentin Shevko <tuikiken@gmail.com>
2013-11-22 02:34:33 +0300

    Add update statistics
    After the upgrade is interesting to know what's new.

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,6 +1,6 @@
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 cd "$ZSH"
-if git pull --rebase origin master
+if git pull --rebase --stat origin master
 then
   printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
   printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
