b53d132c5daf6517a07fcaa58247e2f6e131bd79
Gaetan Semet <gaetan@xeberon.net>
2013-11-25 11:35:04 +0100

    trying to fix theme_chooser

--- tools/theme_chooser.sh
+++ tools/theme_chooser.sh
@@ -7,6 +7,7 @@
 # To Public License, Version 2, as published by Sam Hocevar. See
 # http://sam.zoy.org/wtfpl/COPYING for more details.
 
+ZSH=$(cd $(dirname $0) && cd .. && pwd)
 THEMES_DIR="$ZSH/themes"
 FAVLIST="${HOME}/.zsh_favlist"
 source $ZSH/oh-my-zsh.sh
