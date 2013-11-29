87e6505f6717148051324f043b08a32a97511537
Stefan Tatschner <stefan@sevenbyte.org>
2013-11-29 14:24:14 +0100

    Added/modified some useful aliases.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -6,6 +6,11 @@ alias po='popd'
 alias ...='cd ../..'
 alias -- -='cd -'
 
+# Prevent headaches
+alias cp='cp -v'
+alias rm='rm -v'
+alias mv='mv -v'
+
 # Super user
 alias _='sudo'
 alias please='sudo'
@@ -27,9 +32,9 @@ else
 fi
 # List direcory contents
 alias lsa='ls -lah'
-alias l='ls -la'
-alias ll='ls -l'
-alias la='ls -lA'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
 alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
