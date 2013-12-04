d8993c991905a761bc0781b64865e714744088c1
Jeff LANCE <jeff.lance@mala.fr>
2013-12-04 18:14:57 +0100

    Fixed version of macports.plugin.zsh to fix issue #1983
    As macports user i think it may be useful to put the export command
    from macports in the plugin.
    MacPorts adds this line when finishes its installation.
    So if macports already installed, when oh-my-zsh install script runs,
    the line disappear.
    If macports is installed after oh-my-zsh, the added lines at the end of
    the .zshrc need to be removed.
    
    This update fix issue #1983.

--- plugins/macports/macports.plugin.zsh
+++ plugins/macports/macports.plugin.zsh
@@ -1,4 +1,9 @@
-#Aliases
+# The export line may need to be commented out in the .zshrc file
+# as MacPorts adds this line when it finished install 
+# Add MacPorts to PATH
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+
+# Aliases
 alias pc="sudo port clean --all installed"
 alias pi="sudo port install $1"
 alias psu="sudo port selfupdate"
