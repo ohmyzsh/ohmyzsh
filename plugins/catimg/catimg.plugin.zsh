63811e9d8b0d3e4fc6a9cfc279c4edea7eb5a4a4
Eduardo San Martin Morote <posva13@gmail.com>
2013-12-09 08:54:38 +0100

    catimg plugin allow to print an image to the stdout using convert

---
+++ plugins/catimg/catimg.plugin.zsh
@@ -0,0 +1,16 @@
+################################################################################
+# catimg script by Eduardo San Martin Morote aka Posva                         #
+# http://posva.net                                                             #
+#                                                                              #
+# Ouput the content of an image to the stdout using the 256 colors of the      #
+# terminal.                                                                    #
+# Github: https://github.com/posva/catimg                                      #
+################################################################################
+
+if [[ -x  `which convert` ]]; then
+  function catimg() {
+    source $(dirname $0)/catimg.sh $@
+  }
+else
+  echo "catimg need convert (ImageMagick) to work)"
+fi
