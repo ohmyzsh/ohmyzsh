60dc9fe409412882cd17fd91b2b0faa7e2a8b912
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-12-13 19:05:35 -0300

    Prevents oh-my-zsh loading the .zshenv twice

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,6 +1,6 @@
 # Check for updates on initial load...
 if [ "$DISABLE_AUTO_UPDATE" != "true" ]; then
-  /usr/bin/env ZSH=$ZSH DISABLE_UPDATE_PROMPT=$DISABLE_UPDATE_PROMPT zsh $ZSH/tools/check_for_upgrade.sh
+  /usr/bin/env ZSH=$ZSH DISABLE_UPDATE_PROMPT=$DISABLE_UPDATE_PROMPT zsh -f $ZSH/tools/check_for_upgrade.sh
 fi
 
 # Initializes Oh My Zsh
