4e635728d7df6f6446e29c617effb163b4b8ef55
Marc Cornellà <marc.cornella@live.com>
2013-12-14 04:30:29 +0100

    Revert previous commit, escape %

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -27,7 +27,7 @@ function omz_termsupport_preexec {
   emulate -L zsh
   setopt extended_glob
   local CMD=${1[(wr)^(*=*|sudo|ssh|rake|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
-  local LINE='${2}'
+  local LINE="${2:gs/%/%%}"
   title "$CMD" '%100>...>$LINE%<<'
 }
 
