7bef81aaa6b6f1ef047d21dc2e33a119418cd8c0
bmallred <bryan.allred@gmail.com>
2013-12-23 08:12:48 -0500

    Fixed variable name in BZR and HG lib files.

--- lib/bzr.zsh
+++ lib/bzr.zsh
@@ -3,7 +3,7 @@
 function bzr_prompt_info() {
 	BZR_BRANCH=$(command bzr nick 2> /dev/null | grep -v "ERROR" | cut -d ":" -f2 | awk -F / '{print $1}')
 
-	if [[ -n "$BZR_CB" ]]; then
+	if [[ -n "$BZR_BRANCH" ]]; then
 		echo "$ZSH_THEME_SCM_PROMPT_PREFIX$BZR_BRANCH$(parse_bzr_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 	fi
 }
--- lib/hg.zsh
+++ lib/hg.zsh
@@ -2,7 +2,7 @@
 function hg_prompt_info() {
     HG_BRANCH=$(command hg branch 2> /dev/null | grep -v ^abort)
 
-    if [[ -n "$HG_CB" ]]; then
+    if [[ -n "$HG_BRANCH" ]]; then
         echo "$ZSH_THEME_HG_PROMPT_PREFIX$HG_BRANCH$(parse_hg_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
     fi
 }
