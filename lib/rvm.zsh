f7d46ec4a6f62b4672ffad64656691fb5feac879
LFDM <1986gh@gmail.com>
2013-12-31 00:35:57 +0100

    Fixes rvm_prompt_info() in lib/rvm.zsh

--- lib/rvm.zsh
+++ lib/rvm.zsh
@@ -2,7 +2,7 @@
 function rvm_prompt_info() {
   [ -f $HOME/.rvm/bin/rvm-prompt ] || return
   local rvm_prompt
-  rvm_prompt=$($HOME/.rvm/bin/rvm-prompt ${ZSH_THEME_RVM_PROMPT_OPTIONS} 2>/dev/null)
+  rvm_prompt=$(eval "$HOME/.rvm/bin/rvm-prompt $ZSH_THEME_RVM_PROMPT_OPTIONS" 2>/dev/null)
   [[ "${rvm_prompt}x" == "x" ]] && return
   echo "${ZSH_THEME_RVM_PROMPT_PREFIX:=(}${rvm_prompt}${ZSH_THEME_RVM_PROMPT_SUFFIX:=)}"
 }
