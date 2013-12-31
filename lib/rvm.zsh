cec97ea2d66a4e856ac0be07da0ec77d3f9c3254
LFDM <1986gh@gmail.com>
2013-12-31 02:11:02 +0100

    Joins ruby prompt files and adds ruby_prompt_info.

--- lib/rvm.zsh
+++
@@ -1,8 +0,0 @@
-# get the name of the ruby version
-function rvm_prompt_info() {
-  [ -f $HOME/.rvm/bin/rvm-prompt ] || return 1
-  local rvm_prompt
-  rvm_prompt=$(eval "$HOME/.rvm/bin/rvm-prompt $ZSH_THEME_RVM_PROMPT_OPTIONS" 2>/dev/null)
-  [[ "${rvm_prompt}x" == "x" ]] && return 1
-  echo "${ZSH_THEME_RVM_PROMPT_PREFIX:=(}${rvm_prompt}${ZSH_THEME_RVM_PROMPT_SUFFIX:=)}"
-}
