0e7f4af3902cce7c816a3c0b562824017eb3ae66
Chen Houwu <chenhouwu@gmail.com>
2014-01-04 18:51:44 +0800

    fix

--- plugins/nodeenv/nodeenv.plugin.zsh
+++ plugins/nodeenv/nodeenv.plugin.zsh
@@ -20,7 +20,6 @@ if [[ ! $DISABLE_NODEENV_CD -eq 1 ]]; then
             else
                 NODEENV_NAME=""
             fi
-            source $NODEENV_NAME/bin/activate && export CD_NODEENV="$NODEENV_NAME"
             if [[ "$NODEENV_NAME" != "" ]]; then
                 source $NODEENV_NAME/bin/activate && export CD_NODEENV="$NODEENV_NAME"
             elif [ $CD_NODEENV ]; then
@@ -44,3 +43,22 @@ if [[ ! $DISABLE_NODEENV_CD -eq 1 ]]; then
         set -A chpwd_functions nodeenv_cwd
     fi
 fi
+
+export NODE_VIRTUAL_ENV_DISABLE_PROMPT=1
+
+ZSH_THEME_NODEENV_PROMPT_PREFIX="("
+ZSH_THEME_NODEENV_PROMPT_SUFFIX=")"
+
+function nodeenv_prompt_info() {
+    if [ -n "$NODE_VIRTUAL_ENV" ]; then
+
+        if [ "`basename "$NODE_VIRTUAL_ENV"`" = "__" ] ; then
+            # special case for Aspen magic directories
+            # see http://www.zetadev.com/software/aspen/
+            local name="[`basename \`dirname "$NODE_VIRTUAL_ENV"\``]"
+        else
+            local name=`basename "$NODE_VIRTUAL_ENV"`
+        fi
+        echo "$ZSH_THEME_NODEENV_PROMPT_PREFIX$name$ZSH_THEME_NODEENV_PROMPT_SUFFIX"
+    fi
+}
