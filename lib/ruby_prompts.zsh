af4245d123b8b7b40bf2a4ab43cdef15589b3b13
LFDM <1986gh@gmail.com>
2014-01-06 09:57:09 +0100

    Adds all other dummy implementations.

--- lib/ruby_prompts.zsh
+++ lib/ruby_prompts.zsh
@@ -1,3 +1,10 @@
+function chruby_prompt_info hg_prompt_info pyenv_prompt_info \
+  rbenv_prompt_info svn_prompt_info vi_mode_prompt_info \
+  virtualenv_prompt_info {
+  return 1
+}
+
+# oh-my-zsh supports an rvm prompt by default
 # get the name of the rvm ruby version
 function rvm_prompt_info() {
   [ -f $HOME/.rvm/bin/rvm-prompt ] || return 1
@@ -7,12 +14,8 @@ function rvm_prompt_info() {
   echo "${ZSH_THEME_RVM_PROMPT_PREFIX:=(}${rvm_prompt}${ZSH_THEME_RVM_PROMPT_SUFFIX:=)}"
 }
 
-# using the chruby and/or rbenv plugins will override
-# these with real implementations
-function chruby_prompt_info rbenv_prompt_info {
-  return 1
-}
-
+# use this to enable users to see their ruby version, no matter which
+# version management system they use
 function ruby_prompt_info() {
   echo $(rvm_prompt_info || rbenv_prompt_info || chruby_prompt_info)
 }
