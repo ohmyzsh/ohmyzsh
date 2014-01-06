de0cabd1801ca50c568e5062411556ecaac88011
LFDM <1986gh@gmail.com>
2014-01-06 09:57:23 +0100

    Adds documentation.

--- lib/ruby_prompts.zsh
+++ lib/ruby_prompts.zsh
@@ -1,3 +1,15 @@
+# *_prompt_info functions for usage in your prompt
+#
+# Plugin creators, please add your *_prompt_info function to the list
+# of dummy implementations to help theme creators not receiving errors
+# without the need of implementing conditional clauses.
+#
+# See also lib/bzr.zsh, lib/git.zsh and lib/nvm.zsh for
+# git_prompt_info, bzr_prompt_info and nvm_prompt_info
+
+# Dummy implementations that return false to prevent command_not_found
+# errors with themes, that implement these functions
+# Real implementations will be used when the respective plugins are loaded
 function chruby_prompt_info hg_prompt_info pyenv_prompt_info \
   rbenv_prompt_info svn_prompt_info vi_mode_prompt_info \
   virtualenv_prompt_info {
