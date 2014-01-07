0b74a39ec348e7c0eea210ec50c6048a86887240
Brice Dutheil <brice.dutheil@gmail.com>
2014-01-07 02:10:34 +0100

    Inline `parse_svn` to avoid leaky state
    As noted in review, `in_svn` is not properly reset to false. This is better anyway to not leak state.

--- plugins/svn-fast-info/svn-fast-info.plugin.zsh
+++ plugins/svn-fast-info/svn-fast-info.plugin.zsh
@@ -9,18 +9,14 @@
 #
 # *** IMPORTANT *** DO NO USE with the simple svn plugin, this plugin acts as a replacement of it.
 
-function parse_svn() {
-	info=$(svn info 2>&1) || return; # capture stdout and stdout
-    in_svn=true
-	repo_need_upgrade="$(svn_repo_need_upgrade $info)"
-    svn_branch_name="$(svn_get_branch_name $info)"
-    svn_dirty="$(svn_dirty_choose)"
-    svn_repo_name="$(svn_get_repo_name $info)"
-    svn_rev="$(svn_get_revision $info)"
-}
-
 function svn_prompt_info() {
-    eval parse_svn
+	info=$(svn info 2>&1) || return; # capture stdout and stdout
+	in_svn=true
+	repo_need_upgrade="$(svn_repo_need_upgrade $info)"
+	svn_branch_name="$(svn_get_branch_name $info)"
+	svn_dirty="$(svn_dirty_choose)"
+	svn_repo_name="$(svn_get_repo_name $info)"
+	svn_rev="$(svn_get_revision $info)"
 
 	if [ ! -z $repo_need_upgrade ]; then
 		echo $ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_PREFIX$ZSH_PROMPT_BASE_COLOR\
@@ -28,13 +24,13 @@ $repo_need_upgrade\
 $ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR
 	fi
 
-    if [[ ${in_svn} == true && -z $repo_need_upgrade ]]; then
-        echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_PREFIX\
+	if [[ ${in_svn} == true && -z $repo_need_upgrade ]]; then
+		echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_PREFIX\
 $ZSH_THEME_REPO_NAME_COLOR${svn_branch_name}\
 $ZSH_PROMPT_BASE_COLOR${svn_dirty}\
 $ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_SUFFIX\
 $ZSH_PROMPT_BASE_COLOR"
-    fi
+	fi
 }
 
 
@@ -47,24 +43,24 @@ function svn_repo_need_upgrade() {
 function svn_get_branch_name() {
 	info=$1
 	[ -z "${info}" ] && info=$(svn info 2> /dev/null)
-    echo $info | grep '^URL:' | egrep -o '(tags|branches)/[^/]+|trunk' | egrep -o '[^/]+$' | read SVN_URL
-    echo $SVN_URL
+	echo $info | grep '^URL:' | egrep -o '(tags|branches)/[^/]+|trunk' | egrep -o '[^/]+$' | read SVN_URL
+	echo $SVN_URL
 }
 
 function svn_get_repo_name() {
 	info=$1
 	[ -z "${info}" ] && info=$(svn info 2> /dev/null)
-    echo $info | sed -n 's/Repository\ Root:\ .*\///p' | read SVN_ROOT
-    echo $info | sed -n "s/URL:\ .*$SVN_ROOT\///p"
+	echo $info | sed -n 's/Repository\ Root:\ .*\///p' | read SVN_ROOT
+	echo $info | sed -n "s/URL:\ .*$SVN_ROOT\///p"
 }
 
 function svn_get_revision() {
 	info=$1
 	[ -z "${info}" ] && info=$(svn info 2> /dev/null)
-    echo $info 2> /dev/null | sed -n s/Revision:\ //p
+	echo $info 2> /dev/null | sed -n s/Revision:\ //p
 }
 
 function svn_dirty_choose() {
-    svn status | grep -E '^\s*[ACDIM!?L]' > /dev/null 2>/dev/null && echo $ZSH_THEME_SVN_PROMPT_DIRTY && return
-    echo $ZSH_THEME_SVN_PROMPT_CLEAN
+	svn status | grep -E '^\s*[ACDIM!?L]' > /dev/null 2>/dev/null && echo $ZSH_THEME_SVN_PROMPT_DIRTY && return
+	echo $ZSH_THEME_SVN_PROMPT_CLEAN
 }
