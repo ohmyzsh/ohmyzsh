b0b79e0abae5f1d228e16c36174bad2bf7ba5fee
LFDM <1986gh@gmail.com>
2014-01-07 19:08:20 +0100

    No need to eval in rvm_prompt_info!
    Just activate word-splitting.

--- lib/prompt_info_functions.zsh
+++ lib/prompt_info_functions.zsh
@@ -21,7 +21,7 @@ function chruby_prompt_info hg_prompt_info pyenv_prompt_info \
 function rvm_prompt_info() {
   [ -f $HOME/.rvm/bin/rvm-prompt ] || return 1
   local rvm_prompt
-  rvm_prompt=$(eval "$HOME/.rvm/bin/rvm-prompt $ZSH_THEME_RVM_PROMPT_OPTIONS" 2>/dev/null)
+  rvm_prompt=$($HOME/.rvm/bin/rvm-prompt ${=ZSH_THEME_RVM_PROMPT_OPTIONS} 2>/dev/null)
   [[ "${rvm_prompt}x" == "x" ]] && return 1
   echo "${ZSH_THEME_RVM_PROMPT_PREFIX:=(}${rvm_prompt}${ZSH_THEME_RVM_PROMPT_SUFFIX:=)}"
 }
