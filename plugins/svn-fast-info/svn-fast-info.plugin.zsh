b3049ce534e2f3de6132a0760b47a1e74becf5cf
Brice Dutheil <brice.dutheil@gmail.com>
2014-01-08 02:50:58 +0100

    Added further cleanup and svn status information

--- plugins/svn-fast-info/svn-fast-info.plugin.zsh
+++ plugins/svn-fast-info/svn-fast-info.plugin.zsh
@@ -14,7 +14,7 @@ function svn_prompt_info() {
   info=$(svn info 2>&1) || return 1; # capture stdout and stderr
   local repo_need_upgrade=$(svn_repo_need_upgrade $info)
 
-  if [ -n $repo_need_upgrade ]; then
+  if [[ -n $repo_need_upgrade ]]; then
     printf '%s%s%s%s%s%s%s\n' \
       $ZSH_PROMPT_BASE_COLOR \
       $ZSH_THEME_SVN_PROMPT_PREFIX \
@@ -22,27 +22,27 @@ function svn_prompt_info() {
       $repo_need_upgrade \
       $ZSH_PROMPT_BASE_COLOR \
       $ZSH_THEME_SVN_PROMPT_SUFFIX \
-      $ZSH_PROMPT_BASE_COLOR \
+      $ZSH_PROMPT_BASE_COLOR
   else
-    # something left for you to fix -
-    # repo name and rev aren't used here, did you forget them?
-    # especially since you set a repo name color
-    # if the prompt is alright this way, leave it as is and just
-    # delete the comment. The functions itself could stay imo,
-    # gives others the chance to use them.
-    printf '%s%s%s%s%s%s%s%s%s\n' \
+    printf '%s%s%s %s%s:%s%s%s%s%s' \
       $ZSH_PROMPT_BASE_COLOR \
       $ZSH_THEME_SVN_PROMPT_PREFIX \
-      $ZSH_THEME_REPO_NAME_COLOR \
-      $(svn_get_branch_name $info)
-      ${svn_branch_name}\
-      $ZSH_PROMPT_BASE_COLOR
-      $(svn_dirty_choose $info)
-      $ZSH_PROMPT_BASE_COLOR
-      $ZSH_THEME_SVN_PROMPT_SUFFIX\
+      \
+      "$(svn_status_info $info)" \
+      $ZSH_PROMPT_BASE_COLOR \
+      \
+      $ZSH_THEME_BRANCH_NAME_COLOR \
+      $(svn_get_branch_name $info) \
+      $ZSH_PROMPT_BASE_COLOR \
+      \
+      $(svn_get_revision $info) \
+      $ZSH_PROMPT_BASE_COLOR \
+      \
+      $ZSH_THEME_SVN_PROMPT_SUFFIX \
       $ZSH_PROMPT_BASE_COLOR
   fi
 }
+
 
 function svn_repo_need_upgrade() {
   grep -q "E155036" <<< ${1:-$(svn info 2> /dev/null)} && \
@@ -50,29 +50,25 @@ function svn_repo_need_upgrade() {
 }
 
 function svn_get_branch_name() {
-  echo ${1:-$(svn info 2> /dev/null)} |\
-    grep '^URL:' | egrep -o '(tags|branches)/[^/]+|trunk' |\
-    egrep -o '[^/]+$'
+  grep '^URL:' <<< "${1:-$(svn info 2> /dev/null)}" | egrep -o '(tags|branches)/[^/]+|trunk'	
 }
 
-function svn_get_repo_name() {
-  # I think this can be further cleaned up as well, not sure how,
-  # as I can't test it
-  local svn_root
-  local info=${1:-$(svn info 2> /dev/null)}
-  echo $info | sed 's/Repository\ Root:\ .*\///p' | read svn_root
-  echo $info | sed "s/URL:\ .*$svn_root\///p"
+function svn_get_repo_root_name() {
+  grep '^Repository\ Root:' <<< "${1:-$(svn info 2> /dev/null)}" | sed 's#.*/##'
 }
 
 function svn_get_revision() {
-  # does this work as it should?
-  echo ${1:-$(svn info 2> /dev/null)} | sed 's/Revision: //p'
+  echo "${1:-$(svn info 2> /dev/null)}" | sed -n 's/Revision: //p'
 }
 
-function svn_dirty_choose() {
-  if svn status | grep -E '^\s*[ACDIM!?L]' &> /dev/null; then
-    echo $ZSH_THEME_SVN_PROMPT_DIRTY
-  else
-    echo $ZSH_THEME_SVN_PROMPT_CLEAN
-  fi
+function svn_status_info() {
+  local svn_status_string="$ZSH_THEME_SVN_PROMPT_CLEAN"
+  local svn_status="$(svn status 2> /dev/null)";
+  if grep -E '^\s*A' &> /dev/null <<< $svn_status; then svn_status_string="$svn_status_string ${ZSH_THEME_SVN_PROMPT_ADDITIONS:-+}"; fi
+  if grep -E '^\s*D' &> /dev/null <<< $svn_status; then svn_status_string="$svn_status_string ${ZSH_THEME_SVN_PROMPT_DELETIONS:-✖}"; fi
+  if grep -E '^\s*M' &> /dev/null <<< $svn_status; then svn_status_string="$svn_status_string ${ZSH_THEME_SVN_PROMPT_MODIFICATIONS:-✎}"; fi
+  if grep -E '^\s*[R~]' &> /dev/null <<< $svn_status; then svn_status_string="$svn_status_string ${ZSH_THEME_SVN_PROMPT_REPLACEMENTS:-∿}"; fi
+  if grep -E '^\s*\?' &> /dev/null <<< $svn_status; then svn_status_string="$svn_status_string ${ZSH_THEME_SVN_PROMPT_UNTRACKED:-?}"; fi
+  if grep -E '^\s*[CI!L]' &> /dev/null <<< $svn_status; then svn_status_string="$svn_status_string ${ZSH_THEME_SVN_PROMPT_DIRTY:-'!'}"; fi
+  echo $svn_status_string
 }
