42fb8c577acf832e8d8d8eacae3b0170b8691bc4
LFDM <1986gh@gmail.com>
2014-01-10 23:50:19 +0100

    Refactors history alias and moves it.
    More compact case statement instead of the ugly if elif... else
    statement. Moves it to history.zsh

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -4,19 +4,5 @@ alias please='sudo'
 
 #alias g='grep -in'
 
-# Show history
-if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]
-then
-    alias history='fc -fl 1'
-elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]
-then
-    alias history='fc -El 1'
-elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ]
-then
-    alias history='fc -il 1'
-else
-    alias history='fc -l 1'
-fi
-
 alias afind='ack-grep -il'
 
