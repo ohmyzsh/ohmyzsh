644aac2bf5c6c5204b29cb0d5366124892e18185
Gaetan Semet <gaetan@xeberon.net>
2014-02-03 14:18:06 +0100

    moved my git plugin to git2
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- templates/zshrc-mac.zsh
+++ templates/zshrc-mac.zsh
@@ -32,7 +32,7 @@ DISABLE_VENV_CD="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git python vim-override common-aliases zsh-syntax-highlighting jump coffee cp
+plugins=(git2 python vim-override common-aliases zsh-syntax-highlighting jump coffee cp
          rsync git-remote-branch command-not-found debian dircycle encode64 lol extract common-aliases
          pip brew sublime grin)
 
