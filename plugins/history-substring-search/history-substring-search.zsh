bd7f5e873b1d5e580442a71dc70b8db45208dd78
Florian Franzen <FlorianFranzen@gmail.com>
2014-02-09 14:06:58 +0100

    Makes history-substring-search use term specific up and down buttons. Fixes #2089.

--- plugins/history-substring-search/history-substring-search.zsh
+++ plugins/history-substring-search/history-substring-search.zsh
@@ -163,8 +163,9 @@ function history-substring-search-down() {
 zle -N history-substring-search-up
 zle -N history-substring-search-down
 
-bindkey '\e[A' history-substring-search-up
-bindkey '\e[B' history-substring-search-down
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
 
 #-----------------------------------------------------------------------------
 # implementation details
