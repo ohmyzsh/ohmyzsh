bfc406d18dece83cb28d614b1387d87e08c143ab
Marten Lienen <marten.lienen@gmail.com>
2014-02-19 17:01:35 +0100

    Remove duplicate `gd` alias

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -13,7 +13,6 @@ alias gup='git pull --rebase'
 compdef _git gup=git-fetch
 alias gp='git push'
 compdef _git gp=git-push
-alias gd='git diff'
 gdv() { git diff -w "$@" | view - }
 compdef _git gdv=git-diff
 alias gc='git commit -v'
