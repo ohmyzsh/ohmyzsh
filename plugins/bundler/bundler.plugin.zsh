30bae92900289b3430bba3a223c39c44ee5d6f7d
Daniel Bye <dan.bye@proquest.co.uk>
2014-02-19 12:05:31 +0000

    Allow FreeBSD to correctly detect number of CPUs on host.
    Use the same scheme as Darwin - sysctl instead of nproc, which doesn't exist in FreeBSD

--- plugins/bundler/bundler.plugin.zsh
+++ plugins/bundler/bundler.plugin.zsh
@@ -19,7 +19,7 @@ bi() {
   if _bundler-installed && _within-bundled-project; then
     local bundler_version=`bundle version | cut -d' ' -f3`
     if [[ $bundler_version > '1.4.0' || $bundler_version = '1.4.0' ]]; then
-      if [[ "$(uname)" == 'Darwin' ]]
+      if [[ "$(uname)" == 'Darwin' || "$(uname)" == 'FreeBSD' ]]
       then
         local cores_num="$(sysctl hw.ncpu | awk '{print $2}')"
       else
