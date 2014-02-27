ffa0907fbd9dd5130e97fa732e37604e66aa273f
Bob Williams <bobwilliams.ii@gmail.com>
2014-02-26 19:35:24 -0500

    Adds command line aliases useful for dealing with JSON

--- plugins/jsontools/jsontools.plugin.zsh
+++ plugins/jsontools/jsontools.plugin.zsh
@@ -5,9 +5,7 @@ if [[ $(whence $JSONTOOLS_METHOD) = "" ]]; then
     JSONTOOLS_METHOD=""
 fi
 
-if [[ $(whence node) != "" && ( "x$JSONTOOLS_METHOD" = "x"  || "x$JSONTOOLS_METHOD" = "xnode" ) ]]; then
-	alias pp_json='xargs -0 node -e "console.log(JSON.stringify(JSON.parse(process.argv[1]), null, 4));"'
-elif [[ $(whence python) != "" && ( "x$JSONTOOLS_METHOD" = "x" || "x$JSONTOOLS_METHOD" = "xpython" ) ]]; then
+if [[ $(whence python) != "" && ( "x$JSONTOOLS_METHOD" = "x" || "x$JSONTOOLS_METHOD" = "xpython" ) ]]; then
 	alias pp_json='python -mjson.tool'
 elif [[ $(whence ruby) != "" && ( "x$JSONTOOLS_METHOD" = "x" || "x$JSONTOOLS_METHOD" = "xruby" ) ]]; then
 	alias pp_json='ruby -e "require \"json\"; require \"yaml\"; puts JSON.parse(STDIN.read).to_yaml"'
