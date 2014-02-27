cd67d8c75b9902934ac058b8e50bac222c2f2031
Bob Williams <bobwilliams.ii@gmail.com>
2014-02-27 10:56:07 -0500

    adding support for node

--- plugins/jsontools/jsontools.plugin.zsh
+++ plugins/jsontools/jsontools.plugin.zsh
@@ -5,7 +5,9 @@ if [[ $(whence $JSONTOOLS_METHOD) = "" ]]; then
     JSONTOOLS_METHOD=""
 fi
 
-if [[ $(whence python) != "" && ( "x$JSONTOOLS_METHOD" = "x" || "x$JSONTOOLS_METHOD" = "xpython" ) ]]; then
+if [[ $(whence node) != "" && ( "x$JSONTOOLS_METHOD" = "x"  || "x$JSONTOOLS_METHOD" = "xnode" ) ]]; then
+    alias pp_json='node -e "console.log(JSON.stringify(process.argv[1]), null, 4)"'
+elif [[ $(whence python) != "" && ( "x$JSONTOOLS_METHOD" = "x" || "x$JSONTOOLS_METHOD" = "xpython" ) ]]; then
 	alias pp_json='python -mjson.tool'
 elif [[ $(whence ruby) != "" && ( "x$JSONTOOLS_METHOD" = "x" || "x$JSONTOOLS_METHOD" = "xruby" ) ]]; then
 	alias pp_json='ruby -e "require \"json\"; require \"yaml\"; puts JSON.parse(STDIN.read).to_yaml"'
