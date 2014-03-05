fec8c1d4874533db8f88ae6d271e4c70a39d7dc7
Sean Carolan <scarolan@gmail.com>
2014-03-05 14:12:55 -0500

    Added command line notifier for vi-mode

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -22,7 +22,7 @@ bindkey -v
 
 # if mode indicator wasn't setup by theme, define default
 if [[ "$MODE_INDICATOR" == "" ]]; then
-  MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$reset_color%}"
+  MODE_INDICATOR="%{$fg[red]%}[CMD]%{$reset_color%}"
 fi
 
 function vi_mode_prompt_info() {
