1e93130b1f87a2747159e06ad338b65d211ccb0e
Alexei Danchenkov <adanchenkov@gmail.com>
2014-03-21 09:24:59 -0700

    Added a forward-delete fallback to bind Delete key
    Delete key (of Fn+Delete on Mac) would print "~" instead of deletion.

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -53,6 +53,10 @@ fi
 bindkey '^?' backward-delete-char                     # [Backspace] - delete backward
 if [[ "${terminfo[kdch1]}" != "" ]]; then
   bindkey "${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
+else
+  bindkey "^[[3~" delete-char
+  bindkey "^[3;5~" delete-char
+  bindkey "\e[3~" delete-char
 fi
 
 # consider emacs keybindings:
