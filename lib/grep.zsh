cd2b97fa94d0f422ceaa83a53434b16aac915a54
Marc Cornellà <marc.cornella@live.com>
2014-03-23 02:03:12 +0100

    Fix export syntax of $GREP_OPTIONS
    Fixes #2641, #2642

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -8,5 +8,6 @@ GREP_OPTIONS=
 for PATTERN in .cvs .git .hg .svn; do
     GREP_OPTIONS+="--exclude-dir=$PATTERN "
 done
-export GREP_OPTIONS+='--color=auto '
+GREP_OPTIONS+="--color=auto"
+export GREP_OPTIONS="$GREP_OPTIONS"
 export GREP_COLOR='1;32'
