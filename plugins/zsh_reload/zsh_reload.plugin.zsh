e183de3999808c4b70147bc722cd639a3be0514c
Marc Cornellà <marc.cornella@live.com>
2014-03-25 22:16:36 +0100

    Use cache folder inside $ZSH, delete with unaliased rm

--- plugins/zsh_reload/zsh_reload.plugin.zsh
+++ plugins/zsh_reload/zsh_reload.plugin.zsh
@@ -1,13 +1,12 @@
-zsh_cache=$HOME/.zsh_cache
-mkdir -p $zsh_cache
-
 # reload zshrc
 function src()
 {
   autoload -U compinit zrecompile
-  compinit -d $zsh_cache/zcomp-$HOST
-  for f in $HOME/.zshrc $zsh_cache/zcomp-$HOST; do
-    zrecompile -p $f && rm -f $f.zwc.old
+  compinit -d "$ZSH/cache/zcomp-$HOST"
+
+  for f in ~/.zshrc "$ZSH/cache/zcomp-$HOST"; do
+    zrecompile -p $f && command rm -f $f.zwc.old
   done
+
   source ~/.zshrc
 }
