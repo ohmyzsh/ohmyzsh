af39ef9d2316e3a37a4d5f524332ddb74d38ab44
Nathan Stilwell <nstilwell@gilt.com>
2014-03-27 14:15:37 -0400

    Gerrit plugin to support drafts

--- plugins/gerrit/gerrit.plugin.zsh
+++ plugins/gerrit/gerrit.plugin.zsh
@@ -6,7 +6,8 @@ function gerrit_usage {
   echo  "   push             Push changes without going through a review. ";
   echo  "   pull             Pull latest changes and rebase ... or something. ";
   echo  "   patch            Work with gerrit patchset";
-  echo  "   review           Push changes and submit changes for review";
+  echo  "   review           Submit changes for review";
+  echo  "   draft            Submit changes for review as draft";
   echo  "   setup-reviewers  Set current repo to automatically have reviewers from your team";
   echo  "   add-reviewer     Add a reviewer to the repo";
   echo  "   clone            Clone a repo from gerrit, requires repo name";
@@ -20,8 +21,9 @@ function gerrit_patch_usage () {
   echo
   echo  " try these :";
   echo
-  echo  "   commit          amend the patchset commit";
-  echo  "   review          push the patchset back to the gerrit review";
+  echo  "   commit          Amend the patchset commit";
+  echo  "   review          Submit patchset back to the gerrit for review";
+  echo  "   draft           Submit patchset back to the gerrit for review as draft";
   echo
 }
 
@@ -31,6 +33,10 @@ function gerrit_push () {
 
 function gerrit_review () {
   git push origin HEAD:refs/for/$1;
+}
+
+function gerrit_draft () {
+  git push origin HEAD:refs/drafts/$1;
 }
 
 function gerrit_pull () {
@@ -45,6 +51,7 @@ function gerrit_patch () {
   else
     [ $1 = "commit" ] && git commit --amend;
     [ $1 = "review" ] && gerrit_review "master";
+    [ $1 = "draft" ] && gerrit_draft "master";
   fi
 }
 
@@ -101,6 +108,7 @@ function gerrit () {
     [ $1 = "patch" ] && gerrit_patch $2
     [ $1 = "push" ] && gerrit_push $branch;
     [ $1 = "review" ] && gerrit_review $branch;
+    [ $1 = "draft" ] && gerrit_draft $branch;
     [ $1 = "pull" ] && gerrit_pull $branch;
     [ $1 = "setup-reviewers" ] && gerrit_set_team_reviewers;
     [ $1 = "add-reviewer" ] && gerrit_add_reviewer $2;
