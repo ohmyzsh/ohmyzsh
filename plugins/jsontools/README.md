400643ff5b20521aebe16ca36fde34bde212db89
Bob Williams <bobwilliams.ii@gmail.com>
2014-03-27 10:34:54 -0400

    adding urldecode_json to compliment urlencode_json and updating readme.md; slight tweak to urlencode_json from previous commit

--- plugins/jsontools/README.md
+++ plugins/jsontools/README.md
@@ -7,6 +7,7 @@ Handy command line tools for dealing with json data.
 - **pp_json** - pretty prints json
 - **is_json** - returns true if valid json; false otherwise
 - **urlencode_json** - returns a url encoded string for the given json 
+- **urldecode_json** - returns decoded json for the given url encoded string
 
 ## Usage
 Usage is simple...just take your json data and pipe it into the appropriate jsontool.
@@ -32,4 +33,10 @@ less data.json | is_json
 ```sh
 # json data directly from the command line
 echo '{"b":2, "a":1}' | urlencode_json
+```
+
+##### urldecode_json
+```sh
+# url encoded string to decode
+echo '%7B%22b%22:2,%20%22a%22:1%7D%0A' | urldecode_json
 ```
--- plugins/jsontools/jsontools.plugin.zsh
+++ plugins/jsontools/jsontools.plugin.zsh
@@ -8,7 +8,8 @@ fi
 if [[ $(whence node) != "" && ( "x$JSONTOOLS_METHOD" = "x"  || "x$JSONTOOLS_METHOD" = "xnode" ) ]]; then
 	alias pp_json='xargs -0 node -e "console.log(JSON.stringify(JSON.parse(process.argv[1]), null, 4));"'
 	alias is_json='xargs -0 node -e "try {json = JSON.parse(process.argv[1]);} catch (e) { console.log(false); json = null; } if(json) { console.log(true); }"'
-	alias urlencode_json='xargs -0 node -e "console.log(encodeURIComponent(JSON.stringify(process.argv[1])))"'
+	alias urlencode_json='xargs -0 node -e "console.log(encodeURIComponent(process.argv[1]))"'
+	alias urldecode_json='xargs -0 node -e "console.log(decodeURIComponent(process.argv[1]))"'
 elif [[ $(whence python) != "" && ( "x$JSONTOOLS_METHOD" = "x" || "x$JSONTOOLS_METHOD" = "xpython" ) ]]; then
 	alias pp_json='python -mjson.tool'
 	alias is_json='python -c "
@@ -22,12 +23,17 @@ else:
 sys.exit(0)"'
 	alias urlencode_json='python -c "
 import urllib, json, sys;
-print urllib.quote_plus(json.dumps(sys.stdin.read()))
+print urllib.quote_plus(sys.stdin.read())
+sys.exit(0)"'
+	alias urldecode_json='python -c "
+import urllib, json, sys;
+print urllib.unquote_plus(sys.stdin.read())
 sys.exit(0)"'
 elif [[ $(whence ruby) != "" && ( "x$JSONTOOLS_METHOD" = "x" || "x$JSONTOOLS_METHOD" = "xruby" ) ]]; then
 	alias pp_json='ruby -e "require \"json\"; require \"yaml\"; puts JSON.parse(STDIN.read).to_yaml"'
 	alias is_json='ruby -e "require \"json\"; begin; JSON.parse(STDIN.read); puts true; rescue Exception => e; puts false; end"'
 	alias urlencode_json='ruby -e "require \"uri\"; puts URI.escape(STDIN.read)"'
+	alias urldecode_json='ruby -e "require \"uri\"; puts URI.unescape(STDIN.read)"'
 fi
 
 unset JSONTOOLS_METHOD
