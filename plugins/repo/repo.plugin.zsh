74416cf0e3a93fb377ec345cda271734e8b9847d
Gaetan Semet <gaetan@xeberon.net>
2014-04-08 14:33:41 +0200

    new repo aliases: rscb and rscbrra
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- plugins/repo/repo.plugin.zsh
+++ plugins/repo/repo.plugin.zsh
@@ -25,3 +25,11 @@ compdef _repo ru='repo upload'
 
 alias rst='repo status'
 compdef _repo rst='repo status'
+
+# Repo start current branch on all projects
+alias rscb='echo "Starting branch $(git branch | sed -n "/\* /s///p") on all projects" && repo start $(git branch | sed -n "/\* /s///p") --all'
+compdef _repo rscb='repo start'
+
+# Repo start and rebase current branch on all projects
+alias rscbrra='echo "Starting & Rebasing branch $(git branch | sed -n "/\* /s///p") on all projects" && rscb && echo "Rebasing..." && rra'
+compdef _repo rscbrra='repo rebase'
