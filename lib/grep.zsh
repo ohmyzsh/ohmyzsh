8b973aa43affce72b78a40fcca1c091fc3ab4ff7
Laban Mwangi <lmwangi@gmail.com>
2014-04-10 12:36:08 +0200

    Fix grep bug that breaks grep by passing unknown options to legacy grep versions

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -5,9 +5,14 @@
 
 # avoid VCS folders
 GREP_OPTIONS=
-for PATTERN in .cvs .git .hg .svn; do
-    GREP_OPTIONS+="--exclude-dir=$PATTERN "
-done
+# --exclude-dir is only available on 2.5.3 and later versions of grep
+if ! grep --version | head -n 1 | egrep " [0-2]\.[0-5]\.[0-2]" 2>&1 >/dev/null;
+then
+    for PATTERN in .cvs .git .hg .svn; do
+        GREP_OPTIONS+="--exclude-dir=$PATTERN "
+    done
+fi
+
 GREP_OPTIONS+="--color=auto"
 export GREP_OPTIONS="$GREP_OPTIONS"
 export GREP_COLOR='1;32'
