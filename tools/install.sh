8fd3ed6f43170a0433fa087e92b8705ae25ab1a0
Aaron Biggs <abiggs@ou.edu>
2014-04-13 12:25:35 -0500

    Commented out Path copy. Shouldn't overwrite path.

--- tools/install.sh
+++ tools/install.sh
@@ -286,4 +286,36 @@ main() {
 	exec zsh -l
 }
 
+<<<<<<< HEAD
 main "$@"
+=======
+echo "\033[0;34mLooking for an existing zsh config...\033[0m"
+if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
+then
+  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.pre-oh-my-zsh\033[0m";
+  mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+fi
+
+echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+# echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
+# sed -i -e "/export PATH=/ c\\
+# export PATH=\"$PATH\"
+# " ~/.zshrc
+
+echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
+chsh -s `which zsh`
+
+echo "\033[0;32m"'         __                                     __   '"\033[0m"
+echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
+echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
+echo "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"\033[0m"
+echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m"
+echo "\033[0;32m"'                        /____/                       '"\033[0m"
+
+echo "\n\n \033[0;32m....is now installed.\033[0m"
+echo "\n\n \033[0;32mPlease look over the ~/.zshrc file to select plugins, themes, and options.\033[0m"
+/usr/bin/env zsh
+source ~/.zshrc
+>>>>>>> Commented out Path copy. Shouldn't overwrite path.
