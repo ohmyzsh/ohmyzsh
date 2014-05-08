c9a5f41f76ced28044832f1606b8b310ba664ed4
Trevor Strieber <trevor@strieber.org>
2014-05-08 14:19:37 -0400

    Fixing typo.

--- plugins/colorize/colorize.plugin.zsh
+++ plugins/colorize/colorize.plugin.zsh
@@ -1,4 +1,4 @@
-# Plugin for highligthing file content
+# Plugin for highlighting file content
 # Plugin highlights file content based on the filename extension.
 # If no highlighting method supported for given extension then it tries 
 # guess it by looking for file content.
