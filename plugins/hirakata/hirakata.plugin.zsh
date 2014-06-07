35ad9d637a687b55817009707b64c60c4035ae74
HardCand01 <dibe.zack@gmail.com>
2014-06-07 22:02:47 +0200

    add: romaji/hiragana/katakana options & sound management

--- plugins/hirakata/hirakata.plugin.zsh
+++ plugins/hirakata/hirakata.plugin.zsh
@@ -1,12 +1,32 @@
-p="$ZSH/plugins/hirakata/"
+p="$ZSH_CUSTOM/plugins/hirakata/"
 f=$(cat $p"files/hk_symbols.txt")
 
 function hirakata() {
-    random_symbol=$(echo "$f" | shuf -n1)
+    random_symbol=$(echo "$fh\n$fk" | shuf -n1)
+    romaji=0
+
+    for arg in "$@"
+    do
+        case "$arg" in
+            "romaji")
+                romaji=1
+                ;;
+            "hiragana")
+                random_symbol=$(echo "$fh" | shuf -n1)
+                ;;
+            "katakana")
+                random_symbol=$(echo "$fk" | shuf -n1)
+                ;;
+        esac
+    done
 
     symbol=$(echo $random_symbol | cut -d ' ' -f1)
     sound=$(echo $random_symbol | cut -d ' ' -f2)
+    (mpg321 -q $HK/sounds/$sound & ) > /dev/null 2>&1
+    echo -n $symbol
 
-    ( mpg321 $p'sounds/'$sound & ) > /dev/null 2>&1
-    echo $symbol
+    if [ $romaji -eq 1 ]; then
+        romaji=$(echo $sound | cut -d '.' -f1)
+        echo -n " ($romaji)"
+    fi
 }
