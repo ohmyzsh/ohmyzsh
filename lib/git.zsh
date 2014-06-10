532590f74bab9f664e1bdaad0baab852dd37c68f
ncanceill <nicolas.canceill@ens-cachan.org>
2014-06-10 09:50:53 +0200

    return 0 when not a git repo before, 128 was returned, which could display an error, but out of a git repo this should exit silently fixes #2226

--- lib/git.zsh
+++ lib/git.zsh
@@ -2,7 +2,7 @@
 function git_prompt_info() {
   if [[ "$(git config --get oh-my-zsh.hide-status)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
+    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
     echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
