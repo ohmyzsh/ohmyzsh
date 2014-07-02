8e52e8946df319900735025048f96680984bd87b
Travis <kuba.marchwicki@gmail.com>
2014-07-02 13:30:08 +0200

    Adding vanilla '-P' in completion suggestions

--- plugins/mvn/mvn.plugin.zsh
+++ plugins/mvn/mvn.plugin.zsh
@@ -232,7 +232,7 @@ function listMavenCompletions {
         archetype:generate generate-sources
         cobertura:cobertura
         -Dtest= `if [ -d ./src/test/java ] ; then find ./src/test/java -type f -name '*.java' | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi`
-        $profiles
+        -P $profiles
     )
 
 }
