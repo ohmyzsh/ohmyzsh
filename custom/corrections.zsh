218fd8c020a5e3a3aafba70cf1f403c7b4c59d7b
Nathan Stilwell <nstilwell@gilt.com>
2014-07-03 15:27:45 -0400

    tweaking corrections

--- custom/corrections.zsh
+++ custom/corrections.zsh
@@ -3,6 +3,6 @@ alias hg='nocorrect hg'
 alias figlet="nocorrect figlet"
 alias ksdiff="nocorrect ksdiff"
 alias scala="nocorrect scala"
-alias grunt="nocorrect grunt"
 alias idea="nocorrect idea"
 alias rake="noglob rake"
+alias ...="noglob ..."
