9f6d971a5f181023a0b35a9c85c232c9d06995cd
Ben Zörb <ben@sommerlaune.com>
2014-07-17 11:27:00 +0200

    #2914 fixed symfony2 autocomplete

--- plugins/symfony2/symfony2.plugin.zsh
+++ plugins/symfony2/symfony2.plugin.zsh
@@ -1,23 +1,24 @@
 # Symfony2 basic command completion
 
+_symfony_console () {
+  echo "php $(find . -maxdepth 2 -mindepth 1 -name 'console')"
+}
+
 _symfony2_get_command_list () {
-	php $(find . -maxdepth 2 -mindepth 1 -name 'console')  --no-ansi | sed "1,/Available commands/d" | awk '/^  [a-z]+/ { print $1 }'
+   `_symfony_console` --no-ansi | sed "1,/Available commands/d" | awk '/^  [a-z]+/ { print $1 }'
 }
 
 _symfony2 () {
-  if [ -f $(find . -maxdepth 2 -mindepth 1 -name 'console')  ]; then
-    compadd `_symfony2_get_command_list`
-  fi
+   compadd `_symfony2_get_command_list`
 }
 
-compdef _symfony2 $(find . -maxdepth 2 -mindepth 1 -name 'console')
+compdef _symfony2 '`_symfony_console`'
 compdef _symfony2 sf
 
 #Alias
-alias sf='php $(find . -maxdepth 2 -mindepth 1 -name 'console') '
-alias sfcl='php $(find . -maxdepth 2 -mindepth 1 -name 'console')  cache:clear'
-alias sfcw='php $(find . -maxdepth 2 -mindepth 1 -name 'console')  cache:warmup'
-alias sfroute='php $(find . -maxdepth 2 -mindepth 1 -name 'console')  router:debug'
-alias sfcontainer='php $(find . -maxdepth 2 -mindepth 1 -name 'console') container:debug'
-alias sfgb='php $(find . -maxdepth 2 -mindepth 1 -name 'console')  generate:bundle'
-
+alias sf='`_symfony_console`'
+alias sfcl='sf cache:clear'
+alias sfcw='sf cache:warmup'
+alias sfroute='sf router:debug'
+alias sfcontainer='sf container:debug'
+alias sfgb='sf generate:bundle'
