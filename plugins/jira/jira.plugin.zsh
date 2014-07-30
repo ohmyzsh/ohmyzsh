d718348d69a24b754132a85eb7fabf388808b2bc
mattkatz <github@morelightmorelight.com>
2014-07-30 13:00:32 -0400

    add cygwin support and detection
    you can action item in cygwin with cygstart or explorer

--- plugins/jira/jira.plugin.zsh
+++ plugins/jira/jira.plugin.zsh
@@ -14,6 +14,8 @@ open_jira_issue () {
   local open_cmd
   if [[ $(uname -s) == 'Darwin' ]]; then
     open_cmd='open'
+  elif [[ $(uname -s) =~ 'CYGWIN*' ]]; then
+    open_cmd='cygstart'
   else
     open_cmd='xdg-open'
   fi
