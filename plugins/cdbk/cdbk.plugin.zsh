a9003004494f2c5f7d2467ab44b602fd8eaad20c
Mike Dacre <mike.dacre@gmail.com>
2014-08-08 13:23:23 -0700

    Improved description

--- plugins/cdbk/cdbk.plugin.zsh
+++ plugins/cdbk/cdbk.plugin.zsh
@@ -1,38 +1,38 @@
 # -----------------------------------------------------------------------------
 #
-#      Program: cdbk.zsh (cd bookmarking for zsh)
+#       Program: cdbk.zsh (cd bookmarking for zsh)
 #
-#        Usage: cdbk {-a,-r,-d} <name> [path] (run with no paramaters for info)
+#         Usage: cdbk {-a,-r,-d} <name> [path] (run with no paramaters for info)
 #
-# Requirements: Needs to be sourced from a zsh startup file, or use oh-my-zsh
+#  Requirements: Needs to be sourced from a zsh startup file, or use oh-my-zsh
 # 
-#   Decription: cdbk() is a simple zsh function to make management of zsh
-#               named directories easier.  It keeps all named directories in a
-#               file and uses grep, sed, echo, and perl to parse and modify 
-#               this file in order to add, change or remove bookmarks.
+#    Revision #: 1.0
+# Last modified: 2014-08-08 13:23
 #
-#               Because it uses the zsh named directory function, full zsh path
-#               completion is possible.  Further, very simple cdbk completion is
-#               included to make replacing and deleting bookmarks easier.
+#    Decription: cdbk() is a simple zsh function to make management of zsh
+#                named directories easier.  It keeps all named directories in a
+#                file and uses grep, sed, echo, and perl to parse and modify 
+#                this file in order to add, change or remove bookmarks.
+#        
+#                Because it uses the zsh named directory function, full zsh path
+#                completion is possible.  Further, very simple cdbk completion is
+#                included to make replacing and deleting bookmarks easier.
 #
-#               This program was heavily inspired by Stan Angeloff's function 
-#               of the same name found here:
-#               http://blog.angeloff.name/post/1027007406/cd-with-bookmarks-and-auto-completion-for-zsh
+#                This program was heavily inspired by Stan Angeloff's function 
+#                of the same name found here:
+#                http://blog.angeloff.name/post/1027007406/cd-with-bookmarks-and-auto-completion-for-zsh
 #
-#               This file also provides the function folder_name(), which returns
-#               a formatted list of the names of the current folder for use in 
-#               a prompt. To include the folder name in your prompt use e.g.:
-#               export PROMPT=$PROMPT $(folder_name)
+#                This file also provides the function folder_name(), which returns
+#                a formatted list of the names of the current folder for use in 
+#                a prompt. To include the folder name in your prompt use e.g.:
+#                export PROMPT=$PROMPT $(folder_name)
 #
-#         Bugs: None that I know of
+#          Bugs: None that I know of
 #
-#   Created by: Mike Dacre 
-#   Created on: 19-11-11
+#    Created by: Mike Dacre 
+#    Created on: 19-11-11
 #
-#   Revision #: 0.9.6-beta
-#      Updated: 23-11-11 10:11:27
-#
-#      License: MIT License - Open Source. Use as you wish
+#       License: MIT License - Open Source. Use as you wish
 #    
 # -----------------------------------------------------------------------------
 
