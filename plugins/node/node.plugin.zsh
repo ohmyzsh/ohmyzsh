cade541ea5afdf4a226c2643baa8930579177e35
Matthias Gliwka <matthias@gliwka.de>
2014-08-19 21:30:28 +0200

    Added autocompletion for node-docs sections

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -11,3 +11,13 @@ function node-docs {
 
   $open_cmd "http://nodejs.org/docs/$(node --version)/api/all.html#all_$1"
 }
+
+_node-docs-get-sections () {
+	curl -s "http://nodejs.org/docs/$(node --version)/api/all.html#all_$1" | grep -o -E 'href="#all_([^"]+)"' | cut -d'"' -f2 | cut -c 6-
+}
+
+_node-docs-complete () {
+	compadd -S '' $(_node-docs-get-sections)
+}
+
+compdef _node-docs-complete node-docs
