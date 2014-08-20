f6806d23ee7f3972ec23e99d73a0690b7b684138
Fredrik Fornwall <fredrik@fornwall.net>
2014-08-20 23:52:05 +0200

    Replace /usr/bin/env with env
    Some environments (such as Android) does not have /usr/bin.

--- tools/check_for_upgrade.sh
+++ tools/check_for_upgrade.sh
@@ -9,7 +9,7 @@ function _update_zsh_update() {
 }
 
 function _upgrade_zsh() {
-  /usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
+  env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
   # update the zsh file
   _update_zsh_update
 }
