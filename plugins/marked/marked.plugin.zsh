9be81015d0fcb7864be9cdfe550aeee20c52bf7a
Chris Smith <csmith@bloomhealthco.com>
2014-08-27 08:31:18 -0500

    Adding quotes around $1

--- plugins/marked/marked.plugin.zsh
+++ plugins/marked/marked.plugin.zsh
@@ -3,9 +3,9 @@
 # If marked is passed a file, open it in Marked
 #
 function marked() {
-    if [ $1 ]
+    if [ "$1" ]
     then
-        open -a "marked 2.app" $1
+        open -a "marked 2.app" "$1"
     else
         open -a "marked 2.app"
     fi
