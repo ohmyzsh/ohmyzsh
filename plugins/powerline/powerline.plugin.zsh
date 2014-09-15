64cee207b975513202678e3c4d438a70104fbc22
Peter Butkovic <butkovic@gmail.com>
2014-09-15 14:12:05 +0200

    added powerline plugin

---
+++ plugins/powerline/powerline.plugin.zsh
@@ -0,0 +1,4 @@
+# please note: you need to install powerline based on official docs first: https://powerline.readthedocs.org/en/latest/installation.html
+
+export PATH="$PATH:$HOME/.local/bin"
+. $HOME/.local/lib/python*/site-packages/powerline/bindings/zsh/powerline.zsh
