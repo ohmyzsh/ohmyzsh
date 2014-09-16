2b06c13b7398af4fa26549fbe13e1ddeb1fe0a72
Moinak Ghosh <moinakg@gmail.com>
2014-09-16 22:29:52 +0530

    Update Solaris changes to use OSTYPE and zsh datetime  module.

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,4 +1,3 @@
-PLAT=`uname -s`
 # Check for updates on initial load...
 if [ "$DISABLE_AUTO_UPDATE" != "true" ]; then
   env ZSH=$ZSH DISABLE_UPDATE_PROMPT=$DISABLE_UPDATE_PROMPT zsh -f $ZSH/tools/check_for_upgrade.sh
