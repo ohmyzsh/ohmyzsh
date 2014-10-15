b452cafb163cde6619ac741027251bed2709f78e
Marc Cornellà <marc.cornella@live.com>
2014-10-15 17:17:04 +0200

    Follow-redirect and silent mode curl throughout gitignore
    This commit completes previous efforts and standardizes both curl commands
    into using silent mode and following redirects in case the URL changes
    again in the future.

--- plugins/gitignore/gitignore.plugin.zsh
+++ plugins/gitignore/gitignore.plugin.zsh
@@ -1,7 +1,7 @@
-function gi() { curl -L https://www.gitignore.io/api/$@ ;}
+function gi() { curl -sL https://www.gitignore.io/api/$@ ;}
 
 _gitignoreio_get_command_list() {
-  curl -s https://www.gitignore.io/api/list | tr "," "\n"
+  curl -sL https://www.gitignore.io/api/list | tr "," "\n"
 }
 
 _gitignoreio () {
