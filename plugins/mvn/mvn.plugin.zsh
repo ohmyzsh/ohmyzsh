77c8d5c99e56fba898b0b8804e73eaf8d420d7e2
Bryan Banz <bryan.banz@firemon.com>
2014-10-22 12:09:53 -0500

    Enable running maven from a project's subdirectory

--- plugins/mvn/mvn.plugin.zsh
+++ plugins/mvn/mvn.plugin.zsh
@@ -39,7 +39,10 @@ mvn-color()
   echo -ne ${RESET_FORMATTING}
   )
 }
- 
+
+# Run mvn against the pom found in a project's root directory (assumes a git repo)
+alias mvn!='mvn -f $(git rev-parse --show-toplevel || echo ".")/pom.xml'
+
 # Override the mvn command with the colorized one.
 #alias mvn="mvn-color"
 
