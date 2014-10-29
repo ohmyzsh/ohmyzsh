2d6e5c545ecc6d35a2a4e781395a56a2dbfd2cf8
lightlfyan <lightlfyan@gmail.com>
2014-10-29 12:38:53 +0800

    Update git.zsh
    show the current branch upstream

--- lib/git.zsh
+++ lib/git.zsh
@@ -3,7 +3,10 @@ function git_prompt_info() {
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    upstream=$(command git rev-parse --abbrev-ref --symbolic-full-name @{u} 2> /dev/null) || \
+    upstream="empty"
+    symbol=" => "
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)${symbol}${upstream}$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
 
