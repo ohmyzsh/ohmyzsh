0bd3c9996fa58494fbefaad483abbe60ed332889
Marc Cornellà <marc.cornella@live.com>
2014-12-13 19:13:27 +0100

    Extract VCS folders definition to avoid repetition

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -6,18 +6,17 @@
 GREP_OPTIONS="--color=auto"
 
 # avoid VCS folders (if the necessary grep flags are available)
+VCS_FOLDERS="{.cvs,.git,.hg,.svn}"
+
 grep-flag-available() {
     echo | grep $1 "" >/dev/null 2>&1
 }
 if grep-flag-available --exclude-dir=.cvs; then
-    for PATTERN in .cvs .git .hg .svn; do
-        GREP_OPTIONS+=" --exclude-dir=$PATTERN"
-    done
+    GREP_OPTIONS+=" --exclude-dir=$VCS_FOLDERS"
 elif grep-flag-available --exclude=.cvs; then
-    for PATTERN in .cvs .git .hg .svn; do
-        GREP_OPTIONS+=" --exclude=$PATTERN"
-    done
+    GREP_OPTIONS+=" --exclude=$VCS_FOLDERS"
 fi
+unset VCS_FOLDERS
 unfunction grep-flag-available
 
 export GREP_OPTIONS="$GREP_OPTIONS"
