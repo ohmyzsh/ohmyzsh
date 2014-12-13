711843153d6e53a89d37931217f59c4f6a3a35db
Marc Cornellà <marc.cornella@live.com>
2014-12-13 19:36:57 +0100

    Tidy up the grep.zsh library

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -1,23 +1,24 @@
-#
-# Color grep results
-# Examples: http://rubyurl.com/ZXv
-#
+# is x grep argument available?
+grep-flag-available() {
+    echo | grep $1 "" >/dev/null 2>&1
+}
 
+# color grep results
 GREP_OPTIONS="--color=auto"
 
-# avoid VCS folders (if the necessary grep flags are available)
+# ignore VCS folders (if the necessary grep flags are available)
 VCS_FOLDERS="{.bzr,.cvs,.git,.hg,.svn}"
 
-grep-flag-available() {
-    echo | command grep $1 "" >/dev/null 2>&1
-}
 if grep-flag-available --exclude-dir=.cvs; then
     GREP_OPTIONS+=" --exclude-dir=$VCS_FOLDERS"
 elif grep-flag-available --exclude=.cvs; then
     GREP_OPTIONS+=" --exclude=$VCS_FOLDERS"
 fi
-unset VCS_FOLDERS
-unfunction grep-flag-available
 
+# export grep settings
 export GREP_OPTIONS="$GREP_OPTIONS"
 export GREP_COLOR='1;32'
+
+# clean up
+unset VCS_FOLDERS
+unfunction grep-flag-available
