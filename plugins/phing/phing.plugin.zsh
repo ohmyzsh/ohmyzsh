a1ffa2c965fc6749647c09374a32918082a926bb
Paweł Lenart <pawel.lenart@xstream.net>
2014-12-16 08:34:50 +0100

    Remove warning lines from phing completion
    Remove "Warning:" lines from phing targets list. Warning lines are
    returned by phing when a target has no tasks or dependencies.

--- plugins/phing/phing.plugin.zsh
+++ plugins/phing/phing.plugin.zsh
@@ -1,6 +1,6 @@
 _phing () {
   if [ -f build.xml ]; then
-    PHING_TARGETS=$(phing -l|grep -v "\[property\]"|grep -v "Buildfile"|sed 1d|grep -v ":$" |grep -v "^\-*$"|awk '{print $1}')
+    PHING_TARGETS=$(phing -l|grep -v "\[property\]"|grep -v "Buildfile"|sed 1d|grep -v ":$" |grep -v "^\-*$"|grep -v "Warning:"|awk '{print $1}')
     compadd $(echo $PHING_TARGETS)
   fi
 }
