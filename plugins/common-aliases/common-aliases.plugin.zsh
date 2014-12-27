14e50824a91b5ecf6aa5fc7c68ea50f58848fd42
Galexrt <Galexrt@googlemail.com>
2014-12-27 20:19:21 +0100

    Update common-aliases.plugin.zsh

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -4,16 +4,16 @@
 
 # ls, the common ones I use a lot shortened for rapid fire usage
 alias l='ls -laFh'    #size,show type,human readable,show hidden
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
-alias ldot='ls -ld .*'
-alias lS='ls -1FSsh'
+alias la='ls -laAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -taRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -latFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -la'      #long list
+alias ldot='ls -lda .*'
+alias lS='ls -1FSsha'
 alias lart='ls -1Fcart'
-alias lrt='ls -1Fcrt'
+alias lrt='ls -1Fcrta'
 
-alias zshrc='vim ~/.zshrc' # Quick access to the ~/.zshrc file
+alias zshrc='nano ~/.zshrc' # Quick access to the ~/.zshrc file
 
 alias grep='grep --color'
 alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
