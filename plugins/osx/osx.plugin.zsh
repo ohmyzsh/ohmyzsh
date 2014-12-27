40eae5175d753475615f18d0a5805008cdb81b95
Vadim <moshasmail@gmail.com>
2014-12-28 01:54:18 +0200

    remove trash command from osx plugin

--- plugins/osx/osx.plugin.zsh
+++ plugins/osx/osx.plugin.zsh
@@ -138,23 +138,6 @@ function man-preview() {
   man -t "$@" | open -f -a Preview
 }
 
-function trash() {
-  local trash_dir="${HOME}/.Trash"
-  local temp_ifs="$IFS"
-  IFS=$'\n'
-  for item in "$@"; do
-    if [[ -e "$item" ]]; then
-      item_name="$(basename $item)"
-      if [[ -e "${trash_dir}/${item_name}" ]]; then
-        mv -f "$item" "${trash_dir}/${item_name} $(date "+%H-%M-%S")"
-      else
-        mv -f "$item" "${trash_dir}/"
-      fi
-    fi
-  done
-  IFS=$temp_ifs
-}
-
 function vncviewer() {
   open vnc://$@
 }
