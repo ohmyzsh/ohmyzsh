7d30246d3101099875cdfc1345fbd94e468fcb00
Rotem Yaari <vmalloc@gmail.com>
2014-12-29 16:50:59 +0200

    Yet another dircycle fix (hopefully the last one)
    The right directions are `-1` for left, `+0` for right cycling

--- plugins/dircycle/dircycle.plugin.zsh
+++ plugins/dircycle/dircycle.plugin.zsh
@@ -2,7 +2,7 @@
 # dircycle plugin: enables cycling through the directory
 # stack using Ctrl+Shift+Left/Right
 
-eval "insert-cycledleft () { zle push-line; LBUFFER='pushd -q +1'; zle accept-line }"
+eval "insert-cycledleft () { zle push-line; LBUFFER='pushd -q -1'; zle accept-line }"
 zle -N insert-cycledleft
 bindkey "\e[1;6D" insert-cycledleft
 eval "insert-cycledright () { zle push-line; LBUFFER='pushd -q +0'; zle accept-line }"
