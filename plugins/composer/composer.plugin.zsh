7c41728d73dff8d1733a4d9673d4d0027001390f
Marc Cornellà <marc.cornella@live.com>
2015-01-04 22:24:55 +0100

    Remove composer.json file requirement
    This is because we can't be sure which arguments require a json file
    and which don't, so the `else' statement will always be difficult to
    check for correctness.

--- plugins/composer/composer.plugin.zsh
+++ plugins/composer/composer.plugin.zsh
@@ -20,18 +20,15 @@ _composer () {
   _arguments \
     '1: :->command'\
     '*: :->args'
-  if [ -f composer.json ]; then
-    case $state in
-      command)
-        compadd `_composer_get_command_list`
-        ;;
-      *)
-        compadd `_composer_get_required_list`
-        ;;
-    esac
-  else
-    compadd create-project init search selfupdate show
-  fi
+
+  case $state in
+    command)
+      compadd $(_composer_get_command_list)
+      ;;
+    *)
+      compadd $(_composer_get_required_list)
+      ;;
+  esac
 }
 
 compdef _composer composer
