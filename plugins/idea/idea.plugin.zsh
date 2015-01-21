55447fb47d14d324d31629d133be791843185848
Dozer <mail@dozer.cc>
2015-01-21 17:27:46 +0800

    add idea plugin

---
+++ plugins/idea/idea.plugin.zsh
@@ -0,0 +1,16 @@
+local _idea_paths > /dev/null 2>&1
+_idea_paths=(
+    "$HOME/Applications/IntelliJ IDEA 14.app"
+    "/Applications/IntelliJ IDEA 14.app"
+    "$HOME/Applications/IntelliJ IDEA 13.app"
+    "/Applications/IntelliJ IDEA 13.app"
+    "$HOME/Applications/IntelliJ IDEA 12.app"
+    "/Applications/IntelliJ IDEA 12.app"
+)
+
+for _idea_path in $_idea_paths; do
+    if [[ -a $_idea_path ]]; then
+        alias idea="open -a '$_idea_path'"
+        break
+    fi
+done
