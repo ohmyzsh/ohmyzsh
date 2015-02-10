732c7b8070a75ac5ad5384b4d22d19f499e8424f
Marc Cornellà <marc.cornella@live.com>
2015-02-10 12:24:05 +0100

    Simplify adding workon_cwd to chpwd hook

--- plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
+++ plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
@@ -64,12 +64,7 @@ if [[ ! $DISABLE_VENV_CD -eq 1 ]]; then
 
   # Append workon_cwd to the chpwd_functions array, so it will be called on cd
   # http://zsh.sourceforge.net/Doc/Release/Functions.html
-  # TODO: replace with 'add-zsh-hook chpwd workon_cwd' when oh-my-zsh min version is raised above 4.3.4
-  if (( ${+chpwd_functions} )); then
-    if (( $chpwd_functions[(I)workon_cwd] == 0 )); then
-      set -A chpwd_functions $chpwd_functions workon_cwd
-    fi
-  else
-    set -A chpwd_functions workon_cwd
+  if ! (( $chpwd_functions[(I)workon_cwd] )); then
+    chpwd_functions+=(workon_cwd)
   fi
 fi
