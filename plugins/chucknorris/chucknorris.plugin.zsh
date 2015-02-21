cb0718c14785de7cf808ef4e2d7ddf5ab3eda8e0
Andrew Janke <janke@pobox.com>
2015-02-21 18:24:07 -0500

    chucknorris: locate plugin dir using $0 instead of hardcoded $ZSH

--- plugins/chucknorris/chucknorris.plugin.zsh
+++ plugins/chucknorris/chucknorris.plugin.zsh
@@ -1,12 +1,11 @@
-() {
-  # Automatically generate or update Chuck's compiled fortune data file
-  local fdir=$ZSH/plugins/chucknorris/fortunes
-  if [[ ! -f $fdir/chucknorris.dat ]] || [[ $fdir/chucknorris.dat -ot $fdir/chucknorris ]]; then
-    strfile $fdir/chucknorris $fdir/chucknorris.dat
-  fi
+# Automatically generate or update Chuck's compiled fortune data file
+DIR=${0:h}/fortunes
+if [[ ! -f $DIR/chucknorris.dat ]] || [[ $DIR/chucknorris.dat -ot $DIR/chucknorris ]]; then
+  strfile $DIR/chucknorris $DIR/chucknorris.dat
+fi
 
-  # Aliases
-  alias chuck="fortune -a $fdir"
-  alias chuck_cow="chuck | cowthink"
-}
+# Aliases
+alias chuck="fortune -a $DIR"
+alias chuck_cow="chuck | cowthink"
 
+unset DIR
