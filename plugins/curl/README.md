d78e5c2f904cdce8976259e908555fb024b9a9da
Ismael Puerto <ismaelpf@axwsismaelpf.central.inditex.grp>
2015-02-25 17:42:41 +0100

    modified:   README.md 	modified:   curl.plugin.zsh

--- plugins/curl/README.md
+++ plugins/curl/README.md
@@ -2,4 +2,11 @@
 
 ## Functions explained
 
+ * curlj - pretty json (require Python 2.6)
+
 ## Aliases explained
+
+ * curlh - show header only
+ * curls - download filename
+ * curlt - show response time in milliseconds
+ * curlrc - show http code
--- plugins/curl/curl.plugin.zsh
+++ plugins/curl/curl.plugin.zsh
@@ -1,5 +1,10 @@
-alias jcurl=jsoncurl
+alias curlj=jsoncurl
 function jsoncurl()
 {
     curl -s $1 |python -m json.tool
 }
+
+alias curlh="curl -siv -o /dev/null"
+alias curls="curl -O"
+alias curlt="curl -o /dev/null -s -w %{time_total}'\\n'"
+alias curlrc="curl -s -o /dev/null -w %{http_code}'\\n'"
