90506cdcec825ceb399696c38a6e9a382855b7a7
Rob Loach <robloach@gmail.com>
2015-03-02 11:56:34 -0500

    Remove virtualenv code from autoenv plugin

--- plugins/autoenv/autoenv.plugin.zsh
+++ plugins/autoenv/autoenv.plugin.zsh
@@ -7,23 +7,3 @@ if ! source $HOME/.autoenv/activate.sh 2>/dev/null; then
   echo '--------------------------'
   return 1
 fi
-
-# The use_env call below is a reusable command to activate/create a new Python
-# virtualenv, requiring only a single declarative line of code in your .env files.
-# It only performs an action if the requested virtualenv is not the current one.
-
-use_env() {
-    typeset venv
-    venv="$1"
-    if [[ "${VIRTUAL_ENV:t}" != "$venv" ]]; then
-        if workon | grep -q "$venv"; then
-            workon "$venv"
-        else
-            echo -n "Create virtualenv $venv now? (Yn) "
-            read answer
-            if [[ "$answer" == "Y" ]]; then
-                mkvirtualenv "$venv"
-            fi
-        fi
-    fi
-}
