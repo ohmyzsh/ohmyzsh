8933f80a76915924472c8cf1f0cfe344f45bd280
seonggwang.gwon <g.passcode@gmail.com>
2015-03-18 11:19:34 +0900

    Update oh-my-zsh.sh
    Fix comment typo

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -14,7 +14,7 @@ if [[ -z "$ZSH_CUSTOM" ]]; then
     ZSH_CUSTOM="$ZSH/custom"
 fi
 
-# Set ZSH_CACHE_DIR to the path where cache files sould be created
+# Set ZSH_CACHE_DIR to the path where cache files should be created
 # or else we will use the default cache/
 if [[ -z "$ZSH_CACHE_DIR" ]]; then
   ZSH_CACHE_DIR="$ZSH/cache/"
