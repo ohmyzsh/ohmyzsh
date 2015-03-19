6fe47dab82c3e3c9c747cf094348a902e7e52c7b
Javier Tia <javier.tia@gmail.com>
2015-03-18 18:00:48 -0600

    Add sc-daemon-reload alias for systemd

--- plugins/systemd/systemd.plugin.zsh
+++ plugins/systemd/systemd.plugin.zsh
@@ -5,7 +5,7 @@ user_commands=(
 sudo_commands=(
   start stop reload restart try-restart isolate kill
   reset-failed enable disable reenable preset mask unmask
-  link load cancel set-environment unset-environment)
+  link load cancel set-environment unset-environment daemon-reload)
 
 for c in $user_commands; do; alias sc-$c="systemctl $c"; done
 for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
