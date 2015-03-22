1dd09dc2a25e95ec750f5ee21e49919ddbc3bdf7
ChaYoung You <yousbe@gmail.com>
2015-03-23 02:32:31 +0900

    Remove `--max-count` option from `git log`

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -60,9 +60,9 @@ compdef gcount=git
 alias gcl='git config --list'
 alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
-alias glg='git log --stat --max-count=10'
+alias glg='git log --stat'
 compdef _git glg=git-log
-alias glgg='git log --graph --max-count=10'
+alias glgg='git log --graph'
 compdef _git glgg=git-log
 alias glgga='git log --graph --decorate --all'
 compdef _git glgga=git-log
