a6092784a822127968b111fef7f0a0c94a80b07f
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-04-19 22:27:03 +0530

    Added github in web_search plugin.

--- plugins/web-search/web-search.plugin.zsh
+++ plugins/web-search/web-search.plugin.zsh
@@ -11,6 +11,7 @@ function web_search() {
     yahoo       "https://search.yahoo.com/search?p="
     duckduckgo  "https://www.duckduckgo.com/?q="
     yandex      "https://yandex.ru/yandsearch?text="
+    github      "https://github.com/search?q="
   )
 
   # define the open command
@@ -49,6 +50,7 @@ alias google='web_search google'
 alias yahoo='web_search yahoo'
 alias ddg='web_search duckduckgo'
 alias yandex='web_search yandex'
+alias github='web_search github'
 
 #add your own !bang searches here
 alias wiki='web_search duckduckgo \!w'
