2bf4b6e5446478f9b30a44caebdc1cdc7174597a
Marcos Alano <marcoshalano@gmail.com>
2015-04-26 17:06:05 -0300

    Added auto-completion for external commands.

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -32,6 +32,9 @@ else
   zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
 fi
 
+# auto-complete external commands
+zstyle ':completion:*' rehash true
+
 # disable named-directories autocompletion
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 
