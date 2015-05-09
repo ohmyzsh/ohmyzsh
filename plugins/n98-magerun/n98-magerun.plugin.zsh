2449e41248f03f4313e67294883cb928d597ad27
Andrew Dwyer <andrewrdwyer@gmail.com>
2015-05-09 15:43:30 +0930

    Add file completion as optional argument

--- plugins/n98-magerun/n98-magerun.plugin.zsh
+++ plugins/n98-magerun/n98-magerun.plugin.zsh
@@ -7,11 +7,19 @@
 
 # n98-magerun basic command completion
 _n98_magerun_get_command_list () {
-  n98-magerun.phar --no-ansi | sed "1,/Available commands/d" | awk '/^ +[a-z\-:]+/ { print $1 }'
+  $_comp_command1 --no-ansi | sed "1,/Available commands/d" | awk '/^ +[a-z\-:]+/ { print $1 }'
 }
 
+
 _n98_magerun () {
-  compadd `_n98_magerun_get_command_list`
+  _arguments '1: :->command' '*:optional arg:_files'
+
+  case $state in
+    command)
+      compadd $(_n98_magerun_get_command_list)
+      ;;
+    *)
+  esac
 }
 
 compdef _n98_magerun n98-magerun.phar
