9cbec4d6717e6989f23f5e2976877482a9e9f571
fjospinas <francisco.ospina@gmail.com>
2015-05-15 10:29:38 -0500

    Add plugin for dnf (New package manager and package management library for Fedora) based in yum plugin

---
+++ plugins/dnf/dnf.plugin.zsh
@@ -0,0 +1,16 @@
+## Based in yum plugin
+## Aliases
+
+alias dnfs="dnf search"                       # search package
+alias dnfp="dnf info"                         # show package info
+alias dnfl="dnf list"                         # list packages
+alias dnfgl="dnf grouplist"                   # list package groups
+alias dnfli="dnf list installed"              # print all installed packages
+alias dnfmc="dnf makecache"                   # rebuilds the dnf package list
+
+alias dnfu="sudo dnf upgrade"                  # upgrate packages
+alias dnfi="sudo dnf install"                 # install package
+alias dnfgi="sudo dnf groupinstall"           # install package group
+alias dnfr="sudo dnf remove"                  # remove package
+alias dnfgr="sudo dnf groupremove"            # remove pagage group
+alias dnfc="sudo dnf clean all"               # clean cache
