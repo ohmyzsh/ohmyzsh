bd96038b5c8cb1f1df28ef02dd34efade426db98
KhasMek <Boushh@gmail.com>
2015-05-21 09:10:09 -0600

    Plugins: repo - add more aliases.

--- plugins/repo/repo.plugin.zsh
+++ plugins/repo/repo.plugin.zsh
@@ -16,3 +16,15 @@ compdef _repo ru='repo upload'
 
 alias rst='repo status'
 compdef _repo rst='repo status'
+
+alias rsto='repo status -o'
+compdef _repo rsto='repo status -o'
+
+alias rfa='repo forall -c'
+compdef _repo rfa='repo forall -c'
+
+alias rfap='repo forall -p -c'
+compdef _repo rfap='repo forall -p -c'
+
+alias rinf='repo info'
+compdef _repo rinf='repo info'
