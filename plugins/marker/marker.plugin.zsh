64ed689ee21e01ee19b81ad2f28f54521786266a
Amine Hajyoussef <hajyoussef.amine@gmail.com>
2015-05-22 17:08:46 +0100

    comment out default bindings

--- plugins/marker/marker.plugin.zsh
+++ plugins/marker/marker.plugin.zsh
@@ -1,7 +1,8 @@
 if [[ -s "$HOME/.local/share/marker/marker.sh" ]]; then
-    export MARKER_KEY_MARK='\C-k'
-    export MARKER_KEY_GET='\C-@'
-    export MARKER_KEY_NEXT_PLACEHOLDER='\C-t'
+    # Key bindings
+    # export MARKER_KEY_MARK='\C-k'
+    # export MARKER_KEY_GET='\C-@'
+    # export MARKER_KEY_NEXT_PLACEHOLDER='\C-t'
    source "$HOME/.local/share/marker/marker.sh"
 fi
 
