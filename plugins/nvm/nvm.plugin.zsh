d6181dda511d1eb7775dba2df1ae237a7763522b
Diogo Azevedo <diogoazevedos@gmail.com>
2015-06-05 12:35:36 -0300

    Add brew support for nvm plugin inspired in rbenv plugin

--- plugins/nvm/nvm.plugin.zsh
+++ plugins/nvm/nvm.plugin.zsh
@@ -1,3 +1,25 @@
-# The addition 'nvm install' attempts in ~/.profile
+_homebrew-installed() {
+  type brew &> /dev/null
+}
 
-[[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
+_nvm-from-homebrew-installed() {
+  brew --prefix nvm &> /dev/null
+}
+
+FOUND_NVM=0
+nvmdirs=("$HOME/.nvm" "/usr/local/nvm" "/opt/nvm" "/usr/local/opt/nvm")
+if _homebrew-installed && _nvm-from-homebrew-installed ; then
+    nvmdirs=($(brew --prefix nvm) "${rbenvdirs[@]}")
+fi
+
+for nvmdir in "${nvmdirs[@]}" ; do
+  if [ -s $nvmdir/nvm.sh -a $FOUND_NVM -eq 0 ]; then
+    FOUND_NVM=1
+    if [[ $NVM_DIR = '' ]]; then
+      NVM_DIR=$nvmdir
+    fi
+    export NVM_DIR
+    source $nvmdir/nvm.sh
+  fi
+done
+unset nvmdir
