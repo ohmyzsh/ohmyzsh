bc00043f5f09e11c2209cd58b987c474a9c05da9
fjospinas <francisco.ospina@gmail.com>
2015-06-06 00:05:29 -0500

    Fix coments alignment

--- plugins/dnf/dnf.plugin.zsh
+++ plugins/dnf/dnf.plugin.zsh
@@ -8,7 +8,7 @@ alias dnfgl="dnf grouplist"                   # list package groups
 alias dnfli="dnf list installed"              # print all installed packages
 alias dnfmc="dnf makecache"                   # rebuilds the dnf package list
 
-alias dnfu="sudo dnf upgrade"                  # upgrate packages
+alias dnfu="sudo dnf upgrade"                 # upgrade all packages
 alias dnfi="sudo dnf install"                 # install package
 alias dnfgi="sudo dnf groupinstall"           # install package group
 alias dnfr="sudo dnf remove"                  # remove package
