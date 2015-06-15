029d6fab82bd20b326d298d4b76ce44113692efa
Max <ehlers.maximilian@gmail.com>
2015-06-15 23:23:20 +0200

    fixes atom plugin to work on ubuntu/linux

--- plugins/atom/atom.plugin.zsh
+++ plugins/atom/atom.plugin.zsh
@@ -5,10 +5,16 @@ _atom_paths=(
 )
 
 for _atom_path in $_atom_paths; do
-    if [[ -a $_atom_path ]]; then
-        alias at="open -a '$_atom_path'"
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+          alias at="/usr/bin/atom"
+    elif [[ -a $_atom_path ]]; then
+          alias at="open -a '$_atom_path'"
         break
     fi
 done
 
 alias att='at .'
+
+
+
+
