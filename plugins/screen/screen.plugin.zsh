b6beded74c60e947796bc1e612b769b7acafbc75
Werner Freund <wsfreund@gmail.com>
2015-06-17 17:46:39 +0200

    Added parameter for fixed title

--- plugins/screen/screen.plugin.zsh
+++ plugins/screen/screen.plugin.zsh
@@ -42,13 +42,13 @@ if [[ "$TERM" == screen* ]]; then
     local -a cmd; cmd=(${(z)1}) # the command string
     eval "tab_title=$TAB_TITLE_PREFIX:$TAB_TITLE_EXEC"
     eval "tab_hardstatus=$TAB_HARDSTATUS_PREFIX:$TAB_HARDSTATUS_EXEC"
-    screen_set $tab_title $tab_hardstatus
+    screen_set ${MANUAL_TITLE:-$tab_title} ${MANUAL_TITLE:-$tab_hardstatus}
   }
   # called by zsh before showing the prompt
   function precmd()
   {
     eval "tab_title=$TAB_TITLE_PREFIX:$TAB_TITLE_PROMPT"
     eval "tab_hardstatus=$TAB_HARDSTATUS_PREFIX:$TAB_HARDSTATUS_PROMPT"
-    screen_set $tab_title $tab_hardstatus
+    screen_set ${MANUAL_TITLE:-tab_title} ${MANUAL_TITLE:-$tab_hardstatus}
   }
 fi
