edc8a5a6254d7b33d5af13c87066c82ab64b5216
Charles-André Assus <hartosquatique@gmail.com>
2015-07-01 13:12:01 +0200

    Add the right command for zwip alias :D

--- plugins/zeus/zeus.plugin.zsh
+++ plugins/zeus/zeus.plugin.zsh
@@ -33,7 +33,7 @@ alias zunner='zeus runner'
 # Cucumber
 alias zcu='zeus cucumber'
 alias zucumber='zeus cucumber'
-alias zwip='zeus rake cucumber:wip'
+alias zwip='zeus cucumber --profile wip'
 
 # Rspec
 alias zspec='zeus rspec'
