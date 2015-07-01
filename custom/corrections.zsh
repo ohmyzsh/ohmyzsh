edc084dd72bdaf771af937285c72ee71dc12cc4a
Nathan Stilwell <nstilwell@gilt.com>
2015-07-01 14:33:33 -0400

    :fire: simplifying the corrections list

--- custom/corrections.zsh
+++ custom/corrections.zsh
@@ -1,8 +1,5 @@
-alias ack='nocorrect ack'
 alias hg='nocorrect hg'
 alias figlet="nocorrect figlet"
-alias ksdiff="nocorrect ksdiff"
 alias scala="nocorrect scala"
-alias idea="nocorrect idea"
 alias rake="noglob rake"
 alias ...="noglob ..."
