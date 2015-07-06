b1da3bbc6f5e26cd0b06cd858e696d4f47f340d2
Patryk Małek <malekpatryk@gmail.com>
2015-07-06 22:00:53 +0200

    Adding ability to add JIRA_PREFIX as an env variable (e.g. in .zshrc) and minor refactor to bash curly variable braces

--- plugins/jira/jira.plugin.zsh
+++ plugins/jira/jira.plugin.zsh
@@ -1,6 +1,7 @@
 # To use: add a .jira-url file in the base of your project
 #         You can also set JIRA_URL in your .zshrc or put .jira-url in your home directory
-#         .jira-url in the current directory takes precedence
+#         .jira-url in the current directory takes precedence. The same goes with .jira-prefix
+#         and JIRA_PREFIX.
 #
 # If you use Rapid Board, set:
 #JIRA_RAPID_BOARD="true"
@@ -22,8 +23,8 @@ open_jira_issue () {
     jira_url=$(cat .jira-url)
   elif [ -f ~/.jira-url ]; then
     jira_url=$(cat ~/.jira-url)
-  elif [[ "x$JIRA_URL" != "x" ]]; then
-    jira_url=$JIRA_URL
+  elif [[ "${JIRA_URL}" != "" ]]; then
+    jira_url=${JIRA_URL}
   else
     echo "JIRA url is not specified anywhere."
     return 1
@@ -33,6 +34,8 @@ open_jira_issue () {
     jira_prefix=$(cat .jira-prefix)
   elif [ -f ~/.jira-prefix ]; then
     jira_prefix=$(cat ~/.jira-prefix)
+  elif [[ "${JIRA_PREFIX}" != "" ]]; then
+    jira_prefix=${JIRA_PREFIX}
   else
     jira_prefix=""
   fi
@@ -51,7 +54,7 @@ open_jira_issue () {
       echo "Opening issue #$1"
     fi
     
-    if [[ "x$JIRA_RAPID_BOARD" = "xtrue" ]]; then
+    if [[ "$JIRA_RAPID_BOARD" = "true" ]]; then
       $open_cmd  "$jira_url/issues/$jira_prefix$1$addcomment"
     else
       $open_cmd  "$jira_url/browse/$jira_prefix$1$addcomment"
@@ -61,7 +64,7 @@ open_jira_issue () {
 
 jira_name () {
   if [[ -z "$1" ]]; then
-    if [[ "x${JIRA_NAME}" != "x" ]]; then
+    if [[ "${JIRA_NAME}" != "" ]]; then
       jira_name=${JIRA_NAME}
     else
       echo "JIRA_NAME not specified"
