470fea06792826740b690883abb78d1d4cd5aba5
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-07-07 09:47:49 +0530

    adds an alias to search for the rails routes that pattern match a string

--- plugins/rails/rails.plugin.zsh
+++ plugins/rails/rails.plugin.zsh
@@ -58,6 +58,7 @@ alias rdmtc='rake db:migrate db:test:clone'
 alias rlc='rake log:clear'
 alias rn='rake notes'
 alias rr='rake routes'
+alias rrg='rake routes | grep '
 
 # legacy stuff
 alias sstat='thin --stats "/thin/stats" start'
