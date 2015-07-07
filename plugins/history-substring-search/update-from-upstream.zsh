7dea9db5e4f904d74c6f0c80a6e8497efb8ab0bf
Andrew Janke <andrew@apjanke.net>
2015-07-07 16:48:05 -0400

    history-substring-search: update to upstream version 2015-09-28
    Updates OMZ's copy to commit 2c295432175990c1bb4e90bc13f609daa67a25d6 from zsh-users/zsh-history-substring-search

--- plugins/history-substring-search/update-from-upstream.zsh
+++ plugins/history-substring-search/update-from-upstream.zsh
@@ -24,20 +24,21 @@
 set -e
 
 
-UPSTREAM_BASE=zsh-history-substring-search
-UPSTREAM_REPO=zsh-users/$UPSTREAM_BASE
+upstream_basename=zsh-history-substring-search
+plugin_basename=history-substring-search
+UPSTREAM_REPO=zsh-users/$upstream_basename
 need_repo_cleanup=false
 upstream_github_url="https://github.com/$UPSTREAM_REPO"
 
 if [[ -z "$UPSTREAM_REPO_PATH" ]]; then
   # Do a clean checkout
   my_tempdir=$(mktemp -d -t omz-update-histsubstrsrch)
-  UPSTREAM_REPO_PATH="$my_tempdir/$UPSTREAM_BASE"
+  UPSTREAM_REPO_PATH="$my_tempdir/$upstream_basename"
   git clone "$upstream_github_url" "$UPSTREAM_REPO_PATH"
   need_repo_cleanup=true
   print "Checked out upstream repo to $UPSTREAM_REPO_PATH"
 else
-	print "Using existing zsh-history-substring-search repo at $UPSTREAM_REPO_PATH"
+	print "Using existing $upstream_basename repo at $UPSTREAM_REPO_PATH"
 fi
 
 upstream="$UPSTREAM_REPO_PATH"
@@ -45,14 +46,16 @@ upstream="$UPSTREAM_REPO_PATH"
 # Figure out what we're pulling in
 upstream_sha=$(cd $upstream && git rev-parse HEAD)
 upstream_commit_date=$(cd $upstream && git log  -1 --pretty=format:%ci)
+upstream_just_date=${${=upstream_commit_date}[1]}
 print "upstream SHA:         $upstream_sha"
-print "upstream commit date: $upstream_commit_date"
+print "upstream commit time: $upstream_commit_date"
+print "upstream commit date: $upstream_just_date"
 print
 
 # Copy the files over, using the OMZ plugin's names where needed
 cp -v "$upstream"/* .
-mv zsh-history-substring-search.plugin.zsh history-substring-search.plugin.zsh
-mv zsh-history-substring-search.zsh history-substring-search.zsh
+mv -v zsh-history-substring-search.zsh $plugin_basename.zsh
+mv -v zsh-history-substring-search.plugin.zsh $plugin_basename.plugin.zsh
 
 if [[ $need_repo_cleanup == true ]]; then
 	print "Removing temporary repo at $my_tempdir"
@@ -63,19 +66,38 @@ fi
 
 print
 print "Updating files with OMZ-specific stuff"
+print
+
+# OMZ binds the keys as part of the plugin loading
+
+cat >> $plugin_basename.plugin.zsh <<EOF
+
+
+# Bind terminal-specific up and down keys
+
+if [[ -n "\$terminfo[kcuu1]" ]]; then
+  bindkey "\$terminfo[kcuu1]" history-substring-search-up
+fi
+if [[ -n "\$terminfo[kcud1]" ]]; then
+  bindkey "\$terminfo[kcud1]" history-substring-search-down
+fi
+
+EOF
 
 # Tack OMZ-specific notes on to readme
+
 thin_line="------------------------------------------------------------------------------"
 cat >> README.md <<EOF
 
 $thin_line
-Oh My Zsh Notes
+Oh My Zsh Distribution Notes
 $thin_line
 
-This is Oh My Zsh's repackaging of zsh-history-substring-search as an OMZ module
-inside the Oh My Zsh distribution.
+What you are looking at now is Oh My Zsh's repackaging of zsh-history-substring-search 
+as an OMZ module inside the Oh My Zsh distribution.
 
-The upstream repo, $UPSTREAM_REPO, can be found on GitHub at $upstream_github_url.
+The upstream repo, $UPSTREAM_REPO, can be found on GitHub at 
+$upstream_github_url.
 
 This downstream copy was last updated from the following upstream commit:
 
@@ -84,11 +106,22 @@ This downstream copy was last updated from the following upstream commit:
 
 Everything above this section is a copy of the original upstream's README, so things
 may differ slightly when you're using this inside OMZ. In particular, you do not
-need to set up key bindings yourself in \`~/.zshrc\`; the OMZ plugin does that for
-you.
+need to set up key bindings for the up and down arrows yourself in \`~/.zshrc\`; the OMZ 
+plugin does that for you. You may still want to set up additional emacs- or vi-specific
+bindings as mentioned above.
 
 EOF
 
-print
-print "Done OK"
+# Announce success and generate git commit messages
+
+cat <<EOF
+Done OK
+
+Now you can check the results and commit like this:
+
+  git add *
+  git commit -m "history-substring-search: update to upstream version $upstream_just_date" \\
+      -m "Updates OMZ's copy to commit $upstream_sha from $UPSTREAM_REPO"
+
+EOF
 
