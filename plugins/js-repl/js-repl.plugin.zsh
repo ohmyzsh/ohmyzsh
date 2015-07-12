e803c95261d19540597c861b61bd0624a75201de
Deepak Kannan <kannan.deepak@gmail.com>
2015-07-12 15:52:18 +0530

    Add new plugin js-repl for javascript REPL

---
+++ plugins/js-repl/js-repl.plugin.zsh
@@ -0,0 +1,5 @@
+if [[ "$OSTYPE" = darwin* ]] ; then
+    # https://trac.webkit.org/wiki/JSC
+    # jsc (or JavaScriptCore) is a Safari JavaScript REPL
+    alias jsc="/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc"
+fi
