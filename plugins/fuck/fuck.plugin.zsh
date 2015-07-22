8fffd639be45ba1e76cb8cb617a661f8433583f8
Black Roland <black-roland@github.com>
2015-07-22 17:20:52 +0900

    Thefuck: combined with @cristim plugin. Code formating

--- plugins/fuck/fuck.plugin.zsh
+++
@@ -1,6 +0,0 @@
-if [[ -n $commands[thefuck] ]]; then
-  alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
-  alias FUCK='fuck'
-else
-  echo 'thefuck is not installed, you should "pip install thefuck" first'
-fi
