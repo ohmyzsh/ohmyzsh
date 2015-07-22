8e8a13398e71b969dc39189c0fedcc4733a911df
Black Roland <black-roland@github.com>
2015-07-22 17:20:52 +0900

    Thefuck: combined with @cristim plugin. Code formating

--- plugins/thefuck/thefuck.plugin.zsh
+++ plugins/thefuck/thefuck.plugin.zsh
@@ -1,6 +1,14 @@
+if [[ -z $commands[thefuck] ]]; then
+    echo 'thefuck is not installed, you should "pip install thefuck" first'
+    return -1
+fi
+
+alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
+alias FUCK='fuck'
+
 fuck-command-line() {
-    FUCK=$(thefuck $(fc -ln -1 | tail -n 1) 2> /dev/null)
-    [ -z $FUCK ] && echo -n -e "\a" && return
+    FUCK="$(thefuck $(fc -ln -1 | tail -n 1) 2> /dev/null)"
+    [[ -z $FUCK ]] && echo -n -e "\a" && return
     BUFFER=$FUCK
     zle end-of-line
 }
