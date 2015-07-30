e02991b69eb49aa3a0c93d10172e84444e94295e
Roland Chernov <mail@9241406380.org>
2015-07-30 21:04:58 +0900

    Thefuck: fix global variable assignment

--- plugins/thefuck/thefuck.plugin.zsh
+++ plugins/thefuck/thefuck.plugin.zsh
@@ -7,7 +7,7 @@ fi
 eval "$(thefuck-alias)"
 
 fuck-command-line() {
-    FUCK="$(THEFUCK_REQUIRE_CONFIRMATION=0 thefuck $(fc -ln -1 | tail -n 1) 2> /dev/null)"
+    local FUCK="$(THEFUCK_REQUIRE_CONFIRMATION=0 thefuck $(fc -ln -1 | tail -n 1) 2> /dev/null)"
     [[ -z $FUCK ]] && echo -n -e "\a" && return
     BUFFER=$FUCK
     zle end-of-line
