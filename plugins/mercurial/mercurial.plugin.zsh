881edd93924bf895b2b9ba2aee83ce4331222c4a
Alejandro Baez <code@baez.nyc>
2015-08-07 17:32:10 -0400

    mercurial: updated prompt_info to hg_get_id

--- plugins/mercurial/mercurial.plugin.zsh
+++ plugins/mercurial/mercurial.plugin.zsh
@@ -29,9 +29,13 @@ function hg_get_branch_name() {
     echo $(hg branch)
 }
 
+function hg_get_id() {
+    echo $(hg id -i -b -B -t)
+}
+
 function hg_prompt_info {
   if [ $(in_hg) ]; then
-    _DISPLAY=$(hg_get_branch_name)
+    _DISPLAY=$(hg_get_id)
     echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_HG_PROMPT_PREFIX\
 $ZSH_THEME_REPO_NAME_COLOR$_DISPLAY$ZSH_PROMPT_BASE_COLOR$ZSH_PROMPT_BASE_COLOR$(hg_dirty)$ZSH_THEME_HG_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR"
     unset _DISPLAY
