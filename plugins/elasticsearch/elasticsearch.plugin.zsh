aa9a96f327fb0c1d09ac93ab02af4c33687c710a
Fedele Mantuano <f.mantuano@certego.net>
2015-08-24 12:17:06 +0200

    Added GET API Elasticsearch plugin
    Manage _cat API
    
    Added PUT, POST, DELETE commands.
    Added options for autocomplete

---
+++ plugins/elasticsearch/elasticsearch.plugin.zsh
@@ -0,0 +1,45 @@
+# ------------------------------------------------------------------------------
+# Description
+# -----------
+#
+# You can use Elastichsearch GET API like in Marvel sense console:
+# GET /
+# GET _cluster/health
+# GET _nodes/stats
+# GET _stats/fielddata?fields=*
+# GET _nodes/stats/indices/fielddata?fields=*
+#
+# You have do install json_reformat (http://dev.man-online.org/package/main/yajl-tools/)
+# to format json
+#
+# ------------------------------------------------------------------------------
+# Author
+# -------
+#
+# * Fedele Mantuano (Twitter: @fedelemantuano)
+#
+# ------------------------------------------------------------------------------
+
+alias ech='GET _cluster/health'
+alias ecs='GET _cluster/state'
+alias ens='GET _nodes/stats'
+alias esf='GET _stats/fielddata'
+
+# GET
+if which json_reformat > /dev/null 2>&1; then 
+    GET() { curl -s -XGET http://localhost:9200/"$@" | json_reformat 2>/dev/null || curl -s -XGET http://localhost:9200/"$@" }
+else
+    GET() { curl -s -XGET http://localhost:9200/"$@" }
+fi
+
+# HEAD
+HEAD() { curl -s -XHEAD -i http://localhost:9200/"$@" }
+
+# DELETE
+DELETE() { curl -s -XDELETE http://localhost:9200/"$@" }
+
+# PUT
+PUT() { curl -s -XPUT http://localhost:9200/"$@" }
+
+# POST
+POST() { curl -s -XPOST http://localhost:9200/"$@" }
