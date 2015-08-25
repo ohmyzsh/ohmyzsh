6e93bfa3b91d434dbf0577cd3c4b5da6c502f8af
Aaron Biggs <abiggs@ou.edu>
2015-08-25 10:50:02 -0500

    Add Docker Compose aliases.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -46,3 +46,9 @@ alias dp='~/Dropbox\ \(Personal\)'
 alias dminit="docker-machine create --driver virtualbox dev"
 alias dmup="docker-machine start dev && eval \"\$(docker-machine env dev)\""
 alias dm="docker-machine"
+
+# Docker Compose
+alias dc="docker-compose"
+alias dcup="docker-compose up -d"
+alias dcdown="docker-compose stop"
+alias dcrm="docker-compose stop && docker-compose rm -f"
