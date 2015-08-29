7ec78c3636e65beb27b798c43986443391ba5a3d
Hannah Ward <hannah.ward9001@gmail.com>
2015-08-29 03:29:09 +0100

    Added Void Linux Plugin

---
+++ plugins/voidlinux/voidlinux.plugin.zsh
@@ -0,0 +1,15 @@
+## Oh-my-zsh Void Linux Plugin ##
+## Author: FloatingGhost       ##
+
+alias xbi="xbps-install"
+alias xbq="xbps-query"
+alias xbc="xbps-create"
+alias xbdb="xbps-pkgdb"
+alias xdr="xbps-remove"
+alias xdrc="xbps-reconfigure"
+alias xbfb="xbps-fbulk"
+alias xbri="xbps-rindex"
+alias xbuh="xbps-uhelper"
+alias xbuu="xbps-uunshare"
+alias xbvers="xbps-checkvers"
+alias xbgrph="xbps-dgraph"
