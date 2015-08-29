278a8723355b486314635cc6a7f7a2589f8fe012
Hannah Ward <harry.ward9001@gmail.com>
2015-08-29 10:30:08 +0100

    Typo

--- plugins/voidlinux/voidlinux.plugin.zsh
+++ plugins/voidlinux/voidlinux.plugin.zsh
@@ -5,11 +5,15 @@ alias xbi="xbps-install"
 alias xbq="xbps-query"
 alias xbc="xbps-create"
 alias xbdb="xbps-pkgdb"
-alias xdr="xbps-remove"
-alias xdrc="xbps-reconfigure"
+alias xbr="xbps-remove"
+alias xbrc="xbps-reconfigure"
 alias xbfb="xbps-fbulk"
 alias xbri="xbps-rindex"
 alias xbuh="xbps-uhelper"
 alias xbuu="xbps-uunshare"
 alias xbvers="xbps-checkvers"
 alias xbgrph="xbps-dgraph"
+
+alias service="sv"
+
+
