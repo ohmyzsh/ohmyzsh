3906f128084584a4553f9a02a1d96dbbc2e354fa
Marc Cornellà <marc.cornella@live.com>
2015-09-04 01:43:48 +0200

    Return error if no xcode files where found

--- plugins/xcode/xcode.plugin.zsh
+++ plugins/xcode/xcode.plugin.zsh
@@ -2,8 +2,10 @@
 function xc {
   local xcode_proj
   xcode_proj=(*.{xcworkspace,xcodeproj}(N))
+
   if [[ ${#xcode_proj} -eq 0 ]]; then
     echo "No xcworkspace/xcodeproj file found in the current directory."
+    return 1
   else
     echo "Found ${xcode_proj[1]}"
     open "${xcode_proj[1]}"
