d0427aa818653d501b7bd80e518a006d210a5e29
Juraj Fiala <doctorjellyface@riseup.net>
2015-09-06 13:01:09 +0200

    Added aliases for --now commands
    Also removed the old sc-launch alias because this is just better.

--- plugins/systemd/systemd.plugin.zsh
+++ plugins/systemd/systemd.plugin.zsh
@@ -9,4 +9,7 @@ sudo_commands=(
 
 for c in $user_commands; do; alias sc-$c="systemctl $c"; done
 for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
-alias sc-launch="sudo systemctl start $c && sudo systemctl enable $c"
+
+alias sc-enable-now="sc-enable --now"
+alias sc-disable-now="sc-disable --now"
+alias sc-mask-now="sc-mask --now"
