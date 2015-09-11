953e4d892631f93e31cfdfb72199fb8f413c6b49
Ben Klein <robobenklein@gmail.com>
2015-09-10 22:19:53 -0400

    Make Linux compatible
    Just an if/else to determine if we're OSX or Linux and alias `at` appropriately.

--- plugins/atom/atom.plugin.zsh
+++ plugins/atom/atom.plugin.zsh
@@ -1,14 +1,22 @@
-local _atom_paths > /dev/null 2>&1
-_atom_paths=(
-    "$HOME/Applications/Atom.app"
-    "/Applications/Atom.app"
-)
+# Gets OS Type
+unamestr=$(uname -s)
 
-for _atom_path in $_atom_paths; do
-    if [[ -a $_atom_path ]]; then
-        alias at="open -a '$_atom_path'"
-        break
-    fi
-done
+# If OSX
+if [[ "$unamestr" == 'Darwin' ]]; then
+    local _atom_paths > /dev/null 2>&1
+    _atom_paths=(
+        "$HOME/Applications/Atom.app"
+        "/Applications/Atom.app"
+    )
 
-alias att='at .'
+    for _atom_path in $_atom_paths; do
+        if [[ -a $_atom_path ]]; then
+            alias at="open -a '$_atom_path'"
+            break
+        fi
+    done
+# If Linux
+elif [[ "$unamestr" == 'Linux' ]]; then
+    # Alerts the user if 'atom' is not a found command.
+    type atom >/dev/null 2>&1 && alias at="atom" || { echo >&2 "You have enabled the atom oh-my-zsh plugin on Linux, but atom is not a recognized command. Please make sure you have it installed before using this plugin."; }
+fi
