d5f14aac027b4b47e3a7a1843b3710bcfa07dbf9
Ramses Ladlani <ramses.ladlani@fishingcactus.com>
2015-09-14 11:02:00 +0200

    Fixed atom plugin for cygwin users

--- plugins/atom/atom.plugin.zsh
+++ plugins/atom/atom.plugin.zsh
@@ -1,14 +1,34 @@
-local _atom_paths > /dev/null 2>&1
-_atom_paths=(
-    "$HOME/Applications/Atom.app"
-    "/Applications/Atom.app"
-)
+if [ Cygwin = "$(uname -o 2>/dev/null)" ]; then
+    local _atom_path > /dev/null 2>&1
 
-for _atom_path in $_atom_paths; do
+    _atom_path=${LOCALAPPDATA}/atom/bin/atom
+
     if [[ -a $_atom_path ]]; then
-        alias at="open -a '$_atom_path'"
-        break
+        cyg_open_atom()
+        {
+            if [[ -n $1 ]]; then
+                ${_atom_path} `cygpath -w $1`
+            else
+                ${_atom_path}
+            fi
+        }
+
+        alias at=cyg_open_atom
     fi
-done
+else
+    local _atom_paths > /dev/null 2>&1
+
+    _atom_paths=(
+        "$HOME/Applications/Atom.app"
+        "/Applications/Atom.app"
+    )
+
+    for _atom_path in $_atom_paths; do
+        if [[ -a $_atom_path ]]; then
+            alias at="open -a '$_atom_path'"
+            break
+        fi
+    done
+fi
 
 alias att='at .'
