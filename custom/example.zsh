6805d4515099d99068ef38e82ecd23c2199fd281
Preston Davis <pwdavis@gmail.com>
2015-09-22 18:17:23 -0400

    My shortcuts

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,3 +3,7 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+alias p='tail -1 /home/pdavis/notes.txt | pbcopy'
+alias pbcopy='xsel --clipboard --input'
+alias rosh='ssh pdavis-admin@opsware.discovery.com -p 2222'
+alias console='cu -t -l /dev/ttyUSB0 -s 115200'
