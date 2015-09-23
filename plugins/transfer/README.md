2514dbf1d05296904d6bedfb8f7df9e9726c208b
Gabriel Kaam <gabriel@kaam.fr>
2015-09-23 10:32:29 +0200

    Adds transfer.sh plugin

---
+++ plugins/transfer/README.md
@@ -0,0 +1,6 @@
+# Transfer.sh
+
+[Transfer.sh](https://transfer.sh/) plugin - easy file sharing from the command line
+
+## Usage
+Just type `transfer hello.txt` and let the magic happen
---
+++ plugins/transfer/transfer.plugin.zsh
@@ -0,0 +1,20 @@
+function transfer() {
+  if [ $# -eq 0 ]; then
+    echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
+    return 1
+  fi
+
+  tmpfile=$( mktemp -t transferXXX )
+  if tty -s; then
+    basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+    curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile
+  else
+    curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile
+  fi
+
+  cat $tmpfile
+  rm -f $tmpfile
+}
+
+alias transfer=transfer
+
