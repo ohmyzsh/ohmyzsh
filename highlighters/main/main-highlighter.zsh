ohmyzsh/ohmyzsh

commit c2b9327b0763f3457fb09db17e22ee9e1e024792

Support literal newlines, part 2

highlighters/main/main-highlighter.zsh
@@ -102,28 +102,24 @@ _zsh_highlight_main_highlighter()
     local substr_color=0
     local style_override=""
     [[ $start_pos -eq 0 && $arg = 'noglob' ]] && highlight_glob=false
-    # advance $start_pos, skipping over whitespace in $buf.
-    ((start_pos+=${#buf[$start_pos+1,-1]}-${#${buf[$start_pos+1,-1]##([[:space:]]|\\[[:space:]])#}}))
-    ((end_pos=$start_pos+${#arg}))
 
-    # Deal with faked colons.
-    #
-    # We can't use the (Z+n+) flag because that elides the end-of-command
-    # token altogether, so 'echo foo\necho bar' (two commands) becomes
-    # indistinguishable from 'echo foo echo bar' (one command with three
-    # words for arguments).
+    # advance $start_pos, skipping over whitespace in $buf.
     if [[ $arg == ';' ]] ; then
-      if [[ $buf[start_pos+1] == ';' ]] ; then
-        # Literally-input colon. That's the normal case.
-      elif [[ $buf[start_pos] == $'\n' ]] ; then
-        # Newline token rendered as a colon.  Empirically we're off-by-one.
-        (( start_pos -= 1 ))
-        (( end_pos -= 1 ))
-      else
-        # Currently, there aren't any other sources of ";" (SEPER) tokens
-        # besides literally-input semicolons and (Z+n+)-converted newlines,
-        # If that ever changes, handle them here.
-      fi
+      # We're looking for either a semicolon or a newline, whichever comes
+      # first.  Both of these are rendered as a ";" (SEPER) by the ${(z)..}
+      # flag.
+      #
+      # We can't use the (Z+n+) flag because that elides the end-of-command
+      # token altogether, so 'echo foo\necho bar' (two commands) becomes
+      # indistinguishable from 'echo foo echo bar' (one command with three
+      # words for arguments).
+      local needle=$'[;\n]'
+      integer offset=${${buf[start_pos+1,-1]}[(i)$needle]}
+      (( start_pos += offset ))
+      (( end_pos += offset ))
+    else
+      ((start_pos+=${#buf[$start_pos+1,-1]}-${#${buf[$start_pos+1,-1]##([[:space:]]|\\[[:space:]])#}}))
+      ((end_pos=$start_pos+${#arg}))
     fi
 
     # Parse the sudo command line

highlighters/main/test-data/vanilla-newline.zsh
@@ -29,12 +29,12 @@
 # -------------------------------------------------------------------------------------------------
 
 PREBUFFER=$'echo foo; echo bar\n\n\n'
-BUFFER='echo baz; echo qux'
+BUFFER=' echo baz; echo qux'
 
 expected_region_highlight=(
-  "0 4 $ZSH_HIGHLIGHT_STYLES[builtin]" # echo
-  "5 7 $ZSH_HIGHLIGHT_STYLES[default]" # baz
-  "8 9 $ZSH_HIGHLIGHT_STYLES[default]" # semicolon
-  "11 14 $ZSH_HIGHLIGHT_STYLES[builtin]" # echo
-  "16 18 $ZSH_HIGHLIGHT_STYLES[default]" # qux
+  "1 5 $ZSH_HIGHLIGHT_STYLES[builtin]" # echo
+  "6 8 $ZSH_HIGHLIGHT_STYLES[default]" # baz
+  "9 10 $ZSH_HIGHLIGHT_STYLES[default]" # semicolon
+  "12 15 $ZSH_HIGHLIGHT_STYLES[builtin]" # echo
+  "17 19 $ZSH_HIGHLIGHT_STYLES[default]" # qux
 )

commit 5fb4cb2f72ff3bf2cde7d6c8eb5593e1c1a19539

Fix regression: Highlight ';' as commandseparator.
This was broken by c2b9327b0763f3457fb09db17e22ee9e1e024792
and tracked as zsh-users/zsh-syntax-highlighting#199.

This fixes the vanilla-newline.zsh test, which was was (consciously) broken
by the previous commit.

highlighters/main/main-highlighter.zsh
@@ -120,8 +120,8 @@ _zsh_highlight_main_highlighter()
       # words for arguments).
       local needle=$'[;\n]'
       integer offset=${${buf[start_pos+1,-1]}[(i)$needle]}
-      (( start_pos += offset ))
-      (( end_pos += offset ))
+      (( start_pos += offset - 1 ))
+      (( end_pos = start_pos + $#arg ))
     else
       ((start_pos+=${#buf[$start_pos+1,-1]}-${#${buf[$start_pos+1,-1]##([[:space:]]|\\[[:space:]])#}}))
       ((end_pos=$start_pos+${#arg}))

highlighters/main/test-data/commandseparator.zsh
@@ -1,0 +1,38 @@
+#!/usr/bin/env zsh
+# -------------------------------------------------------------------------------------------------
+# Copyright (c) 2015 zsh-syntax-highlighting contributors
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted
+# provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this list of conditions
+#    and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice, this list of
+#    conditions and the following disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the zsh-syntax-highlighting contributors nor the names of its contributors
+#    may be used to endorse or promote products derived from this software without specific prior
+#    written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# -------------------------------------------------------------------------------------------------
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
+# -------------------------------------------------------------------------------------------------
+
+ZSH_HIGHLIGHT_STYLES[commandseparator]=$unused_highlight
+
+BUFFER=':; pwd'
+
+expected_region_highlight=(
+  "2 2 $ZSH_HIGHLIGHT_STYLES[commandseparator]" # ;
+  "4 6 $ZSH_HIGHLIGHT_STYLES[builtin]" # pwd
+)