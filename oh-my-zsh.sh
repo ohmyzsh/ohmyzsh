a22e9c781caacde7fa11bb86d7a1405b726f4027
Andrew Janke <andrew@apjanke.net>
2015-09-28 18:09:29 -0400

    completion: turn off the #3889 compfix check by default

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -11,6 +11,8 @@ fpath=($ZSH/functions $ZSH/completions $fpath)
 # Load all stock functions (from $fpath files) called below.
 autoload -U compaudit compinit
 
+: ${ZSH_DISABLE_COMPFIX:=true}
+
 # Set ZSH_CUSTOM to the path where your custom config files
 # and plugins exists, or else we will use the default custom/
 if [[ -z "$ZSH_CUSTOM" ]]; then
@@ -62,13 +64,17 @@ if [ -z "$ZSH_COMPDUMP" ]; then
   ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
 fi
 
-# If completion insecurities exist, warn the user without enabling completions.
-if ! compaudit &>/dev/null; then
-  # This function resides in the "lib/compfix.zsh" script sourced above.
-  handle_completion_insecurities
-# Else, enable and cache completions to the desired file.
+if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
+  # If completion insecurities exist, warn the user without enabling completions.
+  if ! compaudit &>/dev/null; then
+    # This function resides in the "lib/compfix.zsh" script sourced above.
+    handle_completion_insecurities
+  # Else, enable and cache completions to the desired file.
+  else
+    compinit -d "${ZSH_COMPDUMP}"
+  fi
 else
-  compinit -d "${ZSH_COMPDUMP}"
+  compinit -i -d "${ZSH_COMPDUMP}"
 fi
 
 # Load all of the plugins that were defined in ~/.zshrc
