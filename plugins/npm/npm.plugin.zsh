aae1256c031b9a4c6dc6a5b79e9f4205a05fe625
Xavier Haniquaut <xavhan@hotmail.com>
2015-09-29 10:20:55 +0200

    Add alias for npm global install

--- plugins/npm/npm.plugin.zsh
+++ plugins/npm/npm.plugin.zsh
@@ -5,6 +5,9 @@ eval "$(npm completion 2>/dev/null)"
 # - https://twitter.com/substack/status/23122603153150361
 # Thus, we've used camelCase for the following aliases:
 
+# Install dependencies globally
+alias npmg="npm i -g "
+
 # Install and save to dependencies in your package.json
 # npms is used by https://www.npmjs.com/package/npms
 alias npmS="npm i -S "
@@ -12,6 +15,7 @@ alias npmS="npm i -S "
 # Install and save to dev-dependencies in your package.json
 # npmd is used by https://github.com/dominictarr/npmd
 alias npmD="npm i -D "
+
 # Execute command from node_modules folder based on current directory
 # i.e npmE gulp
 alias npmE='PATH="$(npm bin)":"$PATH"'
