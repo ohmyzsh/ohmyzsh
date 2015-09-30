882710ee8b408ad5c111a5db1a068000016c61c4
Xavier Haniquaut <xavhan@hotmail.com>
2015-09-30 16:42:34 +0200

    Remove broken link and move npmg alias position

--- plugins/npm/npm.plugin.zsh
+++ plugins/npm/npm.plugin.zsh
@@ -1,12 +1,10 @@
 eval "$(npm completion 2>/dev/null)"
 
-
-# npm package names are lowercase
-# - https://twitter.com/substack/status/23122603153150361
-# Thus, we've used camelCase for the following aliases:
-
 # Install dependencies globally
 alias npmg="npm i -g "
+
+# npm package names are lowercase
+# Thus, we've used camelCase for the following aliases:
 
 # Install and save to dependencies in your package.json
 # npms is used by https://www.npmjs.com/package/npms
