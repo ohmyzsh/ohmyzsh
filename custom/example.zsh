80704610b8734b26635f50b5c5ae2619d1269e2a
Preston Davis <pwdavis@gmail.com>
2015-10-11 20:50:54 -0400

    Updated example.sh

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,11 +3,8 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
-<<<<<<< HEAD
 alias p='tail -1 /home/pdavis/notes.txt | pbcopy'
 alias pbcopy='xsel --clipboard --input'
 alias rosh='ssh pdavis-admin@opsware.discovery.com -p 2222'
 alias console='cu -t -l /dev/ttyUSB0 -s 115200'
-=======
 [[ $TERM != "screen" ]] && exec tmux
->>>>>>> 2877573c110ffc587f156ed6601ac732f647466a
