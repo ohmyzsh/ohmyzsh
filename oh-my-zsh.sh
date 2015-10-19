f0042fcf3b84b975d70e243a7c9b52a2a6f2922d
Marco Farrugia <Marco.Farrugia@twosigma.com>
2015-10-19 10:18:13 -0400

    Convert to 2 space indents.

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -67,16 +67,16 @@ fi
 
 if [[ $ZSH_DISABLE_COMPINIT != true ]]; then
   if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
-      # If completion insecurities exist, warn the user without enabling completions.
-      if ! compaudit &>/dev/null; then
-        # This function resides in the "lib/compfix.zsh" script sourced above.
-        handle_completion_insecurities
-      # Else, enable and cache completions to the desired file.
-      else
-        compinit -d "${ZSH_COMPDUMP}"
-      fi
+    # If completion insecurities exist, warn the user without enabling completions.
+    if ! compaudit &>/dev/null; then
+      # This function resides in the "lib/compfix.zsh" script sourced above.
+      handle_completion_insecurities
+    # Else, enable and cache completions to the desired file.
+    else
+      compinit -d "${ZSH_COMPDUMP}"
+    fi
   else
-      compinit -i -d "${ZSH_COMPDUMP}"
+    compinit -i -d "${ZSH_COMPDUMP}"
   fi
 fi
 # Load all of the plugins that were defined in ~/.zshrc
