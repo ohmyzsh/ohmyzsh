c57bc668d394d3c5be327ffb954a375312ce6608
Sam Garson <samtgarson@gmail.com>
2015-10-27 17:20:52 +0000

    Add alias for git push to origin/current branch

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -177,6 +177,7 @@ alias gpoat='git push origin --all && git push origin --tags'
 compdef _git gpoat=git-push
 alias gpu='git push upstream'
 alias gpv='git push -v'
+alias gpcb='git push origin $(current_branch)'
 
 alias gr='git remote'
 alias gra='git remote add'
