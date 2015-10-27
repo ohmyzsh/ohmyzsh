9816fcf05586bcd24f2299649acc1fc46c72581e
Daniel Shahaf <d.s@daniel.shahaf.name>
2015-10-27 05:09:13 +0000

    redirections: Test an infix redirection.

--- highlighters/main/test-data/prefix-redirection.zsh
+++ highlighters/main/test-data/prefix-redirection.zsh
@@ -27,12 +27,13 @@
 # vim: ft=zsh sw=2 ts=2 et
 # -------------------------------------------------------------------------------------------------
 
-BUFFER='>/tmp >/tmp sudo echo foo'
+BUFFER='>/tmp >/tmp sudo echo >/tmp foo'
 
 expected_region_highlight=(
   "2  5  $ZSH_HIGHLIGHT_STYLES[path]"       # /tmp
   "8  11 $ZSH_HIGHLIGHT_STYLES[path]"       # /tmp
   "13 16 $ZSH_HIGHLIGHT_STYLES[precommand]" # sudo
   "18 21 $ZSH_HIGHLIGHT_STYLES[builtin]"    # echo
-  "23 25 $ZSH_HIGHLIGHT_STYLES[default]"    # foo
+  "24 27 $ZSH_HIGHLIGHT_STYLES[path]"       # /tmp
+  "29 31 $ZSH_HIGHLIGHT_STYLES[default]"    # foo
 )
