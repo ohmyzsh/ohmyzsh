20e973bf3dd59aa9388e1d4c5673e5fdffe1c8d7
Preston Davis <pdavis@pebsung.pebcac.local>
2015-10-30 02:49:59 -0400

    Updated example.zsh

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,3 +3,16 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+alias p='tail -1 /home/pdavis/notes.txt | pbcopy'
+alias pbcopy='xsel --clipboard --input'
+alias rosh='ssh pdavis-admin@opsware.discovery.com -p 2222'
+alias console='cu -t -l /dev/ttyUSB0 -s 115200'
+alias open='ssh $1 -l pdavis-admin'
+alias home='ssh netinstall@pebcac.org'
+alias myrouter='ssh tunnel@192.168.10.1'
+alias coreswitch='ssh tunnel@192.168.10.3'
+alias pswitch='ssh tunnel@192.168.10.4'
+alias jumphost='ssh pdavis@jumphost.pebcac.org'
+
+# Start with tmux
+[[ $TERM != "screen" ]] && exec tmux
