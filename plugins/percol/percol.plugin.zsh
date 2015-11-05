395f05f69033dbbd2a7726765f9499cae6fd11d8
codefalling <code.falling@gmail.com>
2015-11-05 10:08:13 +0800

    add plugin:percol

---
+++ plugins/percol/percol.plugin.zsh
@@ -0,0 +1,22 @@
+if which percol &> /dev/null; then
+    function percol_select_history() {
+        local tac
+        tac="tail -r"
+        BUFFER=$(fc -l -n 1 | eval $tac | percol --query "$LBUFFER")
+        CURSOR=$#BUFFER
+        zle -R -c
+    }
+
+    zle -N percol_select_history
+    bindkey '^R' percol_select_history
+fi
+
+if which marks &> /dev/null; then
+    function percol_select_marks() {
+        BUFFER=$(marks | percol --query "$LBUFFER" | awk '{print $3}')
+        CURSOR=$#BUFFER # move cursor
+        zle -R -c       # refresh
+    }
+    zle -N percol_select_marks
+    bindkey '^B' percol_select_marks
+fi
