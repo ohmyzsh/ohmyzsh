299cd4777a8b9da1934a1ed0c80767eea6691a70
Andrew Janke <andrew@apjanke.net>
2015-11-06 17:20:11 -0500

    Themes: switch to using ruby_prompt_info()
    Changes themes displaying RVM or other Ruby version info to use the central
    ruby_prompt_info function. This supports more Ruby versioning mechanisms,
    reduces copy-and-paste code, and avoids "zsh: no such file or directory: rvm-prompt"
    when run on machines that do not have RVM installed.
    
    Changes the prefix/suffix variable names to ZSH_THEME_RUBY_PROMPT_PREFIX and
    ZSH_THEME_RUBY_PROMPT_SUFFIX, since they apply to all Ruby versioning mechanisms,
    not just RVM.
    
    Allows empty ZSH_THEME_RUBY_PROMPT_PREFIX and ZSH_THEME_RUBY_PROMPT_SUFFIX.

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -50,3 +50,5 @@ ZSH_THEME_GIT_PROMPT_PREFIX="git:("         # Prefix at the very beginning of th
 ZSH_THEME_GIT_PROMPT_SUFFIX=")"             # At the very end of the prompt
 ZSH_THEME_GIT_PROMPT_DIRTY="*"              # Text to display if the branch is dirty
 ZSH_THEME_GIT_PROMPT_CLEAN=""               # Text to display if the branch is clean
+ZSH_THEME_RUBY_PROMPT_PREFIX="("
+ZSH_THEME_RUBY_PROMPT_SUFFIX=")"
