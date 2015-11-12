acb0a82c1dfa3e941c5f7ea77dfb2d15d195c8a4
Tristan Mahé <gled@remote-shell.net>
2015-11-12 09:59:08 -0800

    virtualenvwrapper: add the option DEACTIVATE_VENV_ASAP to cope with issue #4603

--- plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
+++ plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
@@ -61,7 +61,7 @@ if [[ ! $DISABLE_VENV_CD -eq 1 ]]; then
       else
         ENV_NAME=""
       fi
-      if [[ "$ENV_NAME" != "" && ("$ENV_NAME" = "$CD_VIRTUAL_ENV" || "$CD_VIRTUAL_ENV" = "" ) ]]; then
+      if [[ "$ENV_NAME" != "" && ( ($DEACTIVATE_VENV_ASAP -eq 1 && ("$ENV_NAME" = "$CD_VIRTUAL_ENV" || "$CD_VIRTUAL_ENV" = "") ) || (-z $DEACTIVATE_VENV_ASAP || $DEACTIVATE_VENV_ASAP -eq 0 ) ) ]]; then
         # Activate the environment only if it is not already active
         if [[ "$VIRTUAL_ENV" != "$WORKON_HOME/$ENV_NAME" ]]; then
           if [[ -e "$WORKON_HOME/$ENV_NAME/bin/activate" ]]; then
