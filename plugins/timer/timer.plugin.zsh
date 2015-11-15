120e8620af6e1d7d01159614186403c7a816457d
Robert Strack <robert.strack@gmail.com>
2015-11-14 23:48:26 -0500

    cleaning up

--- plugins/timer/timer.plugin.zsh
+++ plugins/timer/timer.plugin.zsh
@@ -3,12 +3,12 @@ preexec() {
 }
 
 precmd() {
-  if [ -n "${__timer_cmd_start_time}" ]; then
+  if [ -n "$__timer_cmd_start_time" ]; then
     local cmd_end_time=$(date '+%s')
-    local tdiff=$((${cmd_end_time} - ${__timer_cmd_start_time}))
+    local tdiff=$((cmd_end_time - __timer_cmd_start_time))
     unset __timer_cmd_start_time
     local tdiffstr="$((tdiff / 60))m$((tdiff % 60))s"
-    local cols=$(($COLUMNS - ${#tdiffstr#0m} - 2))
+    local cols=$((COLUMNS - ${#tdiffstr#0m} - 2))
     echo -e "\033[1A\033[${cols}C \`${tdiffstr#0m}"
   fi
 }
