641c9da07c62d5c9cbe7c2ce834ba89a8050a63f
Nathan Stilwell <nathanstilwell@gmail.com>
2015-11-23 23:13:08 -0500

    more docker help

--- plugins/dock/dock.plugin.zsh
+++ plugins/dock/dock.plugin.zsh
@@ -1,28 +1,76 @@
 function dock_usage () {
-  echo
-  echo  " usage: dock <command>"
-  echo
-  echo  "   clean             Remove all containers";
-  echo  "   help              show Docker cheat sheet";
+cat <<-USAGE
+
+  usage: dock <command>
+
+    clean             Remove all stopped containers
+    info              Info on Docker setup
+    help              show Docker cheat sheet
+USAGE
 }
 
 function dock_clean {
   docker rm $(docker ps -a -q);
+  docker rmi $(docker images | grep "^<none>" | awk "{print $3}");
+}
+
+function dock_info {
+cat <<-INFO
+  You are (probably) running docker locally on a Mac using Docker Toolbox.
+
+  Docker Machine is the new boot2docker thing. You have a virtual machine
+  created called 'default'. You can check the status by running.
+
+  docker-machine status default
+
+  If it's not 'Running', you can bring it up with docker-machine start
+
+  You should have this in your profile:
+
+  ### Docker
+  export DOCKER_TLS_VERIFY="1"
+  export DOCKER_HOST="tcp://192.168.99.100:2376"
+  export DOCKER_CERT_PATH="/Users/nathan/.docker/machine/machines/default"
+  export DOCKER_MACHINE_NAME="default"
+
+  and, you should have this in your /etc/hosts
+
+  ## Docker Machine
+  192.168.99.100 docker d
+
+  So, you can access local running containers, by going to:
+
+  http://d:<port of container>
+INFO
 }
 
 function dock_help {
-  echo
-  echo "  # build a tagged docker docker image";
-  echo "  docker build -t <tag name> <folder>";
-  echo
-  echo "  # run a docker image as a daemon on a port";
-  echo "  docker run -d --name <give it a name> -p <inside port : outside port> <tag>";
-  echo
-  echo "  # run bash in a container";
-  echo "  docker exec -ti <name, tag, or id> bash";
-  echo
-  echo "  # tunnel to boot2docker";
-  echo "  boot2docker ssh -L 5000:localhost:5000";
+cat <<-HELP
+  # list local images
+  docker images
+
+  # build a tagged docker docker image;
+  docker build -t <tag name> <folder>;
+
+  ## Example:
+  docker build -t chicksphotocracy/photoop:latest .
+
+  # run a docker image as a daemon on a port;
+  docker run -d --name <give it a name> -p <inside port : outside port> <tag>;
+
+  ## Example:
+  docker run -t -i -p 5678:5678 chicksphotocracy/photoop:1.9.0
+
+  # run bash in a container;
+  docker exec -ti <name, tag, or id> bash;
+
+  # remove an image
+  docker rmi <tag, name, id>
+
+  # remove a container
+  docker rm <tag, name, id>
+
+HELP
 }
 
 function dock () {
@@ -30,6 +78,7 @@ function dock () {
     dock_usage;
   else
     [ "$1" = "clean" ] && dock_clean "$2"
+    [ "$1" = "info" ] && dock_info
     [ "$1" = "help" ] && dock_help
  fi
 }
