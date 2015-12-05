b4bca682db5e64609dbe2d062eb9906609a68f15
Sen Zhang <szhang.hust@gmail.com>
2015-12-05 16:57:05 +0800

    modify Plugin:themes, better support for custom themes

--- plugins/themes/themes.plugin.zsh
+++ plugins/themes/themes.plugin.zsh
@@ -1,24 +1,35 @@
 function theme
 {
     if [ -z "$1" ] || [ "$1" = "random" ]; then
-	themes=($ZSH/themes/*zsh-theme)
-	N=${#themes[@]}
-	((N=(RANDOM%N)+1))
-	RANDOM_THEME=${themes[$N]}
-	source "$RANDOM_THEME"
-	echo "[oh-my-zsh] Random theme '$RANDOM_THEME' loaded..."
+        themes=($ZSH/themes/*zsh-theme)
+        N=${#themes[@]}
+        ((N=(RANDOM%N)+1))
+        RANDOM_THEME=${themes[$N]}
+        source "$RANDOM_THEME"
+        echo "[oh-my-zsh] Random theme '$RANDOM_THEME' loaded..."
     else
-	if [ -f "$ZSH_CUSTOM/$1.zsh-theme" ]
-	then
-	    source "$ZSH_CUSTOM/$1.zsh-theme"
-	else
-	    source "$ZSH/themes/$1.zsh-theme"
-	fi
+        if [ -f "$ZSH_CUSTOM/themes/$1.zsh-theme" ]
+        then
+            source "$ZSH_CUSTOM/themes/$1.zsh-theme"
+        elif [ -f "$ZSH_CUSTOM/$1.zsh-theme" ]
+        then
+            source "$ZSH_CUSTOM/$1.zsh-theme"
+        else
+            source "$ZSH/themes/$1.zsh-theme"
+        fi
     fi
 }
 
 function lstheme
 {
-    cd $ZSH/themes
-    ls *zsh-theme | sed 's,\.zsh-theme$,,'
+    # default themes
+    ls $ZSH/themes | grep '.zsh-theme$' | sed 's,\.zsh-theme$,,'
+
+    # custom themes
+    ls $ZSH_CUSTOM | grep '.zsh-theme$' | sed 's,\.zsh-theme$,,'
+
+    if [ -d "$ZSH_CUSTOM/themes" ]
+    then
+        ls $ZSH_CUSTOM/themes | grep '.zsh-theme$' | sed 's,\.zsh-theme$,,'
+    fi
 }
