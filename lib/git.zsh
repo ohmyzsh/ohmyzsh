f04e71f115bb12b9220fa93c1174c4a74243c3fb
Steven Spasbo <stevenspasbo@gmail.com>
2015-12-17 01:27:36 -0800

    Added 'command' to git_commits_behind function

--- lib/git.zsh
+++ lib/git.zsh
@@ -85,7 +85,7 @@ function git_commits_ahead() {
 
 # Gets the number of commits behind remote
 function git_commits_behind() {
-  if git rev-parse --git-dir > /dev/null 2>&1; then
+  if $(command git rev-parse --git-dir > /dev/null 2>&1); then
     echo $(git rev-list --count $(current_branch)..$(git rev-parse --abbrev-ref --symbolic-full-name @{u}))
   fi
 }
