43be8baf77213b8f6910fd9126b7708d46bdbd1d
Carlos Cardoso <cadu@oscardoso.com.br>
2016-02-01 17:15:08 -0200

    template updates

--- templates/zshrc-linux.zsh
+++ templates/zshrc-linux.zsh
@@ -5,8 +5,7 @@ ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="afowler"
-# ZSH_THEME="cadusk"
+ZSH_THEME="nicoulaj"
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
