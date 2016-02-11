39b58bc6a2a7c5ee851c1a2b1c79a50a8e15e324
Marc Cornellà <marc.cornella@live.com>
2016-02-11 21:38:10 +0100

    Fix deprecated dnf commands
    Update -> upgrade
    Erase -> remove

--- plugins/dnf/dnf.plugin.zsh
+++ plugins/dnf/dnf.plugin.zsh
@@ -7,9 +7,9 @@ alias dnfmc="dnf makecache"                 # Generate metadata cache
 alias dnfp="dnf info"                       # Show package information
 alias dnfs="dnf search"                     # Search package
 
-alias dnfu="sudo dnf update"                # Upgrade package
+alias dnfu="sudo dnf upgrade"               # Upgrade package
 alias dnfi="sudo dnf install"               # Install package
 alias dnfgi="sudo dnf groupinstall"         # Install package group
-alias dnfr="sudo dnf erase"                 # Remove package
+alias dnfr="sudo dnf remove"                # Remove package
 alias dnfgr="sudo dnf groupremove"          # Remove package group
 alias dnfc="sudo dnf clean all"             # Clean cache
