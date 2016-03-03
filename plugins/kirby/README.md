657bd8744d787c8c1125cfec485681fc6cb43a8b
M.C. Brouwer <matthijs@artandflywork.org>
2016-03-03 11:17:46 +0100

    added kirby CLI plugin

---
+++ plugins/kirby/README.md
@@ -0,0 +1,2 @@
+#Kirby CLI oh-my-zsh plugin
+Adds autocompletion and aliasses for the Kirby CLI. A tool for the amazingly easy to use Getkirby CMS.
---
+++ plugins/kirby/kirby.plugin.zsh
@@ -0,0 +1,32 @@
+# installation
+alias ki='kirby install'
+alias kiplain='kirby install --kit plainkit'
+alias kistart='kirby install --kit starterkit'
+alias kilang='kirby install --kit langkit'
+alias kic='kirby install:core'
+alias kip='kirby install:panel'
+alias kii='kirby install:index.php'
+alias kih='kirby install:htaccess'
+alias kbstr='kirby install:core && kirby install:panel'
+
+# plugins
+alias kpin'plugin:install'
+alias kpup'plugin:update'
+
+# version & updates
+alias kup='kirby update'
+alias kv='kirby version'
+
+# make stuff
+alias kmblue='kirby make:blueprint'
+alias kmctrl='kirby make:controller'
+alias kmsnip='kirby make:snippet'
+alias kmtpl='kirby make:template'
+alias kmusr='kirby make:user'
+alias kmtag='kirby make:tag'
+alias kmplug='kirby make:plugin'
+
+# cache clearing
+alias kcc='kirby clear:cache'
+alias kct='kirby clear:thumbs'
+alias kca='kirby clear:cache && kirby clear:thumbs'
