7dbafc971465bc67273f1adc7ba0acc38600c931
Jeff LANCE <jeff.lance@mala.fr>
2016-03-04 21:45:25 +0100

    Update aliases, environment paths and functions

---
+++ aliases.zsh
@@ -0,0 +1,20 @@
+# Directories
+DATA=/Volumes/DATA
+hash -d data="$DATA"
+hash -d crs="$DATA/cours"
+hash -d dev="$DATA/dev"
+hash -d dld="$DATA/downloads"
+hash -d doc="$DATA/documents"
+
+# Suffixes
+#alias -s jpg='feh -dx'
+#alias -s png='feh -dx'
+#alias -s svg='firefox'
+alias -s asy='asy'
+
+# ZSH
+alias zshcfg="nano $HOME/.zshrc"
+alias zshrld="source $HOME/.zshrc"
+
+# Midnight Commander
+#alias mc='LD_PRELOAD=/usr/lib/libtrash.so.3.3 mc'
