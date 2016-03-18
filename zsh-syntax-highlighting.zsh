ohmyzsh/ohmyzsh

commit 7e7e291b2047e89419ab77fffe2f310b8c7d79e0

widget binding: Remove an unnecessary layer of indirection.
The 'eval' was never needed, and the lack of proper quoting of the interpolated
variables caused issues, such as #278.

Fixes zsh-users/zsh-syntax-highlighting#278.

zsh-syntax-highlighting.zsh
@@ -223,18 +223,18 @@ _zsh_highlight_bind_widgets()
       user:_zsh_highlight_widget_*);;
 
       # User defined widget: override and rebind old one with prefix "orig-".
-      user:*) eval "zle -N orig-$cur_widget ${widgets[$cur_widget]#*:}; \
-                    _zsh_highlight_widget_$cur_widget() { _zsh_highlight_call_widget orig-$cur_widget -- \"\$@\" }; \
-                    zle -N $cur_widget _zsh_highlight_widget_$cur_widget";;
+      user:*) zle -N orig-$cur_widget ${widgets[$cur_widget]#*:}
+              _zsh_highlight_widget_$cur_widget() { _zsh_highlight_call_widget orig-$cur_widget -- "$@" };
+              zle -N $cur_widget _zsh_highlight_widget_$cur_widget;;
 
       # Completion widget: override and rebind old one with prefix "orig-".
-      completion:*) eval "zle -C orig-$cur_widget ${${widgets[$cur_widget]#*:}/:/ }; \
-                          _zsh_highlight_widget_$cur_widget() { _zsh_highlight_call_widget orig-$cur_widget -- \"\$@\" }; \
-                          zle -N $cur_widget _zsh_highlight_widget_$cur_widget";;
+      completion:*) zle -C orig-$cur_widget ${${widgets[$cur_widget]#*:}/:/ }; \
+                    _zsh_highlight_widget_$cur_widget() { _zsh_highlight_call_widget orig-$cur_widget -- "$@" }; \
+                    zle -N $cur_widget _zsh_highlight_widget_$cur_widget;;
 
       # Builtin widget: override and make it call the builtin ".widget".
-      builtin) eval "_zsh_highlight_widget_$cur_widget() { _zsh_highlight_call_widget .$cur_widget -- \"\$@\" }; \
-                     zle -N $cur_widget _zsh_highlight_widget_$cur_widget";;
+      builtin) _zsh_highlight_widget_$cur_widget() { _zsh_highlight_call_widget .$cur_widget -- "$@" }; \
+               zle -N $cur_widget _zsh_highlight_widget_$cur_widget;;
 
       # Default: unhandled case.
       *) echo "zsh-syntax-highlighting: unhandled ZLE widget '$cur_widget'" >&2 ;;

commit 11d378332e875f5c215f62de82f68deeba912d76

widget binding: Further unbreak the build.
7e7e291b2047 broke 'zle -C' completions, as reported in #278 just now.

zsh-syntax-highlighting.zsh
@@ -234,7 +234,7 @@ _zsh_highlight_bind_widgets()
               zle -N $cur_widget _zsh_highlight_widget_$cur_widget;;
 
       # Completion widget: override and rebind old one with prefix "orig-".
-      completion:*) zle -C orig-$cur_widget ${${widgets[$cur_widget]#*:}/:/ }
+      completion:*) zle -C orig-$cur_widget ${${(s.:.)widgets[complete-word]}[2,3]} 
                     eval "_zsh_highlight_widget_${(q)cur_widget}() { _zsh_highlight_call_widget orig-${(q)cur_widget} -- \"\$@\" }"
                     zle -N $cur_widget _zsh_highlight_widget_$cur_widget;;
 