2cb0adf628da06692871f790d6e9311a64237582
Pwnage Pineapple <pwnage-pineapple@users.noreply.github.com>
2016-04-01 09:33:46 -0500

    Corrected an incorrect yaourt alias
    alias yasu='yaourt --sucre' -> alias yasu='yaourt -Syua --noconfirm'

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -6,20 +6,20 @@ if (( $+commands[yaourt] )); then
   upgrade () {
     yaourt -Syu
   }
-  alias yaconf='yaourt -C'        # Fix all configuration files with vimdiff
+  alias yaconf='yaourt -C'                   # Fix all configuration files with vimdiff
   # Pacman - https://wiki.archlinux.org/index.php/Pacman_Tips
-  alias yaupg='yaourt -Syua'        # Synchronize with repositories before upgrading packages (AUR packages too) that are out of date on the local system.
-  alias yasu='yaourt --sucre'      # Same as yaupg, but without confirmation
-  alias yain='yaourt -S'           # Install specific package(s) from the repositories
-  alias yains='yaourt -U'          # Install specific package not from the repositories but from a file
-  alias yare='yaourt -R'           # Remove the specified package(s), retaining its configuration(s) and required dependencies
-  alias yarem='yaourt -Rns'        # Remove the specified package(s), its configuration(s) and unneeded dependencies
-  alias yarep='yaourt -Si'         # Display information about a given package in the repositories
-  alias yareps='yaourt -Ss'        # Search for package(s) in the repositories
-  alias yaloc='yaourt -Qi'         # Display information about a given package in the local database
-  alias yalocs='yaourt -Qs'        # Search for package(s) in the local database
-  alias yalst='yaourt -Qe'         # List installed packages, even those installed from AUR (they're tagged as "local")
-  alias yaorph='yaourt -Qtd'       # Remove orphans using yaourt
+  alias yaupg='yaourt -Syua'                 # Synchronize with repositories before upgrading packages (AUR packages too) that are out of date on the local system.
+  alias yasu='yaourt -Syua --noconfirm'      # Same as yaupg, but without confirmation
+  alias yain='yaourt -S'                     # Install specific package(s) from the repositories
+  alias yains='yaourt -U'                    # Install specific package not from the repositories but from a file
+  alias yare='yaourt -R'                     # Remove the specified package(s), retaining its configuration(s) and required dependencies
+  alias yarem='yaourt -Rns'                  # Remove the specified package(s), its configuration(s) and unneeded dependencies
+  alias yarep='yaourt -Si'                   # Display information about a given package in the repositories
+  alias yareps='yaourt -Ss'                  # Search for package(s) in the repositories
+  alias yaloc='yaourt -Qi'                   # Display information about a given package in the local database
+  alias yalocs='yaourt -Qs'                  # Search for package(s) in the local database
+  alias yalst='yaourt -Qe'                   # List installed packages, even those installed from AUR (they're tagged as "local")
+  alias yaorph='yaourt -Qtd'                 # Remove orphans using yaourt
   # Additional yaourt alias examples
   if (( $+commands[abs] && $+commands[aur] )); then
     alias yaupd='yaourt -Sy && sudo abs && sudo aur'  # Update and refresh the local package, ABS and AUR databases against repositories
