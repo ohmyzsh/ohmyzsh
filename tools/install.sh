f6999347d946aa418ae20fe47f15d030f5e8472b
Adil Hafeez <adil@dropbox.com>
2016-04-02 00:40:25 +0000

    Update default repo

--- tools/install.sh
+++ tools/install.sh
@@ -53,7 +53,7 @@ main() {
     echo "Error: git is not installed"
     exit 1
   }
-  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
+  env git clone --depth=1 https://github.com/adilhafeez/oh-my-zsh.git $ZSH || {
     printf "Error: git clone of oh-my-zsh repo failed\n"
     exit 1
   }
