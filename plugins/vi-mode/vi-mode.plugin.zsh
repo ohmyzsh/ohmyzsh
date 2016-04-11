1ccd3215e77f2b11d0273be5b65d640c3f530528
Douglas Drumond <douglas@cafelinear.com>
2016-04-11 14:49:19 -0300

    Add ctrl-a/ctrl-e support to move to bol/eol in vi-mode
    Signed-off-by: Douglas Drumond <douglas@cafelinear.com>

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -31,6 +31,10 @@ bindkey '^w' backward-kill-word
 # allow ctrl-r to perform backward search in history
 bindkey '^r' history-incremental-search-backward
 
+# allow ctrl-a and ctrl-e to move to beginning/end of line
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
+
 # if mode indicator wasn't setup by theme, define default
 if [[ "$MODE_INDICATOR" == "" ]]; then
   MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$reset_color%}"
