87555551a1f1012cc8d92b5a54695cf41de13c63
Aaron Biggs <abiggs@ou.edu>
2016-04-22 08:09:50 -0500

    Update Docker aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -48,8 +48,9 @@ alias dw='~/Dropbox'
 alias dp='~/Dropbox\ \(Personal\)'
 
 # Docker Machine
-alias dminit="docker-machine create --driver virtualbox dev"
-alias dmup="docker-machine start dev && eval \"\$(docker-machine env dev)\""
+alias dminit="docker-machine create --driver virtualbox default"
+alias dmup="docker-machine start default; eval $(docker-machine env default)"
+alias dmdown="docker-machine stop"
 alias dm="docker-machine"
 
 # Docker Compose
@@ -57,6 +58,9 @@ alias dc="docker-compose"
 alias dcup="docker-compose up -d"
 alias dcdown="docker-compose stop"
 alias dcrm="docker-compose stop && docker-compose rm -f"
+
+# Setup Docker Development Environment
+alias devup="docker-machine start default; eval $(docker-machine env default); docker-compose -f ~/Dropbox/Code/docker/development/docker-compose.yml up -d"
 
 # SSH
 alias casapps="ssh 10.26.0.80"
