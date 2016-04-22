12bb3c4c6c654daf70149a0d7ec750446d1ed417
Aaron Biggs <abiggs@ou.edu>
2016-04-22 08:39:41 -0500

    Add eval statements to Docker exec

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -63,8 +63,8 @@ alias dcrm="docker-compose stop && docker-compose rm -f"
 alias devup="docker-machine start default; eval \"\$(docker-machine env default)\"; docker-compose -f ~/Dropbox/Code/docker/development/docker-compose.yml up -d"
 
 # Enter Docker containers
-alias devweb="docker exec -it web bash"
-alias devdb="docker exec -it db bash"
+alias devweb="eval \"\$(docker-machine env default)\"; docker exec -it web bash"
+alias devdb="eval \"\$(docker-machine env default)\"; docker exec -it db bash"
 
 # SSH
 alias casapps="ssh 10.26.0.80"
