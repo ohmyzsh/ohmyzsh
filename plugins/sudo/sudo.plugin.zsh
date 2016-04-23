ed9a63f5c0125cf5fb63b9466ffebc4e69c07ba1
naegi <gouge.tristan@gmail.com>
2016-04-23 19:25:36 +0200

    Add sudoedit support

--- plugins/sudo/sudo.plugin.zsh
+++ plugins/sudo/sudo.plugin.zsh
@@ -16,6 +16,12 @@ sudo-command-line() {
     [[ -z $BUFFER ]] && zle up-history
     if [[ $BUFFER == sudo\ * ]]; then
         LBUFFER="${LBUFFER#sudo }"
+    elif [[ $BUFFER == $EDITOR\ * ]]; then
+        LBUFFER="${LBUFFER#$EDITOR }"
+        LBUFFER="sudoedit $LBUFFER"
+    elif [[ $BUFFER == sudoedit\ * ]]; then
+        LBUFFER="${LBUFFER#sudoedit }"
+        LBUFFER="$EDITOR $LBUFFER"
     else
         LBUFFER="sudo $LBUFFER"
     fi
