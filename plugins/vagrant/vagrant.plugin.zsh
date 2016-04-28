1a5b4db3650100c632e5491cc01b471d902eed06
Josef Glatz <josefglatz@gmail.com>
2016-04-28 09:51:56 +0200

    Add vagrant aliases
    Add some common vagrant command aliases.

---
+++ plugins/vagrant/vagrant.plugin.zsh
@@ -0,0 +1,13 @@
+#
+# Aliases
+# (sorted alphabetically)
+#
+
+alias vgls='vagrant global-status'
+alias vh='vagrant halt'
+alias vp='vagrant provision'
+alias vr='vagrant reload'
+alias vs='vagrant status'
+alias vssh='vagrant ssh'
+alias vu='vagrant up'
+alias vup='vagrant up'
