a64d2551acb2df90eba8bd7881d98d600ed562e1
Vital Kolas <vital.kolas@gmail.com>
2016-05-12 10:40:24 +0300

    Exclude .idea folder from grep search scope

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -11,12 +11,14 @@ if grep-flag-available --color=auto; then
 fi
 
 # ignore VCS folders (if the necessary grep flags are available)
-VCS_FOLDERS="{.bzr,CVS,.git,.hg,.svn}"
+VCS_FOLDERS=".bzr,CVS,.git,.hg,.svn"
+IDE_FOLDERS=".idea"
+EXC_FOLDERS="{$VCS_FOLDERS,$IDE_FOLDERS}"
 
 if grep-flag-available --exclude-dir=.cvs; then
-    GREP_OPTIONS+=" --exclude-dir=$VCS_FOLDERS"
+    GREP_OPTIONS+=" --exclude-dir=$EXC_FOLDERS"
 elif grep-flag-available --exclude=.cvs; then
-    GREP_OPTIONS+=" --exclude=$VCS_FOLDERS"
+    GREP_OPTIONS+=" --exclude=$EXC_FOLDERS"
 fi
 
 # export grep settings
@@ -25,4 +27,6 @@ alias grep="grep $GREP_OPTIONS"
 # clean up
 unset GREP_OPTIONS
 unset VCS_FOLDERS
+unset IDE_FOLDERS
+unset EXC_FOLDERS
 unfunction grep-flag-available
