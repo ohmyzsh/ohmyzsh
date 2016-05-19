b6294f5843fb040445fb030e258fa7e888681cfc
Ivan Mironov <ivan-mironov@binomo.com>
2016-05-19 13:05:21 +0300

    Add initial list of aliases

---
+++ plugins/arcanist/README.md
@@ -0,0 +1,10 @@
+## arcanist
+
+**Maintainer:** [@emzar](https://github.com/emzar)
+
+This plugin adds many useful aliases.
+
+### Usage
+
+See the [wiki](https://github.com/robbyrussell/oh-my-zsh/wiki/Plugin:arcanist) for a list of aliases provided by the plugin.
+
---
+++ plugins/arcanist/arcanist.plugin.zsh
@@ -0,0 +1,15 @@
+#
+# Aliases
+# (sorted alphabetically)
+#
+
+alias ara='arc amend'
+alias arb='arc branch'
+alias arc='arc cover'
+alias arci='arc commit'
+alias ard='arc diff'
+alias are='arc export'
+alias arh='arc help'
+alias arl='arc land'
+alias arli='arc list'
+alias arp='arc patch'
