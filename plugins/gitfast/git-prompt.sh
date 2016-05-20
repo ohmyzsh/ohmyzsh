55b3e6a9fcf594c125528d0359e37cf337b8efdf
Felipe Contreras <felipe.contreras@gmail.com>
2016-05-19 23:32:44 -0500

    gitfast: fix the prompt colour
    We can add colour in Zsh without the need of pcmode.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- plugins/gitfast/git-prompt.sh
+++ plugins/gitfast/git-prompt.sh
@@ -502,9 +502,11 @@ __git_ps1 ()
 
 	local z="${GIT_PS1_STATESEPARATOR-" "}"
 
-	# NO color option unless in PROMPT_COMMAND mode
-	if [ $pcmode = yes ] && [ -n "${GIT_PS1_SHOWCOLORHINTS-}" ]; then
-		__git_ps1_colorize_gitstring
+	# NO color option unless in PROMPT_COMMAND mode or it's Zsh
+	if [ -n "${GIT_PS1_SHOWCOLORHINTS-}" ]; then
+		if [ $pcmode = yes ] || [ -n "${ZSH_VERSION-}" ]; then
+			__git_ps1_colorize_gitstring
+		fi
 	fi
 
 	b=${b##refs/heads/}
