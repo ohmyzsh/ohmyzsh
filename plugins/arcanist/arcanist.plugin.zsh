c2168d741f129efb52ff86e0c6bcd1efe191a58b
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-22 03:47:55 +0300

    Arcanist plugin (#5106)
    * Add initial list of aliases
    
    * Add lint and diff aliases
    
    * Remove usage section

---
+++ plugins/arcanist/arcanist.plugin.zsh
@@ -0,0 +1,21 @@
+#
+# Aliases
+# (sorted alphabetically)
+#
+
+alias ara='arc amend'
+alias arb='arc branch'
+alias arc='arc cover'
+alias arci='arc commit'
+
+alias ard='arc diff'
+alias ardnu='arc diff --nounit'
+alias ardnupc='arc diff --nounit --plan-changes'
+alias ardpc='arc diff --plan-changes'
+
+alias are='arc export'
+alias arh='arc help'
+alias arl='arc land'
+alias arli='arc lint'
+alias arls='arc list'
+alias arp='arc patch'
