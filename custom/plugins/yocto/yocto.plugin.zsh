c74255958fd06266cc5db9442bd95d77c3b3180d
Gaetan Semet <gaetan@xeberon.net>
2016-05-26 16:43:50 +0200

    alias and function for yocto
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- custom/plugins/yocto/yocto.plugin.zsh
+++ custom/plugins/yocto/yocto.plugin.zsh
@@ -2,3 +2,52 @@
 autoload -U +X bashcompinit && bashcompinit
 
 alias bb='bitbake'
+
+function bb-enter-env(){
+    cd ~/projects/yocto
+    source ./build/envsetup.sh
+    lunch sofia_lte-eng
+}
+
+function bb-publish(){
+    (
+        cd ~/projects/yocto
+        bitbake publish
+    )
+}
+
+function bb-bootimage(){
+    (
+        cd ~/projects/yocto
+        bitbake bootimage-fls
+    )
+}
+
+function flash-bootimage(){
+    (
+        cd ~/projects/yocto/out/pub/sofia-lte/images/sofia-lte/signed_fls
+        /opt/intel/platformflashtool/bin/downloadTool \
+            --library=/opt/intel/platformflashtool/lib/libDownloadTool.so \
+            --verbose 4 \
+            psi_flash_signed.fls \
+            boot-sofia-lte_signed.fls
+    )
+}
+
+function flash-all(){
+    (
+        cd ~/projects/yocto/out/pub/sofia-lte/images/sofia-lte/signed_fls
+        /opt/intel/platformflashtool/bin/downloadTool \
+            --library=/opt/intel/platformflashtool/lib/libDownloadTool.so \
+            --verbose 4 \
+            psi_flash_signed.fls \
+            boot-sofia-lte_signed.fls \
+            mobilevisor_signed.fls \
+            mvconfig_smp_signed.fls \
+            secvm_signed.fls \
+            slb_signed.fls \
+            splash_img_signed.fls \
+            system-sofia-lte_signed.fls \
+            ucode_patch_signed.fls
+    )
+}
