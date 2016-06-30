ee7a9f6fe679239f7c21f32df135f9c79b606336
Miguel Vaello Martínez <miguel.vaellomartinez@gmail.com>
2016-06-30 12:25:00 +0200

    jhbuild: add some missing commands and update README (#5195)
    Commands: make, tinderbox.
    
    Closes #5195
    
    Co-authored-by: Marc Cornellà <marc.cornella@live.com>

--- plugins/jhbuild/jhbuild.plugin.zsh
+++ plugins/jhbuild/jhbuild.plugin.zsh
@@ -1,5 +1,6 @@
-# Aliases
-#
+# JHBuild Aliases
+
+# Base
 alias jh='jhbuild'
 # Build
 alias jhb='jhbuild build'
@@ -7,12 +8,14 @@ alias jhbo='jhbuild buildone'
 # Checks
 alias jhckb='jhbuild checkbranches'
 alias jhckm='jhbuild checkmodulesets'
-# Info & list
+# Info & List
 alias jhi='jhbuild info'
 alias jhl='jhbuild list'
 # Clean
 alias jhc='jhbuild clean'
 alias jhco='jhbuild cleanone'
+# Make
+alias jhm='jhbuild make'
 # Run
 alias jhr='jhbuild run'
 # Depends
@@ -25,6 +28,5 @@ alias jhuo='jhbuild updateone'
 alias jhun='jhbuild uninstall'
 # Shell
 alias jhsh='jhbuild shell'
-
-
-
+# Tinderbox
+alias jht='jhbuild tinderbox'
