af73b03df6c7a4e8319c0882f28ed89be7f2a6ca
Ahmad Awais <mrahmadawais@gmail.com>
2016-08-02 19:49:34 +0500

    Enhancement: Show/Hide OSX hidden files 💯

--- plugins/osx/osx.plugin.zsh
+++ plugins/osx/osx.plugin.zsh
@@ -260,3 +260,7 @@ EOF
 	esac
 	osascript -e "tell application \"iTunes\" to $opt"
 }
+
+# Show/hide hidden files in the Finder
+alias showfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hidefiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
