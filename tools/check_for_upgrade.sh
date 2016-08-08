803d29c7c62471b4c672220b54baa308debfe923
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-08 22:27:23 +0200

    Replace /bin/sh with sh for portability
    This makes things work even on system lacking /bin/sh, such as on
    non-rooted Android systems.

--- tools/check_for_upgrade.sh
+++ tools/check_for_upgrade.sh
@@ -11,7 +11,7 @@ function _update_zsh_update() {
 }
 
 function _upgrade_zsh() {
-  env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
+  env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
   # update the zsh file
   _update_zsh_update
 }
