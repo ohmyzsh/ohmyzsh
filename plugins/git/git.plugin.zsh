0f26baba8e0215152dff3c23e5523cb5371b009f
Simen Bekkhus <sbekkhus91@gmail.com>
2016-08-11 02:47:54 +0200

    (git): Add gpsup alias (#5287)

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -148,6 +148,8 @@ compdef _git ggpush=git-checkout
 
 alias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
 
+alias gpsup='git push --set-upstream origin $(git_current_branch)'
+
 alias gignore='git update-index --assume-unchanged'
 alias gignored='git ls-files -v | grep "^[[:lower:]]"'
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
