81ca3182a126025638d61d590df2f58cb6c5dc37
Marc Cornellà <marc.cornella@live.com>
2016-08-21 19:51:08 +0200

    Fix syntax and formatting of rake-fast plugin

--- plugins/rake-fast/rake-fast.plugin.zsh
+++ plugins/rake-fast/rake-fast.plugin.zsh
@@ -1,18 +1,9 @@
-_rake_refresh () {
-  if [ -f .rake_tasks ]; then
-    rm .rake_tasks
-  fi
-  echo "Generating .rake_tasks..." > /dev/stderr
-  _rake_generate
-  cat .rake_tasks
-}
-
 _rake_does_task_list_need_generating () {
-  [[ ! -f .rake_tasks ]] || [[ Rakefile -nt .rake_tasks ]] || (_is_rails_app && _tasks_changed)
+  [[ ! -f .rake_tasks ]] || [[ Rakefile -nt .rake_tasks ]] || { _is_rails_app && _tasks_changed }
 }
 
 _is_rails_app () {
-  [[ -e "bin/rails" ]] || [ -e "script/rails" ]
+  [[ -e "bin/rails" ]] || [[ -e "script/rails" ]]
 }
 
 _tasks_changed () {
@@ -33,14 +24,20 @@ _rake_generate () {
 }
 
 _rake () {
-  if [ -f Rakefile ]; then
+  if [[ -f Rakefile ]]; then
     if _rake_does_task_list_need_generating; then
-      echo "\nGenerating .rake_tasks..." > /dev/stderr
+      echo "\nGenerating .rake_tasks..." >&2
       _rake_generate
     fi
-    compadd `cat .rake_tasks`
+    compadd $(cat .rake_tasks)
   fi
 }
-
 compdef _rake rake
-alias rake_refresh='_rake_refresh'
+
+rake_refresh () {
+  [[ -f .rake_tasks ]] && rm -f .rake_tasks
+
+  echo "Generating .rake_tasks..." >&2
+  _rake_generate
+  cat .rake_tasks
+}
