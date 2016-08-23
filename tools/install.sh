00c15fe85456591770ec34f8b89e1293110e99b9
Joel Kuzmarski <leoj3n@gmail.com>
2016-08-23 10:14:59 -0500

    exit 0 -> exit

--- tools/install.sh
+++ tools/install.sh
@@ -209,7 +209,7 @@ main() {
     # Zsh exits immeditaly after invocation if STDIN is not a TTY, so it would be
     # pointless to exec it in the first place. Let's just print a message instead.
     printf "${BLUE}No input TTY! To begin using Oh My Zsh, start a new zsh session!${NORMAL}\n"
-    exit 0
+    exit
   elif [ -n "$CHSH_ZSH" ]; then
     exec "$CHSH_ZSH" --LOGIN
   else
