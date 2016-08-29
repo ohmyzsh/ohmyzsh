85efb334b3ee4856939f0e65953297d8b6927b9a
Gaetan Semet <gaetan@xeberon.net>
2016-08-29 18:01:23 +0200

    enable tig plugin

--- templates/zshrc-mac.zsh
+++ templates/zshrc-mac.zsh
@@ -36,7 +36,7 @@ plugins=(git2 python repo intel-repo cp buildbot rsync git-extras git-remote-bra
          command-not-found debian dircycle encode64 lol extract launch_trial \
          vim-scp ufw sublime pylint pep8 autopep8 \
          common-aliases buildbot_dev zsh-syntax-highlighting \
-         txw jump grin pip colored-man osx)
+         txw jump grin pip colored-man osx tig)
 
 source $ZSH/oh-my-zsh.sh
 # User configuration
