2b6cbc6b24ce367bdc26753abe9de94c2f7a6fe6
Marc Cornellà <marc.cornella@live.com>
2016-09-02 20:25:28 +0200

    Add Droplr README

--- plugins/droplr/droplr.rb
+++ plugins/droplr/droplr.rb
@@ -1,17 +1,4 @@
 #!/usr/bin/env ruby
-#
-# droplr
-# Fabio Fernandes | http://fabiofl.me
-#
-# Use Droplr from the comand line to upload files and shorten links.
-#
-# Examples:
-# 
-# droplr ./path/to/file/
-# droplr http://example.com
-#
-# This needs Droplr.app to be installed and loged in.
-# Also, Mac only.
 
 if ARGV[0].nil?
    puts "You need to specify a parameter."
