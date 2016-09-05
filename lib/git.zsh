2b661f52c4f2c1779b86d61eff89e4147aef94c6
Marc Cornellà <marc.cornella@live.com>
2016-09-05 11:22:48 +0200

    git: output nothing when no commits ahead or behind
    This fixes old git_commits_ahead behavior and changes git_commits_behind
    to have the same behavior.
    
    Fixes #5355

--- lib/git.zsh
+++ lib/git.zsh
@@ -76,16 +76,21 @@ function git_current_branch() {
 
 # Gets the number of commits ahead from remote
 function git_commits_ahead() {
-  if $(command git rev-parse --git-dir > /dev/null 2>&1); then
-    local COMMITS="$(git rev-list --count @{upstream}..HEAD)"
-    echo "$ZSH_THEME_GIT_COMMITS_AHEAD_PREFIX$COMMITS$ZSH_THEME_GIT_COMMITS_AHEAD_SUFFIX"
+  if command git rev-parse --git-dir &>/dev/null; then
+    local commits="$(git rev-list --count @{upstream}..HEAD)"
+    if [[ "$commits" != 0 ]]; then
+      echo "$ZSH_THEME_GIT_COMMITS_AHEAD_PREFIX$commits$ZSH_THEME_GIT_COMMITS_AHEAD_SUFFIX"
+    fi
   fi
 }
 
 # Gets the number of commits behind remote
 function git_commits_behind() {
-  if $(command git rev-parse --git-dir > /dev/null 2>&1); then
-    echo $(git rev-list --count HEAD..@{upstream})
+  if command git rev-parse --git-dir &>/dev/null; then
+    local commits="$(git rev-list --count HEAD..@{upstream})"
+    if [[ "$commits" != 0 ]]; then
+      echo "$ZSH_THEME_GIT_COMMITS_BEHIND_PREFIX$commits$ZSH_THEME_GIT_COMMITS_BEHIND_SUFFIX"
+    fi
   fi
 }
 
