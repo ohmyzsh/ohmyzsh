d95d7a73e05ba9143a2aa4dc5b0b5ab16ff34514
Marc Cornellà <marc.cornella@live.com>
2016-09-20 21:34:22 +0200

    cp plugin: change cpv to function so that completion works

--- plugins/cp/cp.plugin.zsh
+++ plugins/cp/cp.plugin.zsh
@@ -11,4 +11,7 @@
 #  -e /dev/null - only work on local files
 #  -- - everything after this is an argument, even if it looks like an option
 
-alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
+cpv() {
+    rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress -- "$@"
+}
+compdef _files cpv
