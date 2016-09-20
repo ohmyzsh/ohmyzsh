9699f997bf85dba03295c783f8bc04860861ab50
Luis Ferrer-Labarca <ferrerluis97@gmail.com>
2016-09-19 22:50:16 -0400

    Added 'gbd' alias for 'git branch -d' (#5417)

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -46,6 +46,7 @@ alias gapa='git add --patch'
 
 alias gb='git branch'
 alias gba='git branch -a'
+alias gbd='git branch -d'
 alias gbda='git branch --no-color --merged | command grep -vE "^(\*|\s*(master|develop|dev)\s*$)" | command xargs -n 1 git branch -d'
 alias gbl='git blame -b -w'
 alias gbnm='git branch --no-merged'
