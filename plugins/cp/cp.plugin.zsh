ef82e79731392d85771899ee759157dc27363e10
Marc Cornellà <marc.cornella@live.com>
2016-09-20 21:45:47 +0200

    cp plugin: add a README file

--- plugins/cp/cp.plugin.zsh
+++ plugins/cp/cp.plugin.zsh
@@ -1,16 +1,3 @@
-#Show progress while file is copying
-
-# Rsync options are:
-#  -p - preserve permissions
-#  -o - preserve owner
-#  -g - preserve group
-#  -h - output in human-readable format
-#  --progress - display progress
-#  -b - instead of just overwriting an existing file, save the original
-#  --backup-dir=/tmp/rsync - move backup copies to "/tmp/rsync"
-#  -e /dev/null - only work on local files
-#  -- - everything after this is an argument, even if it looks like an option
-
 cpv() {
     rsync -pogb -hhh --backup-dir=/tmp/rsync -e /dev/null --progress -- "$@"
 }
