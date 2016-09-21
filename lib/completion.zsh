7b103506a7ae2a352da748c3755d3fb778dcde9e
Gravemind <gravemind2a@gmail.com>
2016-09-22 00:30:35 +0200

    Fix hyphen and underscore filename completion
    This deletes the previous hack that allowed completing files with
    the extension: e.g. `abcd.z` to `abcdefg.z`.
    
    It is still possible to use `abcd[TAB].z`, and hyphens or underscores
    are very much more important than this other trick.
    
    Source:
    https://github.com/robbyrussell/oh-my-zsh/issues/1398#issuecomment-169163149
    
    Signed-off-by: Marc Cornellà <marc.cornella@live.com>

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -12,14 +12,14 @@ zmodload -i zsh/complist
 
 ## case-insensitive (all),partial-word and then substring completion
 if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+  zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
   unset CASE_SENSITIVE
 else
   if [ "x$HYPHEN_INSENSITIVE" = "xtrue" ]; then
-    zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+    zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
     unset HYPHEN_INSENSITIVE
   else
-    zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+    zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
   fi
 fi
 
