1427fbffef68d5796c8296ba105f325598b809f8
slavaGanzin <slava.ganzin@gmail.com>
2016-09-29 22:14:04 +0300

    redirect output

--- plugins/git-auto-fetch/git-auto-fetch.plugin.zsh
+++ plugins/git-auto-fetch/git-auto-fetch.plugin.zsh
@@ -1,5 +1,5 @@
 function git_fetch_on_chpwd {
-  ([[ -d .git ]] && git fetch --all >! ./.git/FETCH_LOG &)
+  ([[ -d .git ]] && git fetch --all &>! ./.git/FETCH_LOG &)
 }
-chpwd_functions=(${chpwd_functions[@]} "git_fetch_on_chpwd")
-unset git_fetch_on_cpwd
+chpwd_functions+=(git_fetch_on_chpwd)
+git_fetch_on_chpwd
