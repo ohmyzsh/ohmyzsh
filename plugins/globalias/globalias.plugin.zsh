0bb09bc7785ce63c1e8ae2714a8f91d8c296d616
slavaGanzin <slava.ganzin@gmail.com>
2016-10-04 14:03:37 +0300

    README for globalias

--- plugins/globalias/globalias.plugin.zsh
+++ plugins/globalias/globalias.plugin.zsh
@@ -12,3 +12,6 @@ bindkey -M viins " " globalias
 # control-space to make a normal space
 bindkey -M emacs "^ " magic-space
 bindkey -M viins "^ " magic-space
+
+# normal space during searches
+bindkey -M isearch " " magic-space
