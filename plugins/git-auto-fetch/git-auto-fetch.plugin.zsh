ac7dcdb21cfb57180c7d8007d95232ee62086997
slavaGanzin <slava.ganzin@gmail.com>
2016-10-06 16:55:21 +0300

    add colors

--- plugins/git-auto-fetch/git-auto-fetch.plugin.zsh
+++ plugins/git-auto-fetch/git-auto-fetch.plugin.zsh
@@ -5,9 +5,9 @@ function git_fetch_on_chpwd {
 function git-auto-fetch {
   [[ ! -d .git ]] && return
   if [[ -f ".git/NO_AUTO_FETCH" ]]; then
-    rm ".git/NO_AUTO_FETCH" && echo "disabled"
+    rm ".git/NO_AUTO_FETCH" && echo "${fg_bold[red]}disabled${reset_color}"
   else
-    touch ".git/NO_AUTO_FETCH" && echo "enabled"
+    touch ".git/NO_AUTO_FETCH" && echo "${fg_bold[green]}enabled${reset_color}"
   fi
 }
 chpwd_functions+=(git_fetch_on_chpwd)
