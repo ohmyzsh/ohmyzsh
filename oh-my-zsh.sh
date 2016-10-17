edf1c0b35e7b7f26ab0603700bcd70b297563c1f
Mason <masoncodes0@gmail.com>
2016-10-17 09:44:38 -0500

    os X --> macOS

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -53,7 +53,7 @@ done
 
 # Figure out the SHORT hostname
 if [[ "$OSTYPE" = darwin* ]]; then
-  # OS X's $HOST changes with dhcp, etc. Use ComputerName if possible.
+  # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
   SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST=${HOST/.*/}
 else
   SHORT_HOST=${HOST/.*/}
