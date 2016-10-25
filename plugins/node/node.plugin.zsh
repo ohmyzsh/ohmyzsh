d3939a5f7d48c82b35c36e3fd554c5a60e185255
Sachin George Thomas <sachq1@gmail.com>
2016-10-25 19:55:46 +0530

    add conditions to open module specific node documentation

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -1,5 +1,9 @@
 # Open the node api for your current version to the optional section.
 # TODO: Make the section part easier to use.
 function node-docs {
-  open_command "http://nodejs.org/docs/$(node --version)/api/all.html#all_$1"
+  if [ $# -eq 0 ]; then
+    open_command "http://nodejs.org/docs/$(node --version)/api/all.html"
+  else
+    open_command "http://nodejs.org/docs/$(node --version)/api/$1.html"
+  fi
 }
