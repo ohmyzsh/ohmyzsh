9a7c56dcddfd007499bc59560c1de282611f6813
Marc Cornellà <marc.cornella@live.com>
2016-10-26 20:04:14 +0200

    zsh_reload: fix code style and indent with tabs

--- plugins/zsh_reload/zsh_reload.plugin.zsh
+++ plugins/zsh_reload/zsh_reload.plugin.zsh
@@ -1,13 +1,11 @@
-# reload zshrc
-function src()
-{
-  local cache=$ZSH_CACHE_DIR
-  autoload -U compinit zrecompile
-  compinit -d "$cache/zcomp-$HOST"
+src() {
+	local cache="$ZSH_CACHE_DIR"
+	autoload -U compinit zrecompile
+	compinit -d "$cache/zcomp-$HOST"
 
-  for f in ~/.zshrc "$cache/zcomp-$HOST"; do
-    zrecompile -p $f && command rm -f $f.zwc.old
-  done
+	for f in ~/.zshrc "$cache/zcomp-$HOST"; do
+		zrecompile -p $f && command rm -f $f.zwc.old
+	done
 
-  source ~/.zshrc
+	source ~/.zshrc
 }
