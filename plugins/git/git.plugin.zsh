48bb2c2c08d788dc5a2b493b059676ed8ced10b2
George Rawlinson <george@rawlinson.net.nz>
2016-10-27 13:49:56 +1300

    Add alias to git plugin - gdcaw

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -85,6 +85,7 @@ alias gcs='git commit -S'
 alias gd='git diff'
 alias gdca='git diff --cached'
 alias gdct='git describe --tags `git rev-list --tags --max-count=1`'
+alias gdcaw='git diff --cached --word-diff'
 alias gdt='git diff-tree --no-commit-id --name-only -r'
 alias gdw='git diff --word-diff'
 
