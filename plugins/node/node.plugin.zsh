a7643c554c246a901fb4a35b3f40002778eeceb8
Marc Cornellà <marc.cornella@live.com>
2016-11-02 15:51:07 +0100

    node plugin: simplify URL computation

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -1,9 +1,6 @@
 # Open the node api for your current version to the optional section.
 # TODO: Make the section part easier to use.
 function node-docs {
-  if [ $# -eq 0 ]; then
-    open_command "http://nodejs.org/docs/$(node --version)/api/all.html"
-  else
-    open_command "http://nodejs.org/docs/$(node --version)/api/$1.html"
-  fi
+  local section=${1:-all}
+  open_command "http://nodejs.org/docs/$(node --version)/api/$section.html"
 }
