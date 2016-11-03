1a3e952fe4eacd4080ad858ff04dfa1ac270a975
Fiete Börner <fieteboerner@googlemail.com>
2016-11-03 20:55:56 +0100

    add docker aliases to docker plugin
    use dk as base

---
+++ plugins/docker/docker.plugin.zsh
@@ -0,0 +1,22 @@
+#
+# Aliases
+# (sorted alphabetically)
+#
+
+alias dk='docker'
+
+alias dkb='docker build'
+
+alias dkex='docker exec'
+alias dkexit='docker exec -it'
+
+alias dknet='docker network'
+
+alias dkps='docker ps'
+alias dkpsa='docker ps -a'
+
+alias dkr='docker run'
+alias dkrit='docker run -it'
+
+alias dkv='docker version'
+alias dkvol='docker volume'
