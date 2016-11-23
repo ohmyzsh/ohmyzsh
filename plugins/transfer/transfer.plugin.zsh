ed8aa3428c90401f8ff061ec07da2a9378143f64
Ilyes Kechidi <ilyes.spd@gmail.com>
2016-11-23 02:21:18 +0100

    added a transfer.sh plugin created a function to easily upload files to transfer.sh file sharing site Usage : transfer file.txt

---
+++ plugins/transfer/transfer.plugin.zsh
@@ -0,0 +1,7 @@
+# transfer.sh Easy file sharing from the command line
+# transfer Plugin
+# Usage Example :
+# > transfer file.txt
+
+transfer() { if [ $# -eq 0 ]; then echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi 
+tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }
