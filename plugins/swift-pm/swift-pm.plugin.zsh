037764acc366bae0dc0f01c221c7a23ef04d3c83
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-11-28 21:08:34 +0100

    Add plugin for Swift Package Manager (#5670)
    * Add plugin for Swift Package Manager
    * swift-package-manager: Rename plugin to 'swift-pm'

---
+++ plugins/swift-pm/swift-pm.plugin.zsh
@@ -0,0 +1,8 @@
+# Some aliases to make your life with the Swift Package Manager faster ✌️
+
+alias spi='swift package init'
+alias spf='swift package fetch'
+alias spu='swift package update'
+alias spx='swift package generate-xcodeproj'
+alias sps='swift package show-dependencies'
+alias spd='swift package dump-package'
