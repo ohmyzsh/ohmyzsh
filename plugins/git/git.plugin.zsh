740a01b8d1a83a50488cc09aeb6947928df1131b
Joseph Milla <josephmilla@users.noreply.github.com>
2016-12-22 10:52:41 -0800

    Add 'git remote show origin' as grso

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -211,6 +211,7 @@ alias grt='cd $(git rev-parse --show-toplevel || echo ".")'
 alias gru='git reset --'
 alias grup='git remote update'
 alias grv='git remote -v'
+alias grso='git remote show origin'
 
 alias gsb='git status -sb'
 alias gsd='git svn dcommit'
