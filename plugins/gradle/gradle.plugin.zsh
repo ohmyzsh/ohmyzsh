910d6f3c0ec3cd51ab8aa7d5590590e21a547355
Eric Wendelin <eric@gradle.com>
2017-01-01 14:49:08 -0700

    Sort gradle options for autocompletion
    This will allow us to more easily keep the options list up-to-date

--- plugins/gradle/gradle.plugin.zsh
+++ plugins/gradle/gradle.plugin.zsh
@@ -24,29 +24,29 @@ function _gradle_core_commands() {
 function _gradle_arguments() {
     _arguments -C \
     '-a[Do not rebuild project dependencies]' \
-    '-h[Help]' \
-    '-D[System property]' \
-    '-d[Log at the debug level]' \
-    '--gui[Launches the Gradle GUI app]' \
-    '--stop[Stop the Gradle daemon]' \
-    '--daemon[Use the Gradle daemon]' \
-    '--no-daemon[Do not use the Gradle daemon]' \
-    '--rerun-task [Specifies that any task optimization is ignored.]' \
-    '-i[Log at the info level]' \
-    '-m[Dry run]' \
-    '-P[Set a project property]' \
-    '-p[Specifies the start directory]' \
-    '--profile[Profile the build time]' \
-    '-q[Log at the quiet level (only show errors)]' \
-    '-v[Print the Gradle version info]' \
-    '-x[Specify a task to be excluded]' \
     '-b[Specifies the build file.]' \
     '-c[Specifies the settings file.]' \
-    '--continue[Continues task execution after a task failure.]' \
+    '-d[Log at the debug level]' \
     '-g[Specifies the Gradle user home directory.]' \
-    '-I[Specifies an initialization script.]' \
-    '--refresh-dependencies[Refresh the state of dependencies.]' \
+    '-h[Help]' \
+    '-i[Log at the info level]' \
+    '-m[Dry run]' \
+    '-p[Specifies the start directory]' \
+    '-q[Log at the quiet level (only show errors)]' \
     '-u[Don''t search in parent directories for a settings.gradle file.]' \
+    '-v[Print the Gradle version info]' \
+    '-x[Specify a task to be excluded]' \
+    '-D[System property]' \
+    '-I[Specifies an initialization script.]' \
+    '-P[Set a project property]' \
+    '--continue[Continues task execution after a task failure.]' \
+    '--daemon[Use the Gradle daemon]' \
+    '--gui[Launches the Gradle GUI app]' \
+    '--no-daemon[Do not use the Gradle daemon]' \
+    '--profile[Profile the build time]' \
+    '--refresh-dependencies[Refresh the state of dependencies.]' \
+    '--rerun-task [Specifies that any task optimization is ignored.]' \
+    '--stop[Stop the Gradle daemon]' \
     '*::command:->command' \
     && return 0
 }
