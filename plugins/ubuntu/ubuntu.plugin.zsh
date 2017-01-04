f1835104023a2c7be28c0b46955f9695db7030bc
Alexander Frolov <thephilosoft@gmail.com>
2017-01-05 00:34:38 +0400

    rude solution to conflict with ag command
    there it is http://packages.ubuntu.com/search?keywords=silversearcher-ag

--- plugins/ubuntu/ubuntu.plugin.zsh
+++ plugins/ubuntu/ubuntu.plugin.zsh
@@ -27,7 +27,10 @@ compdef _afu afu='sudo apt-file update'
 alias ppap='sudo ppa-purge'
 compdef _ppap ppap='sudo ppa-purge'
 
-alias ag='sudo apt-get'            # age - but without sudo
+AG_COMMAND=$(command -v ag)
+if [[ -z $AG_COMMAND ]]; then
+    alias ag='sudo apt-get'            # age - but without sudo
+fi
 alias aga='sudo apt-get autoclean' # aac
 alias agb='sudo apt-get build-dep' # abd
 alias agc='sudo apt-get clean'     # adc
@@ -41,7 +44,9 @@ alias agug='sudo apt-get upgrade' # ag
 alias aguu='sudo apt-get update && sudo apt-get upgrade'      #adg
 alias agar='sudo apt-get autoremove'
 
-compdef _ag ag='sudo apt-get'
+if [[ -z $AG_COMMAND ]]; then
+    compdef _ag ag='sudo apt-get'
+fi
 compdef _aga aga='sudo apt-get autoclean'
 compdef _agb agb='sudo apt-get build-dep'
 compdef _agc agc='sudo apt-get clean'
