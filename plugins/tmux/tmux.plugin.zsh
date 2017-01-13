7326e8feaa3721b79fc19f5b49169507992386c4
hjpotter92 <hjpotter92+github@gmail.com>
2017-01-13 12:03:34 +0530

    Add `-u` parameter to the tmux command to support unicode in certain themes
    Based on an answer provided here: https://askubuntu.com/q/410048

--- plugins/tmux/tmux.plugin.zsh
+++ plugins/tmux/tmux.plugin.zsh
@@ -2,9 +2,9 @@
 # Aliases
 #
 
-alias ta='tmux attach -t'
-alias tad='tmux attach -d -t'
-alias ts='tmux new-session -s'
+alias ta='tmux -u attach -t'
+alias tad='tmux -u attach -d -t'
+alias ts='tmux -u new-session -s'
 alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
