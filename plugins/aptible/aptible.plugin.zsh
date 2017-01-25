fc50d39e0e746767b40662d0d0fe7e0abec003d8
Frederick Zhang <frederick888@tsundere.moe>
2017-01-25 21:38:52 +1100

    aptible plugin init

---
+++ plugins/aptible/aptible.plugin.zsh
@@ -0,0 +1,9 @@
+# Authors:
+# https://github.com/Frederick888
+#
+# Aptible CLI oh-my-zsh Plugin
+
+# Aliases ###################################################################
+
+alias 'aptible-zsh-clear'='rm -f /tmp/aptible_dbs_zsh_cache /tmp/aptible_envs_zsh_cache /tmp/aptible_apps_zsh_cache'
+
