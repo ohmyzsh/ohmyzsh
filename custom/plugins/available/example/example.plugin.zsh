ffad598bb6ed0c39da4a03726202f20929aafc3a
Michel Massaro <michel.massaro@gmail.com>
2017-01-26 12:17:50 +0100

    Add first plugin description

--- custom/plugins/available/example/example.plugin.zsh
+++ custom/plugins/available/example/example.plugin.zsh
@@ -1,2 +1,4 @@
+# plugin-description : The description of the plugin must be done in this way
+
 # Add your own custom plugins in the custom/plugins directory. Plugins placed
 # here will override ones with the same name in the main plugins directory.
