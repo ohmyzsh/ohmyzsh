be1d17a70286dc8c3f8ed13db53dd165985fa1bb
Shubham Chaudhary <shubhamchaudhary92@gmail.com>
2017-01-27 14:40:18 +0530

    Add aliases for supervisor

--- plugins/supervisor/supervisor.plugin.zsh
+++ plugins/supervisor/supervisor.plugin.zsh
@@ -1,1 +1,14 @@
 # DECLARION: This plugin was created by hhatto. What I did is just making a portal from https://bitbucket.org/hhatto/zshcompfunc4supervisor.
+
+alias sup='sudo supervisorctl'
+alias supad='sudo supervisorctl add'
+alias supa='sudo supervisorctl avail'
+alias suprl='sudo supervisorctl reload'
+alias suprm='sudo supervisorctl remove'
+alias suprr='sudo supervisorctl reread'
+alias suprs='sudo supervisorctl restart'
+alias sups='sudo supervisorctl status'
+alias supsr='sudo supervisorctl start'
+alias supso='sudo supervisorctl stop'
+alias supt='sudo supervisorctl tail'
+alias supu='sudo supervisorctl update'
