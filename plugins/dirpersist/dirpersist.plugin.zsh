85decc5ffa5e3b546da3787bdf13911c822d8db0
Julian Kahnert <mail@juliankahnert.de>
2017-02-11 23:53:50 +0100

    don't add duplicates to history

--- plugins/dirpersist/dirpersist.plugin.zsh
+++ plugins/dirpersist/dirpersist.plugin.zsh
@@ -6,7 +6,7 @@ DIRSTACKSIZE=${DIRSTACKSIZE:-20}
 dirstack_file=${dirstack_file:-${HOME}/.zdirs}
 
 if [[ -f ${dirstack_file} ]] && [[ ${#dirstack[*]} -eq 0 ]] ; then
-  dirstack=( ${(f)"$(< $dirstack_file)"} )
+  dirstack=( ${(uf)"$(< $dirstack_file)"} )
   # "cd -" won't work after login by just setting $OLDPWD, so
   [[ -d $dirstack[1] ]] && cd $dirstack[1] && cd $OLDPWD
 fi
