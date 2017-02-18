f797c489ddd046cb281f6a87d93ca8932b5bec18
Pawel Olejniczak <pawel.olejniczak@gmail.com>
2017-02-18 18:30:06 +0700

    Envy - add command for editing profile

--- plugins/envy/envy.plugin.zsh
+++ plugins/envy/envy.plugin.zsh
@@ -6,29 +6,41 @@ function _envy_start() {
 }
 
 function _envy_load() {
-	local new_env_path="${ENVY_CONFIG_DIR}/${1}"
+	local new_profile_path="${ENVY_CONFIG_DIR}/${1}"
 
-	if [[ ! -f ${new_env_path} ]]; then
-		echo "Cannot find envy file '${new_env_path}'"
+	if [[ ! -f ${new_profile_path} ]]; then
+		echo "Cannot find envy file '${new_profile_path}'"
 		return 1
 	fi
 
-	source ${new_env_path}
+	source ${new_profile_path}
 }
 
 function _envy_create() {
-	local new_env_path="${ENVY_CONFIG_DIR}/${1}"
+	local new_profile_path="${ENVY_CONFIG_DIR}/${1}"
 
 	[[ -z "${1}" ]] && echo "No profile name argument" && return 1
 	[[ ! -d "${ENVY_CONFIG_DIR}" ]] && mkdir -p ${ENVY_CONFIG_DIR}
-	echo "#!/bin/bash" > ${new_env_path}
-	echo "Created empty profile: ${new_env_path}"
+	echo "#!/bin/bash" > ${new_profile_path}
+	echo "Created empty profile: ${new_profile_path}"
+}
+
+function _envy_edit() {
+	local profile_path="${ENVY_CONFIG_DIR}/${1}"
+
+	[[ -z "${1}" ]] && echo "No profile name argument" && return 1
+	[[ ! -f "${profile_path}" ]] && echo "Profile does not exists" && return 1
+	${EDITOR} ${profile_path}
 }
 
 function envy() {
 	case "${1}" in
 		create)
 			_envy_create ${2}
+			;;
+
+		edit)
+			_envy_edit ${2}
 			;;
 
 		*)
