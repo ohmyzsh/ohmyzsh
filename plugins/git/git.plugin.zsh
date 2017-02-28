fc12a787a990687b4945d68269cea06d288a19dd
Rui Maranhao <rui@computer.org>
2017-02-28 11:37:22 +0000

    Fix in awk command.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -240,4 +240,4 @@ alias glum='git pull upstream master'
 alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip-- [skip ci]"'
 
-alias grrb='git fetch -p && for branch in `git branch -vv | grep ': gone]' | gawk '{print $1}'`; do git branch -D $branch; done'
+alias grrb='git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done'
