3bd020d56d0e0496a7fa0cafb866285d5e04f115
Ian Egner <ian@egnerphotography.co.uk>
2017-04-07 15:08:36 +0100

    Update yarn.plugin.zsh
    Add new commands and arguments

--- plugins/yarn/yarn.plugin.zsh
+++ plugins/yarn/yarn.plugin.zsh
@@ -13,6 +13,7 @@ _yarn ()
     _installopts=(
         '(--flat)--flat[Only allow one version of a package. On the first run this will prompt you to choose a single version for each package that is depended on at multiple version ranges.]'
         '(--har)--har[Outputs an HTTP archive from all the network requests performed during the installation.]'
+        '(--modules-folder)--modules-folder[<path> - Specifies an alternate location for the node_modules directory, instead of the default ./node_modules.]'
         '(--no-lockfile)--no-lockfile[Don’t read or generate a yarn.lock lockfile.]'
         '(--pure-lockfile)--pure-lockfile[Don’t generate a yarn.lock lockfile.]'
     )
@@ -26,16 +27,24 @@ _yarn ()
       'init:Initialize for the development of a package.' \
       'add:Add a package to use in your current package.' \
       'install:Install all the dependencies listed within package.json in the local node_modules folder.' \
-      'publish:Publish a package to a package manager.' \
+      'publish:Publish a package to to the npm registry.' \
+      'tag:Add, remove, or list tags on a package.' \
       'remove:Remove a package that will no longer be used in your current package.' \
       'cache:Clear the local cache. It will be populated again the next time yarn or yarn install is run.' \
       'clean:Frees up space by removing unnecessary files and folders from dependencies.' \
       'check:Verifies that versions of the package dependencies in the current project’s package.json matches that of yarn’s lock file.' \
-      'ls:List all installed packages.' \
+      'list:List all installed packages.' \
       'global:Makes binaries available to use on your operating system.' \
       'info:<package> [<field>] - fetch information about a package and return it in a tree format.' \
       'outdated:Checks for outdated package dependencies.' \
-      'run:Runs a defined package script.' \
+      'run:Runs a defined package script in package.json.' \
+      'test:Runs the defined test script in package.json.' \
+      'link:Symlink a package folder during development.' \
+      'unlink:Unlink a previously created symlink for a package.' \
+      'licenses ls:List licenses for installed packages.' \
+      'login:Prompt you for your username and email for the npm registry.' \
+      'logout:Clear npm registry username and email.' \
+      'pack:Creates a compressed gzip archive of package dependencies.' \
       'self-update:Updates Yarn to the latest version.' \
       'upgrade:Upgrades packages to their latest version based on the specified range.' \
       'why:<query> - Show information about why a package is installed.'
