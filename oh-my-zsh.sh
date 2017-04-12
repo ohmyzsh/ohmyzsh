4c9a66ced234288c3756fc95958ba32bfdeeeb44
Nathan Stilwell <nathanstilwell@gmail.com>
2017-04-12 00:39:38 -0400

    Taking out plugins
    I might find something later

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -9,20 +9,6 @@ for config_file ($ZSH/lib/*.zsh); do
   source $config_file
 done
 
-is_plugin() {
-  local base_dir=$1
-  local name=$2
-  test -f $base_dir/plugins/$name/$name.plugin.zsh \
-    || test -f $base_dir/plugins/$name/_$name
-}
-# Add all defined plugins to fpath. This must be done
-# before running compinit.
-for plugin ($plugins); do
-  if is_plugin $ZSH $plugin; then
-    fpath=($ZSH/plugins/$plugin $fpath)
-  fi
-done
-
 # Figure out the SHORT hostname
 if [ -n "$commands[scutil]" ]; then
   # OS X
@@ -38,14 +24,6 @@ ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
 autoload -U compinit
 compinit -i -d "${ZSH_COMPDUMP}"
 
-# Load all of the plugins that were defined in ~/.zshrc
-# for plugin ($plugins); do
-#   if [ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]; then
-#     source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
-#   elif [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
-#     source $ZSH/plugins/$plugin/$plugin.plugin.zsh
-#   fi
-# done
 
 # load theme
 test -f "$ZSH/themes/$THEME" && source "$ZSH/themes/$THEME"
