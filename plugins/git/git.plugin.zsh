0779b68d6116c8b5632e75600aab47b8e6316efc
Simen Bekkhus <sbekkhus91@gmail.com>
2017-04-17 14:03:24 +0200

    Add no-gpg-sign to gwip

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -238,4 +238,4 @@ alias gupv='git pull --rebase -v'
 alias glum='git pull upstream master'
 
 alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
-alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip-- [skip ci]"'
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip-- [skip ci]"'
