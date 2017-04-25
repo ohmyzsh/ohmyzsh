d384ccf31dca8b0a768c463087f156b7302c77c5
Raphael Stolt <raphael.stolt@gmail.com>
2017-04-25 19:52:55 +0200

    feat(composer): add `cuh` alias for updating global packages (#6048)
    Closes #6048

--- plugins/composer/composer.plugin.zsh
+++ plugins/composer/composer.plugin.zsh
@@ -48,6 +48,7 @@ alias crm='composer remove'
 alias cs='composer show'
 alias csu='composer self-update'
 alias cu='composer update'
+alias cuh='composer update --working-dir=$(composer config -g home)'
 
 
 ## If Composer not found, try to add known directories to $PATH
