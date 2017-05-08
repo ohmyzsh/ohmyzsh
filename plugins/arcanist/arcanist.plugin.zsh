182671929a5440a98378ef23c506032861ee19b0
ZviRackover <zvirack@gmail.com>
2017-05-08 12:00:16 +0300

    Plugin,Arcanist: Add completion for the Arcanist plugin
    Copied from the Arcanist source tree:
    https://github.com/phacility/arcanist/blob/master/resources/shell/bash-completion

--- plugins/arcanist/arcanist.plugin.zsh
+++ plugins/arcanist/arcanist.plugin.zsh
@@ -19,3 +19,32 @@ alias arl='arc land'
 alias arli='arc lint'
 alias arls='arc list'
 alias arpa='arc patch'
+
+#
+# Completion
+#
+
+autoload -U +X bashcompinit && bashcompinit
+
+_arc ()
+{
+  CUR="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=()
+  OPTS=$(echo | arc shell-complete --current ${COMP_CWORD} -- ${COMP_WORDS[@]})
+
+  if [ $? -ne 0 ]; then
+    return $?
+  fi
+
+  if [ "$OPTS" = "FILE" ]; then
+    COMPREPLY=( $(compgen -f -- ${CUR}) )
+    return 0
+  fi
+
+  if [ "$OPTS" = "ARGUMENT" ]; then
+    return 0
+  fi
+
+  COMPREPLY=( $(compgen -W "${OPTS}" -- ${CUR}) )
+}
+complete -F _arc -o filenames arc
