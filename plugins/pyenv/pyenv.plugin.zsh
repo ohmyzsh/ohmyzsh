2ad9b3d9770db36e9d147145a8137bc78adbb893
David Pärsson <david.parsson@amra.se>
2017-06-16 14:46:37 +0200

    Remove special handling of pyenv from homebrew
    Installing pyenv's Python versions under `brew --prefix pyenv` will
    make the user have to re-install all Python versions everytime pyenv
    is updated. This is because `brew --prefix pyenv` by links to a
    Homebrew Cellar directory containing pyenv's version name.

--- plugins/pyenv/pyenv.plugin.zsh
+++ plugins/pyenv/pyenv.plugin.zsh
@@ -1,16 +1,5 @@
-_homebrew-installed() {
-    type brew &> /dev/null
-}
-
-_pyenv-from-homebrew-installed() {
-    brew --prefix pyenv &> /dev/null
-}
-
 FOUND_PYENV=0
 pyenvdirs=("$HOME/.pyenv" "/usr/local/pyenv" "/opt/pyenv")
-if _homebrew-installed && _pyenv-from-homebrew-installed ; then
-    pyenvdirs=($(brew --prefix pyenv) "${pyenvdirs[@]}")
-fi
 
 for pyenvdir in "${pyenvdirs[@]}" ; do
     if [ -d $pyenvdir/bin -a $FOUND_PYENV -eq 0 ] ; then
