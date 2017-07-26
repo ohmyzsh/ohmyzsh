1c53d07aae1367a2aa557bb353a7b0900786af14
Balázs Györkös <balazs.gyorkos@gmail.com>
2017-07-26 07:40:02 +0200

    added some alias
    for laravel magicians

--- plugins/laravel/laravel.plugin.zsh
+++ plugins/laravel/laravel.plugin.zsh
@@ -1,3 +1,9 @@
 #!zsh
 alias artisan='php artisan'
+alias art='php artisan'
 alias bob='php artisan bob::build'
+alias tkr='php artisan tinker'
+alias routes='php artisan route:list'
+alias clear='php artisan view:clear; composer dump-autoload'
+
+
