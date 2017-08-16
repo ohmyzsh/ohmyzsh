f33378f79208d33d886473aa1929fb5fa9071ce1
Petr Šabata <contyk@redhat.com>
2017-08-16 23:37:06 +0200

    Include the latest systemctl commands
    Based on systemd-233.  I'm still keeping the old, now unsupported
    commands for backwards compatibility as well.
    
    Signed-off-by: Petr Šabata <contyk@redhat.com>

--- plugins/systemd/systemd.plugin.zsh
+++ plugins/systemd/systemd.plugin.zsh
@@ -1,9 +1,14 @@
 user_commands=(
   cat
+  get-default
   help
   is-active
   is-enabled
+  is-failed
+  is-system-running
+  list-dependencies
   list-jobs
+  list-sockets
   list-timers
   list-unit-files
   list-units
@@ -12,23 +17,46 @@ user_commands=(
   status)
 
 sudo_commands=(
+  add-requires
+  add-wants
   cancel
+  daemon-reexec
+  daemon-reload
+  default
   disable
   edit
+  emergency
   enable
+  halt
+  hibernate
+  hybrid-sleep
+  import-environment
   isolate
+  kexec
   kill
   link
+  list-machines
   load
   mask
+  poweroff
   preset
+  preset-all
+  reboot
   reenable
   reload
+  reload-or-restart
   reset-failed
+  rescue
   restart
+  revert
+  set-default
   set-environment
+  set-property
   start
   stop
+  suspend
+  switch-root
+  try-reload-or-restart
   try-restart
   unmask
   unset-environment)
