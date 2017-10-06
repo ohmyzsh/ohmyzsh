91b09eb848a33e6c7187d6add40df6e787fc564e
Mahmoud Zalt <mahmoudz.me@gmail.com>
2017-10-06 07:25:01 +0300

    Add a message informing users about parsing errors
    I couldn't tell why I got this error `.env: line 129: syntax error near unexpected token `('` after adding a bunch of plugins.
    I didn't knew what most of the plugins do, nor what the `dotenv` plugin is doing behind the scene!
    After figuring out the problem, I though it would be much better displaying an error message only in case there are some parsing errors. Now I would get this:
    
    ```
    Found some errors while parsing your .env file:
    .env: line 129: syntax error near unexpected token `('
    ```

--- plugins/dotenv/dotenv.plugin.zsh
+++ plugins/dotenv/dotenv.plugin.zsh
@@ -2,7 +2,11 @@
 
 source_env() {
   if [[ -f .env ]]; then
-    source .env
+	if errs=$(bash -n .env 2>&1); 
+		then source .env; 
+	else 
+		printf '%s\n' "Found some errors while parsing your .env file: " "$errs"; 
+	fi
   fi
 }
 
