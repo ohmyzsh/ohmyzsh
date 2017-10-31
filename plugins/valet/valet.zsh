82d489f6081740ae78db862e2561f3fbe456f094
travoltron <ben@travoltron.com>
2017-10-31 14:53:10 -0400

    Create Valet aliases
    https://laravel.com/docs/5.5/valet is an amazing, lightweight dev tool for building Laravel/PHP sites.

---
+++ plugins/valet/valet.zsh
@@ -0,0 +1,13 @@
+#Alias
+alias vlt='valet'
+
+alias vltin='valet install'
+alias vltuin='valet uninstall'
+alias vlttld='valet domain'
+alias vltgo='valet start'
+alias vltstop='valet stop'
+alias vltl='valet link'
+alias vltunl='valet unlink'
+alias vlto='valet open'
+alias vltsh='valet share'
+alias vlttld='valet domain'
