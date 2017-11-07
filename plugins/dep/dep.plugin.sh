64e07869c2f1cc0e31da16b8fafa9ae3a9b74913
Muhammad Shoaib <muhammad.shoaib@foodora.com>
2017-11-07 06:40:46 +0100

    Added Dep(go dependency manager) aliases

---
+++ plugins/dep/dep.plugin.sh
@@ -0,0 +1,8 @@
+ alias depi='dep init'
+ alias deps='dep status'
+ alias depsd='deps -dot'
+ alias depe='dep ensure'
+ alias depea='depe -add'
+ alias depeu='depeu -update'
+ alias depp='dep prune'
+
