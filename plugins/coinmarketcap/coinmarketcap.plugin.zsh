0eba6e5eaa794685b9959a8ae9fcaf1a3b050f10
Daniel Main <daniel.main@software-creation.de>
2017-11-18 18:46:19 +0100

    applied coding style of oh-my-zsh

--- plugins/coinmarketcap/coinmarketcap.plugin.zsh
+++ plugins/coinmarketcap/coinmarketcap.plugin.zsh
@@ -3,27 +3,24 @@
 #
 # See README.md for details
 
-function coinmarketcap() {
-  curl -s https://api.coinmarketcap.com/v1/ticker/ > $HOME/.coinmarketcap.json
-  curl -s https://api.coinmarketcap.com/v1/ticker/bitcoin-gold/ > $HOME/.coinmarketcap-btg.json
+coinmarketcap() {
+  curl -s https://api.coinmarketcap.com/v1/ticker/ > $HOME/.coinmarketcap.json || exit 1
+  curl -s https://api.coinmarketcap.com/v1/ticker/bitcoin-gold/ > $HOME/.coinmarketcap-btg.json || exit 1
   if [[ -n "$1" ]]; then
-    coin=$1
-    symbols=(`cat $HOME/.coinmarketcap.json | grep -Po '(?<="symbol": ")[^"]*' | awk -F "\n" '{print $1}'`)
-    price=(`cat $HOME/.coinmarketcap.json | grep -Po '(?<="price_usd": ")[^"]*' | awk -F "\n" '{print $1}'`)
-    if [[ "${coin}" == "ALL" ]]; then
-      for ((i=0; i<${#symbols[*]}; i++));
-      do
-        echo -e ${symbols[i]}"\t : \t"${price[i]}
+    readonly COIN=$1
+    readonly SYMBOLS=(`cat $HOME/.coinmarketcap.json | grep -Po '(?<="symbol": ")[^"]*' | awk -F "\n" '{print $1}' || exit 1`)
+    readonly PRICE=(`cat $HOME/.coinmarketcap.json | grep -Po '(?<="price_usd": ")[^"]*' | awk -F "\n" '{print $1}' || exit 1`)
+    if [[ "${COIN}" == "ALL" ]]; then
+      for ((i=0; i<${#SYMBOLS[*]}; i++)); do
+        echo -e ${SYMBOLS[i]}"\t : \t"${PRICE[i]}
       done
-    elif [[ "${coin}" == "BTG" ]]; then
-      btg_symbols=(`cat $HOME/.coinmarketcap-btg.json | grep -Po '(?<="symbol": ")[^"]*'`)
-      btg_price=(`cat $HOME/.coinmarketcap-btg.json | grep -Po '(?<="price_usd": ")[^"]*'`)
-      echo -e ${btg_price}
+    elif [[ "${COIN}" == "BTG" ]]; then
+      readonly BTG_PRICE=(`cat $HOME/.coinmarketcap-btg.json | grep -Po '(?<="price_usd": ")[^"]*'`)
+      echo -e ${BTG_PRICE}
     else
-      for ((i=0; i<${#symbols[*]}; i++));
-      do
-          if [[ "${symbols[i]}" == "${coin}" ]]; then
-            echo -e ${price[i]}
+      for ((i=0; i<${#SYMBOLS[*]}; i++)); do
+          if [[ "${SYMBOLS[i]}" == "${COIN}" ]]; then
+            echo -e ${PRICE[i]}
           fi
       done
     fi
@@ -31,6 +28,7 @@ function coinmarketcap() {
     echo "Usage example for Bitcoin Gold: \"coin BTG\""
     echo "Usage example all crypto currencies: \"coin ALL\""
   fi
+  return 1
 }
 
 alias coin=coinmarketcap
