72f7634af5a1849d16d868b1a36759912a0fd085
Gaetan Semet <gaetan@xeberon.net>
2017-11-28 23:15:41 +0100

    subliminal magic
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

---
+++ custom/plugins/subliminal/subliminal.plugin.zsh
@@ -0,0 +1,1 @@
+alias subliminal-dl-en="subliminal --addic7ed $ADDIC7ED_USERNAME $ADDIC7ED_PASSWORD --opensubtitles $OPENSUBTITLES_USERNAME $ADDIC7ED_PASSWORD  download --language eng --language fra "
