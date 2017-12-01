30045ab16c523eaa47dd3e396acb9efb9e997eeb
Patrick Artounian <partounian@users.noreply.github.com>
2017-12-01 11:05:34 -0800

    Remove afind which adds .23 seconds to startup

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -26,13 +26,6 @@ env_default LESS '-R'
 alias _='sudo'
 alias please='sudo'
 
-## more intelligent acking for ubuntu users
-if which ack-grep &> /dev/null; then
-  alias afind='ack-grep -il'
-else
-  alias afind='ack -il'
-fi
-
 # only define LC_CTYPE if undefined
 if [[ -z "$LC_CTYPE" && -z "$LC_ALL" ]]; then
 	export LC_CTYPE=${LANG%%:*} # pick the first entry from LANG
