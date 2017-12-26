bfeba5550919c6e016e49c06090acc0d1dafd4b0
drymer <drymer@autistici.org>
2017-12-26 14:11:16 +0100

    Add kube context manager

---
+++ plugins/kube/kube.plugin.zsh
@@ -0,0 +1,23 @@
+export KUBE_HOME=~/.kube
+
+function kgp {
+  echo $KUBECONFIG
+}
+
+function ksp {
+  local rprompt=${RPROMPT/<aws:$(kgp)>/}
+
+  export KUBE_PROFILE=$1
+  export KUBECONFIG=$KUBE_HOME/$1-config
+
+  export RPROMPT="<kube:$KUBE_PROFILE>$rprompt"
+}
+
+function kube_profiles {
+  reply=($(ls $KUBE_HOME/*-config |
+	       awk -F'/' '{print $5}' |
+	       sed "s/-config$/ /g" |
+	       tr -d '\n'))
+}
+
+compctl -K kube_profiles ksp
