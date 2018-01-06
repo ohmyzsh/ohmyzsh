17dbeddfdf98c45f9eb471b29d1e5bf4d3fb9040
Abhinav <abhinav.manchanda@gmail.com>
2018-01-06 10:39:57 +0530

    Added more aliases for docker.

--- plugins/docker/docker.plugin.zsh
+++ plugins/docker/docker.plugin.zsh
@@ -10,10 +10,27 @@ alias dt='docker top'
 alias dv='docker version'
 alias dpo='docker container port'
 alias dpu='docker pull'
-alias dx='docker exec'
+alias dx='docker container exec'
 alias dbl='docker build'
 alias dhh='docker help'
+alias dcin='docker container inspect'
 
 #docker images
 alias dirm='docker image rm'
 alias dils='docker image ls'
+alias dit='docker image tag'
+alias dip='docker image push'
+alias dib='docker image build'
+alias dii='docker image inspect'
+
+
+#docker network
+alias dnls='docker network ls'
+alias dni='docker network inspect'
+alias dnc='docker network create'
+alias dncn='docker network connect'
+alias dndcn='docker network disconnect'
+
+#docker volume
+alias dvl='docker volume ls'
+alias dvclean='docker volume rm $(docker volume ls -qf dangling=true)'
