675a2d9922ffc1f2cf88dd3d871f22136b7e525c
Billy Mathews <billy@billym.co.uk>
2018-01-17 00:08:38 +0000

    feat(tmux): add `tmuxconf` alias to edit .tmux.conf (#6555)
    Closes #6555

--- plugins/tmux/tmux.plugin.zsh
+++ plugins/tmux/tmux.plugin.zsh
@@ -11,6 +11,7 @@ alias ts='tmux new-session -s'
 alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
+alias tmuxconf='$EDITOR ~/.tmux.conf'
 
 # CONFIGURATION VARIABLES
 # Automatically start tmux
