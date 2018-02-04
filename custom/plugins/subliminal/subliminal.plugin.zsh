666cb354bb2bc386c367e70db211ad518530d3d6
Gaetan Semet <gaetan@xeberon.net>
2018-02-04 16:39:14 +0100

    update subliminal alias

--- custom/plugins/subliminal/subliminal.plugin.zsh
+++ custom/plugins/subliminal/subliminal.plugin.zsh
@@ -1,1 +1,6 @@
-alias subliminal-dl-en="subliminal --addic7ed $ADDIC7ED_USERNAME $ADDIC7ED_PASSWORD --opensubtitles $OPENSUBTITLES_USERNAME $ADDIC7ED_PASSWORD  download --language eng --language fra "
+
+OPENSUBTITLES_USERNAME=gaetan@xeberon.net
+OPENSUBTITLE_PASSWORD=stibbons
+ADDIC7ED_USERNAME=donatelo.michelangelo@gmail.com
+ADDIC7ED_PASSWORD=stibbons
+alias subliminal-dl-en="subliminal --addic7ed $ADDIC7ED_USERNAME $ADDIC7ED_PASSWORD --opensubtitles $OPENSUBTITLES_USERNAME $OPENSUBTITLE_PASSWORD  download --language eng --language fra "
