bff731c56c1e08d15d88012123bf3383a475c77a
Navelpluisje <erwin@navelpluisje.nl>
2018-04-18 14:49:11 +0200

    Create cordova.plugin.zsh
    Added some default aliases for the use with cordove

---
+++ plugins/cordova/cordova.plugin.zsh
@@ -0,0 +1,29 @@
+#
+# Aliases for cordova
+# (sorted alphabetically)
+#
+
+#==============================================================================
+# Global Aliases
+#==============================================================================
+
+alias co='cordova'
+alias cob='cordova build'
+alias cor='cordova run'
+alias coc='cordova clean'
+
+#==============================================================================
+# Platform aliases
+#==============================================================================
+
+alias cop='cordova platform list'
+alias copa='cordova platform add'
+alias copr='cordova platform remove'
+
+#==============================================================================
+# Plugin aliases
+#==============================================================================
+
+alias copl='cordova plugin list'
+alias copla='cordova plugin add'
+alias coplr='cordova plugin remove'
