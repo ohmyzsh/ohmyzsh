0bb2fcaf21d53034df90d9402d950fb3df2c5514
Rob Loach <robloach@gmail.com>
2018-04-18 11:30:48 -0400

    composer: Fix path check syntax

--- plugins/composer/composer.plugin.zsh
+++ plugins/composer/composer.plugin.zsh
@@ -56,5 +56,5 @@ if hash composer 2>/dev/null; then
   export PATH=$PATH:$(composer global config bin-dir --absolute 2>/dev/null)
 else
   [ -d $HOME/.composer/vendor/bin ] && export PATH=$PATH:$HOME/.composer/vendor/bin
-  [ -d $HOME/.config/composer/vendor/bin] && export PATH=$PATH:$HOME/.config/composer/vendor/bin
+  [ -d $HOME/.config/composer/vendor/bin ] && export PATH=$PATH:$HOME/.config/composer/vendor/bin
 fi
