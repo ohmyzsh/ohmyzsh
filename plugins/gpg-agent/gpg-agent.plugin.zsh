9a71864288e5fb3898b0764db3e547cf7bb7328c
Chris Fleming <me@chrisfleming.org>
2018-04-21 21:26:36 +0100

    Fix gpg-agent plugin checks (#6469)
    * Always try and start gpg-agent, with --use-standard-socket it will try and use a standard socket directory. It won't start multiple agents if agent is already running. In addition, XDG_RUNTIME_DIR isn't always set
    
    * ssh socket if broken if --daemon is run again, so onky start if we don't have a socket
    
    * Removed unnecessary allocation of GPG_SSH_AUTH_SOCK

--- plugins/gpg-agent/gpg-agent.plugin.zsh
+++ plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,14 +1,16 @@
-# Enable gpg-agent if it is not running
-GPG_AGENT_SOCKET="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
-if [ ! -S $GPG_AGENT_SOCKET ]; then
-  gpg-agent --daemon >/dev/null 2>&1
-  export GPG_TTY=$(tty)
-fi
+# Enable gpg-agent if it is not running-
+# --use-standard-socket will work from version 2 upwards
 
-# Set SSH to use gpg-agent if it is configured to do so
-GNUPGCONFIG="${GNUPGHOME:-"$HOME/.gnupg"}/gpg-agent.conf"
-if [ -r "$GNUPGCONFIG" ] && grep -q enable-ssh-support "$GNUPGCONFIG"; then
+AGENT_SOCK=`gpgconf --list-dirs | grep agent-socket | cut -d : -f 2`
+
+if [ ! -S ${AGENT_SOCK} ]; then
+  gpg-agent --daemon --use-standard-socket >/dev/null 2>&1
+fi
+export GPG_TTY=$(tty)
+
+# Set SSH to use gpg-agent if it's enabled
+if [ -S "${AGENT_SOCK}.ssh" ]; then
+  export SSH_AUTH_SOCK="${AGENT_SOCK}.ssh"
   unset SSH_AGENT_PID
-  export SSH_AUTH_SOCK=$GPG_AGENT_SOCKET
 fi
 
