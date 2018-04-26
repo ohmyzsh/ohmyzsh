39c230e9d160958c829dcfc9acb8b36c68d4398b
Chris Fleming <chflemin@chflemin-virtualbox.edi.is.keysight.com>
2018-04-26 17:20:26 +0100

    Don't set SSH_AUTH_SOCK if the ssh-agent plugin is enabled

--- plugins/gpg-agent/gpg-agent.plugin.zsh
+++ plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,7 +1,5 @@
 # Enable gpg-agent if it is not running-
 # --use-standard-socket will work from version 2 upwards
-typeset _gpg_ssh_socket
-
 AGENT_SOCK=`gpgconf --list-dirs | grep agent-socket | cut -d : -f 2`
 
 if [ ! -S ${AGENT_SOCK} ]; then
@@ -9,12 +7,10 @@ if [ ! -S ${AGENT_SOCK} ]; then
 fi
 export GPG_TTY=$(tty)
 
-# Set SSH to use gpg-agent if it's enabled, and we've set config 
-zstyle -b :omz:plugins:gpg-agent ssh-socket _gpg_ssh_socket
-
-if [[ $_gpg_ssh_socket == "yes" && -S "${AGENT_SOCK}.ssh" ]]; then
+# Set SSH to use gpg-agent if it's enabled, and we're not using the ssh-agent plugin
+echo "$plugins" | fgrep -q "ssh-agent"
+if [[ $? -eq 1 && -S "${AGENT_SOCK}.ssh" ]]; then
   export SSH_AUTH_SOCK="${AGENT_SOCK}.ssh"
   unset SSH_AGENT_PID
 fi
 
-unset _gpg_ssh_socket
