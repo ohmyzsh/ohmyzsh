6e0343afab6eef47a25df774bffd1f29226ad2c0
Chris Fleming <chflemin@chflemin-virtualbox.edi.is.keysight.com>
2018-04-26 17:16:51 +0100

    z-style configuration for enabling ssh

--- plugins/gpg-agent/gpg-agent.plugin.zsh
+++ plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,5 +1,6 @@
 # Enable gpg-agent if it is not running-
 # --use-standard-socket will work from version 2 upwards
+typeset _gpg_ssh_socket
 
 AGENT_SOCK=`gpgconf --list-dirs | grep agent-socket | cut -d : -f 2`
 
@@ -8,9 +9,12 @@ if [ ! -S ${AGENT_SOCK} ]; then
 fi
 export GPG_TTY=$(tty)
 
-# Set SSH to use gpg-agent if it's enabled
-if [ -S "${AGENT_SOCK}.ssh" ]; then
+# Set SSH to use gpg-agent if it's enabled, and we've set config 
+zstyle -b :omz:plugins:gpg-agent ssh-socket _gpg_ssh_socket
+
+if [[ $_gpg_ssh_socket == "yes" && -S "${AGENT_SOCK}.ssh" ]]; then
   export SSH_AUTH_SOCK="${AGENT_SOCK}.ssh"
   unset SSH_AGENT_PID
 fi
 
+unset _gpg_ssh_socket
