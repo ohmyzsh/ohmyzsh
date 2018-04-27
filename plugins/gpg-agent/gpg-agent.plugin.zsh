61497a3dfda96ef33958cda9749be8b622d27cdd
Marc Cornellà <marc.cornella@live.com>
2018-04-27 17:02:29 +0200

    Use idiomatic zsh and fix general style

--- plugins/gpg-agent/gpg-agent.plugin.zsh
+++ plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,16 +1,14 @@
 # Enable gpg-agent if it is not running-
 # --use-standard-socket will work from version 2 upwards
-AGENT_SOCK=`gpgconf --list-dirs | grep agent-socket | cut -d : -f 2`
+AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
 
-if [ ! -S ${AGENT_SOCK} ]; then
-  gpg-agent --daemon --use-standard-socket >/dev/null 2>&1
+if [[ ! -S "$AGENT_SOCK" }]; then
+  gpg-agent --daemon --use-standard-socket &>/dev/null
 fi
-export GPG_TTY=$(tty)
+export GPG_TTY=$TTY
 
 # Set SSH to use gpg-agent if it's enabled, and we're not using the ssh-agent plugin
-echo "$plugins" | fgrep -q "ssh-agent"
-if [[ $? -eq 1 && -S "${AGENT_SOCK}.ssh" ]]; then
-  export SSH_AUTH_SOCK="${AGENT_SOCK}.ssh"
+if [[ ${+plugins[(r)ssh-agent} -ne 0 && -S "$AGENT_SOCK.ssh" ]]; then
+  export SSH_AUTH_SOCK="$AGENT_SOCK.ssh"
   unset SSH_AGENT_PID
 fi
-
