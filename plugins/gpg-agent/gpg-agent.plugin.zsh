54a59d1189bb104b489774c90c9624e25877788e
Marc Cornellà <marc.cornella@live.com>
2018-04-27 17:56:21 +0200

    Revert to checking if `enable-ssh-support` is set
    Fixes #6772

--- plugins/gpg-agent/gpg-agent.plugin.zsh
+++ plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,16 +1,16 @@
 # Enable gpg-agent if it is not running-
 # --use-standard-socket will work from version 2 upwards
 
-AGENT_SOCK=`gpgconf --list-dirs | grep agent-socket | cut -d : -f 2`
+AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
 
-if [ ! -S ${AGENT_SOCK} ]; then
-  gpg-agent --daemon --use-standard-socket >/dev/null 2>&1
+if [[ ! -S $AGENT_SOCK ]]; then
+  gpg-agent --daemon --use-standard-socket &>/dev/null
 fi
-export GPG_TTY=$(tty)
+export GPG_TTY=$TTY
 
 # Set SSH to use gpg-agent if it's enabled
-if [ -S "${AGENT_SOCK}.ssh" ]; then
-  export SSH_AUTH_SOCK="${AGENT_SOCK}.ssh"
+GNUPGCONFIG="${GNUPGHOME:-"$HOME/.gnupg"}/gpg-agent.conf"
+if [[ -r $GNUPGCONFIG ]] && command grep -q enable-ssh-support "$GNUPGCONFIG"; then
+  export SSH_AUTH_SOCK="$AGENT_SOCK.ssh"
   unset SSH_AGENT_PID
 fi
-
