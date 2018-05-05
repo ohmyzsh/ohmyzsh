c4981bae0a91ce6d4b0717558342584cc6491199
Marc Cornellà <marc.cornella@live.com>
2018-05-05 21:27:48 +0200

    installer: check if zsh in path instead of /etc/shells
    Fixes #4955
    
    Closes #5931
    Closes #6398
    
    Co-authored-by: Void <vst4rbot@gmail.com>
    Co-authored-by: Kaleb Elwert <belak@coded.io>

--- tools/install.sh
+++ tools/install.sh
@@ -24,12 +24,10 @@ main() {
   # which may fail on systems lacking tput or terminfo
   set -e
 
-  CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
-  if [ ! $CHECK_ZSH_INSTALLED -ge 1 ]; then
+  if command -v zsh >/dev/null 2>&1; then
     printf "${YELLOW}Zsh is not installed!${NORMAL} Please install zsh first!\n"
     exit
   fi
-  unset CHECK_ZSH_INSTALLED
 
   if [ ! -n "$ZSH" ]; then
     ZSH=~/.oh-my-zsh
