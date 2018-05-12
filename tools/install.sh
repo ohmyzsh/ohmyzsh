da4977f60e69c39108dc8290c56ece44a7064b92
anandvijaysingh <avsmips@gmail.com>
2018-05-12 21:14:34 +0530

    Update install.sh
    Correction in user-default-shell

--- tools/install.sh
+++ tools/install.sh
@@ -84,7 +84,7 @@ main() {
     # If this platform provides a "chsh" command (not Cygwin), do it, man!
     if hash chsh >/dev/null 2>&1; then
       printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
+      chsh -s $(grep /zsh$ /etc/shells | tail -1) $(whoami)
     # Else, suggest the user do so manually.
     else
       printf "I can't change your shell automatically because this system does not have chsh.\n"
