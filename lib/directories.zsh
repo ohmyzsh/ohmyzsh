a32dec8bd4649e39ff51fe56f47568a6d92ae78f
Kevin Potts <fancydude@Kevins-MacBook-Pro.local>
2018-06-16 08:39:50 -0600

    Added some help and an example to the directories folder for nOObs like myself to get going with using alias

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -1,4 +1,15 @@
 # Changing/making/removing directory
+# Example of using an alias to automate the creation of a basic
+# environment as follows:
+# alias -g makestuff='mkdir dev; touch dev/app.js dev/index.html dev/style.css; cd dev; subl . '
+# The -g means 'global'; so this will work while located in any directory.
+# 'makestuff' is the alias; meaning if you type makestuff into zsh, the rest of the
+# code is executed.
+# Everything within the ' ' is what we want executed. If you're unsure of the syntax, feel free
+# to test some things out in the terminal, as the syntax is the same. Happy customizing! 
+# Also feel free to uncomment (delete #) on the makestuff alias and see how it works for you.
+# Note: the subl . at the end is to open sublime. That will have to be changed to open the text editor
+# of your preference if you're not on sublime.
 setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
