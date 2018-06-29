787c6899d4db04b3e6cef9a02c6a2ff3d896ece3
Marc Cornellà <marc.cornella@live.com>
2018-06-29 17:50:32 +0200

    rand-quote: update URL
    Fixes #6949

--- plugins/rand-quote/rand-quote.plugin.zsh
+++ plugins/rand-quote/rand-quote.plugin.zsh
@@ -1,4 +1,4 @@
-# Get a random quote fron the site http://www.quotationspage.com/random.php3
+# Get a random quote fron the site http://www.quotationspage.com/random.php
 # Created by Eduardo San Martin Morote aka Posva
 # http://posva.github.io
 # Sun Jun 09 10:59:36 CEST 2013 
@@ -13,7 +13,7 @@ END_COLOR="\e[m"
 if [[ -x `which curl` ]]; then
     function quote()
     {
-        Q=$(curl -s --connect-timeout 2 "http://www.quotationspage.com/random.php3" | iconv -c -f ISO-8859-1 -t UTF-8 | grep -m 1 "dt ")
+        Q=$(curl -s --connect-timeout 2 "http://www.quotationspage.com/random.php" | iconv -c -f ISO-8859-1 -t UTF-8 | grep -m 1 "dt ")
         TXT=$(echo "$Q" | sed -e 's/<\/dt>.*//g' -e 's/.*html//g' -e 's/^[^a-zA-Z]*//' -e 's/<\/a..*$//g')
         W=$(echo "$Q" | sed -e 's/.*\/quotes\///g' -e 's/<.*//g' -e 's/.*">//g')
         if [ "$W" -a "$TXT" ]; then
