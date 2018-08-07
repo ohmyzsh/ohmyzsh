c781d708da064b42af19e20113d042b65e886d94
Marc Cornellà <marc.cornella@live.com>
2018-08-08 00:05:34 +0200

    dotenv: test and warn of incorrect.env syntax
    Fixes #6337

--- plugins/dotenv/dotenv.plugin.zsh
+++ plugins/dotenv/dotenv.plugin.zsh
@@ -1,5 +1,8 @@
 source_env() {
   if [[ -f .env ]]; then
+    # test .env syntax
+    zsh -fn .env || echo 'dotenv: error when sourcing `.env` file' >&2
+
     if [[ -o a ]]; then
       source .env
     else
