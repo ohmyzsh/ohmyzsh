1525e4c655639c153b35334b24d7e13cb41014cd
benwilcock <benwilcock@gmail.com>
2018-08-08 16:09:57 +0100

    Added a small selection of aliases and functions to simplify working with the cloudfoundry cli tool.

---
+++ plugins/cloudfoundry/cloudfoundry.plugin.zsh
@@ -0,0 +1,30 @@
+# Some Useful CloudFoundry Aliases & Functions
+alias cfl="cf login"
+alias cft="cf target"
+alias cfa="cf apps"
+alias cfs="cf services"
+alias cfm="cf marketplace"
+alias cfp="cf push"
+alias cfcs="cf create-service"
+alias cfbs="cf bind-service"
+alias cfus="cf unbind-service"
+alias cfds="cf delete-service"
+alias cfup="cf cups"
+alias cfp="cf push"
+alias cflg="cf logs"
+alias cfr="cf routes"
+alias cfe="cf env"
+alias cfsh="cf ssh"
+alias cfsc="cf scale"
+alias cfev="cf events"
+function cfh.() { export CF_HOME=$(pwd)/.cf }
+function cfh~() { export CF_HOME=~/.cf }
+function cfhu() { unset CF_HOME }
+function cfpm() { cf push -f $1 }
+function cflr() { cf logs $1 --recent }
+function cfsrt() { cf start $1 }
+function cfstp() { cf stop $1 }
+function cfstg() { cf restage $1 }
+function cfdel() { cf delete $1 }
+function cfsrtall() cf apps | awk '/stopped/ { system("cf start " $1)}'
+function cfstpall() cf apps | awk '/started/ { system("cf stop " $1)}'
