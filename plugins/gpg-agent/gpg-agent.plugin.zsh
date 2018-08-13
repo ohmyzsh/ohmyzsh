4c62852a78aa10e9d9aa8b2b2c25cd5e6fae8ab9
Dapeng Gao <peter@dpgao.cc>
2018-08-13 11:42:22 +0800

    Improvements to the gpg-agent plugin
    *   Get SSH_AUTH_SOCK directly from `gpgconf` and only when the current
    shell is not a child of `gpg-agent`.
    
    *   Update the agent's tty before each command execution to ensure that
    passphrase prompts are displayed in the correct tty.

--- plugins/gpg-agent/gpg-agent.plugin.zsh
+++ plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,16 +1,16 @@
-# Enable gpg-agent if it is not running-
-# --use-standard-socket will work from version 2 upwards
-
-AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
-
-if [[ ! -S $AGENT_SOCK ]]; then
-  gpg-agent --daemon --use-standard-socket &>/dev/null
-fi
 export GPG_TTY=$TTY
 
-# Set SSH to use gpg-agent if it's enabled
+gpg-connect-agent /bye &>/dev/null
+
 GNUPGCONFIG="${GNUPGHOME:-"$HOME/.gnupg"}/gpg-agent.conf"
 if [[ -r $GNUPGCONFIG ]] && command grep -q enable-ssh-support "$GNUPGCONFIG"; then
-  export SSH_AUTH_SOCK="$AGENT_SOCK.ssh"
   unset SSH_AGENT_PID
+  if [[ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]]; then
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+  fi
+  autoload -U add-zsh-hook
+  add-zsh-hook preexec _gpg-agent-update-tty
+  function _gpg-agent-update-tty {
+    gpg-connect-agent updatestartuptty /bye &>/dev/null
+  }
 fi
