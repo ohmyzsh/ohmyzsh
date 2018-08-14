b2dff9de9fb5e713fa1e5fc625b017f94710ad6a
youhealthy <tl1848@nyu.edu>
2018-08-14 08:26:31 -0400

    tony venv promt

--- plugins/virtualenv/virtualenv.plugin.zsh
+++ plugins/virtualenv/virtualenv.plugin.zsh
@@ -3,5 +3,10 @@ function virtualenv_prompt_info(){
   echo "${ZSH_THEME_VIRTUALENV_PREFIX:=[}${VIRTUAL_ENV:t}${ZSH_THEME_VIRTUALENV_SUFFIX:=]}"
 }
 
+function tony_virtualenv_prompt_info(){
+  [[ -n ${VIRTUAL_ENV} ]] || return
+  echo "${ZSH_THEME_VIRTUALENV_PREFIX:=__}${VIRTUAL_ENV:t}${ZSH_THEME_VIRTUALENV_SUFFIX:=__}"
+}
+
 # disables prompt mangling in virtual_env/bin/activate
 export VIRTUAL_ENV_DISABLE_PROMPT=1
