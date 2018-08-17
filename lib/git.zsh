7b84ae58e15cb54494070ea5cc341cde28f70501
Bruno Borges <bruno.borges@gmail.com>
2018-08-17 15:43:21 -0700

    Show which remote is set for upstream along with branch

--- lib/git.zsh
+++ lib/git.zsh
@@ -2,8 +2,8 @@
 function git_prompt_info() {
   local ref
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
-    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+    _tmp=$(command git symbolic-ref -q HEAD 2> /dev/null) || return 0
+    ref=$(command git for-each-ref --format='%(upstream:short)' ${_tmp} 2> /dev/null) || return 0
     echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
