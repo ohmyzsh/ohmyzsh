7d4e213a107647310b44a7b7934cdff3f7f97c8b
Bruno Borges <bruno.borges@gmail.com>
2018-08-31 15:54:43 -0700

    show branch when no remote

--- lib/git.zsh
+++ lib/git.zsh
@@ -2,9 +2,13 @@
 function git_prompt_info() {
   local ref
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
-    _tmp=$(command git symbolic-ref -q HEAD 2> /dev/null) || return 0
-    ref=$(command git for-each-ref --format='%(upstream:short)' ${_tmp} 2> /dev/null) || return 0
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    branch=$(command git symbolic-ref -q HEAD 2> /dev/null) || return 0
+    ref=$(command git for-each-ref --format='%(upstream:short)' ${branch} 2> /dev/null) || return 0
+    info=$(command echo "${ref#refs/heads/}")
+    if [[ "${info}" == "" ]]; then
+      info=$(command echo "<no-remote>/$(git rev-parse --abbrev-ref HEAD)" 2> /dev/null) || return 0
+    fi
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${info}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
 
