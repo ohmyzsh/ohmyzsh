3f972c4ecc524e76e8d0cef777eeed7b3e503933
Keith Yao <i@yaofur.com>
2018-09-17 11:14:29 +0200

    [nvm.plugin] speed-up nvm loading

--- plugins/nvm/nvm.plugin.zsh
+++ plugins/nvm/nvm.plugin.zsh
@@ -1,5 +1,9 @@
 # Set NVM_DIR if it isn't already defined
 [[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
 
-# Load nvm if it exists
-[[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+# Load nvm if it exists, but without use
+[[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh" --no-use 
+
+# Alias hack
+alias node='unalias node ; unalias npm ; nvm use default ; node $@'
+alias npm='unalias node ; unalias npm ; nvm use default ; npm $@'
