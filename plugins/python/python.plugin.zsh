c7d903b77e93f70849d7fdcbefc2a8831006c4dd
Batuhan's Unmaintained Account <batuhanosmantaskaya@gmail.com>
2018-09-23 12:12:27 -0400

    Remove Optional Static Type Checker's (mypy) Cache Files on `pyclean`
    Remove Optional Static Type Checker's (mypy) Cache Files on `pyclean`

--- plugins/python/python.plugin.zsh
+++ plugins/python/python.plugin.zsh
@@ -1,12 +1,13 @@
 # Find python file
 alias pyfind='find . -name "*.py"'
 
-# Remove python compiled byte-code in either current directory or in a
+# Remove python compiled byte-code and mypy cache in either current directory or in a
 # list of specified directories
 function pyclean() {
     ZSH_PYCLEAN_PLACES=${*:-'.'}
     find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
     find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
+    find ${ZSH_PYCLEAN_PLACES} -type d -name ".mypy_cache" -delete
 }
 
 # Grep among .py files
