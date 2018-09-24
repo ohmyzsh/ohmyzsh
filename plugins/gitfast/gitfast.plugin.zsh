7b3c15e16772ef6cbe23aa13dc1224499f240367
Marc Cornellà <marc.cornella@live.com>
2018-09-24 17:58:52 +0200

    gitfast: stop loading git plugin
    Also, use zsh parameter expansion to get the directory path of
    the plugin.

--- plugins/gitfast/gitfast.plugin.zsh
+++ plugins/gitfast/gitfast.plugin.zsh
@@ -1,8 +1,9 @@
-dir=$(dirname $0)
-source $dir/../git/git.plugin.zsh
-source $dir/git-prompt.sh
+dir="${0:A:h}"
+source "$dir/git-prompt.sh"
 
 function git_prompt_info() {
   dirty="$(parse_git_dirty)"
   __git_ps1 "${ZSH_THEME_GIT_PROMPT_PREFIX//\%/%%}%s${dirty//\%/%%}${ZSH_THEME_GIT_PROMPT_SUFFIX//\%/%%}"
 }
+
+unset dir
