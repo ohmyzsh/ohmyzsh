852a7a89388a23f95cacdfbad9e33f67231f6d7f
Marc Cornellà <marc.cornella@live.com>
2018-10-03 18:08:12 +0200

    fix wrong alias syntax

--- plugins/macports/macports.plugin.zsh
+++ plugins/macports/macports.plugin.zsh
@@ -1,8 +1,6 @@
-#Aliases
 alias pc="sudo port clean --all installed"
-alias pi="sudo port install $1"
+alias pi="sudo port install"
 alias psu="sudo port selfupdate"
 alias puni="sudo port uninstall inactive"
 alias puo="sudo port upgrade outdated"
 alias pup="psu && puo"
-
