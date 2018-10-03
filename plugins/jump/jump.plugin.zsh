d3f994e4f63d92d88879706f99544ab417abc8f1
Rubén Durán Balda <ruduran@users.noreply.github.com>
2018-10-03 22:41:11 +0200

    Fix coding style on the jump plugin
    Use two spaces instead of one tab

--- plugins/jump/jump.plugin.zsh
+++ plugins/jump/jump.plugin.zsh
@@ -9,49 +9,49 @@
 export MARKPATH=$HOME/.marks
 
 jump() {
-	cd -P "$MARKPATH/$1" 2>/dev/null || {echo "No such mark: $1"; return 1}
+  cd -P "$MARKPATH/$1" 2>/dev/null || {echo "No such mark: $1"; return 1}
 }
 
 mark() {
-	if [[ ( $# == 0 ) || ( "$1" == "." ) ]]; then
-		MARK=$(basename "$PWD")
-	else
-		MARK="$1"
-	fi
-	if read -q \?"Mark $PWD as ${MARK}? (y/n) "; then
-		mkdir -p "$MARKPATH"; ln -sfn "$PWD" "$MARKPATH/$MARK"
-	fi
+  if [[ ( $# == 0 ) || ( "$1" == "." ) ]]; then
+    MARK=$(basename "$PWD")
+  else
+    MARK="$1"
+  fi
+  if read -q \?"Mark $PWD as ${MARK}? (y/n) "; then
+    mkdir -p "$MARKPATH"; ln -sfn "$PWD" "$MARKPATH/$MARK"
+  fi
 }
 
 unmark() {
-	rm -i "$MARKPATH/$1"
+  rm -i "$MARKPATH/$1"
 }
 
 marks() {
-	for link in $MARKPATH/*(@); do
-		local markname="$fg[cyan]${link:t}$reset_color"
-		local markpath="$fg[blue]$(readlink $link)$reset_color"
-		printf "%s\t" $markname
-		printf -- "-> %s \t\n" $markpath
-	done
+  for link in $MARKPATH/*(@); do
+    local markname="$fg[cyan]${link:t}$reset_color"
+    local markpath="$fg[blue]$(readlink $link)$reset_color"
+    printf "%s\t" $markname
+    printf -- "-> %s \t\n" $markpath
+  done
 }
 
 _completemarks() {
-	if [[ $(ls "${MARKPATH}" | wc -l) -gt 1 ]]; then
-		reply=($(ls $MARKPATH/**/*(-) | grep : | sed -E 's/(.*)\/([_a-zA-Z0-9\.\-]*):$/\2/g'))
-	else
-		if readlink -e "${MARKPATH}"/* &>/dev/null; then
-			reply=($(ls "${MARKPATH}"))
-		fi
-	fi
+  if [[ $(ls "${MARKPATH}" | wc -l) -gt 1 ]]; then
+    reply=($(ls $MARKPATH/**/*(-) | grep : | sed -E 's/(.*)\/([_a-zA-Z0-9\.\-]*):$/\2/g'))
+  else
+    if readlink -e "${MARKPATH}"/* &>/dev/null; then
+      reply=($(ls "${MARKPATH}"))
+    fi
+  fi
 }
 compctl -K _completemarks jump
 compctl -K _completemarks unmark
 
 _mark_expansion() {
-	setopt extendedglob
-	autoload -U modify-current-argument
-	modify-current-argument '$(readlink "$MARKPATH/$ARG")'
+  setopt extendedglob
+  autoload -U modify-current-argument
+  modify-current-argument '$(readlink "$MARKPATH/$ARG")'
 }
 zle -N _mark_expansion
 bindkey "^g" _mark_expansion
