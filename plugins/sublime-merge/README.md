e5dc21f1444c0562e3b344f6c38ecd803057b296
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2018-10-04 16:19:41 +0200

    created sublime-merge.plugin.zsh based on sublime.plugin.zsh to alias `sm` for Sublime Merge

---
+++ plugins/sublime-merge/README.md
@@ -0,0 +1,17 @@
+## sublime
+
+Plugin for Sublime Merge, a cross platform text and code editor, available for Linux, Mac OS X, and Windows.
+
+### Requirements
+
+ * [Sublime Merge](https://www.sublimemerge.com)
+
+### Usage
+
+ * If `sm` command is called without an argument, launch Sublime Merge
+
+ * If `sm` is passed a directory, `cd` to it and open it in Sublime Merge
+
+ * If `ssm` command is called, it is equivalent to `sm .`, opening the current folder in Sublime Merge
+
+ * If `ssm` command is called, it is like `sudo sm`, opening the file or folder in Sublime Merge. Useful for editing system protected files.
---
+++ plugins/sublime-merge/sublime-merge.plugin.zsh
@@ -0,0 +1,53 @@
+# Sublime Merge Aliases
+
+() {
+
+	if [[ "$OSTYPE" == linux* ]]; then
+		local _sublime_linux_paths
+		_sublime_linux_paths=(
+			"$HOME/bin/sublime_merge"
+			"/opt/sublime_merge/sublime_merge"
+			"/usr/bin/sublime_merge"
+			"/usr/local/bin/sublime_merge"
+			"/usr/bin/sublime_merge"
+			)
+		for _sublime_merge_path in $_sublime_linux_paths; do
+			if [[ -a $_sublime_merge_path ]]; then
+				sm_run() { $_sublime_merge_path "$@" >/dev/null 2>&1 &| }
+				ssm_run_sudo() {sudo $_sublime_merge_path "$@" >/dev/null 2>&1}
+				alias ssm=ssm_run_sudo
+				alias sm=sm_run
+				break
+			fi
+		done
+	elif  [[ "$OSTYPE" = darwin* ]]; then
+		local _sublime_darwin_paths
+		_sublime_darwin_paths=(
+			"/usr/local/bin/sublime_merge"
+			"/Applications/Sublime Merge.app/Contents/MacOS/sublime_merge"
+			"$HOME/Applications/Sublime Merge.app/Contents/MacOS/sublime_merge"
+			)
+		for _sublime_merge_path in $_sublime_darwin_paths; do
+			if [[ -a $_sublime_merge_path ]]; then
+				subm () { "$_sublime_merge_path" "$@" }
+				alias sm=subm
+				break
+			fi
+		done
+	elif [[ "$OSTYPE" = 'cygwin' ]]; then
+		local sublime_merge_cygwin_paths
+		sublime_merge_cygwin_paths=(
+			"$(cygpath $ProgramW6432/Sublime\ Merge)/sublime_merge.exe"
+			)
+		for _sublime_merge_path in $_sublime_merge_cygwin_paths; do
+			if [[ -a $_sublime_merge_path ]]; then
+				subm () { "$_sublime_merge_path" "$@" }
+				alias sm=subm
+				break
+			fi
+		done
+	fi
+
+}
+
+alias smt='sm .'
