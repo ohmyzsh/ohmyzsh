5c91cfcb061aafbffe7b60f22c23a3ed580fdf2a
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-11-01 15:29:05 +0100

    capistrano: add README (#7376)

--- plugins/capistrano/capistrano.plugin.zsh
+++ plugins/capistrano/capistrano.plugin.zsh
@@ -1,7 +1,7 @@
-# Added `shipit` because `cap` is a reserved word. `cap` completion doesn't work.
+# Added `capit` because `cap` is a reserved word. `cap` completion doesn't work.
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Modules.html#The-zsh_002fcap-Module
 
-func capit() {
+function capit() {
   if [ -f Gemfile ]
   then
     bundle exec cap $*
