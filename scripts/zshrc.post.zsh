f193f30d147dc78546701a999c1f453bc5e0bd80
bluekrow <ndioses@gmail.com>
2018-11-02 22:53:53 +0100

    Added funcs and aliases for dev tools

--- scripts/zshrc.post.zsh
+++ scripts/zshrc.post.zsh
@@ -1,11 +1,31 @@
 #####ALIASES#######
+#DIRS
+alias cdhome="cd ~/"
+#DEV
 alias gdiff="git difftool -y --no-symlinks"
 alias gdiffdir="git difftool -d -y --no symlinks"
+alias vs="vstudio"
+alias vc="vcode"
+
+
 ####FUNCTIONS#######
-function bc() {
-    "/mnt/c/Program Files/Beyond Compare 4/BCompare.exe" "`echo $1 | sed 's_/mnt/c_C:_'`" "`echo $2 | sed 's_/mnt/c_C:_'`"
-}
 function test()
 {
-    echo $(pwd)/$1
+    echo $(pwd -P)/$1 | sed 's_/mnt/d_D:_'
+    echo $(pwd -P)/$1 | sed 's_/mnt/\([:alpha:]\)_\1:_'
+    echo $(pwd -P)/$1 | sed -r 's_/mnt/([[:alpha:]])_\u\1:_'
+}
+
+function bc() {
+    /mnt/c/Program\ Files/Beyond\ Compare\ 4/BCompare.exe `echo $1 | sed 's_/mnt/d_D:_'` `echo $2 | sed 's_/mnt/d_D:_'`
+}
+
+function vstudio()
+{
+    (/mnt/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/Common7/IDE/devenv.exe `echo $(pwd -P)/$1 | sed -r 's_/mnt/([[:alpha:]])_\u\1:_'` >/dev/null 2>&1 &)
+}
+
+function vcode()
+{
+    (/mnt/c/Users/Nino/AppData/Local/Programs/Microsoft\ VS\ Code/Code.exe `echo $(pwd -P)/$1 | sed -r 's_/mnt/([[:alpha:]])_\u\1:_'` >/dev/null 2>&1 &)
 }
--- scripts/zshrc.pre.zsh
+++ scripts/zshrc.pre.zsh
@@ -1,8 +1,10 @@
 #####CONFIGURATIONS#######
 DEFAULT_USER="raven"
-eval `dircolors ~/dircolors`
+eval `dircolors ~/.dircolors`
 ZSH_DISABLE_COMPFIX=true
 #####EXPORTS#######
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 export TMPDIR='/mnt/c/Users/Nino/AppData/Local/Temp'
 export EDITOR='nano'
+#####OPTIONS#######
+unsetopt BG_NICE
