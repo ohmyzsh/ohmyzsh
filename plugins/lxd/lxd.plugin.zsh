01aa9416c0c16070053b276c393d51499783334a
Renato dos Santos <renato.santos@wplex.com.br>
2018-12-03 17:19:37 -0200

    add subcommads

--- plugins/lxd/lxd.plugin.zsh
+++ plugins/lxd/lxd.plugin.zsh
@@ -2,6 +2,10 @@ _lxc_get_command_list () {
     $_comp_command1 | sed "1,/Available Commands/d" | awk '/^[ \t]*[a-z]+/ { print $1 }'
 }
 
+_lxc_get_subcommand_list () {
+    $_comp_command1 ${words[2]} | sed "1,/Available Commands/d" | awk '/^[ \t]*[a-z]+/ { print $1 }'
+}
+
 _lxc () {
   local curcontext="$curcontext" state line
   typeset -A opt_args
@@ -14,12 +18,9 @@ _lxc () {
       compadd $(_lxc_get_command_list)
       ;;
     *)
-      compadd "nothing.."
+        compadd $(_lxc_get_subcommand_list)
       ;;
   esac
 }
 
 compdef _lxc lxc
-
-$(_lxc_get_command_list )
-echo $state
