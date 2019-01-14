7cd9342dce5d53618002960aa8ba47c760bba172
Bruno Borges <bruno.borges@microsoft.com>
2019-01-14 20:55:23 +0800

    merged

--- lib/git.zsh
+++ lib/git.zsh
@@ -75,13 +75,16 @@ function git_remote_status() {
 # it's not a symbolic ref, but in a Git repo.
 function git_current_branch() {
   local ref
-  ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null)
+  #ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null)
+  _tmp=$(command git symbolic-ref -q HEAD)
+  ref=$(command git for-each-ref --format='%(upstream:short)' ${_tmp})
   local ret=$?
   if [[ $ret != 0 ]]; then
     [[ $ret == 128 ]] && return  # no git repo.
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
   fi
-  echo ${ref#refs/heads/}
+  #echo ${ref#refs/heads/}
+  echo ${ref}
 }
 
 
