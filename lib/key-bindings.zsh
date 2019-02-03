070949570766f8d21a8003d5ec29162a4171d56f
Gregg Tavares <github@greggman.com>
2019-02-03 18:15:14 +0900

    example fix for curso up/down consistency issue

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -31,13 +31,15 @@ fi
 if [[ "${terminfo[kcuu1]}" != "" ]]; then
   autoload -U up-line-or-beginning-search
   zle -N up-line-or-beginning-search
-  bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+  #bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+  bindkey "^[[A" up-line-or-beginning-search
 fi
 # start typing + [Down-Arrow] - fuzzy find history backward
 if [[ "${terminfo[kcud1]}" != "" ]]; then
   autoload -U down-line-or-beginning-search
   zle -N down-line-or-beginning-search
-  bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+  #bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+  bindkey "^[[B" up-line-or-beginning-search
 fi
 
 if [[ "${terminfo[khome]}" != "" ]]; then
