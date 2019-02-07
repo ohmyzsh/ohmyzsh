1a4b5463a0daf1f1498273a7ecf94d8e49e1bd91
Peter Theill <peter@theill.com>
2019-02-07 10:20:45 +0100

    Add Givero (supports good causes) as search engine
    Will add "givero" as a keyword for searching Givero, a relative new search engine donating revenue to good causes around the world.

--- plugins/web-search/web-search.plugin.zsh
+++ plugins/web-search/web-search.plugin.zsh
@@ -17,6 +17,7 @@ function web_search() {
     ecosia      "https://www.ecosia.org/search?q="
     goodreads   "https://www.goodreads.com/search?q="
     qwant       "https://www.qwant.com/?q="
+    givero      "https://www.givero.com/search?q="
   )
 
   # check whether the search engine is supported
@@ -51,6 +52,7 @@ alias baidu='web_search baidu'
 alias ecosia='web_search ecosia'
 alias goodreads='web_search goodreads'
 alias qwant='web_search qwant'
+alias givero='web_search givero'
 
 #add your own !bang searches here
 alias wiki='web_search duckduckgo \!w'
