1cde46cccbd40ab0c0667f543e14f38507f6d468
Gaetan Semet <gaetan@xeberon.net>
2019-02-14 00:28:05 +0100

    rework subliminal dl

--- custom/plugins/subliminal/subliminal.plugin.zsh
+++ custom/plugins/subliminal/subliminal.plugin.zsh
@@ -1,6 +1,8 @@
 
-OPENSUBTITLES_USERNAME=gaetan@xeberon.net
-OPENSUBTITLE_PASSWORD=stibbons
-ADDIC7ED_USERNAME=donatelo.michelangelo@gmail.com
-ADDIC7ED_PASSWORD=stibbons
-alias subliminal-dl-en="subliminal --addic7ed $ADDIC7ED_USERNAME $ADDIC7ED_PASSWORD --opensubtitles $OPENSUBTITLES_USERNAME $OPENSUBTITLE_PASSWORD  download --language eng --language fra "
+# Add the following lines in your .profile:
+
+# export OPENSUBTITLES_USERNAME=xxx
+# export OPENSUBTITLES_PASSWORD=****
+# export ADDIC7ED_USERNAME=xxx
+# export ADDIC7ED_PASSWORD=***
+# alias subliminal-dl-en="subliminal --addic7ed $ADDIC7ED_USERNAME $ADDIC7ED_PASSWORD --opensubtitles $OPENSUBTITLES_USERNAME $OPENSUBTITLE_PASSWORD  download --language eng --language fra "
