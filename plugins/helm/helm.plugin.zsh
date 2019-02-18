239938d3618c5e022faf98920ab8a0ae60718fcf
Pavel Dmytrenko <mail@pavdmyt.com>
2019-02-18 12:10:27 +0200

    Fix helm completion

--- plugins/helm/helm.plugin.zsh
+++ plugins/helm/helm.plugin.zsh
@@ -3,5 +3,5 @@
 # Copy from kubectl : https://github.com/pstadler
 
 if [ $commands[helm] ]; then
-  source <(helm completion zsh)
+  source <(helm completion zsh | sed -E 's/\["(.+)"\]/\[\1\]/g')
 fi
