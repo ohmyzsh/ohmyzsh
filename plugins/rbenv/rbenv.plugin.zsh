30f5ec283c26bd5430539aa4886a1d38b0673748
Oleg Smelov <OlegSmelov@users.noreply.github.com>
2019-03-19 17:05:22 +0200

    Speed up rbenv plugin

--- plugins/rbenv/rbenv.plugin.zsh
+++ plugins/rbenv/rbenv.plugin.zsh
@@ -1,57 +1,56 @@
-_homebrew-installed() {
-  type brew &> /dev/null
-}
+FOUND_RBENV=$+commands[rbenv]
 
-FOUND_RBENV=0
-rbenvdirs=("$HOME/.rbenv" "/usr/local/rbenv" "/opt/rbenv" "/usr/local/opt/rbenv")
-if _homebrew-installed && rbenv_homebrew_path=$(brew --prefix rbenv 2>/dev/null); then
-    rbenvdirs=($rbenv_homebrew_path "${rbenvdirs[@]}")
-    unset rbenv_homebrew_path
-    if [[ $RBENV_ROOT = '' ]]; then 
-      RBENV_ROOT="$HOME/.rbenv"
+if [[ $FOUND_RBENV -ne 1 ]]; then
+  rbenvdirs=("$HOME/.rbenv" "/usr/local/rbenv" "/opt/rbenv" "/usr/local/opt/rbenv")
+  for dir in $rbenvdirs; do
+    if [[ -d $dir/bin ]]; then
+      export PATH="$PATH:$dir/bin"
+      FOUND_RBENV=1
+      break
     fi
+  done
 fi
 
-for rbenvdir in "${rbenvdirs[@]}" ; do
-  if [ -d $rbenvdir/bin -a $FOUND_RBENV -eq 0 ] ; then
-    FOUND_RBENV=1
-    if [[ $RBENV_ROOT = '' ]]; then
-      RBENV_ROOT=$rbenvdir
+if [[ $FOUND_RBENV -ne 1 ]]; then
+  if (( $+commands[brew] )) && dir=$(brew --prefix rbenv 2>/dev/null); then
+    if [[ -d $dir/bin ]]; then
+      export PATH="$PATH:$dir/bin"
+      FOUND_RBENV=1
     fi
-    export RBENV_ROOT
-    export PATH=${rbenvdir}/bin:$PATH
-    eval "$(rbenv init --no-rehash - zsh)"
-
-    alias rubies="rbenv versions"
-    alias gemsets="rbenv gemset list"
-
-    function current_ruby() {
-      echo "$(rbenv version-name)"
-    }
-
-    function current_gemset() {
-      echo "$(rbenv gemset active 2&>/dev/null | sed -e ":a" -e '$ s/\n/+/gp;N;b a' | head -n1)"
-    }
-
-    function gems {
-      local rbenv_path=$(rbenv prefix)
-      gem list $@ | sed -E \
-        -e "s/\([0-9a-z, \.]+( .+)?\)/$fg[blue]&$reset_color/g" \
-        -e "s|$(echo $rbenv_path)|$fg[magenta]\$rbenv_path$reset_color|g" \
-        -e "s/$current_ruby@global/$fg[yellow]&$reset_color/g" \
-        -e "s/$current_ruby$current_gemset$/$fg[green]&$reset_color/g"
-    }
-
-    function rbenv_prompt_info() {
-      if [[ -n $(current_gemset) ]] ; then
-        echo "$(current_ruby)@$(current_gemset)"
-      else
-        echo "$(current_ruby)"
-      fi
-    }
   fi
-done
-unset rbenvdir
+fi
+
+if [[ $FOUND_RBENV -eq 1 ]]; then
+  eval "$(rbenv init --no-rehash - zsh)"
+
+  alias rubies="rbenv versions"
+  alias gemsets="rbenv gemset list"
+
+  function current_ruby() {
+    echo "$(rbenv version-name)"
+  }
+
+  function current_gemset() {
+    echo "$(rbenv gemset active 2&>/dev/null | sed -e ":a" -e '$ s/\n/+/gp;N;b a' | head -n1)"
+  }
+
+  function gems {
+    local rbenv_path=$(rbenv prefix)
+    gem list $@ | sed -E \
+      -e "s/\([0-9a-z, \.]+( .+)?\)/$fg[blue]&$reset_color/g" \
+      -e "s|$(echo $rbenv_path)|$fg[magenta]\$rbenv_path$reset_color|g" \
+      -e "s/$current_ruby@global/$fg[yellow]&$reset_color/g" \
+      -e "s/$current_ruby$current_gemset$/$fg[green]&$reset_color/g"
+  }
+
+  function rbenv_prompt_info() {
+    if [[ -n $(current_gemset) ]] ; then
+      echo "$(current_ruby)@$(current_gemset)"
+    else
+      echo "$(current_ruby)"
+    fi
+  }
+fi
 
 if [ $FOUND_RBENV -eq 0 ] ; then
   alias rubies='ruby -v'
