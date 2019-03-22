ae6e643befbd3f6dff665141b3d09e08bd9a4d58
Nico Sommi <nicosommi@gmail.com>
2019-03-22 10:50:12 -0300

    Add passphrase remembering feature helper function

--- custom/dev.zsh
+++ custom/dev.zsh
@@ -2,3 +2,10 @@ workspace=~/workspace
 ide=code
 
 alias ws="cd $workspace"
+
+remember-passphrase-per-session() {
+  # TODO: copy to clipboard for macos and linux instead, but don't automate
+  echo "Add this at the first line in with the editor: AddKeysToAgent yes"
+  read -s -k '?Press any key to continue.'
+  $ide ~/.ssh/config
+}
