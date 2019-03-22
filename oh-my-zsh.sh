ohmyzsh/ohmyzsh

commit 779ff24c2d0091ebbddae938d12b3f117123fc29

init: notify user if plugin isn't found (#7679)

oh-my-zsh.sh
@@ -83,6 +83,8 @@ for plugin ($plugins); do
     source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
   elif [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
     source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+  else
+    echo "Warning: plugin $plugin not found"
   fi
 done
 

commit 3daae246351920367b2db2bf5661750dada73fb1

init: fix erroneous plugin not found warning fix: #7685 bug introduced in #7679

oh-my-zsh.sh
@@ -83,7 +83,8 @@ for plugin ($plugins); do
     source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
   elif [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
     source $ZSH/plugins/$plugin/$plugin.plugin.zsh
-  else
+  elif ! [ -f $ZSH/plugins/$plugin/_$plugin ] \
+    && ! [ -f $ZSH_CUSTOM/plugins/$plugin/_$plugin ]; then
     echo "Warning: plugin $plugin not found"
   fi
 done