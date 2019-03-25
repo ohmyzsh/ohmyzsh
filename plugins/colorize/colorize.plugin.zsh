1e17bec5b4477597f6a5a4391a8a830fcd4abee2
Francisco Zuviría <FranciscodeZuviria@fusap.com.ar>
2019-03-24 22:31:31 -0300

    plugin.colorize new-feature: cless   cless is an alias for a colorized less wrappper: colorize_via_pygmentize_less   note that cless opens many files as independent files, navigatable with :n and :p   I rearanged README.md so that the colorized behavior is not in the usage area
      bugfix: quoting on "$@"
        (I know zsh expands this correctly by default, but not with my shellopts :D)

--- plugins/colorize/colorize.plugin.zsh
+++ plugins/colorize/colorize.plugin.zsh
@@ -1,7 +1,11 @@
 # easier alias to use the plugin
 alias ccat='colorize_via_pygmentize'
+alias cless='colorize_via_pygmentize_less'  
 
 colorize_via_pygmentize() {
+    # # I have been having problems with pygmentize signal handling
+    # trap 'exit' TERM INT EXIT;  # keep this at hand
+
     if ! (( $+commands[pygmentize] )); then
         echo "package 'Pygments' is not installed!"
         return 1
@@ -16,7 +20,7 @@ colorize_via_pygmentize() {
     # guess lexer from file extension, or
     # guess it from file contents if unsuccessful
     local FNAME lexer
-    for FNAME in $@
+    for FNAME in "$@"
     do
         lexer=$(pygmentize -N "$FNAME")
         if [[ $lexer != text ]]; then
@@ -26,3 +30,25 @@ colorize_via_pygmentize() {
         fi
     done
 }
+
+colorize_via_pygmentize_less() (   
+    # this function is a subshell so tmp_files can be shared to cleanup function
+    declare -a tmp_files 
+    
+    cleanup () {
+        [[ "${#tmp_files[@]}" != "0" ]] && rm -f "${tmp_files[@]}"
+        if [ -n "$1" ]; then kill -$1 $$; fi #signal propagation
+    }
+    trap 'cleanup' EXIT
+    trap 'cleanup HUP' HUP
+    trap 'cleanup TERM' TERM
+    trap 'cleanup INT' INT
+    
+    while (( $# != 0 )); do     #TODO: filter out less opts
+        tmp_file="$(mktemp --tmpdir tmp.colorize.XXXX.$( sed -E 's/[^a-Z0-9]/./g' <<< "$1"))"
+        tmp_files+=("$tmp_file")
+        colorize_via_pygmentize "$1" > "$tmp_file"
+        shift 1;
+    done
+    less -f "${tmp_files[@]}"
+)
