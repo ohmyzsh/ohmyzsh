31d9482b20ca9cb4bce275dc37102af973a51ac5
Gaetan Semet <gaetan@xeberon.net>
2019-04-03 11:01:32 +0200

    add path

--- templates/zshrc-linux.zsh
+++ templates/zshrc-linux.zsh
@@ -110,11 +110,20 @@ if [ -d "$HOME/.local/bin/" ]; then
     export PATH="$HOME/.local/bin/:$PATH"
 fi
 
-if [ -d "$HOME/.pyenv/bin" ]; then
-    export PATH="$HOME/.pyenv/bin:$PATH"
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
+if [ -d "$HOME/.yarn/bin/" ]; then
+    export PATH="$HOME/.yarn/bin/:$PATH"
 fi
+
+if [ -d "$HOME/.nvm/" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+fi
+
+# if [ -d "$HOME/.pyenv/bin" ]; then
+#     export PATH="$HOME/.pyenv/bin:$PATH"
+#     eval "$(pyenv init -)"
+#     eval "$(pyenv virtualenv-init -)"
+# fi
 
 # added by travis gem
 [ -f $HOME/.travis/travis.sh ] && source $HOME/.travis/travis.sh
