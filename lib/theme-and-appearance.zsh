30594886bfebefa0c7706dc82f533735d3a58e4b
Marc Cornellà <marc.cornella@live.com>
2019-04-08 23:01:55 +0200

    lib: delete PS1 default
    A default PS1 doesn't make sense: the user will either choose a theme or not
    care about a default prompt since they can set it themselves.
    
    Fixes #7054

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -45,9 +45,6 @@ setopt prompt_subst
 
 [[ -n "$WINDOW" ]] && SCREEN_NO="%B$WINDOW%b " || SCREEN_NO=""
 
-# Apply theming defaults
-PS1="%n@%m:%~%# "
-
 # git theming default: Variables for theming the git info prompt
 ZSH_THEME_GIT_PROMPT_PREFIX="git:("         # Prefix at the very beginning of the prompt, before the branch name
 ZSH_THEME_GIT_PROMPT_SUFFIX=")"             # At the very end of the prompt
