8d7ce18e121b4fd2768906840cd5d38e5ecc148c
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2019-04-09 23:10:13 +0200

    vi-mode: remove TRAPWINCH from plugin (#7753)
    * Remove TRAPWINCH; it's unrelated to vi-mode
    
    * Update prompt with ad hoc KEYMAP variable
      Fixes mode indicator when the terminal window is resized

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -1,12 +1,10 @@
 # Updates editor information when the keymap changes.
 function zle-keymap-select() {
+  # update keymap variable for the prompt
+  VI_KEYMAP=$KEYMAP
+
   zle reset-prompt
   zle -R
-}
-
-# Ensure that the prompt is redrawn when the terminal size changes.
-TRAPWINCH() {
-  zle && { zle -R; zle reset-prompt }
 }
 
 zle -N zle-keymap-select
@@ -42,7 +40,7 @@ if [[ "$MODE_INDICATOR" == "" ]]; then
 fi
 
 function vi_mode_prompt_info() {
-  echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
+  echo "${${VI_KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
 }
 
 # define right prompt, if it wasn't defined by a theme
