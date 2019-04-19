c77ce189cd7cd078d74cd3c93d17d0c19814ff8f
Se De <Sede7473@gmail.com>
2019-04-19 07:37:51 +0000

    make necessary variables local and add proper escaping of user input

--- plugins/alias-finder/alias-finder.plugin.zsh
+++ plugins/alias-finder/alias-finder.plugin.zsh
@@ -1,34 +1,35 @@
 alias-finder() {
   for i in $@; do
     case $i in
-      -e|--exact) exact=true;;
-      -r|--relaxed) relaxed=true;;
+      -e|--exact) local exact=true;;
+      -r|--relaxed) local relaxed=true;;
       *) 
         if [[ ! -z $cmd ]]; then
-          cmd="$cmd $i"
+          local cmd="$cmd $i"
         else
-          cmd=$i
+          local cmd=$i
         fi
         ;;
     esac
   done
-  if [[ $cmd != *\[*\]* ]] && [[ $cmd != *\n* ]] && [[ $cmd != *\\* ]]; then
+  cmd=$(sed 's/[].\|$(){}?+*^[]/\\&/g' <<< $cmd)
+  if [[ $cmd != *\n* ]]; then
     while [[ $cmd != "" ]]; do
       if [[ $relaxed = true ]]; then
-        wordStart="'{0,1}"
+        local wordStart="'{0,1}"
       else
-        multiWordEnd="'$"
+        local multiWordEnd="'$"
       fi
-      if echo $cmd | grep '^[^\ ]*$' > /dev/null; then
-        finder=$wordStart$cmd
+      if echo $cmd | grep '^[^ ]*$' > /dev/null; then
+        local finder=$wordStart$cmd
       else
-        einder="'$cmd$multiWordEnd"
+        local finder="'$cmd$multiWordEnd"
       fi
       alias | grep -E "=$finder"
-      if [[ $strict = true ]]; then
+      if [[ $exact = true || $relaxed = true ]]; then
         break
       else
-        cmd=$(echo $cmd | sed -E 's/\ {0,1}[^\ ]*$//') # deletes last word
+        cmd=$(sed -E 's/ {0,1}[^ ]*$//' <<< $cmd) # deletes last word
       fi
     done
   fi
