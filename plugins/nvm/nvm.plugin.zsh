762935e3a3b39a742ad7b79d3731fdc9e3d1c780
Chigozirim C <nobleman.code@gmx.com>
2019-04-27 01:38:14 -0600

    Update nvm.plugin.zsh
    Check the $XDG_CONFIG_HOME/nvm path which is also a valid location to install nvm

--- plugins/nvm/nvm.plugin.zsh
+++ plugins/nvm/nvm.plugin.zsh
@@ -1,6 +1,11 @@
 # Set NVM_DIR if it isn't already defined
 [[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
 
+# https://github.com/nvm-sh/nvm#installation-and-update
+if [[ ! -d "$NVM_DIR/" && -d "${XDG_CONFIG_HOME:-$HOME/.config}/nvm/" ]]; then
+	export NVM_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/nvm"
+fi
+
 # Try to load nvm only if command not already available
 if ! type "nvm" &> /dev/null; then
     # Load nvm if it exists
