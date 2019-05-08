0ef54cb3cb1f17319783b2fd62ed2b04226cbd69
Marc Cornellà <marc.cornella@live.com>
2019-05-08 16:30:50 +0200

    Reuse VSCODE variable

--- plugins/vscode/vscode.plugin.zsh
+++ plugins/vscode/vscode.plugin.zsh
@@ -1,21 +1,22 @@
 # VScode zsh plugin
 # author: https://github.com/MarsiBarsi
 
-local code=${VSCODE:-'code'}
+# Use main Visual Studio Code version by default
+: ${VSCODE:=code}
 
-alias vsc="$code ."
-alias vsca="$code --add"
-alias vscd="$code --diff"
-alias vscg="$code --goto"
-alias vscn="$code --new-window"
-alias vscr="$code --reuse-window"
-alias vscw="$code --wait"
-alias vscu="$code --user-data-dir"
+alias vsc="$VSCODE ."
+alias vsca="$VSCODE --add"
+alias vscd="$VSCODE --diff"
+alias vscg="$VSCODE --goto"
+alias vscn="$VSCODE --new-window"
+alias vscr="$VSCODE --reuse-window"
+alias vscw="$VSCODE --wait"
+alias vscu="$VSCODE --user-data-dir"
 
-alias vsced="$code --extensions-dir"
-alias vscie="$code --install-extension"
-alias vscue="$code --uninstall-extension"
+alias vsced="$VSCODE --extensions-dir"
+alias vscie="$VSCODE --install-extension"
+alias vscue="$VSCODE --uninstall-extension"
 
-alias vscv="$code --verbose"
-alias vscl="$code --log"
-alias vscde="$code --disable-extensions"
+alias vscv="$VSCODE --verbose"
+alias vscl="$VSCODE --log"
+alias vscde="$VSCODE --disable-extensions"
