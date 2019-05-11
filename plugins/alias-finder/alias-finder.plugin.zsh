8d669d3bd040266de37c9f6b42b7252c352d5778
Se De <Sede7473@gmail.com>
2019-05-11 10:31:42 +0000

    Remove broken and unnecessary if statement

--- plugins/alias-finder/alias-finder.plugin.zsh
+++ plugins/alias-finder/alias-finder.plugin.zsh
@@ -16,27 +16,25 @@ alias-finder() {
     esac
   done
   cmd=$(sed 's/[].\|$(){}?+*^[]/\\&/g' <<< $cmd)
-  if [[ $cmd != *\n* ]]; then
-    while [[ $cmd != "" ]]; do
-      if [[ $longer = true ]]; then
-        local wordStart="'{0,1}"
-      else
-        local wordEnd="$"
-        local multiWordEnd="'$"
-      fi
-      if echo $cmd | grep '^[^ ]*$' > /dev/null; then
-        local finder=$wordStart$cmd$wordEnd
-      else
-        local finder="'$cmd$multiWordEnd"
-      fi
-      alias | grep -E "=$finder"
-      if [[ $exact = true || $longer = true ]]; then
-        break
-      else
-        cmd=$(sed -E 's/ {0,1}[^ ]*$//' <<< $cmd) # deletes last word
-      fi
-    done
-  fi
+  while [[ $cmd != "" ]]; do
+    if [[ $longer = true ]]; then
+      local wordStart="'{0,1}"
+    else
+      local wordEnd="$"
+      local multiWordEnd="'$"
+    fi
+    if echo $cmd | grep '^[^ ]*$' > /dev/null; then
+      local finder=$wordStart$cmd$wordEnd
+    else
+      local finder="'$cmd$multiWordEnd"
+    fi
+    alias | grep -E "=$finder"
+    if [[ $exact = true || $longer = true ]]; then
+      break
+    else
+      cmd=$(sed -E 's/ {0,1}[^ ]*$//' <<< $cmd) # deletes last word
+    fi
+  done
 }
 
 preexec() {
