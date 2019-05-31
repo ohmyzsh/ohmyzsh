5ecbaa49c87eefc7aa6d22db1a5d15366e1b84f2
Björn Daase <bjoern@daase.net>
2019-05-31 20:54:18 +0200

    Fix unintentionally bricked autoremove command

--- plugins/debian/debian.plugin.zsh
+++ plugins/debian/debian.plugin.zsh
@@ -66,7 +66,7 @@ if [[ $use_sudo -eq 1 ]]; then
 
     # apt-get only
     alias ads='sudo apt-get dselect-upgrade'
-    alias aar='sudo apt-get autore'
+    alias aar='sudo apt-get autoremove'
 
     # Install all .deb files in the current directory.
     # Warning: you will need to put the glob in single quotes if you use:
