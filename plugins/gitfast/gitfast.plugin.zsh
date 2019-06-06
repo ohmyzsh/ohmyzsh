b6595a273ed10f4f9291ccbf011ff1acb5f34dac
Felipe Contreras <felipe.contreras@gmail.com>
2019-06-06 10:07:00 -0500

    gitfast: simplify plugin
    No need to set and unset a variable we use once.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- plugins/gitfast/gitfast.plugin.zsh
+++ plugins/gitfast/gitfast.plugin.zsh
@@ -1,9 +1,6 @@
-dir="${0:A:h}"
-source "$dir/git-prompt.sh"
+source "${0:A:h}/git-prompt.sh"
 
 function git_prompt_info() {
   dirty="$(parse_git_dirty)"
   __git_ps1 "${ZSH_THEME_GIT_PROMPT_PREFIX//\%/%%}%s${dirty//\%/%%}${ZSH_THEME_GIT_PROMPT_SUFFIX//\%/%%}"
 }
-
-unset dir
