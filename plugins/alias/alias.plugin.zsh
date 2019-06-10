679ff5b366f096dbb27d5b0eaba190476c6716e0
liujianping <liujianping.itech@qq.com>
2019-06-10 11:30:25 +0800

    add macos lock alias

--- plugins/alias/alias.plugin.zsh
+++ plugins/alias/alias.plugin.zsh
@@ -7,3 +7,10 @@ alias af="alias-find "
 ##kubectl alias
 alias kls="kcgc"
 alias kcd="kcuc"
+
+## macos 
+function lock(){
+    /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+}
+
+alias lk="lock"
