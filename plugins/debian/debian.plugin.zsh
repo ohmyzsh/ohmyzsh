a8b9b698c9873e7c09ccc4e322e4f68ca328a14c
Björn Daase <bjoern.daase@student.hpi.de>
2019-06-14 20:50:16 +0200

    [debian plugin] Add new alu and aar alias

--- plugins/debian/debian.plugin.zsh
+++ plugins/debian/debian.plugin.zsh
@@ -58,6 +58,10 @@ if [[ $use_sudo -eq 1 ]]; then
 
     # apt-get only
     alias ads="sudo apt-get dselect-upgrade"
+    alias aar="sudo apt-get autoremove"
+    
+    # apt only
+    alias alu="sudo apt update && apt list -u && sudo apt upgrade"
 
     # Install all .deb files in the current directory.
     # Warning: you will need to put the glob in single quotes if you use:
