84e39885c404b69f9de047a13eb08bbe54bcf95e
Josh Powell <josh@innovativeops.co>
2019-07-11 19:38:28 -0400

    First pass at git-town aliases

---
+++ plugins/git-town/git-town.plugin.zsh
@@ -0,0 +1,19 @@
+#!zsh
+#
+# Installation
+# ------------
+#
+# WIP
+
+#Alias
+| `gt`    | `git town`                 | Git-Town command                       |
+| `gth`   | `git town hack`            | New feature branch off main branch     |
+| `gtsy`  | `git town sync`            | Updates current branch w/ all changes  |
+| `gtnpr` | `git town new-pull-request`| Create a new pull request              |
+| `gtsh`  | `git town ship`            | Delivers feature branch and removes it |
+
+alias gt='git town'
+alias gth='git town hack'
+alias gtsy='git town sync'
+alias gtnpr='git town new-pull-request'
+alias gtsh='git town ship'
