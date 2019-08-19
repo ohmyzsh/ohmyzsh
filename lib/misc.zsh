e604eaf55e1c5b19035778c0e0adb0dd6e344b94
Marc Cornellà <marc.cornella@live.com>
2019-08-19 18:17:17 +0200

    lib: delete LC_CTYPE locale setting which causes problems
    Fixes #7942

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -31,10 +31,5 @@ else
   alias afind='ack -il'
 fi
 
-# only define LC_CTYPE if undefined
-if [[ -z "$LC_CTYPE" && -z "$LC_ALL" ]]; then
-	export LC_CTYPE=${LANG%%:*} # pick the first entry from LANG
-fi
-
 # recognize comments
 setopt interactivecomments
