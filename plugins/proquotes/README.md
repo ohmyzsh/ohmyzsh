3c63be243f56abbdec887280b6b4f10781a797c5
dmurage <dmurage@safaricom.co.ke>
2019-09-07 18:42:11 +0300

    add support for sr

--- plugins/proquotes/README.md
+++ plugins/proquotes/README.md
@@ -1,18 +1,20 @@
 # ProQuotes (Programming Quotes) Plugin
 
-This plugin decorates your terminal with a nice programming quote every time you start a new session.
+This plugin welcomes you to your terminal with a nice programming quote every time you start a new Zsh session.
 
 To use, add `proquotes` to the list of plugins in your `.zshrc` file:
 
 `plugins=(... proquotes)`
 
-Then, re-open your terminal and get the motivation you need to CRUSH IT!
+Then, re-open your terminal and GET the motivation you need to CRUSH IT!
 
-## Screenshot
+## Screenshots
 ![image](https://user-images.githubusercontent.com/22506247/64459179-f5c3d280-d0ff-11e9-9870-4c5784f2e4fc.png)
+
+![image](https://user-images.githubusercontent.com/22506247/64477104-36732880-d1a0-11e9-8309-a36623f32fa6.png)
 
 ## Credits
 [Škola koda](https://github.com/skolakoda) - (@skolakoda) for making the Programming Quotes API. To contibute [click here](https://github.com/skolakoda/programming-quotes-api).
 
 ## Contributions
-Contributions are welcome. Just fork the code and raise a PR [here](https://github.com/murageden/oh-my-zsh) :-)
+Contributions are welcome. Just fork the code and raise a PR [here](https://github.com/murageden/oh-my-zsh/tree/proquotes-plugin/plugins) :-)
--- plugins/proquotes/proquotes.plugin.zsh
+++ plugins/proquotes/proquotes.plugin.zsh
@@ -1,6 +1,6 @@
 ###########################################
 # Programming Quotes plugin for oh-my-zsh  #
-# Available only for Mac Users as of now  #
+# Available only for Mac Users as of now; in two languges EN, SR  #
 # Original Author: Dennis Murage (murageden) #
 # Email: muragemd@gmail.com              #
 ###########################################
@@ -10,6 +10,7 @@ mkdir -p $PRO_Q_HOME
 file_name="$PRO_Q_HOME/quote.txt"
 local _res
 local _lng
+local _lkey
 _lngEN="lang/en"
 _lngSR="lang/sr"
 _api="programming-quotes-api.herokuapp.com"
@@ -18,8 +19,10 @@ _api="programming-quotes-api.herokuapp.com"
 lang=$(osascript -e 'user locale of (get system info)')
 if [[ $lang == *"sr"* || $lang == *"SR"* ]] then # Serbian
     _lng=$_lngSR
+    _lkey="sr"
 else # English
     _lng=$_lngEN
+    _lkey="en"
 fi
 
 # check internet
@@ -31,6 +34,10 @@ if [ $online -eq 0 ]; then # online
 else # offline
     if [ -f $file_name ]; then # file found
         _res=$(<$file_name)
+        if [[ ! $_res == *"$_lkey"* ]]; then # lang not found
+            echo "ERR: proquotes run into an error. See you in the next zsh session ~ Dennis Murage"
+            return
+        fi
     else # no file
         echo "ERR: proquotes run into an error. See you in the next zsh session ~ Dennis Murage"
         return
@@ -38,7 +45,15 @@ else # offline
 fi
 
 # process quote
-_qt=`print $_res | python -c 'import json,sys; obj=json.load(sys.stdin);print obj["en"].encode("ascii", "ignore");'`
+local _qt
+if [[ $_lkey == "en" ]]; then # English
+    _qt=`print $_res | python -c 'import json,sys; obj=json.load(sys.stdin);print obj["en"].encode("ascii", "ignore");'`
+    else # Sr
+        _qt=`print $_res | python -c 'import json,sys; obj=json.load(sys.stdin);print obj["sr"].encode("ascii", "ignore");'`
+fi
+
 _aut=`print $_res | sed -e 's/[{}]/''/g' | sed s/\"//g | awk -v RS=',' -F: '$1=="author"{print $2}'`
+
+# output
 echo "${_qt}" \~ "${_aut}"
 
