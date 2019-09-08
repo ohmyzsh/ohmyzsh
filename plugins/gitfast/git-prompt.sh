4fb50b3333ce8685ce3b6570ef5e075ce2633b6a
Marc Cornellà <marc.cornella@live.com>
2019-09-08 20:28:59 +0200

    Unquote yes in conditional expressions for style consistency

--- plugins/gitfast/git-prompt.sh
+++ plugins/gitfast/git-prompt.sh
@@ -219,7 +219,7 @@ __git_ps1_show_upstream ()
 		if [[ -n "$count" && -n "$name" ]]; then
 			__git_ps1_upstream_name=$(git rev-parse \
 				--abbrev-ref "$upstream" 2>/dev/null)
-			if [ "$pcmode" = "yes" ] && [ "$ps1_expanded" = "yes" ]; then
+			if [ "$pcmode" = yes ] && [ "$ps1_expanded" = yes ]; then
 				p="$p \${__git_ps1_upstream_name}"
 			else
 				p="$p ${__git_ps1_upstream_name}"
@@ -508,13 +508,13 @@ __git_ps1 ()
 
 	# NO color option unless in PROMPT_COMMAND mode or it's Zsh
 	if [ -n "${GIT_PS1_SHOWCOLORHINTS-}" ]; then
-		if [ "$pcmode" = "yes" ] || [ -n "${ZSH_VERSION-}" ]; then
+		if [ "$pcmode" = yes ] || [ -n "${ZSH_VERSION-}" ]; then
 			__git_ps1_colorize_gitstring
 		fi
 	fi
 
 	b=${b##refs/heads/}
-	if [ "$pcmode" = "yes" ] && [ "$ps1_expanded" = "yes" ]; then
+	if [ "$pcmode" = yes ] && [ "$ps1_expanded" = yes ]; then
 		__git_ps1_branch_name=$b
 		b="\${__git_ps1_branch_name}"
 	fi
@@ -522,8 +522,8 @@ __git_ps1 ()
 	local f="$w$i$s$u"
 	local gitstring="$c$b${f:+$z$f}$r$p"
 
-	if [ "$pcmode" = "yes" ]; then
-		if [ "${__git_printf_supports_v-}" != "yes" ]; then
+	if [ "$pcmode" = yes ]; then
+		if [ "${__git_printf_supports_v-}" != yes ]; then
 			gitstring=$(printf -- "$printf_format" "$gitstring")
 		else
 			printf -v gitstring -- "$printf_format" "$gitstring"
