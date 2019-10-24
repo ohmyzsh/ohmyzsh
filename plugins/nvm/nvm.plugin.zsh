b5a8c9f5302318216c081475177766674eaccffa
XuDaojie <xudaojieg@gmail.com>
2019-10-24 17:47:03 +0800

    nvm add brew install path

--- plugins/nvm/nvm.plugin.zsh
+++ plugins/nvm/nvm.plugin.zsh
@@ -1,8 +1,17 @@
 # Set NVM_DIR if it isn't already defined
 [[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
 
-# Try to load nvm only if command not already available
 if ! type "nvm" &> /dev/null; then
-    # Load nvm if it exists
-    [[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+    # Load if it exists
+    if [ ! -f "/usr/local/opt/nvm" ]; then
+        # brew install nvm
+        nvm_sh_dir="/usr/local/opt/nvm"
+        # This loads nvm
+        [ -s "${nvm_sh_dir}/nvm.sh" ] && . "${nvm_sh_dir}/nvm.sh"
+        # This loads nvm bash_completion
+        [ -s "${nvm_sh_dir}/etc/bash_completion" ] && . "${nvm_sh_dir}/etc/bash_completion"
+    else
+        # This loads nvm
+        [[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+    fi
 fi
