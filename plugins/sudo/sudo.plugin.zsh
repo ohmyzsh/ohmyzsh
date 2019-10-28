a761e55475dd137fe79f797d6986571379a1b446
Timothy DeHerrera <tim.deh@pm.me>
2019-10-27 20:41:08 -0600

    sudo: add support for shell redirection
    The sudo plugin is now much more powerful and can handle situations of
    shell redirection. In the past these commands would often fail leaving
    the user to wrap the command in `zsh -c "command"` manually. This is now
    detected and handled automatically.
    
    * detect shell indirection ie [<|>], and wrap with `$SHELL -c "command"`
    * 'off' logic is more complex; refactor into its own function
    * 'sudoedit' now also works with shell aliases set to $EDITOR
    * ensure quoting is handled properly when toggling on and off

--- plugins/sudo/sudo.plugin.zsh
+++ plugins/sudo/sudo.plugin.zsh
@@ -9,19 +9,33 @@
 # -------
 #
 # * Dongweiming <ciici123@gmail.com>
+# * Tim D <tim.deh@pm.me>
 #
 # ------------------------------------------------------------------------------
+setopt extendedglob
+
+sudo-off() {
+    if [[ $BUFFER == sudoedit\ * ]]; then
+        LBUFFER="${LBUFFER#sudoedit }"
+        LBUFFER="${EDITOR:t} $LBUFFER"
+    elif [[  $BUFFER == sudo\ ${SHELL:t}\ -c\ * ]]; then
+        BUFFER="${BUFFER##sudo ${SHELL:t} -c ##[\'\"]#}"
+        BUFFER="${BUFFER%[\"\']}"
+        BUFFER="${BUFFER//\\\"/\"}"
+    else
+        LBUFFER="${LBUFFER#sudo }"
+   fi
+}
 
 sudo-command-line() {
     [[ -z $BUFFER ]] && zle up-history
-    if [[ $BUFFER == sudo\ * ]]; then
-        LBUFFER="${LBUFFER#sudo }"
-    elif [[ $BUFFER == $EDITOR\ * ]]; then
-        LBUFFER="${LBUFFER#$EDITOR }"
+    if [[ $BUFFER == sudo(edit)#\ * ]]; then
+        sudo-off
+    elif [[ $BUFFER == ${EDITOR:t}\ * ]] || [[ $(alias ${BUFFER%% *}) =~ "EDITOR" ]]; then
+        LBUFFER="${LBUFFER#${BUFFER%% *} }"
         LBUFFER="sudoedit $LBUFFER"
-    elif [[ $BUFFER == sudoedit\ * ]]; then
-        LBUFFER="${LBUFFER#sudoedit }"
-        LBUFFER="$EDITOR $LBUFFER"
+    elif [[ ${BUFFER//(\"[^\"]#\"|\'[^\']#\')/} =~ '[<>|]'  ]]; then
+        BUFFER="sudo zsh -c \"${BUFFER//\"/\\\"}\""
     else
         LBUFFER="sudo $LBUFFER"
     fi
