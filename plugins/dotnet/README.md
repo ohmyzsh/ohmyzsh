4cdc34a23a02674cf9e9d4378c4bb1a7949779aa
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-11-01 06:45:34 +0700

    Add .NET Core CLI plugin

---
+++ plugins/dotnet/README.md
@@ -0,0 +1,8 @@
+# dotnet plugin
+
+This plugin adds completion for `dotnet`,
+the [.NET Core CLI](https://dotnet.microsoft.com).
+
+To use it, add `dotnet` to the plugins array in your zshrc file:
+
+    plugins=(... dotnet)
---
+++ plugins/dotnet/dotnet.plugin.zsh
@@ -0,0 +1,11 @@
+# https://docs.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete
+# zsh parameter completion for the dotnet CLI
+
+_dotnet_zsh_complete()
+{
+  local completions=("$(dotnet complete "$words")")
+
+  reply=( "${(ps:\n:)completions}" )
+}
+
+compctl -K _dotnet_zsh_complete dotnet
