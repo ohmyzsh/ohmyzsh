24726678dd39ab69f5a14a789775fe53ccfd7c1d
Marc Cornellà <marc.cornella@live.com>
2019-11-09 12:37:41 +0100

    extract: keep *.gz files with pigz and gunzip
    Fixes #8368

--- plugins/extract/extract.plugin.zsh
+++ plugins/extract/extract.plugin.zsh
@@ -41,7 +41,7 @@ extract() {
 				&& tar --lzma -xvf "$1" \
 				|| lzcat "$1" | tar xvf - ;;
 			(*.tar) tar xvf "$1" ;;
-			(*.gz) (( $+commands[pigz] )) && pigz -d "$1" || gunzip "$1" ;;
+			(*.gz) (( $+commands[pigz] )) && pigz -dk "$1" || gunzip -k "$1" ;;
 			(*.bz2) bunzip2 "$1" ;;
 			(*.xz) unxz "$1" ;;
 			(*.lzma) unlzma "$1" ;;
