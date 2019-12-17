31e8ab905b717325e5e8f8e449a07efd1a2deaeb
Max Clayton Clowes <max.clayton-clowes@pollen.co>
2019-12-17 15:26:13 +0000

    [feat] clear pollen virtualenv

--- custom/plugins/pollen/pollen.plugin.zsh
+++ custom/plugins/pollen/pollen.plugin.zsh
@@ -30,6 +30,16 @@ function pollen-git-commit-jira {
   git add --all && git commit -am "$COMMIT_MESSAGE";
 }
 
+
+function pollen-clear-virtual-env {
+  for FOLDER in ambassador_api ambassador_ui auth_ui bin calvin_ui customer_ui devtools e2e_cypress etl_service finance_service gateway_api infrastructure lib licensed_travel_api logs node_modules notification_service phoenix_ui pollen_ui review_automation_service shortlinks_service styleguide_docs ; do
+    pushd $FOLDER
+      rm -rf .virtualenv;
+    popd
+  done
+}
+
+
 function pollen {
   echo "pollen-server-up - spin up pollen server";
   echo "pollen-dummy-data - seed demot data";
