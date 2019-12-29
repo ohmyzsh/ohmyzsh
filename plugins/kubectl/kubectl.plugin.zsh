9f7bf3f0d3beacb5f6165aad54e0b45e4a109f68
Alaa Qutaish <alaa.qutaish@gmail.com>
2019-12-29 05:58:31 +0100

    Add kubectl log since aliases (#8448)

--- plugins/kubectl/kubectl.plugin.zsh
+++ plugins/kubectl/kubectl.plugin.zsh
@@ -115,7 +115,13 @@ alias kgaa='kubectl get all --all-namespaces'
 
 # Logs
 alias kl='kubectl logs'
+alias kl1h='kubectl logs --since 1h'
+alias kl1m='kubectl logs --since 1m'
+alias kl1s='kubectl logs --since 1s'
 alias klf='kubectl logs -f'
+alias klf1h='kubectl logs --since 1h -f'
+alias klf1m='kubectl logs --since 1m -f'
+alias klf1s='kubectl logs --since 1s -f'
 
 # File copy
 alias kcp='kubectl cp'
