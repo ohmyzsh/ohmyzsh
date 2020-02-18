9baa5718927b968434e15fc01d8364984382a3be
Praveen Dhawan <praveen@pdhawan.com>
2020-02-18 10:17:49 +0530

    Add support for basic github-cli commands

---
+++ plugins/github-cli/github-cli.plugin.zsh
@@ -0,0 +1,27 @@
+#
+# Aliases
+# (sorted alphabetically)
+#
+
+alias ghh='gh help'
+
+alias ghi='gh issue'
+alias ghic='gh issue create'
+alias ghil='gh issue list'
+alias ghis='gh issue status'
+alias ghiv='gh issue view'
+
+alias ghp='gh pr'
+alias ghpc='ghp create'
+alias ghpck='ghp checkout'
+alias ghpl='ghp list'
+alias ghps='ghp status'
+alias ghpv='ghp view'
+
+alias ghr='gh repo'
+alias ghrc='gh repo clone'
+alias ghrcr='gh repo create'
+alias ghrf='gh repo fork'
+alias ghrv='gh repo view'
+
+alias ghv='gh --version'
