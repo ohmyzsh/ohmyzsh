7b932dabd2740f3897438d2745cbcbd8ce1b50a4
Harry ten Berge <htenberge@gmail.com>
2020-02-20 15:37:45 +0100

    openSUSE's Zypper Plugin

---
+++ plugins/zypper/zypper.plugin.zsh
@@ -0,0 +1,12 @@
+## Aliases
+
+alias zppls="zypper ls"               # List repositories
+alias zppll="zypper ll"               # List locked packages
+alias zpps="zypper search"            # Search
+
+alias zzpu="sudo zypper up"           # Update package
+alias zzpd="sudo zypper dup"          # Perform distribution upgrade
+alias zzpi="sudo zypper install"      # Install package
+alias zzpr="sudo zypper remove"       # Remove package
+alias zzpal="sudo zypper al"          # Add package lock
+alias zzprl="sudo zypper rl"          # Remove package lock
