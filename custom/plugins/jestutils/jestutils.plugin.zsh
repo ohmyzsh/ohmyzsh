91c348e9a15102b8f8d59a9e3b64a6ba30adf883
Max Clayton Clowes <max.clayton-clowes@pollen.co>
2020-02-25 13:47:46 +0000

    [feat] pollen jwa

--- custom/plugins/jestutils/jestutils.plugin.zsh
+++ custom/plugins/jestutils/jestutils.plugin.zsh
@@ -16,9 +16,15 @@ function jwa { # jest watch all
   npm run test -- --watchAll --runInBand --bail --verbose ${1};
 }
 
+function pjwa { # jest watch all
+  ## -- means pass params to jest
+  npm run test:unit -- --watchAll --runInBand --bail --verbose ${1};
+}
+
 
 function jest-utils {
   echo "jw <regex pattern for files> - Run tests, watch and bail";
   echo "jwnc <regex pattern for files> - Run tests, watch and bail";
   echo "jwa <regex pattern for files> - Run all tests, watch and bail";
+  echo "pjwa <regex pattern for files> - Pollen edition! Run all tests, watch and bail";
 }
