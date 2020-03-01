dc190d872aa37dc0440284bee6c6bd7744007bf0
Marc Cornellà <marc.cornella@live.com>
2020-03-01 20:17:38 +0100

    Refactor grep.zsh file
    - Move grep-alias path to variable.
    - Use <<< "" instead of piped echo to check grep flags.
    - Remove check for --color only since it's the same release as --exclude.

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -1,39 +1,37 @@
-# see if we already cached the grep alias in past day
-_grep_alias_cache=("$ZSH_CACHE_DIR"/grep_alias.zsh(Nm-24))
-if (( $#_grep_alias_cache )); then
-    source "$ZSH_CACHE_DIR"/grep_alias.zsh
-else
-    # is x grep argument available?
+__GREP_CACHE_FILE="$ZSH_CACHE_DIR"/grep-alias
+
+# See if there's a cache file modified in the last day
+__GREP_ALIAS_CACHES=("$__GREP_CACHE_FILE"(Nm-1))
+if [[ -z "$__GREP_ALIAS_CACHES" ]]; then
     grep-flags-available() {
-        echo | grep "$@" "" >/dev/null 2>&1
+        command grep "$@" "" &>/dev/null <<< ""
     }
 
-    GREP_OPTIONS=""
-
-    # ignore these folders (if the necessary grep flags are available)
+    # Ignore these folders (if the necessary grep flags are available)
     EXC_FOLDERS="{.bzr,CVS,.git,.hg,.svn,.idea,.tox}"
 
+    # Check for --exclude-dir, otherwise check for --exclude. If --exclude
+    # isn't available, --color won't be either (they were released at the same
+    # time (v2.5): http://git.savannah.gnu.org/cgit/grep.git/tree/NEWS?id=1236f007
     if grep-flags-available --color=auto --exclude-dir=.cvs; then
-        GREP_OPTIONS+="--color=auto --exclude-dir=$EXC_FOLDERS"
+        GREP_OPTIONS="--color=auto --exclude-dir=$EXC_FOLDERS"
     elif grep-flags-available --color=auto --exclude=.cvs; then
-        GREP_OPTIONS+="--color=auto --exclude=$EXC_FOLDERS"
-    elif grep-flags-available --color=auto; then
-        GREP_OPTIONS+="--color=auto"
+        GREP_OPTIONS="--color=auto --exclude=$EXC_FOLDERS"
     fi
 
     {
         if [[ -n "$GREP_OPTIONS" ]]; then
             # export grep, egrep and fgrep settings
-            echo alias grep="'grep $GREP_OPTIONS'"
-            echo alias egrep="'egrep $GREP_OPTIONS'"
-            echo alias fgrep="'fgrep $GREP_OPTIONS'"
+            echo "alias grep='grep $GREP_OPTIONS'"
+            echo "alias egrep='egrep $GREP_OPTIONS'"
+            echo "alias fgrep='fgrep $GREP_OPTIONS'"
         fi
-    } > "$ZSH_CACHE_DIR/grep_alias.zsh"
+    } > "$__GREP_CACHE_FILE"
 
-    source "$ZSH_CACHE_DIR/grep_alias.zsh"
-
-    # clean up
+    # Clean up
     unset GREP_OPTIONS EXC_FOLDERS
     unfunction grep-flags-available
 fi
-unset _grep_alias_cache
+
+source "$__GREP_CACHE_FILE"
+unset __GREP_CACHE_FILE __GREP_ALIAS_CACHES
