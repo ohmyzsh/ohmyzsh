d3dfc1371693d2b64ccec249511ea9178b088015
Marc Cornellà <marc.cornella@live.com>
2020-03-02 12:35:58 +0100

    lib: use grep-alias cache only if ZSH_CACHE_DIR is writable
    Fixes #8693

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -2,7 +2,9 @@ __GREP_CACHE_FILE="$ZSH_CACHE_DIR"/grep-alias
 
 # See if there's a cache file modified in the last day
 __GREP_ALIAS_CACHES=("$__GREP_CACHE_FILE"(Nm-1))
-if [[ -z "$__GREP_ALIAS_CACHES" ]]; then
+if [[ -n "$__GREP_ALIAS_CACHES" ]]; then
+    source "$__GREP_CACHE_FILE"
+else
     grep-flags-available() {
         command grep "$@" "" &>/dev/null <<< ""
     }
@@ -12,26 +14,28 @@ if [[ -z "$__GREP_ALIAS_CACHES" ]]; then
 
     # Check for --exclude-dir, otherwise check for --exclude. If --exclude
     # isn't available, --color won't be either (they were released at the same
-    # time (v2.5): http://git.savannah.gnu.org/cgit/grep.git/tree/NEWS?id=1236f007
+    # time (v2.5): https://git.savannah.gnu.org/cgit/grep.git/tree/NEWS?id=1236f007
     if grep-flags-available --color=auto --exclude-dir=.cvs; then
         GREP_OPTIONS="--color=auto --exclude-dir=$EXC_FOLDERS"
     elif grep-flags-available --color=auto --exclude=.cvs; then
         GREP_OPTIONS="--color=auto --exclude=$EXC_FOLDERS"
     fi
 
-    {
-        if [[ -n "$GREP_OPTIONS" ]]; then
-            # export grep, egrep and fgrep settings
-            echo "alias grep='grep $GREP_OPTIONS'"
-            echo "alias egrep='egrep $GREP_OPTIONS'"
-            echo "alias fgrep='fgrep $GREP_OPTIONS'"
+    if [[ -n "$GREP_OPTIONS" ]]; then
+        # export grep, egrep and fgrep settings
+        alias grep="grep $GREP_OPTIONS"
+        alias egrep="egrep $GREP_OPTIONS"
+        alias fgrep="fgrep $GREP_OPTIONS"
+
+        # write to cache file if cache directory is writable
+        if [[ -w "$ZSH_CACHE_DIR" ]]; then
+            alias -L grep egrep fgrep >| "$__GREP_CACHE_FILE"
         fi
-    } > "$__GREP_CACHE_FILE"
+    fi
 
     # Clean up
     unset GREP_OPTIONS EXC_FOLDERS
     unfunction grep-flags-available
 fi
 
-source "$__GREP_CACHE_FILE"
 unset __GREP_CACHE_FILE __GREP_ALIAS_CACHES
