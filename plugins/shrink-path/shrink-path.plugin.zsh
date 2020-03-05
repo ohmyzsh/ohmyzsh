2f345a3d0c9a94891c909a9ae6f6e2e2dc6df5b2
Marc Cornellà <marc.cornella@live.com>
2020-03-05 13:08:05 +0100

    shrink-path: added glob and quote options (#7694)
    * Added -g option to shrink-path plugin.
    
    The new optins adds an asterix to the uniquely shortened name, such
    that, if copy'n'pasted, zsh globbing will expand to the full path name
    of the shortened path:
    
    shrink-path -g:
    "/net/software/modules/modulefiles" -> "/n*/s*/modules/m*"
    
    shrink-path -l -g:
    "/net/software/modules/modulefiles" -> "/n*/s*/modules/modulefiles"
    
    * Updated also the README file with the new -g option.
    
    * shrink-path: improved handling of special cases
    
    This commits improves the handling of special cases in path strings:
    
    - handling of white space in path names
    - handling of single-letter path names
    
    * Update plugins/shrink-path/README.md
    
    Co-Authored-By: Jacob Tomaw <jacob.tomaw@gmail.com>
    
    * Fix typo "Asterix" and replace with "asterisk"
    
    * Add optional quoting support to shrink_path
    
    Co-authored-by: Jacob Tomaw <jacob.tomaw@gmail.com>

--- plugins/shrink-path/shrink-path.plugin.zsh
+++ plugins/shrink-path/shrink-path.plugin.zsh
@@ -9,6 +9,7 @@
 # The following options are available:
 #
 #   -f, --fish       fish simulation, equivalent to -l -s -t.
+#   -g, --glob       Add asterisk to allow globbing of shrunk path (equivalent to -e "*")
 #   -l, --last       Print the last directory's full name.
 #   -s, --short      Truncate directory names to the number of characters given by -#. Without
 #                    -s, names are truncated without making them ambiguous.
@@ -17,6 +18,7 @@
 #   -#               Truncate each directly to at least this many characters inclusive of the
 #                    ellipsis character(s) (defaulting to 1).
 #   -e SYMBOL        Postfix symbol(s) to indicate that a directory name had been truncated.
+#   -q, --quote      Quote special characters in the shrunk path
 #
 # The long options can also be set via zstyle, like
 #   zstyle :prompt:shrink_path fish yes
@@ -42,6 +44,7 @@ shrink_path () {
         typeset -i named=0
         typeset -i length=1
         typeset ellipsis=""
+        typeset -i quote=0
 
         if zstyle -t ':prompt:shrink_path' fish; then
                 lastfull=1
@@ -55,6 +58,8 @@ shrink_path () {
         zstyle -t ':prompt:shrink_path' last && lastfull=1
         zstyle -t ':prompt:shrink_path' short && short=1
         zstyle -t ':prompt:shrink_path' tilde && tilde=1
+        zstyle -t ':prompt:shrink_path' glob && ellipsis='*'
+        zstyle -t ':prompt:shrink_path' quote && quote=1
 
         while [[ $1 == -* ]]; do
                 case $1 in
@@ -70,6 +75,7 @@ shrink_path () {
                         -h|--help)
                                 print 'Usage: shrink_path [-f -l -s -t] [directory]'
                                 print ' -f, --fish      fish-simulation, like -l -s -t'
+                                print ' -g, --glob      Add asterisk to allow globbing of shrunk path (equivalent to -e "*")'
                                 print ' -l, --last      Print the last directory''s full name'
                                 print ' -s, --short     Truncate directory names to the number of characters given by -#. Without'
                                 print '                 -s, names are truncated without making them ambiguous.'
@@ -78,6 +84,7 @@ shrink_path () {
                                 print ' -#              Truncate each directly to at least this many characters inclusive of the'
                                 print '                 ellipsis character(s) (defaulting to 1).'
                                 print ' -e SYMBOL       Postfix symbol(s) to indicate that a directory name had been truncated.'
+                                print ' -q, --quote     Quote special characters in the shrunk path'
                                 print 'The long options can also be set via zstyle, like'
                                 print '  zstyle :prompt:shrink_path fish yes'
                                 return 0
@@ -95,6 +102,12 @@ shrink_path () {
                         -e)
                                 shift
                                 ellipsis="$1"
+                        ;;
+                        -g|--glob)
+                                ellipsis='*'
+                        ;;
+                        -q|--quote)
+                                quote=1
                         ;;
                 esac
                 shift
@@ -140,9 +153,11 @@ shrink_path () {
                         typeset -i dif=$(( ${#dir} - ${#part} - ellen ))
                         if [[ $dif -gt 0 ]]
                         then
+                            (( quote )) && part=${(q)part}
                             part+="$ellipsis"
                         else
                             part="$dir"
+                            (( quote )) && part=${(q)part}
                         fi
                         result+="/$part"
                         cd -q $dir
