0eed09ae74f2296db2c10f0d3381477ed9e48eb7
soredake <soredake@users.noreply.github.com>
2020-03-08 19:15:43 +0200

    rsync: update -z to -zz
    ```
    This rsync lacks old-style --compress due to its external zlib.  Try -zz.
    Continuing without compression.
    ```

--- plugins/rsync/rsync.plugin.zsh
+++ plugins/rsync/rsync.plugin.zsh
@@ -1,4 +1,4 @@
-alias rsync-copy="rsync -avz --progress -h"
-alias rsync-move="rsync -avz --progress -h --remove-source-files"
-alias rsync-update="rsync -avzu --progress -h"
-alias rsync-synchronize="rsync -avzu --delete --progress -h"
+alias rsync-copy="rsync -avzz --progress -h"
+alias rsync-move="rsync -avzz --progress -h --remove-source-files"
+alias rsync-update="rsync -avzzu --progress -h"
+alias rsync-synchronize="rsync -avzzu --delete --progress -h"
