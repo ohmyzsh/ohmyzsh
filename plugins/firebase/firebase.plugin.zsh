12dd539e68db305d55fa34d7b3142c357e285c62
Pascal <pascal@luther.ch>
2020-03-19 15:50:49 +0100

    chore: updated comments

--- plugins/firebase/firebase.plugin.zsh
+++ plugins/firebase/firebase.plugin.zsh
@@ -3,31 +3,31 @@
 # https://firebase.google.com/docs/cli#admin-commands
 # Only implemented often used commands.
 
-# INIT (FBI) - https://firebase.google.com/docs/cli#initialize_a_firebase_project
-alias fbi="firebase init"                               # Initialize a Firebase project
+# INIT (fbi) - https://firebase.google.com/docs/cli#initialize_a_firebase_project
+alias fbi="firebase init"
 
-# USE (FBU) - https://firebase.google.com/docs/cli#add_alias
-alias fbu="firebase use"                                # Use a firebase project
-alias fbua="firebase use -add"                          # Add and use a firebase project
+# USE (fbu) - https://firebase.google.com/docs/cli#add_alias
+alias fbu="firebase use"
+alias fbua="firebase use -add"
 
-# SERVE (FBS) - https://firebase.google.com/docs/cli#test-locally
-alias fbs="firebase serve"                              # Serve a Firebase project locally
-alias fbs0="firebase serve --host 0.0.0.0"              # Serve a Firebase projeczt on 0.0.0.0
-alias fbsh="firebase serve --only hosting"              # Serve a Firebase project locally ()
-alias fbsf="firebase serve --only functions"            # Serve a Firebase project locally ()
+# SERVE (fbs) - https://firebase.google.com/docs/cli#test-locally
+alias fbs="firebase serve"
+alias fbs0="firebase serve --host 0.0.0.0"
+alias fbsh="firebase serve --only hosting"
+alias fbsf="firebase serve --only functions"
 
-# DEPLOY (FBD) - https://firebase.google.com/docs/cli#partial_deploys
-alias fbd="firebase deploy"                             # Deploy Firebase (Complete)
-alias fbdh="firebase deploy --only hosting"             # Deploy Firebase Hosting
-alias fbdd="firebase deploy --only database"            # Deploy Firebase Realtime Database
-alias fbds="firebase deploy --only storage"             # Deploy Firebase Storage
-alias fbdfs="firebase deploy --only firestore"          # Deploy Firebase Firestore
-alias fbdfsr="firebase deploy --only firestore:rules"   # Deploy Firebase Firestore (only Rules)
-alias fbdfsi="firebase deploy --only firestore:indexes" # Deploy Firebase Firestore (only Indexes)
-alias fbdfu="firebase deploy --only functions"          # Deploy Firebase Functions
+# DEPLOY (gbd) - https://firebase.google.com/docs/cli#partial_deploys
+alias fbd="firebase deploy"
+alias fbdh="firebase deploy --only hosting"
+alias fbdd="firebase deploy --only database"
+alias fbds="firebase deploy --only storage"
+alias fbdfs="firebase deploy --only firestore"
+alias fbdfsr="firebase deploy --only firestore:rules"
+alias fbdfsi="firebase deploy --only firestore:indexes"
+alias fbdfu="firebase deploy --only functions"
 
-# FUNCTIONS (FBF) - https://firebase.google.com/docs/cli#delete_functions
-alias fbfd="firebase functions:delete"                  # Delete Firebase Function
+# FUNCTIONS (fbf) - https://firebase.google.com/docs/cli#delete_functions
+alias fbfd="firebase functions:delete"
 
-# MISC (FBx)
-alias fbp="firebase projects:list"                      # List all projects
+# MISC (fb..)
+alias fbp="firebase projects:list"
