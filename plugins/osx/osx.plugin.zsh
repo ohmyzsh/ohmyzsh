1c73e59c8faad123d2ff54621a82ad485910d93d
R Vadai <rvadai@segence.com>
2020-03-22 18:12:00 +0000

    Added osx plugin command to clean purgeable disk space

--- plugins/osx/osx.plugin.zsh
+++ plugins/osx/osx.plugin.zsh
@@ -232,6 +232,18 @@ function rmdsstore() {
   find "${@:-.}" -type f -name .DS_Store -delete
 }
 
+# Cleanes up purgeable disk space on the selected disk
+function clean-purgeable-disk-space(){
+    if ! [ -z $1] ; then
+        echo "Cleaning purgeable files from disk : $1 ...."
+        diskutil secureErase freespace 0 $1
+    else
+        echo "Usage : clean-purgeable-disk-space <disk>"
+        echo "Example : clean-purgeable-disk-space /dev/disk1s1"
+        echo "Get available disks from the 'df -h /' command"
+    fi
+}
+
 # Music / iTunes control function
 source "${0:h:A}/music"
 
