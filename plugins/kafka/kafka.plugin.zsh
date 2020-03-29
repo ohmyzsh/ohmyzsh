e636ce85bb0d35d6f65eb4db0554667eb9cf841e
tinogomes <tinorj@gmail.com>
2020-03-28 21:28:31 -0300

    removed plugin for Kafka

--- plugins/kafka/kafka.plugin.zsh
+++
@@ -1,9 +0,0 @@
-alias kfkt=kafka-topics
-alias kfkcc="kafka-console-consumer"
-alias kfkcp="kafka-console-producer"
-
-alias kfkt-new='kfkt --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic'
-alias kfkt-list='kfkt --describe --bootstrap-server localhost:9092'
-alias kfkt-delete='kfkt --delete --bootstrap-server localhost:9092 --topic'
-
-alias kfkcpt="kfkcp --broker-list localhost:9092 --topic"
