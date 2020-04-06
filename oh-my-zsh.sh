5d3f8bcc9f3ee1271c673d2a9f4d5ea1810e5197
Monson Shao <holymonson@gmail.com>
2020-04-06 18:04:00 +0800

    ZSH_CACHE_DIR should respect XDG_CACHE_HOME first

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,7 +1,7 @@
 # Set ZSH_CACHE_DIR to the path where cache files should be created
 # or else we will use the default cache/
 if [[ -z "$ZSH_CACHE_DIR" ]]; then
-  ZSH_CACHE_DIR="$ZSH/cache"
+  ZSH_CACHE_DIR=${XDG_CACHE_HOME:-"$ZSH/cache"}
 fi
 
 # Migrate .zsh-update file to $ZSH_CACHE_DIR
