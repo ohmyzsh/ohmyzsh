cceb311fb6d355ca9b8f62a6db2424f3a6785c54
Michael Klemm <michael@dontknow.de>
2020-04-06 20:14:35 +0200

    Check if direnv is installed on the system
    Instead of printing that the direnv hook failed for every command
    prompt, this fix changes the plugin to only load the hook if the
    'direnv' command is available on the system.

--- plugins/direnv/direnv.plugin.zsh
+++ plugins/direnv/direnv.plugin.zsh
@@ -1,13 +1,15 @@
-_direnv_hook() {
-  trap -- '' SIGINT;
-  eval "$(direnv export zsh)";
-  trap - SIGINT;
-}
-typeset -ag precmd_functions;
-if [[ -z ${precmd_functions[(r)_direnv_hook]} ]]; then
-  precmd_functions=( _direnv_hook ${precmd_functions[@]} )
-fi
-typeset -ag chpwd_functions;
-if [[ -z ${chpwd_functions[(r)_direnv_hook]} ]]; then
-  chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
+if type "direnv" > /dev/null; then
+  _direnv_hook() {
+    trap -- '' SIGINT;
+    eval "$(direnv export zsh)";
+    trap - SIGINT;
+  }
+  typeset -ag precmd_functions;
+  if [[ -z ${precmd_functions[(r)_direnv_hook]} ]]; then
+    precmd_functions=( _direnv_hook ${precmd_functions[@]} )
+  fi
+  typeset -ag chpwd_functions;
+  if [[ -z ${chpwd_functions[(r)_direnv_hook]} ]]; then
+    chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
+  fi
 fi
