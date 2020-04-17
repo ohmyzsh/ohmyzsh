44c57d7be80da034f4f7cd626a8237ea9b47b639
Filipe Giusti <filipegiusti@gmail.com>
2020-04-17 10:13:40 -0300

    Only loads completion when not a brew installation

--- plugins/asdf/asdf.plugin.zsh
+++ plugins/asdf/asdf.plugin.zsh
@@ -1,17 +1,18 @@
 # Find where asdf should be installed
 ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
-ASDF_COMPLETIONS="$ASDF_DIR/completions"
 
 # If not found, check for Homebrew package
 if [[ ! -f "$ASDF_DIR/asdf.sh" ]] && (( $+commands[brew] )); then
    ASDF_DIR="$(brew --prefix asdf)"
-   ASDF_COMPLETIONS="$ASDF_DIR/etc/bash_completion.d"
 fi
 
 # Load command
 if [[ -f "$ASDF_DIR/asdf.sh" ]]; then
     . "$ASDF_DIR/asdf.sh"
 
-    fpath=($ASDF_COMPLETIONS $fpath)
-    autoload -Uz compinit && compinit
+    # If no Homebrew package, load completions
+    if (( ! $+commands[brew] )); then
+        fpath=("$ASDF_DIR/completions" $fpath)
+        autoload -Uz compinit && compinit
+    fi
 fi
