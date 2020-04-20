e8f50a3f7c23c3729c65fe440f1ca84db33490b1
Carlos Palhares <chjunior@gmail.com>
2020-04-20 14:32:32 -0300

    Update to bitwarden-cli 1.9.0 - add nointeraction flag

--- plugins/bitwarden/bitwarden.plugin.zsh
+++ plugins/bitwarden/bitwarden.plugin.zsh
@@ -1,2 +1,2 @@
 alias bwunlock='export BW_SESSION=`bw unlock --raw`'
-alias bwpass='bw --quiet sync || bwunlock && bw get password'
+alias bwpass='bw --quiet --nointeraction sync || bwunlock && bw get password'
