c686aa3dbb30704b2f8f52bf1aae19a7e41c23c1
tom139 <tom139@users.noreply.github.com>
2020-04-26 12:28:24 +0200

    aws: fix aws_change_access_key function with awscli v2 (#8816)

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -31,12 +31,12 @@ function aws_change_access_key() {
 
   echo Insert the credentials when asked.
   asp "$1" || return 1
-  aws iam create-access-key
-  aws configure --profile "$1"
+  AWS_PAGER="" aws iam create-access-key
+  AWS_PAGER="" aws configure --profile "$1"
 
   echo You can now safely delete the old access key running \`aws iam delete-access-key --access-key-id ID\`
   echo Your current keys are:
-  aws iam list-access-keys
+  AWS_PAGER="" aws iam list-access-keys
 }
 
 function aws_profiles() {
