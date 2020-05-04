dd1a72696f162fd5285a104b5f63970deb66e8b0
Marc Cornellà <marc.cornella@live.com>
2020-05-04 16:51:16 +0200

    core: refresh zcompdump cache file in init script (#8878)

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -61,6 +61,18 @@ if [ -z "$ZSH_COMPDUMP" ]; then
   ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
 fi
 
+# Construct zcompdump OMZ metadata
+zcompdump_metadata="\
+#omz revision: $(cd -q "$ZSH"; git rev-parse HEAD 2>/dev/null)
+#omz fpath: $fpath\
+"
+
+# Delete the zcompdump file if OMZ zcompdump metadata changed
+if ! cmp -s <(command grep '^#omz' "$ZSH_COMPDUMP" 2>/dev/null) <<< "$zcompdump_metadata"; then
+  command rm -f "$ZSH_COMPDUMP"
+  zcompdump_refresh=1
+fi
+
 if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
   source $ZSH/lib/compfix.zsh
   # If completion insecurities exist, warn the user
@@ -71,6 +83,13 @@ else
   # If the user wants it, load from all found directories
   compinit -u -C -d "${ZSH_COMPDUMP}"
 fi
+
+# Append zcompdump metadata if missing
+if (( $zcompdump_refresh )); then
+  echo "\n$zcompdump_metadata" >>! "$ZSH_COMPDUMP"
+fi
+
+unset zcompdump_metadata zcompdump_refresh
 
 
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
