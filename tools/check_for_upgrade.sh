b8f08ddac5de6865386854eecf7ae084241a88b5
Marc Cornellà <marc.cornella@live.com>
2020-05-16 18:33:44 +0200

    update: refactor check_for_upgrade file
    - Move .zsh-update file migration inside check_for_upgrade
    - Use a trap to remove the lock folder
    - Simplify upgrade script call
    - Rename functions

--- tools/check_for_upgrade.sh
+++ tools/check_for_upgrade.sh
@@ -1,62 +1,66 @@
-#!/usr/bin/env zsh
-
-zmodload zsh/datetime
-
-function _current_epoch() {
-  echo $(( $EPOCHSECONDS / 60 / 60 / 24 ))
-}
-
-function _update_zsh_update() {
-  echo "LAST_EPOCH=$(_current_epoch)" >! ${ZSH_CACHE_DIR}/.zsh-update
-}
-
-function _upgrade_zsh() {
-  env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
-  # update the zsh file
-  _update_zsh_update
-}
-
-epoch_target=$UPDATE_ZSH_DAYS
-if [[ -z "$epoch_target" ]]; then
-  # Default to old behavior
-  epoch_target=13
+# Migrate .zsh-update file to $ZSH_CACHE_DIR
+if [[ -f ~/.zsh-update && ! -f "${ZSH_CACHE_DIR}/.zsh-update" ]]; then
+    mv ~/.zsh-update "${ZSH_CACHE_DIR}/.zsh-update"
 fi
 
-# Cancel upgrade if the current user doesn't have write permissions for the
-# oh-my-zsh directory.
-[[ -w "$ZSH" ]] || return 0
-
-# Cancel upgrade if git is unavailable on the system
-whence git >/dev/null || return 0
-
-if mkdir "$ZSH/log/update.lock" 2>/dev/null; then
-  if [ -f ${ZSH_CACHE_DIR}/.zsh-update ]; then
-    . ${ZSH_CACHE_DIR}/.zsh-update
-
-    if [[ -z "$LAST_EPOCH" ]]; then
-      _update_zsh_update
-      rmdir $ZSH/log/update.lock # TODO: fix later
-      return 0
-    fi
-
-    epoch_diff=$(($(_current_epoch) - $LAST_EPOCH))
-    if [ $epoch_diff -gt $epoch_target ]; then
-      if [ "$DISABLE_UPDATE_PROMPT" = "true" ]; then
-        _upgrade_zsh
-      else
-        echo "[Oh My Zsh] Would you like to update? [Y/n]: \c"
-        read line
-        if [[ "$line" == Y* ]] || [[ "$line" == y* ]] || [ -z "$line" ]; then
-          _upgrade_zsh
-        else
-          _update_zsh_update
-        fi
-      fi
-    fi
-  else
-    # create the zsh file
-    _update_zsh_update
-  fi
-
-  rmdir $ZSH/log/update.lock
+# Cancel update if:
+# - the automatic update is disabled.
+# - the current user doesn't have write permissions for the oh-my-zsh directory.
+# - git is unavailable on the system.
+if [[ "$DISABLE_AUTO_UPDATE" = true || ! -w "$ZSH" ]] || ! command -v git &>/dev/null; then
+    return
 fi
+
+
+function current_epoch() {
+    zmodload zsh/datetime
+    echo $(( EPOCHSECONDS / 60 / 60 / 24 ))
+}
+
+function update_last-updated_file() {
+    echo "LAST_EPOCH=$(current_epoch)" >! "${ZSH_CACHE_DIR}/.zsh-update"
+}
+
+function update_ohmyzsh() {
+    ZSH="$ZSH" sh "$ZSH/tools/upgrade.sh"
+    update_last-updated_file
+}
+
+() {
+    emulate -L zsh
+
+    # Check for lock directory
+    if ! command mkdir "$ZSH/log/update.lock" 2>/dev/null; then
+        return
+    fi
+
+    # Remove lock directory on exit
+    trap "rm -rf '$ZSH/log/update.lock'" EXIT INT QUIT
+
+    # Create or update .zsh-update file if missing or malformed
+    if ! source "${ZSH_CACHE_DIR}/.zsh-update" 2>/dev/null || [[ -z "$LAST_EPOCH" ]]; then
+        update_last-updated_file
+        return
+    fi
+
+    # Number of days before trying to update again
+    epoch_target=${UPDATE_ZSH_DAYS:-13}
+    # Test if enough time has passed until the next update
+    if (( ( $(current_epoch) - $LAST_EPOCH ) < $epoch_target )); then
+        return
+    fi
+
+    # Ask for confirmation before updating unless disabled
+    if [[ "$DISABLE_UPDATE_PROMPT" = true ]]; then
+        update_ohmyzsh
+    else
+        echo -n "[oh-my-zsh] Would you like to update? [Y/n] "
+        read -k 1 option
+        case "$option" in
+            [nN]) update_last-updated_file ;;
+            *) update_ohmyzsh ;;
+        esac
+    fi
+}
+
+unset -f current_epoch update_last-updated_file update_ohmyzsh
