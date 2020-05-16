cbd6fbe7a7c3ef260f497704092dc43f2e953c3f
Marc Cornellà <marc.cornella@live.com>
2020-05-16 17:17:38 +0200

    core: enable hist_reduce_blanks
    Fixes #8722

--- lib/history.zsh
+++ lib/history.zsh
@@ -35,6 +35,7 @@ setopt extended_history       # record timestamp of command in HISTFILE
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_reduce_blanks     # remove superfluous blanks from commands added to history
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
