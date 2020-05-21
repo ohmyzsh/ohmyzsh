22f38b9fa7ec4c6050f483a243e323182162a7c2
Marc Cornellà <marc.cornella@live.com>
2020-05-21 20:21:34 +0200

    man: fix indentation

--- plugins/man/man.plugin.zsh
+++ plugins/man/man.plugin.zsh
@@ -13,23 +13,23 @@
 # ------------------------------------------------------------------------------
 
 man-command-line() {
-  # if there is no command typed, use the last command
-  [[ -z "$BUFFER" ]] && zle up-history
+    # if there is no command typed, use the last command
+    [[ -z "$BUFFER" ]] && zle up-history
 
-  # if typed command begins with man, do nothing
-  [[ "$BUFFER" = man\ * ]] && return
+    # if typed command begins with man, do nothing
+    [[ "$BUFFER" = man\ * ]] && return
 
-  # get command and possible subcommand
-  # http://zsh.sourceforge.net/Doc/Release/Expansion.html#Parameter-Expansion-Flags
-  local -a args
-  args=(${${(Az)BUFFER}[1]} ${${(Az)BUFFER}[2]})
+    # get command and possible subcommand
+    # http://zsh.sourceforge.net/Doc/Release/Expansion.html#Parameter-Expansion-Flags
+    local -a args
+    args=(${${(Az)BUFFER}[1]} ${${(Az)BUFFER}[2]})
 
-  # check if man page exists for command and first argument
-  if man "${args[1]}-${args[2]}" >/dev/null 2>&1; then
-    BUFFER="man $args"
-  else
-    BUFFER="man ${args[1]}"
-  fi
+    # check if man page exists for command and first argument
+    if man "${args[1]}-${args[2]}" >/dev/null 2>&1; then
+        BUFFER="man $args"
+    else
+        BUFFER="man ${args[1]}"
+    fi
 }
 
 zle -N man-command-line
