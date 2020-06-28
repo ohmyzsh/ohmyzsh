97cf0bf4bd7655767e9e5ea27ddb6fbe550a305d
Marc Cornellà <marc.cornella@live.com>
2020-06-28 18:54:26 +0200

    update: change dash in function name for sh compat (fixes #9064)

--- tools/check_for_upgrade.sh
+++ tools/check_for_upgrade.sh
@@ -19,13 +19,13 @@ function current_epoch() {
     echo $(( EPOCHSECONDS / 60 / 60 / 24 ))
 }
 
-function update_last-updated_file() {
+function update_last_updated_file() {
     echo "LAST_EPOCH=$(current_epoch)" >! "${ZSH_CACHE_DIR}/.zsh-update"
 }
 
 function update_ohmyzsh() {
     ZSH="$ZSH" sh "$ZSH/tools/upgrade.sh"
-    update_last-updated_file
+    update_last_updated_file
 }
 
 () {
@@ -55,7 +55,7 @@ function update_ohmyzsh() {
 
     # Create or update .zsh-update file if missing or malformed
     if ! source "${ZSH_CACHE_DIR}/.zsh-update" 2>/dev/null || [[ -z "$LAST_EPOCH" ]]; then
-        update_last-updated_file
+        update_last_updated_file
         return
     fi
 
@@ -80,9 +80,9 @@ function update_ohmyzsh() {
         [[ "$option" != $'\n' ]] && echo
         case "$option" in
             [yY$'\n']) update_ohmyzsh ;;
-            [nN]) update_last-updated_file ;;
+            [nN]) update_last_updated_file ;;
         esac
     fi
 }
 
-unset -f current_epoch update_last-updated_file update_ohmyzsh
+unset -f current_epoch update_last_updated_file update_ohmyzsh
