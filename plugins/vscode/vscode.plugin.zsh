7f8b17dd105e505caea4a2d8d783001d6ee460bc
Džiugas Eiva <dziugaseiva@gmail.com>
2020-07-03 22:39:01 +0100

    vscode: Add support for VSCodium Added checks before adding aliases in case no editor found Update README.md to match changes made

--- plugins/vscode/vscode.plugin.zsh
+++ plugins/vscode/vscode.plugin.zsh
@@ -3,28 +3,33 @@
 #   https://github.com/MarsiBarsi (original author)
 #   https://github.com/babakks
 
-# Use the stable VS Code release, unless the Insiders version is the only
-# available installation
-if ! which code > /dev/null && which code-insiders > /dev/null; then
-  : ${VSCODE:=code-insiders}
-else
+# This will pick an appropriate executable in order:
+# VSCode > VSCode Insiders > VSCodium
+if which code > /dev/null; then
   : ${VSCODE:=code}
+elif which code-insiders > /dev/null; then
+  : ${VSCODE:=code-insiders}
+elif which codium > /dev/null; then
+  : ${VSCODE:=codium}
 fi
 
-# Define aliases
-alias vsc="$VSCODE ."
-alias vsca="$VSCODE --add"
-alias vscd="$VSCODE --diff"
-alias vscg="$VSCODE --goto"
-alias vscn="$VSCODE --new-window"
-alias vscr="$VSCODE --reuse-window"
-alias vscw="$VSCODE --wait"
-alias vscu="$VSCODE --user-data-dir"
+# Only if a flavour of VSCode is available the aliases
+# will be available
+if ! [ -z ${VSCODE+x} ]; then
+  alias vsc="$VSCODE ."
+  alias vsca="$VSCODE --add"
+  alias vscd="$VSCODE --diff"
+  alias vscg="$VSCODE --goto"
+  alias vscn="$VSCODE --new-window"
+  alias vscr="$VSCODE --reuse-window"
+  alias vscw="$VSCODE --wait"
+  alias vscu="$VSCODE --user-data-dir"
 
-alias vsced="$VSCODE --extensions-dir"
-alias vscie="$VSCODE --install-extension"
-alias vscue="$VSCODE --uninstall-extension"
+  alias vsced="$VSCODE --extensions-dir"
+  alias vscie="$VSCODE --install-extension"
+  alias vscue="$VSCODE --uninstall-extension"
 
-alias vscv="$VSCODE --verbose"
-alias vscl="$VSCODE --log"
-alias vscde="$VSCODE --disable-extensions"
+  alias vscv="$VSCODE --verbose"
+  alias vscl="$VSCODE --log"
+  alias vscde="$VSCODE --disable-extensions"
+fi
