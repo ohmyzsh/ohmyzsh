221f2899a0c7fa9bf12ff6f5537aa765794f5910
Džiugas Eiva <dziugaseiva@gmail.com>
2020-07-04 21:37:47 +0100

    Efficient checks if VS Code exists or manually defined

--- plugins/vscode/vscode.plugin.zsh
+++ plugins/vscode/vscode.plugin.zsh
@@ -1,22 +1,10 @@
-# VScode zsh plugin
+# VS Code (stable / insiders) / VSCodium zsh plugin
 # Authors:
 #   https://github.com/MarsiBarsi (original author)
 #   https://github.com/babakks
 #   https://github.com/SteelShot
 
-# This will pick an appropriate executable in order:
-# VSCode > VSCode Insiders > VSCodium
-if which code > /dev/null; then
-  : ${VSCODE:=code}
-elif which code-insiders > /dev/null; then
-  : ${VSCODE:=code-insiders}
-elif which codium > /dev/null; then
-  : ${VSCODE:=codium}
-fi
-
-# Only if a flavour of VSCode is available the aliases
-# will be available
-if ! [ -z ${VSCODE+x} ] && which $VSCODE > /dev/null; then
+_vscode-register-aliases () {
   alias vsc="$VSCODE ."
   alias vsca="$VSCODE --add"
   alias vscd="$VSCODE --diff"
@@ -33,4 +21,22 @@ if ! [ -z ${VSCODE+x} ] && which $VSCODE > /dev/null; then
   alias vscv="$VSCODE --verbose"
   alias vscl="$VSCODE --log"
   alias vscde="$VSCODE --disable-extensions"
+}
+
+# Verify if any manual user choice of VS Code exists first.
+# Otherwise, try to detect a flavour of VS Code.
+if ! [ -z ${VSCODE+x} ] && which $VSCODE > /dev/null; then
+  _vscode-register-aliases
+else
+  if which code > /dev/null; then
+    : ${VSCODE:=code}
+  elif which code-insiders > /dev/null; then
+    : ${VSCODE:=code-insiders}
+  elif which codium > /dev/null; then
+    : ${VSCODE:=codium}
+  else
+    return -1
+  fi
+
+  _vscode-register-aliases
 fi
