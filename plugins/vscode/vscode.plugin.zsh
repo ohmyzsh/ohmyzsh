b489eec3eda12c3a9c8ffa10761efc457fdb2865
Marc Cornellà <marc.cornella@live.com>
2020-07-06 13:02:45 +0200

    Redo logic; add 'vsc.' alias and change 'vsc' alias

--- plugins/vscode/vscode.plugin.zsh
+++ plugins/vscode/vscode.plugin.zsh
@@ -4,41 +4,34 @@
 #   https://github.com/babakks
 #   https://github.com/SteelShot
 
-_vscode-register-aliases () {
-  alias vsc="$VSCODE ."
-  alias vsca="$VSCODE --add"
-  alias vscd="$VSCODE --diff"
-  alias vscg="$VSCODE --goto"
-  alias vscn="$VSCODE --new-window"
-  alias vscr="$VSCODE --reuse-window"
-  alias vscw="$VSCODE --wait"
-  alias vscu="$VSCODE --user-data-dir"
-
-  alias vsced="$VSCODE --extensions-dir"
-  alias vscie="$VSCODE --install-extension"
-  alias vscue="$VSCODE --uninstall-extension"
-
-  alias vscv="$VSCODE --verbose"
-  alias vscl="$VSCODE --log"
-  alias vscde="$VSCODE --disable-extensions"
-}
-
 # Verify if any manual user choice of VS Code exists first.
 # Otherwise, try to detect a flavour of VS Code.
-if ! [ -z ${VSCODE+x} ] && which $VSCODE > /dev/null; then
-  _vscode-register-aliases
-else
-  unset VSCODE
-
-  if which code > /dev/null; then
-    : ${VSCODE:=code}
-  elif which code-insiders > /dev/null; then
-    : ${VSCODE:=code-insiders}
-  elif which codium > /dev/null; then
-    : ${VSCODE:=codium}
+if [[ -z "$VSCODE" ]] || ! which $VSCODE &>/dev/null; then
+  if which code &>/dev/null; then
+    VSCODE=code
+  elif which code-insiders &>/dev/null; then
+    VSCODE=code-insiders
+  elif which codium &>/dev/null; then
+    VSCODE=codium
   else
-    return -1
+    return
   fi
-
-  _vscode-register-aliases
 fi
+
+alias vsc="$VSCODE"
+alias vsc.="$VSCODE ."
+alias vsca="$VSCODE --add"
+alias vscd="$VSCODE --diff"
+alias vscg="$VSCODE --goto"
+alias vscn="$VSCODE --new-window"
+alias vscr="$VSCODE --reuse-window"
+alias vscw="$VSCODE --wait"
+alias vscu="$VSCODE --user-data-dir"
+
+alias vsced="$VSCODE --extensions-dir"
+alias vscie="$VSCODE --install-extension"
+alias vscue="$VSCODE --uninstall-extension"
+
+alias vscv="$VSCODE --verbose"
+alias vscl="$VSCODE --log"
+alias vscde="$VSCODE --disable-extensions"
