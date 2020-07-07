7c54c9b854acae89ebdc7d352e2074b121712257
Monson Shao <holymonson@gmail.com>
2020-07-07 15:07:46 +0800

    separate metadata file

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -51,46 +51,29 @@ else
   SHORT_HOST=${HOST/.*/}
 fi
 
-# Save the location of the current completion dump file.
-if [ -z "$ZSH_COMPDUMP" ]; then
-  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
-fi
+# Construct OMZ metadata, if they changed then delete the zcompdump file.
+omz_meta="\
+host: $SHORT_HOST
+zsh version: $ZSH_VERSION
+revision: $(command git -C "$ZSH" rev-parse HEAD 2>/dev/null)
+fpath: $fpath"
 
-# Construct zcompdump OMZ metadata
-zcompdump_revision="#omz revision: $(builtin cd -q "$ZSH"; git rev-parse HEAD 2>/dev/null)"
-zcompdump_fpath="#omz fpath: $fpath"
-
-# Delete the zcompdump file if OMZ zcompdump metadata changed
-if ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null \
-   || ! command grep -q -Fx "$zcompdump_fpath" "$ZSH_COMPDUMP" 2>/dev/null; then
-  command rm -f "$ZSH_COMPDUMP"
-  zcompdump_refresh=1
+if ! command diff -q "$ZSH_CACHE_DIR/ohmyzsh-meta" - <<<"$omz_meta" &>/dev/null; then
+  command rm -f "${ZDOTDIR:-$HOME}/.zcompdump"
+  <<<"$omz_meta" >!"$ZSH_CACHE_DIR/ohmyzsh-meta"
 fi
+unset omz_meta
 
 if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
   source $ZSH/lib/compfix.zsh
   # If completion insecurities exist, warn the user
   handle_completion_insecurities
   # Load only from secure directories
-  compinit -i -C -d "${ZSH_COMPDUMP}"
+  compinit -i -C
 else
   # If the user wants it, load from all found directories
-  compinit -u -C -d "${ZSH_COMPDUMP}"
+  compinit -u -C
 fi
-
-# Append zcompdump metadata if missing
-if (( $zcompdump_refresh )); then
-  # Use `tee` in case the $ZSH_COMPDUMP filename is invalid, to silence the error
-  # See https://github.com/ohmyzsh/ohmyzsh/commit/dd1a7269#commitcomment-39003489
-  tee -a "$ZSH_COMPDUMP" &>/dev/null <<EOF
-
-$zcompdump_revision
-$zcompdump_fpath
-EOF
-fi
-
-unset zcompdump_revision zcompdump_fpath zcompdump_refresh
-
 
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
