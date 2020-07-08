5164dd14e8aa41e4b66a052068e22f8384a4df31
Lutz Horn <code@lhorn.de>
2020-07-08 19:01:15 +0200

    Match an "email" in an AWS profile
    fixes #9060 by matching a `@` in the AWS profile name.

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -41,7 +41,7 @@ function aws_change_access_key() {
 
 function aws_profiles() {
   [[ -r "${AWS_CONFIG_FILE:-$HOME/.aws/config}" ]] || return 1
-  grep '\[profile' "${AWS_CONFIG_FILE:-$HOME/.aws/config}"|sed -e 's/.*profile \([a-zA-Z0-9_\.-]*\).*/\1/'
+  grep '\[profile' "${AWS_CONFIG_FILE:-$HOME/.aws/config}"|sed -e 's/.*profile \([a-zA-Z0-9@_\.-]*\).*/\1/'
 }
 
 function _aws_profiles() {
