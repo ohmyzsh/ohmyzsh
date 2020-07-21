77d90b2408de6bb2323411487605b47d44d3f7b6
bb2020 <bb2020@users.noreply.github.com>
2020-07-21 10:34:10 +0300

    clipboard: rename plugin
    clipboard: update readme and plugin name

---
+++ plugins/clipboard/README.md
@@ -0,0 +1,9 @@
+# clipboard plugin
+
+This plugin integrates zsh kill-ring with system clipboard.
+
+It makes it possible to kill in zsh and paste into browser/editor or vice versa.
+
+Key bindings that rebound by the plugin are `C-y`, `C-k`, `C-u`, `M-d`, `M-backspace` and `M-w`.
+
+The plugin should not change behaviour of these keys.
--- plugins/clipboard/clipboard.plugin.zsh
+++ plugins/clipboard/clipboard.plugin.zsh
@@ -1,40 +1,40 @@
-_kc-yank() {
+_cb-yank() {
   AA=$(clippaste 2>/dev/null) && CUTBUFFER="$AA"
   zle yank
 }
-_kc-kill-line() {
+_cb-kill-line() {
   zle kill-line
   printf "%s" "$CUTBUFFER" | clipcopy 2>/dev/null
 }
-_kc-kill-whole-line() {
+_cb-kill-whole-line() {
   zle kill-whole-line
   printf "%s" "$CUTBUFFER" | clipcopy 2>/dev/null
 }
-_kc-kill-word() {
+_cb-kill-word() {
   zle kill-word
   printf "%s" "$CUTBUFFER" | clipcopy 2>/dev/null
 }
-_kc-backward-kill-word() {
+_cb-backward-kill-word() {
   zle backward-kill-word
   printf "%s" "$CUTBUFFER" | clipcopy 2>/dev/null
 }
-_kc-copy-region-as-kill() {
+_cb-copy-region-as-kill() {
   ## https://unix.stackexchange.com/questions/19947/
   zle copy-region-as-kill
   zle set-mark-command -n -1
   printf "%s" "$CUTBUFFER" | clipcopy 2>/dev/null
 }
 
-zle -N _kc-yank
-zle -N _kc-kill-line
-zle -N _kc-kill-whole-line
-zle -N _kc-kill-word
-zle -N _kc-backward-kill-word
-zle -N _kc-copy-region-as-kill
+zle -N _cb-yank
+zle -N _cb-kill-line
+zle -N _cb-kill-whole-line
+zle -N _cb-kill-word
+zle -N _cb-backward-kill-word
+zle -N _cb-copy-region-as-kill
 
-bindkey '^y'   _kc-yank
-bindkey '^k'   _kc-kill-line
-bindkey '^u'   _kc-kill-whole-line
-bindkey '\ed'  _kc-kill-word
-bindkey '\e^?' _kc-backward-kill-word
-bindkey '\ew'  _kc-copy-region-as-kill
+bindkey '^y'   _cb-yank
+bindkey '^k'   _cb-kill-line
+bindkey '^u'   _cb-kill-whole-line
+bindkey '\ed'  _cb-kill-word
+bindkey '\e^?' _cb-backward-kill-word
+bindkey '\ew'  _cb-copy-region-as-kill
