880dce7fb06c1c8d9d3e71c36ebb2b57d2d1975d
Lucas Larson <LucasLarson@riseup.net>
2020-09-06 12:12:01 -0400

    optimize `grename` logic by reducing `if` condition
    The function to rename a branch using `grename`
    requires both arguments `$1` and `$2`, but `$2`
    requires the existence of `$1`. Only one check is
    needed.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -279,7 +279,7 @@ alias gama='git am --abort'
 alias gamscp='git am --show-current-patch'
 
 function grename() {
-  if [[ -z "$1" || -z "$2" ]]; then
+  if [[ -z "$2" ]]; then
     echo "Usage: $0 old_branch new_branch"
     return 1
   fi
