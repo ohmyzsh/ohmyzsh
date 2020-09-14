65602ce89f3e5b09245f4bb49fb9a9f1530b9bdd
mattmc3 <mattmc3@gmail.com>
2020-09-14 11:49:00 -0400

    Move compinit logic to lib

--- lib/_compinit.zsh
+++ lib/_compinit.zsh
@@ -13,7 +13,7 @@ function handle_completion_insecurities() {
   #     /usr/share/zsh/5.0.6
   #
   # Since the ignorable first line is printed to stderr and thus not captured,
-  # stderr is squelched to prevent this output from leaking to the user. 
+  # stderr is squelched to prevent this output from leaking to the user.
   local -aU insecure_dirs
   insecure_dirs=( ${(f@):-"$(compaudit 2>/dev/null)"} )
 
@@ -42,3 +42,39 @@ function handle_completion_insecurities() {
 
 EOD
 }
+
+function run_compinit() {
+  # Construct zcompdump OMZ metadata
+  local zcompdump_revision="#omz revision: $(builtin cd -q "$ZSH"; git rev-parse HEAD 2>/dev/null)"
+  local zcompdump_fpath="#omz fpath: $fpath"
+  local zcompdump_refresh=
+
+  # Delete the zcompdump file if OMZ zcompdump metadata changed
+  if ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null \
+     || ! command grep -q -Fx "$zcompdump_fpath" "$ZSH_COMPDUMP" 2>/dev/null; then
+    command rm -f "$ZSH_COMPDUMP"
+    zcompdump_refresh=1
+  fi
+
+  if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
+    # If completion insecurities exist, warn the user
+    handle_completion_insecurities
+    # Load only from secure directories
+    compinit -i -C -d "${ZSH_COMPDUMP}"
+  else
+    # If the user wants it, load from all found directories
+    compinit -u -C -d "${ZSH_COMPDUMP}"
+  fi
+
+  # Append zcompdump metadata if missing
+  if (( $zcompdump_refresh )); then
+    # Use `tee` in case the $ZSH_COMPDUMP filename is invalid, to silence the error
+    # See https://github.com/ohmyzsh/ohmyzsh/commit/dd1a7269#commitcomment-39003489
+    tee -a "$ZSH_COMPDUMP" &>/dev/null <<EOF
+
+$zcompdump_revision
+$zcompdump_fpath
+EOF
+    fi
+}
+run_compinit
