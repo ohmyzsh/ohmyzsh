bc421935ba01db08c4b98ccfc37ad064670008f4
chewblacka <jgarcia3788@yahoo.co.uk>
2020-09-14 20:52:01 +0100

    Update z.sh
    Misplaced " in case command prevented excluded directories from being matched

--- plugins/z/z.sh
+++ plugins/z/z.sh
@@ -57,7 +57,7 @@ _z() {
         # don't track excluded directory trees
         local exclude
         for exclude in "${_Z_EXCLUDE_DIRS[@]}"; do
-            case "$*" in "$exclude*") return;; esac
+            case "$*" in "$exclude"*) return;; esac
         done
 
         # maintain the data file
