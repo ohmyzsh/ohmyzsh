83a1ef780c9b588652b247b514cd18833eef12ef
Fabian Bonk <mail@fabianbonk.de>
2020-09-28 20:48:02 +0200

    git: try to run gfa with --jobs=10

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -104,7 +104,11 @@ function gdv() { git diff -w "$@" | view - }
 compdef _git gdv=git-diff
 
 alias gf='git fetch'
-alias gfa='git fetch --all --prune --jobs=10'
+# --jobs=<n> was added in git 2.8
+autoload -Uz is-at-least
+is-at-least 2.8 "$(git --version 2>/dev/null | awk '{print $3}')" \
+  && alias gfa='git fetch --all --prune --jobs=10' \
+  || alias gfa='git fetch --all --prune'
 alias gfo='git fetch origin'
 
 alias gfg='git ls-files | grep'
@@ -240,7 +244,6 @@ alias gss='git status -s'
 alias gst='git status'
 
 # use the default stash push on git 2.13 and newer
-autoload -Uz is-at-least
 is-at-least 2.13 "$(git --version 2>/dev/null | awk '{print $3}')" \
   && alias gsta='git stash push' \
   || alias gsta='git stash save'
