53677a6a577f4883dd70cedc1dd4ecc19ea0ff45
Mark Mercado <mamercad@gmail.com>
2020-10-06 11:59:35 -0400

    Adds grc (Generic Colouriser) plugin

---
+++ plugins/grc/README.md
@@ -0,0 +1,9 @@
+# Generic Colouriser plugin
+
+This plugin adds completion for [Generic Colouriser](https://github.com/garabik/grc):
+
+To use it, add `grc` to the plugins array in your zshrc file:
+
+```zsh
+plugins=(... grc)
+```
---
+++ plugins/grc/grc.plugin.zsh
@@ -0,0 +1,41 @@
+# Source: https://github.com/garabik/grc/blob/master/grc.zsh
+if [[ "$TERM" != dumb ]] && (( $+commands[grc] )) ; then
+
+  # Supported commands
+  cmds=(
+    cc \
+    configure \
+    cvs \
+    df \
+    diff \
+    dig \
+    gcc \
+    gmake \
+    ifconfig \
+    last \
+    ldap \
+    ls \
+    make \
+    mount \
+    mtr \
+    netstat \
+    ping \
+    ping6 \
+    ps \
+    traceroute \
+    traceroute6 \
+    wdiff \
+    whois \
+    iwconfig \
+  );
+
+  # Set alias for available commands.
+  for cmd in $cmds ; do
+    if (( $+commands[$cmd] )) ; then
+      alias $cmd="grc --colour=auto $(whence $cmd)"
+    fi
+  done
+
+  # Clean up variables
+  unset cmds cmd
+fi
