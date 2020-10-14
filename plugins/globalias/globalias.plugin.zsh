53cbd658f5ae6874af0d804cee6748dfba69e786
Marc Cornellà <marc.cornella@live.com>
2020-10-14 17:23:03 +0200

    globalias: use ${(z)var} to split into words using shell parsing

--- plugins/globalias/globalias.plugin.zsh
+++ plugins/globalias/globalias.plugin.zsh
@@ -1,6 +1,8 @@
 globalias() {
-   # Get last word to the left of the cursor
-   local word=${${(A)=LBUFFER}[-1]}
+   # Get last word to the left of the cursor:
+   # (z) splits into words using shell parsing
+   # (A) makes it an array even if there's only one element
+   local word=${${(Az)LBUFFER}[-1]}
    if [[ $GLOBALIAS_FILTER_VALUES[(Ie)$word] -eq 0 ]]; then
       zle _expand_alias
       zle expand-word
