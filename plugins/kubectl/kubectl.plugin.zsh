a7d2540f19deaafbab04bc1a26065189ce4c0aba
Marc Cornellà <marc.cornella@live.com>
2020-10-16 00:51:31 +0200

    Code style change

--- plugins/kubectl/kubectl.plugin.zsh
+++ plugins/kubectl/kubectl.plugin.zsh
@@ -150,22 +150,12 @@ alias kepvc='kubectl edit pvc'
 alias kdpvc='kubectl describe pvc'
 alias kdelpvc='kubectl delete pvc'
 
-# Colored JSON output
-kj() {
-  kubectl "$@" -o json | jq
-}
+# Only run if the user actually has kubectl installed
+if (( $+commands[kubectl] )); then
+  kj() { kubectl "$@" -o json | jq; }
+  kjx() { kubectl "$@" -o json | fx; }
+  ky() { kubectl "$@" -o yaml | yh; }
 
-kjx() {
-  kubectl "$@" -o json | fx
-}
-
-# Colored YAML output
-ky() {
-  kubectl "$@" -o yaml | yh
-}
-
-# only run if the user actually has kubectl installed
-if type kubectl > /dev/null; then
   compdef kj=kubectl
   compdef kjx=kubectl
   compdef ky=kubectl
