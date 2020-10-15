b29eab1e2588a29dacfe682d121b6d00131623a9
Emrah Urhan <raxetul@users.noreply.github.com>
2020-10-15 12:33:26 +0000

    gm- is added for 'git merge -'

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -192,6 +192,7 @@ alias gloga='git log --oneline --decorate --graph --all'
 alias glp="_git_log_prettily"
 
 alias gm='git merge'
+alias gm-='git merge -'
 alias gmom='git merge origin/$(git_main_branch)'
 alias gmt='git mergetool --no-prompt'
 alias gmtvim='git mergetool --no-prompt --tool=vimdiff'
