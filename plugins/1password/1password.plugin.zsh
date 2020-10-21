39844402fc783036253fea42102d3f0ffd7df59a
Ricky Medina <ricky.medina91@gmail.com>
2020-10-21 13:53:58 -0600

    pr comments - redirect output to devnull

--- plugins/1password/1password.plugin.zsh
+++ plugins/1password/1password.plugin.zsh
@@ -1,3 +1,7 @@
+if type op  >/dev/null 2>&1 ; then
+    eval "$(op completion zsh)"; compdef _op op
+fi
+
 # opswd puts the password of the named service into the clipboard. If there's a
 # one time password, it will be copied into the clipboard after 5 seconds. The
 # clipboard is cleared after another 10 seconds.
@@ -7,8 +11,8 @@ function opswd() {
 	return 1
     fi
 
-    if ! type pbcopy; then
-	if ! type xclip; then
+    if ! type pbcopy >/dev/null 2>&1; then
+	if ! type xclip >/dev/null 2>&1; then
 	    echo "must have pbcopy or xclip"
 	    return 1
 	else
