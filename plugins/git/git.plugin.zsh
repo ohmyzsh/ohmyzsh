b09906948af3562ee8f8c57e4f96c58c5bb8745f
Dan Seaman <9471363+danseaman6@users.noreply.github.com>
2020-10-29 15:48:54 -0400

    Update git.plugin.zsh
    Diffing against local main branch - can be useful if you want to see the changes you made summarized without pushing to a remote repo handler.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -98,6 +98,8 @@ alias gdct='git describe --tags $(git rev-list --tags --max-count=1)'
 alias gds='git diff --staged'
 alias gdt='git diff-tree --no-commit-id --name-only -r'
 alias gdw='git diff --word-diff'
+alias gdmh='git diff $(git_main_branch)...HEAD'
+alias gdnomh='git diff --name-only $(git_main_branch)...HEAD'
 
 function gdnolock() {
   git diff "$@" ":(exclude)package-lock.json" ":(exclude)*.lock"
