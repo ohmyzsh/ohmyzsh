1d187c04ffbf867ba03252a3a5432e23db3eb89b
ZhaoChongyan <zhaochongyan2005@outlook.com>
2020-11-04 10:55:19 +0800

    修改：     plugins/command-not-found/command-not-found.plugin.zsh 	新文件：   plugins/gcc/gcc.plugin.zsh

--- plugins/command-not-found/command-not-found.plugin.zsh
+++ plugins/command-not-found/command-not-found.plugin.zsh
@@ -1,8 +1,21 @@
 # Uses the command-not-found package zsh support
 # as seen in https://www.porcheron.info/command-not-found-for-zsh/
 # this is installed in Ubuntu
-
-[[ -e /etc/zsh_command_not_found ]] && source /etc/zsh_command_not_found
+if [ -x /usr/lib/command-not-found -o -x /usr/share/command-not-found/command-not-found ]; then
+	function command_not_found_handle {
+	        # check because c-n-f could've been removed in the meantime
+                if [ -x /usr/lib/command-not-found ]; then
+		   /usr/lib/command-not-found -- "$1"
+                   return $?
+                elif [ -x /usr/share/command-not-found/command-not-found ]; then
+		   /usr/share/command-not-found/command-not-found -- "$1"
+                   return $?
+		else
+		   printf "%s: command not found\n" "$1" >&2
+		   return 127
+		fi
+	}
+fi
 
 # Arch Linux command-not-found support, you must have package pkgfile installed
 # https://wiki.archlinux.org/index.php/Pkgfile#.22Command_not_found.22_hook
---
+++ plugins/gcc/gcc.plugin.zsh
@@ -0,0 +1,3 @@
+# colored GCC warnings and errors
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
