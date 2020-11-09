4f70a512b8f77800ef492ed7ae2b09f4873d55ec
Subhaditya Nath <sn03.general@gmail.com>
2020-11-09 21:09:55 +0530

    sudo: Support aliases to $EDITOR
    If you use some alias for your $EDITOR, say `e`. You type `e /to/file`.
    But that file needs sudo permission. You naturally do <Esc><Esc><Enter>.
    But, to your dismay, it errors out `command not found`, because it
    didn't detect your alias to $EDITOR, thereby failing to convert it to
    `sudoedit`.
    
    This commit tries to detect your alias and convert it to sudoedit.

--- plugins/sudo/sudo.plugin.zsh
+++ plugins/sudo/sudo.plugin.zsh
@@ -9,6 +9,7 @@
 # -------
 #
 # * Dongweiming <ciici123@gmail.com>
+# * Subhaditya Nath <github.com/subnut>
 #
 # ------------------------------------------------------------------------------
 
@@ -17,10 +18,59 @@ sudo-command-line() {
 
     # Save beginning space
     local WHITESPACE=""
-    if [[ ${LBUFFER:0:1} == " " ]] ; then 
+    if [[ ${LBUFFER:0:1} == " " ]] ; then
         WHITESPACE=" "
         LBUFFER="${LBUFFER:1}"
     fi
+
+    # Support for aliases to $EDITOR
+    () {
+        if [[ -z $EDITOR ]]; then                       # EDITOR not set, so no work
+            return
+        fi
+        _EDITOR=$EDITOR                                 # To later restore the original value
+        _CURRENT=$(echo -n $BUFFER | cut -d' ' -f1)     # Get current command (to check for alias)
+        if [[ $_CURRENT == $EDITOR ]]; then             # If current command is same as EDITOR, no need to check further.
+            return 0
+        fi
+        if [[ $_CURRENT == 'sudoedit' ]]; then          # If current command is sudoedit, no need to check further.
+            return 0
+        fi
+        if [[ $EDITOR =~ ' ' ]]; then
+            EDITOR="'$EDITOR"                           # If EDITOR has spaces, then alias will be wrapped in ''
+        fi
+
+        # Check for aliases that match the value of $EDITOR
+        if
+            () {
+                for alias in $(alias | grep "=$EDITOR" | cut -d= -f1)
+                do  if [[ $alias = $_CURRENT ]]; then
+                        EDITOR=$alias   # set EDITOR to the alias
+                        return 0
+                    fi
+                done
+                return 1    # none of these aliases match
+            };
+        then
+            return
+
+        # Maybe alias is set to the variable instead of it's value
+        elif
+            () {
+                for alias in $(alias | grep \=\'\$EDITOR | cut -d= -f1)
+                do  if [[ $alias = $_CURRENT ]]; then
+                        EDITOR=$alias   # set EDITOR to the alias
+                        return 0
+                    fi
+                done
+                return 1    # none of these aliases match
+            }
+        then
+            return
+        fi
+
+        EDITOR=$_EDITOR     # Nothing matched. Restore $EDITOR
+    }
 
     if [[ -n $EDITOR && $BUFFER == $EDITOR\ * ]]; then
         if [[ ${#LBUFFER} -le ${#EDITOR} ]]; then
@@ -49,9 +99,16 @@ sudo-command-line() {
 
     # Preserve beginning space
     LBUFFER="${WHITESPACE}${LBUFFER}"
+
+    # Restore EDITOR if previously changed
+    if [[ -n $_EDITOR ]]; then
+        EDITOR=$_EDITOR
+    fi
 }
 zle -N sudo-command-line
 # Defined shortcut keys: [Esc] [Esc]
 bindkey -M emacs '\e\e' sudo-command-line
 bindkey -M vicmd '\e\e' sudo-command-line
 bindkey -M viins '\e\e' sudo-command-line
+
+# vim: et ts=4
