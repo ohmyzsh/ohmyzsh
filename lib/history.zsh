28ed2880c766eb5a360354fb71d597dbc07abaa0
Marc Cornellà <marc.cornella@live.com>
2020-11-09 12:00:06 +0100

    Revert "lib: remove share_history"
    This reverts commit 23760228908d14a4644718869d5ebfb7b0dde6a7.
    
    Fixes #9430

--- lib/history.zsh
+++ lib/history.zsh
@@ -37,3 +37,4 @@ setopt hist_expire_dups_first # delete duplicates first when HISTFILE size excee
 setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
