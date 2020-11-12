c63d48bc98e0a171c8ffc6cb23d0ffa00e069970
ivan tkachenko <me@ratijas.tk>
2020-11-12 18:39:13 +0300

    colored-man-pages [1/4]: convert tabs to spaces

--- plugins/colored-man-pages/colored-man-pages.plugin.zsh
+++ plugins/colored-man-pages/colored-man-pages.plugin.zsh
@@ -1,39 +1,39 @@
 if [[ "$OSTYPE" = solaris* ]]
 then
-	if [[ ! -x "$HOME/bin/nroff" ]]
-	then
-		mkdir -p "$HOME/bin"
-		cat > "$HOME/bin/nroff" <<EOF
+  if [[ ! -x "$HOME/bin/nroff" ]]
+  then
+    mkdir -p "$HOME/bin"
+    cat > "$HOME/bin/nroff" <<EOF
 #!/bin/sh
 if [ -n "\$_NROFF_U" -a "\$1,\$2,\$3" = "-u0,-Tlp,-man" ]; then
-	shift
-	exec /usr/bin/nroff -u\$_NROFF_U "\$@"
+  shift
+  exec /usr/bin/nroff -u\$_NROFF_U "\$@"
 fi
 #-- Some other invocation of nroff
 exec /usr/bin/nroff "\$@"
 EOF
-		chmod +x "$HOME/bin/nroff"
-	fi
+    chmod +x "$HOME/bin/nroff"
+  fi
 fi
 
 function colored() {
-	command env \
-		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-		LESS_TERMCAP_md=$(printf "\e[1;31m") \
-		LESS_TERMCAP_me=$(printf "\e[0m") \
-		LESS_TERMCAP_se=$(printf "\e[0m") \
-		LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-		LESS_TERMCAP_ue=$(printf "\e[0m") \
-		LESS_TERMCAP_us=$(printf "\e[1;32m") \
-		PAGER="${commands[less]:-$PAGER}" \
-		_NROFF_U=1 \
-		PATH="$HOME/bin:$PATH" \
-			"$@"
+  command env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    PAGER="${commands[less]:-$PAGER}" \
+    _NROFF_U=1 \
+    PATH="$HOME/bin:$PATH" \
+      "$@"
 }
 
 # Colorize man and dman/debman (from debian-goodies)
 function man \
-	dman \
-	debman {
-	colored $0 "$@"
+  dman \
+  debman {
+  colored $0 "$@"
 }
