51772732f54f01522ad5720e8ed0ba16ca3147ae
Alisson B. Santos <alissonbruno.sa@gmail.com>
2020-11-12 16:17:28 +0100

    feat(git): support trunk branches in `git_main_branch` (#9417)

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -31,11 +31,14 @@ function work_in_progress() {
 
 # Check if main exists and use instead of master
 function git_main_branch() {
-  if [[ -n "$(git branch --list main)" ]]; then
-    echo main
-  else
-    echo master
-  fi
+  local branch
+  for branch in main trunk; do
+    if command git show-ref -q --verify refs/heads/$branch; then
+      echo $branch
+      return
+    fi
+  done
+  echo master
 }
 
 #
