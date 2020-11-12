3fb1843358f3bf05465d9bc2abb232023a81d26f
ivan tkachenko <me@ratijas.tk>
2020-11-12 19:45:06 +0300

    colored-man-pages [2/4]: Extract Solaris hack
    Also refactors and documents existing behavior.

--- plugins/colored-man-pages/colored-man-pages.plugin.zsh
+++ plugins/colored-man-pages/colored-man-pages.plugin.zsh
@@ -1,22 +1,14 @@
-if [[ "$OSTYPE" = solaris* ]]
-then
-  if [[ ! -x "$HOME/bin/nroff" ]]
-  then
-    mkdir -p "$HOME/bin"
-    cat > "$HOME/bin/nroff" <<EOF
-#!/bin/sh
-if [ -n "\$_NROFF_U" -a "\$1,\$2,\$3" = "-u0,-Tlp,-man" ]; then
-  shift
-  exec /usr/bin/nroff -u\$_NROFF_U "\$@"
-fi
-#-- Some other invocation of nroff
-exec /usr/bin/nroff "\$@"
-EOF
-    chmod +x "$HOME/bin/nroff"
-  fi
-fi
+# Absolute path to this file's directory.
+typeset __colored_man_pages_dir="${0:A:h}"
 
 function colored() {
+  local -a environment
+
+  # See ./nroff script.
+  if [[ "$OSTYPE" = solaris* ]]; then
+    environment+=( PATH="${__colored_man_pages_dir}:$PATH" )
+  fi
+
   command env \
     LESS_TERMCAP_mb=$(printf "\e[1;31m") \
     LESS_TERMCAP_md=$(printf "\e[1;31m") \
@@ -26,8 +18,7 @@ function colored() {
     LESS_TERMCAP_ue=$(printf "\e[0m") \
     LESS_TERMCAP_us=$(printf "\e[1;32m") \
     PAGER="${commands[less]:-$PAGER}" \
-    _NROFF_U=1 \
-    PATH="$HOME/bin:$PATH" \
+    $environment \
       "$@"
 }
 
