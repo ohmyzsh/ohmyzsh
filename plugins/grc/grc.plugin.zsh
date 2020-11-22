8ac2baba6aff3133d3caa4ea5ab73ce2875ce879
Marc Cornellà <marc.cornella@live.com>
2020-11-22 16:22:32 +0100

    Use function wrappers and fix code style

--- plugins/grc/grc.plugin.zsh
+++ plugins/grc/grc.plugin.zsh
@@ -1,41 +1,44 @@
-# Source: https://github.com/garabik/grc/blob/master/grc.zsh
-if [[ "$TERM" != dumb ]] && (( $+commands[grc] )) ; then
+# Adapted from: https://github.com/garabik/grc/blob/master/grc.zsh
 
-  # Supported commands
-  cmds=(
-    cc \
-    configure \
-    cvs \
-    df \
-    diff \
-    dig \
-    gcc \
-    gmake \
-    ifconfig \
-    last \
-    ldap \
-    ls \
-    make \
-    mount \
-    mtr \
-    netstat \
-    ping \
-    ping6 \
-    ps \
-    traceroute \
-    traceroute6 \
-    wdiff \
-    whois \
-    iwconfig \
-  );
-
-  # Set alias for available commands.
-  for cmd in $cmds ; do
-    if (( $+commands[$cmd] )) ; then
-      alias $cmd="grc --colour=auto $(whence $cmd)"
-    fi
-  done
-
-  # Clean up variables
-  unset cmds cmd
+if [[ "$TERM" = dumb ]] || (( ! $+commands[grc] )); then
+  return
 fi
+
+# Supported commands
+cmds=(
+  cc
+  configure
+  cvs
+  df
+  diff
+  dig
+  gcc
+  gmake
+  ifconfig
+  iwconfig
+  last
+  ldap
+  make
+  mount
+  mtr
+  netstat
+  ping
+  ping6
+  ps
+  traceroute
+  traceroute6
+  wdiff
+  whois
+)
+
+# Set alias for supported commands
+for cmd in $cmds; do
+  if (( $+commands[$cmd] )); then
+    eval "function $cmd {
+      grc --colour=auto \"${commands[$cmd]}\" \"\$@\"
+    }"
+  fi
+done
+
+# Clean up variables
+unset cmds cmd
