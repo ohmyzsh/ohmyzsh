90bef526fb0e9c3079090827e08b3d9a341569ac
Christophe Lecoq <39101348+krisprols@users.noreply.github.com>
2020-12-06 02:01:23 +0100

    using /dev/stdout to export configuration

--- plugins/rclone/rclone.plugin.zsh
+++ plugins/rclone/rclone.plugin.zsh
@@ -3,6 +3,5 @@
 # Docs: https://rclone.org/commands/rclone_genautocomplete_zsh/
 
 if [ $commands[rclone] ]; then
-  source <(rclone genautocomplete zsh /tmp/rclone-plugin.$$ && cat /tmp/rclone-plugin.$$)
-  rm /tmp/rclone-plugin.$$
+  source <(rclone genautocomplete zsh /dev/stdout)
 fi
