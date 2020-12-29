4fcc5c2f3eef961532f0d055feb4c0e08f1b78f5
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-29 14:01:27 +0100

    Changed to compdef impl

--- plugins/gh/gh.plugin.zsh
+++ plugins/gh/gh.plugin.zsh
@@ -1,15 +1,6 @@
 # Autocompletion for the GitHub CLI (gh).
-#
-# Copy from kubectl : https://github.com/pstadler
 
 if (( $+commands[gh] )); then
-    __GH_COMPLETION_FILE="${ZSH_CACHE_DIR}/gh_completion"
-
-    if [[ ! -f $__GH_COMPLETION_FILE || ! -s $__GH_COMPLETION_FILE ]]; then
-        gh completion -s zsh >! $__GH_COMPLETION_FILE
-    fi
-
-    [[ -f $__GH_COMPLETION_FILE ]] && source $__GH_COMPLETION_FILE
-
-    unset __GH_COMPLETION_FILE
+    eval "$(gh completion -s zsh)"
+    compdef _gh gh
 fi
