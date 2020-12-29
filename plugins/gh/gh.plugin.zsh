efb455158f0bfa20e9d57ea6f19ef2a1e3967505
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-29 13:39:27 +0100

    Changed implementation

--- plugins/gh/gh.plugin.zsh
+++ plugins/gh/gh.plugin.zsh
@@ -2,6 +2,14 @@
 #
 # Copy from kubectl : https://github.com/pstadler
 
-if [ $commands[gh] ]; then
-  source <(gh completion --shell zsh)
+if (( $+commands[gh] )); then
+    __GH_COMPLETION_FILE="${ZSH_CACHE_DIR}/gh_completion"
+
+    if [[ ! -f $__GH_COMPLETION_FILE || ! -s $__GH_COMPLETION_FILE ]]; then
+        gh completion -s zsh >! $__GH_COMPLETION_FILE
+    fi
+
+    [[ -f $__GH_COMPLETION_FILE ]] && source $__GH_COMPLETION_FILE
+
+    unset __GH_COMPLETION_FILE
 fi
