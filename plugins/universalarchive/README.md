57d18916755eea4aad243a11e421e637bb3f5e3f
Marc Cornellà <marc.cornella@live.com>
2021-01-05 18:00:20 +0100

    refactor(universalarchive): fix several bugs, style and refactor changes

--- plugins/universalarchive/README.md
+++ plugins/universalarchive/README.md
@@ -1,19 +1,7 @@
 # universalarchive plugin
 
-Lets you compress files by a command `ua <format> <files>` into various compression formats;
-namely, `<format>` could be `zip`, `xz`, ..
-
-## Usage
-
-For example, the command
-```sh
-ua xz *.html
-```
-will compress all `html` files in directory `folder` into `folder.xz`.
-
-This command saves you from having to remember which command line arguments compress a file.
-
-## Installation
+Lets you compress files by a command `ua <format> <files>`, supporting various
+compression formats (e.g. 7z, tar.gz, lzma, ...).
 
 To enable it, add `universalarchive` to the plugins array in your zshrc file:
 
@@ -21,29 +9,38 @@ To enable it, add `universalarchive` to the plugins array in your zshrc file:
 plugins=(... universalarchive)
 ```
 
-## Supported file extensions
+## Usage
 
-| Extension         | Description                          |
-|:------------------|:-------------------------------------|
-| `7z`              | 7zip file                            |
-| `Z`               | Z archive (LZW)                      |
-| `bz2`             | Bzip2 file                           |
-| `gz`              | Gzip file                            |
-| `lzma`            | LZMA archive                         |
-| `lzo`             | LZO archive                          |
-| `rar`             | WinRAR archive                       |
-| `tar`             | Tarball                              |
-| `tar.bz2`         | Tarball with bzip2 compression       |
-| `tar.gz`          | Tarball with gzip compression        |
-| `tar.xz`          | Tarball with lzma2 compression       |
-| `tar.lzma`        | Tarball with lzma compression        |
-| `tbz`             | Tarball with bzip compression        |
-| `tbz2`            | Tarball with bzip2 compression       |
-| `tgz`             | Tarball with gzip compression        |
-| `tlz`             | Tarball with lzma compression        |
-| `txz`             | Tarball with lzma2 compression       |
-| `xz`              | LZMA2 archive                        |
-| `zip`             | Zip archive                          |
-| `zst`             | Zstd archive                         |
+Run `ua <format> <files>` to compress `<files>` into an archive file using `<format>`.
+For example:
+
+```sh
+ua xz *.html
+```
+
+this command will compress all `.html` files in directory `folder` into `folder.xz`.
+
+This plugin saves you from having to remember which command line arguments compress a file.
+
+## Supported compression formats
+
+| Extension        | Description                    |
+|:-----------------|:-------------------------------|
+| `7z`             | 7zip file                      |
+| `bz2`            | Bzip2 file                     |
+| `gz`             | Gzip file                      |
+| `lzma`           | LZMA archive                   |
+| `lzo`            | LZO archive                    |
+| `rar`            | WinRAR archive                 |
+| `tar`            | Tarball                        |
+| `tbz`/`tar.bz2`  | Tarball with bzip2 compression |
+| `tgz`/`tar.gz`   | Tarball with gzip compression  |
+| `tlz`/`tar.lzma` | Tarball with lzma compression  |
+| `txz`/`tar.xz`   | Tarball with lzma2 compression |
+| `tZ`/`tar.Z`     | Tarball with LZW compression   |
+| `xz`             | LZMA2 archive                  |
+| `Z`              | Z archive (LZW)                |
+| `zip`            | Zip archive                    |
+| `zst`            | Zstd archive                   |
 
 See [list of archive formats](https://en.wikipedia.org/wiki/List_of_archive_formats) for more information regarding the archive formats.
--- plugins/universalarchive/universalarchive.plugin.zsh
+++ plugins/universalarchive/universalarchive.plugin.zsh
@@ -1,56 +1,70 @@
-ua() { # compress a file or folder
-  local file
-  local parent
-  local name
+function ua() {
+  local usage=\
+"Archive files and directories using a given compression algorithm.
 
-  local type="$1"
-  local input="$2"
+Usage:   $0 <format> <files>
+Example: $0 tbz PKGBUILD
 
-  if [ -f "$input" ]; then
-    if [ "$#" -eq 2 ]; then
-      name="${input%.[^.]*}"
-    else
-      file="$(realpath "$input")" 
-      parent="${file%/*}" 
-      name="${parent##*/}"
-    fi
-  elif [ -d "$input" ]; then
-    name="${input%/*}"
-  else
-    echo "Please give a valid file or directory name!"
+Supported archive formats are:
+7z, bz2, gz, lzma, lzo, rar, tar, tbz (tar.bz2), tgz (tar.gz),
+tlz (tar.lzma), txz (tar.xz), tZ (tar.Z), xz, Z, zip, and zst."
+
+  if [[ $# -lt 2 ]]; then
+    echo >&2 "$usage"
+    return 1
   fi
 
-  if [ -f "${name}.${type}" ]; then
-    name=$(mktemp "./${name}_XXX.${type}") && rm "$name"
+  local ext="$1"
+  local input="$(realpath "$2")"
+
+  shift
+
+  if [[ ! -e "$input" ]]; then
+    echo >&2 "$input not found"
+    return 1
   fi
 
-  if [ "$#" -ge 1 ]; then
-    shift
+  # generate output file name
+  local output
+  if [[ $# -gt 1 ]]; then
+    output="$(basename "${input%/*}")"
+  elif [[ -f "$input" ]]; then
+    output="$(basename "${input%.[^.]*}")"
+  elif [[ -d "$input" ]]; then
+    output="$(basename "${input}")"
   fi
-  case "$type" in
-    tar)          tar     -cvf            "${name}.tar"       "${@}" ;;
-    tbz|tar.bz)   tar     -cvjf           "${name}.tar.bz2"   "${@}"  ;;
-    tbz2|tar.bz2) tar     -cvjf           "${name}.tar.bz2"   "${@}" ;;
-    txz|tar.xz)   XZ_OPT=-T0 tar -cvJf     "${name}.tar.xz"    "${@}" ;;
-    tgz|tar.gz)   tar      -cvzf           "${name}.tar.gz"    "${@}" ;;
-    tlz|tar.lzma) XZ_OPT=-T0 tar --lzma -cvf "${name}.tar.gz"  "${@}" ;;
-    tZ|tar.Z)     tar      -cvZf           "${name}.tar.Z"     "${@}" ;;
-    zip)          zip      -rull          "${name}.zip"       "${@}" ;;
-    7z|7zip)      7z       u              "${name}.7z"        "${@}" ;;
-    zst)          zstd     -c -T0         "${name}.zst"       "${@}" ;;
-    rar)          rar      a              "${name}.rar"       "${@}" ;;
-    lzo)          lzop     -vc            "${@}"            > "${name}.lzo" ;;
-    gz|gzip)      gzip     -vcf           "${@}"            > "${name}.gz" ;;
-    bz|bzip)      bzip2    -vcf           "${@}"            > "${name}.bz" ;;
-    bz2|bzip2)    bzip2    -vcf           "${@}"            > "${name}.bz2" ;;
-    Z|compress)   compress -vcf           "${@}"            > "${name}.Z" ;;
-    xz)           xz       -vc -T0        "${@}"            > "${name}.xz" ;;
-    lzma)         lzma     -vc -T0        "${@}"            > "${name}.lzma" ;;
-    *)  echo "$0:      archive files or directory using a given compression algorithm."
-        echo "Usage:   $0 <archive type> <files>"
-        echo "Example: $0 tbz2 PKGBUILD"
-        echo "Supported archive types are:"
-        echo "tar, tbz2, txz, tgz, tZ, gz, bz2, Z,"
-        echo "zip, 7z, zst, rar, lzo, xz, and lzma."                   ;;
+
+  # if output file exists, generate a random name
+  if [[ -f "${output}.${ext}" ]]; then
+    output=$(mktemp "${output}_XXX") && rm "$output" || return 1
+  fi
+
+  # add extension
+  output="${output}.${ext}"
+
+  # safety check
+  if [[ -f "$output" ]]; then
+    echo >&2 "output file '$output' already exists. Aborting"
+    return 1
+  fi
+
+  case "$ext" in
+    7z)           7z u                        "${output}"   "${@}" ;;
+    bz2)          bzip2 -vcf                  "${@}" > "${output}" ;;
+    gz)           gzip -vcf                   "${@}" > "${output}" ;;
+    lzma)         lzma -vc -T0                "${@}" > "${output}" ;;
+    lzo)          lzop -vc                    "${@}" > "${output}" ;;
+    rar)          rar a                       "${output}"   "${@}" ;;
+    tar)          tar -cvf                    "${output}"   "${@}" ;;
+    tbz|tar.bz2)  tar -cvjf                   "${output}"   "${@}" ;;
+    tgz|tar.gz)   tar -cvzf                   "${output}"   "${@}" ;;
+    tlz|tar.lzma) XZ_OPT=-T0 tar --lzma -cvf  "${output}"   "${@}" ;;
+    txz|tar.xz)   XZ_OPT=-T0 tar -cvJf        "${output}"   "${@}" ;;
+    tZ|tar.Z)     tar -cvZf                   "${output}"   "${@}" ;;
+    xz)           xz -vc -T0                  "${@}" > "${output}" ;;
+    Z)            compress -vcf               "${@}" > "${output}" ;;
+    zip)          zip -rull                   "${output}"   "${@}" ;;
+    zst)          zstd -c -T0                 "${@}" > "${output}" ;;
+    *) echo >&2 "$usage"; return 1 ;;
   esac
 }
