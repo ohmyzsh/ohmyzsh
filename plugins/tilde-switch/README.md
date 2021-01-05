0ab9c43e73dd6f07b1a39aa65f651c48c4fb0c25
Mevlüt Canvar <mevlutcanvar@gmail.com>
2021-01-06 00:08:35 +0300

    initial tilde-switch plugin commit

---
+++ plugins/tilde-switch/README.md
@@ -0,0 +1,9 @@
+# tilde-switch plugin
+
+By default, on some non-US keyboards of Macbooks like Swedish and Turkish, tilde key switched with a plus-minus key. So this plugin switches back the `±` key to `~` without needing any third-party tool or service.
+
+To use it, add `tilde-switch` to the plugins array of your `.zshrc` file:
+```
+plugins=(... tilde-switch)
+```
+
---
+++ plugins/tilde-switch/tilde-switch.plugin.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+#
+# tilde-switch-zsh
+#
+# This key binding plugin gets tilde key back on MacOS 
+# Swithes back ± key to ~ 
+#
+# Copyright(c) 2021 Mevlut Canvar <mevlutcanvar@gmail.com>
+# MIT Licensed
+#
+
+#
+# Mevlüt Canvar
+# GitHub: https://github.com/mcanvar
+# Twitter: https://twitter.com/mevlutcanvar
+#
+
+#
+# This solution taken from here: https://apple.stackexchange.com/a/353941
+#
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}' > /dev/null
