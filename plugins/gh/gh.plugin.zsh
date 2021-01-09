c8a719ebb87911ee230ce7db474b1652b887f922
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-01-09 19:27:10 +0100

    Changed implementation to use fpath

--- plugins/gh/gh.plugin.zsh
+++ plugins/gh/gh.plugin.zsh
@@ -1,6 +1,31 @@
 # Autocompletion for the GitHub CLI (gh).
 
+autoload -U add-zsh-hook
+
+_gh_completion_auto_update() {
+    if [[ $+commands[gh] -eq 0 \
+        && ( ! -r "$ZSH_CACHE_DIR/gh_version" \
+        || "$(gh --version)" != "$(< "$ZSH_CACHE_DIR/gh_version")" ) ]]; then
+        mkdir -p $ZSH/completions
+        gh completion --shell zsh > $ZSH/completions/_gh
+        gh --version > $ZSH_CACHE_DIR/gh_version
+    fi
+}
+
+PERIOD=86400 # 24h
+add-zsh-hook periodic _gh_completion_auto_update
+
 if (( $+commands[gh] )); then
-    eval "$(gh completion -s zsh)"
-    compdef _gh gh
+    if [[ ! -f "$ZSH/completions/_gh" ]]; then
+        mkdir -p $ZSH/completions
+        gh completion --shell zsh > $ZSH/completions/_gh
+        gh --version > $ZSH_CACHE_DIR/gh_version
+    fi
+
+    if ! (($fpath[(I)$ZSH/completions])); then
+        # When using some package managers, such as zinit, the $ZSH/completions
+        # dir is not added to fpath
+        fpath+=($ZSH/completions)
+    fi
 fi
+
