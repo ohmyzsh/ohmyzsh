99f3fbd2e07f56046a4faec5c9c8344bd5bb5161
Marc Cornellà <hello@mcornella.com>
2021-01-18 12:35:45 +0100

    Fix code style and several bugs

--- plugins/1password/1password.plugin.zsh
+++ plugins/1password/1password.plugin.zsh
@@ -1,37 +1,36 @@
-if type op  >/dev/null 2>&1 ; then
-    eval "$(op completion zsh)"; compdef _op op
+if (( ${+commands[op]} )); then
+  eval "$(op completion zsh)"
+  compdef _op op
 fi
 
 # opswd puts the password of the named service into the clipboard. If there's a
 # one time password, it will be copied into the clipboard after 5 seconds. The
 # clipboard is cleared after another 10 seconds.
 function opswd() {
-    if [[ $# < 1 ]]; then
-	echo "Usage: opswd <service>"
-	return 1
-    fi
+  if [[ $# -lt 1 ]]; then
+    echo "Usage: opswd <service>"
+    return 1
+  fi
 
-    if ! type pbcopy >/dev/null 2>&1; then
-	if ! type xclip >/dev/null 2>&1; then
-	    echo "must have pbcopy or xclip"
-	    return 1
-	else
-	    alias pbcopy='xclip -selection clipboard'
-	fi
-    fi
+  local service=$1
 
-    local service=$1
+  # If not logged in, print error and return
+  op list users > /dev/null || return
 
-    (( ! oploggedin )) && opsignin
+  op get item "$service" \
+  | jq -r '.details.fields[] | select(.designation=="password").value' \
+  | clipcopy
 
-    op get item $service \
-	| jq -r '.details.fields[] | select(.designation=="password").value'\
-	| pbcopy
-
-    ( sleep 5 && op get totp $service | pbcopy
-      sleep 10 && pbcopy < /dev/null 2>&1 & ) &!
+  (
+    sleep 5 && op get totp "$service" 2>/dev/null | clipcopy 2>/dev/null
+    sleep 10 && clipcopy < /dev/null 2>/dev/null &
+  ) &!
 }
 
-function oploggedin() {
-    op list users &> /dev/null
+function _opswd() {
+  local -a services
+  services=("${(@f)$(op list items --categories Login 2>/dev/null | jq -r '.[].overview.title')}")
+  [[ -z "$services" ]] || compadd -a -- services
 }
+
+compdef _opswd opswd
--- plugins/1password/README.md
+++ plugins/1password/README.md
@@ -4,7 +4,9 @@ This plugin adds 1Password functionality to oh-my-zsh.
 
 To use, add `1password` to the list of plugins in your `.zshrc` file:
 
-`plugins=(... 1password)`
+```zsh
+plugins=(... 1password)
+```
 
 Then, you can use the command `opswd` to copy passwords for services into your
 clipboard.
@@ -13,6 +15,12 @@ For example, `opswd GitHub` will put your GitHub password into your clipboard - 
 onetime password exists for the service, your clipboard will contain it after a
 brief pause. Everything is cleared after few seconds.
 
-You need to have
-[1Password's command line utility](https://1password.com/downloads/command-line/)
-installed as well as [`jq`](https://stedolan.github.io/jq/).
+NOTE: you need to be logged in for `opswd` to work. See:
+
+- [Sign in or out](https://support.1password.com/command-line/#sign-in-or-out)
+- [Session management](https://support.1password.com/command-line/#appendix-session-management)
+
+## Requirements
+
+- [1Password's command line utility](https://1password.com/downloads/command-line/).
+- [`jq`](https://stedolan.github.io/jq/).
