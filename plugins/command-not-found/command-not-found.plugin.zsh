6ae78695efa8c222d716db267041f3ad19114d98
Mario A <marioanacl22@gmail.com>
2021-02-14 14:44:59 +0100

    feat(command-not-found): add support for Termux (#9666)

--- plugins/command-not-found/command-not-found.plugin.zsh
+++ plugins/command-not-found/command-not-found.plugin.zsh
@@ -53,3 +53,10 @@ if [[ -x /run/current-system/sw/bin/command-not-found ]]; then
     /run/current-system/sw/bin/command-not-found -- "$@"
   }
 fi
+
+# Termux: https://github.com/termux/command-not-found
+if [[ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]]; then
+  command_not_found_handler() {
+    /data/data/com.termux/files/usr/libexec/termux/command-not-found -- "$1"
+  }
+fi
