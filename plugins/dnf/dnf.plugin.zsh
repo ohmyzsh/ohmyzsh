f12dba0b27802f8d72a64581cd1cfbce1921ac2b
Eduard Lucena <eduardlucena@gmail.com>
2021-03-04 14:05:33 -0300

    Adding -y to install and upgrade commands
    Adding options to use the "Answer yes/Assume yes" into install and upgrade commands

--- plugins/dnf/dnf.plugin.zsh
+++ plugins/dnf/dnf.plugin.zsh
@@ -12,4 +12,10 @@ alias dnfi="sudo dnf install"               # Install package
 alias dnfgi="sudo dnf groupinstall"         # Install package group
 alias dnfr="sudo dnf remove"                # Remove package
 alias dnfgr="sudo dnf groupremove"          # Remove package group
+alias dnfuy="sudo dnf -y upgrade"           # Upgrade package answering "yes" automatically
+alias dnfiy="sudo dnf -y install"           # Install package answering "yes" automatically
+alias dnfgiy="sudo dnf -y groupinstall"     # Install package group answering "yes" automatically
+alias dnfry="sudo dnf -y remove"            # Remove package answering "yes" automatically
+alias dnfgry="sudo dnf -y groupremove"      # Remove package group answering "yes" automatically
+
 alias dnfc="sudo dnf clean all"             # Clean cache
