4ce83d4cf8396ebb1c4b7fe37885392b0c6d8b0b
loynoir <78727408+loynoir@users.noreply.github.com>
2021-04-07 11:17:58 +0000

    Create pnpm.plugin.zsh

---
+++ plugins/pnpm/pnpm.plugin.zsh
@@ -0,0 +1,20 @@
+# extract from `pnpm install-completion zsh`
+
+###-begin-pnpm-completion-###
+if type compdef &>/dev/null; then
+  _pnpm_completion () {
+    local reply
+    local si=$IFS
+
+    IFS=$'\n' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" pnpm completion -- "${words[@]}"))
+    IFS=$si
+
+    if [ "$reply" = "__tabtab_complete_files__" ]; then
+      _files
+    else
+      _describe 'values' reply
+    fi
+  }
+  compdef _pnpm_completion pnpm
+fi
+###-end-pnpm-completion-###
