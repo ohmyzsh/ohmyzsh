25432ec448dda884eb741fa53bbfbfbaea129e79
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-09 10:06:25 -0400

    feat(plugin): adds projen - https://github.com/projen/projen

---
+++ plugins/projen/projen.plugin.zsh
@@ -0,0 +1,17 @@
+function pj_install() {
+  npm install -g projen
+}
+
+alias pgjn='projen new'
+alias pgjv='projen --version'
+
+alias pj='npx projen'
+
+alias pjv='pj --version'
+alias pjb='pj build'
+
+alias pjd='pj deploy'
+alias pjdd='pj diff'
+alias pjD='pj destroy'
+
+alias pjU='pj projen:upgrade'
