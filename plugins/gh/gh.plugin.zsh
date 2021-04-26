e05f1c7351ed9cd359e22bce69ac06e03b3f0b3b
Kalle Jillheden <kalle.jillheden@iver.se>
2021-04-26 13:48:53 +0200

    Removed 'mkdir' and zinit workaround

--- plugins/gh/gh.plugin.zsh
+++ plugins/gh/gh.plugin.zsh
@@ -1,18 +1,11 @@
 # Autocompletion for the GitHub CLI (gh).
 
 if (( $+commands[gh] )); then
-    if [[ ! -r "$ZSH_CACHE_DIR/gh_version" \
-        || "$(gh --version)" != "$(< "$ZSH_CACHE_DIR/gh_version")"
-        || ! -f "$ZSH/plugins/gh/_gh" ]]; then
-        mkdir -p $ZSH/plugins/gh
-        gh completion --shell zsh > $ZSH/plugins/gh/_gh
-        gh --version > $ZSH_CACHE_DIR/gh_version
-    fi
-
-    if ! (($fpath[(I)$ZSH/plugins/gh])); then
-        # When using some package managers, such as zinit, the $ZSH/plugins/gh
-        # dir is not added to fpath
-        fpath+=($ZSH/plugins/gh)
-    fi
+  if [[ ! -r "$ZSH_CACHE_DIR/gh_version" \
+    || "$(gh --version)" != "$(< "$ZSH_CACHE_DIR/gh_version")"
+    || ! -f "$ZSH/plugins/gh/_gh" ]]; then
+    gh completion --shell zsh > $ZSH/plugins/gh/_gh
+    gh --version > $ZSH_CACHE_DIR/gh_version
+  fi
 fi
 
