43b678c6a80dcaff8e29ef86a9a35718c4555a01
Kalle Jillheden <kalle.jillheden@iver.se>
2021-04-26 09:34:47 +0200

    Removed add-zsh-hook

--- plugins/gh/gh.plugin.zsh
+++ plugins/gh/gh.plugin.zsh
@@ -1,22 +1,9 @@
 # Autocompletion for the GitHub CLI (gh).
 
-autoload -U add-zsh-hook
-
-_gh_completion_auto_update() {
+if (( $+commands[gh] )); then
     if [[ $+commands[gh] -eq 0 \
         && ( ! -r "$ZSH_CACHE_DIR/gh_version" \
         || "$(gh --version)" != "$(< "$ZSH_CACHE_DIR/gh_version")" ) ]]; then
-        mkdir -p $ZSH/plugins/gh
-        gh completion --shell zsh > $ZSH/plugins/gh/_gh
-        gh --version > $ZSH_CACHE_DIR/gh_version
-    fi
-}
-
-PERIOD=86400 # 24h
-add-zsh-hook periodic _gh_completion_auto_update
-
-if (( $+commands[gh] )); then
-    if [[ ! -f "$ZSH/plugins/gh/_gh" ]]; then
         mkdir -p $ZSH/plugins/gh
         gh completion --shell zsh > $ZSH/plugins/gh/_gh
         gh --version > $ZSH_CACHE_DIR/gh_version
