ohmyzsh/ohmyzsh

commit 0e5fed193edf0073fab5adaac61f7b912ea86060

feat(ys): add virtualenv prompt info (#8453)

themes/ys.zsh-theme
@@ -37,6 +37,15 @@ ys_hg_prompt_info() {
 	fi
 }
 
+# Virtualenv
+local venv_info='$(virtenv_prompt)'
+YS_THEME_VIRTUALENV_PROMPT_PREFIX=" %{$fg[green]%}"
+YS_THEME_VIRTUALENV_PROMPT_SUFFIX=" %{$reset_color%}%"
+virtenv_prompt() {
+	[[ -n ${VIRTUAL_ENV} ]] || return
+	echo "${YS_THEME_VIRTUALENV_PROMPT_PREFIX}${VIRTUAL_ENV:t}${YS_THEME_VIRTUALENV_PROMPT_SUFFIX}"
+}
+
 local exit_code="%(?,,C:%{$fg[red]%}%?%{$reset_color%})"
 
 # Prompt format:
@@ -57,6 +66,7 @@ PROMPT="
 %{$terminfo[bold]$fg[yellow]%}%~%{$reset_color%}\
 ${hg_info}\
 ${git_info}\
+${venv_info}\
  \
 %{$fg[white]%}[%*] $exit_code
 %{$terminfo[bold]$fg[red]%}$ %{$reset_color%}"

commit 2897a2c10cd42473a438c159beae83d8ed45bb2f

fix(ys): Don't fail if `$VIRTUAL_ENV` is not set in shell with `-o nounset`
Introduced in 0e5fed193edf0073fab5adaac61f7b912ea86060

themes/ys.zsh-theme
@@ -42,7 +42,7 @@ local venv_info='$(virtenv_prompt)'
 YS_THEME_VIRTUALENV_PROMPT_PREFIX=" %{$fg[green]%}"
 YS_THEME_VIRTUALENV_PROMPT_SUFFIX=" %{$reset_color%}%"
 virtenv_prompt() {
-	[[ -n ${VIRTUAL_ENV} ]] || return
+	[[ -n ${VIRTUAL_ENV+set} ]] || return
 	echo "${YS_THEME_VIRTUALENV_PROMPT_PREFIX}${VIRTUAL_ENV:t}${YS_THEME_VIRTUALENV_PROMPT_SUFFIX}"
 }
 