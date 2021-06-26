2372c8dfee64be6b3c6e83c9eee3a3e3477fcaee
Moulishankar10 <moulishankarmr@gmail.com>
2021-06-26 14:15:57 +0530

    Added alias for pypi-ltest in pypi plugin

--- plugins/pypi/pypi.plugin.zsh
+++ plugins/pypi/pypi.plugin.zsh
@@ -1,4 +1,5 @@
 alias pypi-check='pip3 install twine setuptools'
+alias pypi-ltest='python3 setup.py sdist && python3 -m pip install dist/*'
 alias pypi-tupload='python3 setup.py sdist && twine upload --repository testpypi dist/*'
 alias pypi-upload='python3 setup.py sdist && twine upload dist/*'
 
