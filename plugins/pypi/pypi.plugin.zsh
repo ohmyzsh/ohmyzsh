40ab2e14d3be91b66f574f7f7151a9bf90115de3
Moulishankar10 <moulishankarmr@gmail.com>
2021-06-26 14:15:00 +0530

    Added alias for pypi-tupload

--- plugins/pypi/pypi.plugin.zsh
+++ plugins/pypi/pypi.plugin.zsh
@@ -1,3 +1,5 @@
 alias pypi-check='pip3 install twine setuptools'
-alias pypi-upload='python3 setup.py sdist && twine upload dist/'
+alias pypi-tupload='python3 setup.py sdist && twine upload --repository testpypi dist/*'
+alias pypi-upload='python3 setup.py sdist && twine upload dist/*'
+
 
