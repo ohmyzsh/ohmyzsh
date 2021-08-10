a3d15ebc77e044fcaf2e4c8116f61f97f96f6c2f
Marc Cornellà <hello@mcornella.com>
2021-08-10 20:40:41 +0200

    Use precmd to display the octocat after all plugins have been loaded

--- plugins/octozen/octozen.plugin.zsh
+++ plugins/octozen/octozen.plugin.zsh
@@ -4,6 +4,9 @@
 #
 function display_octozen() {
   curl -m 2 -fsL "https://api.github.com/octocat"
+  add-zsh-hook -d precmd display_octozen
 }
 
-display_octozen
+# Display the octocat on the first precmd, after the whole starting process has finished
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd display_octozen
