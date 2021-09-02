409d6120ff26a9235795484f21a322b001e1964c
Vitaly Polonetsky <m_vitaly@topixoft.com>
2021-09-02 09:55:09 -0700

    Guess git main branch name even if it's not checked out locally yet

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -32,12 +32,15 @@ function work_in_progress() {
 # Check if main exists and use instead of master
 function git_main_branch() {
   command git rev-parse --git-dir &>/dev/null || return
+  local ref_prefix
   local branch
-  for branch in main trunk; do
-    if command git show-ref -q --verify refs/heads/$branch; then
-      echo $branch
-      return
-    fi
+  for ref_prefix in heads remotes/origin remotes/upstream; do
+    for branch in main trunk; do
+      if command git show-ref -q --verify refs/$ref_prefix/$branch; then
+        echo $branch
+        return
+      fi
+    done
   done
   echo master
 }
