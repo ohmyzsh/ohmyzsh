53b54f5faa271ee90ec31fcffb559541c82b9703
Vitaly Polonetsky <m_vitaly@topixoft.com>
2021-09-03 02:43:53 -0700

    feat(git): guess main branch name also from remotes (#10158)
    Co-authored-by: Marc Cornellà <hello@mcornella.com>

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -32,10 +32,10 @@ function work_in_progress() {
 # Check if main exists and use instead of master
 function git_main_branch() {
   command git rev-parse --git-dir &>/dev/null || return
-  local branch
-  for branch in main trunk; do
-    if command git show-ref -q --verify refs/heads/$branch; then
-      echo $branch
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
       return
     fi
   done
