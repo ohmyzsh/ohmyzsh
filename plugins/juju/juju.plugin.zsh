3b6e10a6ccc88a683322af49c52369f388ef6329
sed-i <82407168+sed-i@users.noreply.github.com>
2021-09-12 12:22:49 -0400

    address code review comments

--- plugins/juju/juju.plugin.zsh
+++ plugins/juju/juju.plugin.zsh
@@ -3,8 +3,11 @@
 # ---------------------------------------------------------- #
 
 # Load TAB completions
+# You need juju's bash completion script installed. By default bash-completion's                    
+# location will be used (i.e. pkg-config --variable=completionsdir bash-completion).
 () {
-  local completion_file="$(pkg-config --variable=completionsdir bash-completion)/juju"
+  local completion_file="$(pkg-config --variable=completionsdir bash-completion)/juju" ||
+    local completion_file="/usr/share/bash-completion/completions/juju"
   [[ -f "$completion_file" ]] && source "$completion_file"
 }
 
@@ -23,11 +26,11 @@ alias jc='juju config'
 alias jdc='juju destroy-controller --destroy-all-models'
 alias 'jdc!'='juju destroy-controller --destroy-all-models --force --no-wait -y'
 alias jdcds='juju destroy-controller --destroy-all-models --destroy-storage'
-alias jdcds!='juju destroy-controller --destroy-all-models --destroy-storage --force --no-wait -y'
+alias 'jdcds!'='juju destroy-controller --destroy-all-models --destroy-storage --force --no-wait -y'
 alias jdm='juju destroy-model'
-alias jdm!='juju destroy-model --force --no-wait -y'
+alias 'jdm!'='juju destroy-model --force --no-wait -y'
 alias jdmds='juju destroy-model --destroy-storage'
-alias jdmds!='juju destroy-model --destroy-storage --force --no-wait -y'
+alias 'jdmds!'='juju destroy-model --destroy-storage --force --no-wait -y'
 alias jde='juju deploy --channel=edge'
 alias jd='juju deploy'
 alias jdl='juju debug-log --ms'
@@ -44,14 +47,14 @@ alias jra='juju run-action'
 alias jraw='juju run-action --wait'
 alias jrel='juju relate'
 alias jrm='juju remove-application'
-alias jrm!='juju remove-application --force --no-wait'
+alias 'jrm!'='juju remove-application --force --no-wait'
 alias jrmds='juju remove-application --destroy-storage'
-alias jrmds!='juju remove-application --destroy-storage --force --no-wait'
+alias 'jrmds!'='juju remove-application --destroy-storage --force --no-wait'
 alias jrmrel='juju remove-relation'
 alias jrmsas='juju remove-saas'
 alias jrp='juju refresh --path'
 alias jrs='juju remove-storage'
-alias jrs!='juju remove-storage --force'
+alias 'jrs!'='juju remove-storage --force'
 alias jsa='juju scale-application'
 alias jsh='juju ssh'
 alias jshc='juju ssh --container'
@@ -66,57 +69,57 @@ alias jsw='juju switch'
 
 # Get app or unit address
 jaddr() {
-    # $1 = app name
-    # $2 = unit number (optional)
-    
-    if ! command -v jq &> /dev/null
-    then
-        echo "jq is required but could not be found."
-        return 1
-    fi
+  # $1 = app name
+  # $2 = unit number (optional)
+  
+  if ! [[ -x "$(command -v jq)" ]]; then
+    echo "jq is required but could not be found."
+    return 1
+  fi
 
-    
-    if [ "$#" -eq 1 ]; then
-        # Get app address
-        juju status "$1" --format=json | \
-          jq -r ".applications.\"$1\".address"
-        
-    elif [ "$#" -eq 2 ]; then
-        # Get unit address
-        juju status "$1/$2" --format=json | \
-          jq -r ".applications.\"$1\".units.\"$1/$2\".address"
+  if [[ "$#" -eq 1 ]]; then
+    # Get app address
+    juju status "$1" --format=json \
+      | jq -r ".applications.\"$1\".address"
+
+    elif [[ "$#" -eq 2 ]]; then
+      # Get unit address
+      juju status "$1/$2" --format=json \
+        | jq -r ".applications.\"$1\".units.\"$1/$2\".address"
         
     else
-        echo "Invalid number of arguments."
-        echo "Usage:   jaddr <app-name> [<unit-number>]"
-        echo "Example: jaddr karma"
-        echo "Example: jaddr karma 0"
-        return 1
+      echo "Invalid number of arguments."
+      echo "Usage:   jaddr <app-name> [<unit-number>]"
+      echo "Example: jaddr karma"
+      echo "Example: jaddr karma 0"
+      return 1
     fi
 }
 
 # Display app and unit relation data
 jreld() {
-    # $1 = relation name
-    # $2 = app name
-    # $3 = unit number
-    if [ "$#" -ne 3 ]; then
-        echo "Invalid number of arguments."
-        echo "Usage:   jreld <relation-name> <app-name> <unit-number>"
-        echo "Example: jreld karma-dashboard alertmanager 0"
-        return 1
-    fi
+  # $1 = relation name
+  # $2 = app name
+  # $3 = unit number
+  if [[ "$#" -ne 3 ]]; then
+    echo "Invalid number of arguments."
+    echo "Usage:   jreld <relation-name> <app-name> <unit-number>"
+    echo "Example: jreld karma-dashboard alertmanager 0"
+    return 1
+  fi
 
-    local relid=$(juju run "relation-ids $1" --unit $2/$3)
-    if [ -z "$relid" ]; then return 1; fi
-    
-    echo "App data:"
-    juju run "relation-get -r $relid --app - $2" --unit $2/$3
-    
-    echo
-    
-    echo "Unit data:"
-    juju run "relation-get -r $relid - $2" --unit $2/$3
+  local relid=$(juju run "relation-ids $1" --unit $2/$3)
+  if [[ -z "$relid" ]]; then 
+    return 1
+  fi
+
+  echo "App data:"
+  juju run "relation-get -r $relid --app - $2" --unit $2/$3
+
+  echo
+
+  echo "Unit data:"
+  juju run "relation-get -r $relid - $2" --unit $2/$3
 }
 
 # Watch juju status, with optional interval (default: 5 sec)
@@ -124,6 +127,5 @@ wjst() {
   local interval="${1:-5}"
   shift $(( $# > 0 ))
   watch -n "$interval" --color juju status --relations --storage --color "$@"
-  return $?
 }
 
