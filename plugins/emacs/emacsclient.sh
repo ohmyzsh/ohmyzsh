b86958c1a72d78f43f9998fbb4e8bd6060a969d7
Marc Cornellà <hello@mcornella.com>
2021-09-28 10:10:43 +0200

    refactor(emacs): simplify emacsclient wrapper code

--- plugins/emacs/emacsclient.sh
+++ plugins/emacs/emacsclient.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 
-_emacsfun()
-{
-    # get list of emacs frames.
-    frameslist=`emacsclient --alternate-editor '' --eval '(frame-list)' 2>/dev/null | egrep -o '(frame)+'`
+emacsfun() {
+  local frames="$(emacsclient --alternate-editor "" -n -e "(length (frame-list))" 2>/dev/null)"
 
-    if [ "$(echo "$frameslist" | sed -n '$=')" -ge 2 ] ;then
-        # prevent creating another X frame if there is at least one present.
-        emacsclient --alternate-editor "" "$@"
-    else
-        # Create one if there is no X window yet.
-        emacsclient --alternate-editor "" --create-frame "$@"
-    fi
+  # Only create another X frame if there isn't one present
+  if [ -z "$frames" -o "$frames" -lt 2 ]; then
+    emacsclient --alternate-editor "" --create-frame "$@"
+    return $?
+  fi
+
+  emacsclient --alternate-editor "" "$@"
 }
 
 
 # adopted from https://github.com/davidshepherd7/emacs-read-stdin/blob/master/emacs-read-stdin.sh
 # If the second argument is - then write stdin to a tempfile and open the
 # tempfile. (first argument will be `--no-wait` passed in by the plugin.zsh)
-if [ "$#" -ge "2" -a "$2" = "-" ]
-then
-    tempfile="$(mktemp --tmpdir emacs-stdin-$USERNAME.XXXXXXX 2>/dev/null \
-                || mktemp -t emacs-stdin-$USERNAME)" # support BSD mktemp
-    cat - > "$tempfile"
-    _emacsfun --no-wait $tempfile
-else
-    _emacsfun "$@"
+if [ $# -ge 2 -a "$2" = "-" ]; then
+  tempfile="$(mktemp --tmpdir emacs-stdin-$USERNAME.XXXXXXX 2>/dev/null \
+    || mktemp -t emacs-stdin-$USERNAME)" # support BSD mktemp
+  cat - > "$tempfile"
+  emacsfun --no-wait "$tempfile"
+  return $?
 fi
+
+emacsfun "$@"
