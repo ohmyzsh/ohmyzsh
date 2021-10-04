43f179f2768248947b4739918227df5da5e17354
Marc Cornellà <hello@mcornella.com>
2021-10-04 12:30:52 +0200

    Code style fixes

--- plugins/gpg-agent/gpg-agent.plugin.zsh
+++ plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,13 +1,17 @@
 export GPG_TTY=$TTY
 
-if [[ -n $(gpgconf --list-options gpg-agent | awk -F: '$1=="enable-ssh-support" {print $10}') ]]; then
+# Fix for passphrase prompt on the correct tty
+# See https://www.gnupg.org/documentation/manuals/gnupg/Agent-Options.html#option-_002d_002denable_002dssh_002dsupport
+function _gpg-agent_update-tty_preexec {
+  gpg-connect-agent updatestartuptty /bye &>/dev/null
+}
+autoload -U add-zsh-hook
+add-zsh-hook preexec _gpg-agent_update-tty_preexec
+
+# If enable-ssh-support is set, fix ssh agent integration
+if [[ $(gpgconf --list-options gpg-agent | awk -F: '$1=="enable-ssh-support" {print $10}') = 1 ]]; then
   unset SSH_AGENT_PID
   if [[ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]]; then
     export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
   fi
-  autoload -U add-zsh-hook
-  add-zsh-hook preexec _gpg-agent-update-tty
-  function _gpg-agent-update-tty {
-    gpg-connect-agent updatestartuptty /bye &>/dev/null
-  }
 fi
