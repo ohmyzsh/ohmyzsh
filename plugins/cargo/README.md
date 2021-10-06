7bbf17a998ea21ec6a8230fa54a443fbcdd81fa4
Carlo Sala <carlosalag@protonmail.com>
2021-10-06 22:43:09 +0200

    feat(cargo): merging rustup in cargo plugin

--- plugins/cargo/README.md
+++ plugins/cargo/README.md
@@ -1,11 +1,9 @@
 # cargo
 
-This plugin adds completion for the Rust build tool [`Cargo`](https://github.com/rust-lang/cargo).
+This plugin adds completion for the Rust build tool [`cargo`](https://github.com/rust-lang/cargo) and [`rustup`](https://rustup.rs), the toolchain installer for Rust.
 
 To use it, add `cargo` to the plugins array in your zshrc file:
 
 ```zsh
 plugins=(... cargo)
 ```
-
-Updated on March 3rd, 2019, from [Cargo 0.34.0](https://github.com/rust-lang/cargo/releases/tag/0.34.0).
--- plugins/cargo/cargo.plugin.zsh
+++ plugins/cargo/cargo.plugin.zsh
@@ -1,23 +1,41 @@
 if (( $+commands[rustup] && $+commands[cargo] )); then
-  # remove old generated completion file
+  # remove old generated completion files
+  command rm -f "${0:A:h}/_rustup"
   command rm -f "${0:A:h}/_cargo"
 
-  # generate new completion file
-  ver="$(cargo --version)"
-  ver_file="$ZSH_CACHE_DIR/cargo_version"
-  comp_file="$ZSH_CACHE_DIR/completions/_cargo"
+  # generate new completion files
 
-  mkdir -p "${comp_file:h}"
-  (( ${fpath[(Ie)${comp_file:h}]} )) || fpath=("${comp_file:h}" $fpath)
+  # rustup
+  ver1="$(rustup --version 2>/dev/null)"
+  ver_file1="$ZSH_CACHE_DIR/rustup_version"
+  comp_file1="$ZSH_CACHE_DIR/completions/_rustup"
 
-  if [[ ! -f "$comp_file" || ! -f "$ver_file" || "$ver" != "$(< "$ver_file")" ]]; then
-    rustup completions zsh cargo >| "$comp_file"
-    echo "$ver" >| "$ver_file"
+  mkdir -p "${comp_file1:h}"
+  (( ${fpath[(Ie)${comp_file1:h}]} )) || fpath=("${comp_file1:h}" $fpath)
+
+  if [[ ! -f "$comp_file1" || ! -f "$ver_file1" || "$ver1" != "$(< "$ver_file1")" ]]; then
+    rustup completions zsh >| "$comp_file1"
+    echo "$ver1" >| "$ver_file1"
+  fi
+
+  # cargo
+  ver2="$(cargo --version)"
+  ver_file2="$ZSH_CACHE_DIR/cargo_version"
+  comp_file2="$ZSH_CACHE_DIR/completions/_cargo"
+  mkdir -p "${comp_file2:h}"
+  (( ${fpath[(Ie)${comp_file2:h}]} )) || fpath=("${comp_file2:h}" $fpath)
+
+  if [[ ! -f "$comp_file2" || ! -f "$ver_file2" || "$ver2" != "$(< "$ver_file2")" ]]; then
+    rustup completions zsh cargo >| "$comp_file2"
+    echo "$ver2" >| "$ver_file2"
   fi
 
   declare -A _comps
+  autoload -Uz _rustup
+  _comps[rustup]=_rustup
   autoload -Uz _cargo
   _comps[cargo]=_cargo
 
+  unset ver ver_file comp_file
   unset ver ver_file comp_file
 fi
