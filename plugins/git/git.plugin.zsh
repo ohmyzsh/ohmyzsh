bf7c1b160d164f080a276e6ed6b21ef8008079ee
Carlo Sala <carlosalag@protonmail.com>
2021-10-06 10:13:38 +0200

    fix(git)!: rename `gdu` alias to `gdup` (#10263)
    BREAKING CHANGE: the `gdu` alias has been renamed to `gdup` to avoid a
    conflict with `go disk usage` (https://github.com/dundee/gdu).

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -126,7 +126,7 @@ alias gdcw='git diff --cached --word-diff'
 alias gdct='git describe --tags $(git rev-list --tags --max-count=1)'
 alias gds='git diff --staged'
 alias gdt='git diff-tree --no-commit-id --name-only -r'
-alias gdu='git diff @{u}'
+alias gdup='git diff @{upstream}'
 alias gdw='git diff --word-diff'
 
 function gdnolock() {
