89a740be2d5487c96f01e4eaae7e280caf555019
Carlo Sala <carlosalag@protonmail.com>
2021-10-06 22:43:09 +0200

    refactor(rust)!: merge `cargo` and `rustup` plugins into `rust` plugin (#10270)
    BREAKING CHANGE: `cargo` and `rustup` plugins have been moved to the `rust` plugin
    since they're part of the Rust toolchain. If you're using these, update your .zshrc
    file to use `rust` instead.
    
    Closes #10270
    
    Co-authored-by: Marc Cornellà <hello@mcornella.com>

--- plugins/cargo/cargo.plugin.zsh
+++ plugins/cargo/cargo.plugin.zsh
@@ -1,24 +1,11 @@
-if ! (( $+commands[rustup] && $+commands[cargo] )); then
-  return
-fi
+print ${(%):-'%F{yellow}The `cargo` plugin is deprecated and has been moved to the `rust` plugin.'}
+print ${(%):-'Please update your .zshrc to use the `%Brust%b` plugin instead.%f'}
 
 # Remove old generated completion file
-# TODO: 2021-09-15: remove this line
+# TODO: 2021-12-28: remove this line
 command rm -f "${0:A:h}/_cargo"
 
-# Add completions/ folder in $ZSH_CACHE_DIR
-comp_file="$ZSH_CACHE_DIR/completions/_cargo"
-command mkdir -p "${comp_file:h}"
-(( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
-
-# If the completion file doesn't exist yet, we need to autoload it and
-# bind it to `cargo`. Otherwise, compinit will have already done that
-if [[ ! -f "$comp_file" ]]; then
-  autoload -Uz _cargo
-  declare -A _comps
-  _comps[cargo]=_cargo
-fi
-
-# Generate completion file in the background
-rustup completions zsh cargo >| "$comp_file" &|
-unset comp_file
+(( ${fpath[(Ie)$ZSH/plugins/rust]} )) || {
+  fpath=("$ZSH/plugins/rust" $fpath)
+  source "$ZSH/plugins/rust/rust.plugin.zsh"
+}
--- plugins/rustup/rustup.plugin.zsh
+++ plugins/rustup/rustup.plugin.zsh
@@ -1,24 +1,11 @@
-if (( ! $+commands[rustup] )); then
-  return
-fi
+print ${(%):-'%F{yellow}The `rustup` plugin is deprecated and has been moved to the `rust` plugin.'}
+print ${(%):-'Please update your .zshrc to use the `%Brust%b` plugin instead.%f'}
 
 # Remove old generated completion file
-# TODO: 2021-09-15: remove this line
+# TODO: 2021-12-28: remove this line
 command rm -f "${0:A:h}/_rustup"
 
-# Add completions/ folder in $ZSH_CACHE_DIR
-comp_file="$ZSH_CACHE_DIR/completions/_rustup"
-command mkdir -p "${comp_file:h}"
-(( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
-
-# If the completion file doesn't exist yet, we need to autoload it and
-# bind it to `rustup`. Otherwise, compinit will have already done that
-if [[ ! -f "$comp_file" ]]; then
-  autoload -Uz _rustup
-  declare -A _comps
-  _comps[rustup]=_rustup
-fi
-
-# Generate completion file in the background
-rustup completions zsh >| "$comp_file" &|
-unset comp_file
+(( ${fpath[(Ie)$ZSH/plugins/rust]} )) || {
+  fpath=("$ZSH/plugins/rust" $fpath)
+  source "$ZSH/plugins/rust/rust.plugin.zsh"
+}
