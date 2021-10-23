b4191da311abd5265ae8aa22d68fedd0c630bfa9
JamesHopbourn <jameshopbourn@gmail.com>
2021-10-23 16:44:25 +0800

    add alias git diff HEAD
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -121,6 +121,7 @@ alias gcss='git commit -S -s'
 alias gcssm='git commit -S -s -m'
 
 alias gd='git diff'
+alias gdh='git diff HEAD'
 alias gdca='git diff --cached'
 alias gdcw='git diff --cached --word-diff'
 alias gdct='git describe --tags $(git rev-list --tags --max-count=1)'
