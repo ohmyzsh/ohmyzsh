068feca15d09dce6747d0fb6cab2027b938e82bd
Philipp Schmitt <pschmitt@users.noreply.github.com>
2021-12-24 18:09:01 +0100

    feat(zsh-navigation-tools)!: update to zdharma-continuum fork (#10402)
    BREAKING CHANGE: the `zsh-navigation-tools` plugin now bundles the version from the
    zdharma-continuum project, which republished the projects that psprint deleted (we
    don't yet know yet why). In theory this change doesn't break anything, but be aware
    of the change and that the upstream repository now lives somewhere else.
    
    Source: https://github.com/zdharma-continuum/zsh-navigation-tools/commit/67b24e1

--- plugins/zsh-navigation-tools/zsh-navigation-tools.plugin.zsh
+++ plugins/zsh-navigation-tools/zsh-navigation-tools.plugin.zsh
@@ -8,7 +8,7 @@
 #
 
 # According to the standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 export ZNT_REPO_DIR="${0:h}"
