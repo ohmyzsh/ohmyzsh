8e5c90240001b5dcbb2fd2e43dc3e2d6c5b87d64
Marc Cornellà <hello@mcornella.com>
2021-12-29 16:05:48 +0100

    style(terraform): fix code style in `tf_prompt_info`

--- plugins/terraform/terraform.plugin.zsh
+++ plugins/terraform/terraform.plugin.zsh
@@ -1,9 +1,10 @@
 function tf_prompt_info() {
-    # dont show 'default' workspace in home dir
-    [[ "$PWD" == ~ ]] && return
-    # check if in terraform dir
-    if [ -d .terraform ]; then
-      workspace=$(terraform workspace show 2> /dev/null) || return
-      echo "${ZSH_THEME_TF_PROMPT_PREFIX}${workspace}${ZSH_THEME_TF_PROMPT_SUFFIX}"
-    fi
+  # dont show 'default' workspace in home dir
+  [[ "$PWD" != ~ ]] || return
+  # check if in terraform dir
+  [[ -d .terraform ]] || return
+
+  local workspace
+  workspace=$(terraform workspace show 2>/dev/null) || return
+  echo "${ZSH_THEME_TF_PROMPT_PREFIX-[}${workspace:gs/%/%%}${ZSH_THEME_TF_PROMPT_SUFFIX-]}"
 }
