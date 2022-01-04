31d63ea884e8ef56a40bed8771cdd8d3aec131f9
Marc Cornellà <hello@mcornella.com>
2022-01-04 11:38:53 +0100

    fix(kubectx): allow prompt sequences in `kubectx_mapping` (#10562)
    Fixes #10562

--- plugins/kubectx/kubectx.plugin.zsh
+++ plugins/kubectx/kubectx.plugin.zsh
@@ -1,4 +1,4 @@
-typeset -A kubectx_mapping
+typeset -g -A kubectx_mapping
 
 function kubectx_prompt_info() {
   (( $+commands[kubectl] )) || return
@@ -7,5 +7,5 @@ function kubectx_prompt_info() {
 
   # use value in associative array if it exists
   # otherwise fall back to the context name
-  echo "${${kubectx_mapping[$current_ctx]:-$current_ctx}:gs/%/%%}"
+  echo "${kubectx_mapping[$current_ctx]:-${current_ctx:gs/%/%%}}"
 }
