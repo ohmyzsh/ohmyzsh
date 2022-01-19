0b08b70b0cc7ff286ea942f0e20a41f10bef0ee8
Marc Cornellà <hello@mcornella.com>
2022-01-19 17:15:16 +0100

    chore(poetry): fix copy-paste error

--- plugins/poetry/poetry.plugin.zsh
+++ plugins/poetry/poetry.plugin.zsh
@@ -8,7 +8,7 @@ fi
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_poetry" ]]; then
   typeset -g -A _comps
   autoload -Uz _poetry
-  _comps[gh]=_poetry
+  _comps[poetry]=_poetry
 fi
 
 poetry completions zsh >| "$ZSH_CACHE_DIR/completions/_poetry" &|
