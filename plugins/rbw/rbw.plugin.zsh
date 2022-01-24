bcbd2738e231d6d5455d65a1fa3a381a01666c1b
Carlo Sala <carlosalag@protonmail.com>
2022-01-24 16:31:11 +0100

    chore: removing old completion code

--- plugins/rbw/rbw.plugin.zsh
+++ plugins/rbw/rbw.plugin.zsh
@@ -2,12 +2,6 @@ if (( ! $+commands[rbw] )); then
   return
 fi
 
-# TODO: 2021-12-28: remove this bit of code as it exists in oh-my-zsh.sh
-# Add completions folder in $ZSH_CACHE_DIR
-command mkdir -p "$ZSH_CACHE_DIR/completions"
-(( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
-
-
 # If the completion file doesn't exist yet, we need to autoload it and
 # bind it to `rbw`. Otherwise, compinit will have already done that.
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_rbw" ]]; then
