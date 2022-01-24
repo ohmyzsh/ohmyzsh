139a7572948a120ff56845702af35abf45d9b5a5
Carlo Sala <carlosalag@protonmail.com>
2022-01-24 16:49:13 +0100

    feat(postgres): support apple silicon

--- plugins/postgres/postgres.plugin.zsh
+++ plugins/postgres/postgres.plugin.zsh
@@ -1,8 +1,13 @@
 # Aliases to control Postgres
 # Paths noted below are for Postgres installed via Homebrew on OSX
+if [[ -d /opt/homebrew/var ]]; then
+  local DB_LOCATION_DIR="/opt/homebrew/var/postgres"
+else
+  local DB_LOCATION_DIR="/usr/local/var/postgres"
+fi
 
-alias startpost='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
-alias stoppost='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
-alias restartpost='stoppost && sleep 1 && startpost'
-alias reloadpost='pg_ctl reload -D /usr/local/var/postgres -s'
-alias statuspost='pg_ctl status -D /usr/local/var/postgres -s'
+alias startpost="pg_ctl -D $DB_LOCATION_DIR -l $DB_LOCATION_DIR/server.log start"
+alias stoppost="pg_ctl -D $DB_LOCATION_DIR stop -s -m fast"
+alias restartpost="stoppost && sleep 1 && startpost"
+alias reloadpost="pg_ctl reload -D $DB_LOCATION_DIR -s"
+alias statuspost="pg_ctl status -D $DB_LOCATION_DIR -s"
