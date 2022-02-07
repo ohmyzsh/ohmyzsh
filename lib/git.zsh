5b70b1a3acdc4bc5a67486d4a2ba7bed356656ad
Obszczymucha <obszczymucha@gmail.com>
2022-02-07 21:27:57 +1100

    Add git -C support to git_prompt_info.

--- lib/git.zsh
+++ lib/git.zsh
@@ -10,34 +10,56 @@ function __git_prompt_git() {
 }
 
 function git_prompt_info() {
+  readonly dir="$1"
+
+  # Decorates git command if a directory is passed to us.
+  function __git_dir_decorator() {
+    if [[ -n $dir ]]; then
+      __git_prompt_git -C "$dir" "$@"
+    else
+      __git_prompt_git "$@"
+    fi
+  }
+
   # If we are on a folder not tracked by git, get out.
   # Otherwise, check for hide-info at global and local repository level
-  if ! __git_prompt_git rev-parse --git-dir &> /dev/null \
-     || [[ "$(__git_prompt_git config --get oh-my-zsh.hide-info 2>/dev/null)" == 1 ]]; then
+  if ! __git_dir_decorator rev-parse --git-dir &> /dev/null \
+     || [[ "$(__git_dir_decorator config --get oh-my-zsh.hide-info 2>/dev/null)" == 1 ]]; then
     return 0
   fi
 
   local ref
-  ref=$(__git_prompt_git symbolic-ref --short HEAD 2> /dev/null) \
-  || ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) \
+  ref=$(__git_dir_decorator symbolic-ref --short HEAD 2> /dev/null) \
+  || ref=$(__git_dir_decorator rev-parse --short HEAD 2> /dev/null) \
   || return 0
 
   # Use global ZSH_THEME_GIT_SHOW_UPSTREAM=1 for including upstream remote info
   local upstream
   if (( ${+ZSH_THEME_GIT_SHOW_UPSTREAM} )); then
-    upstream=$(__git_prompt_git rev-parse --abbrev-ref --symbolic-full-name "@{upstream}" 2>/dev/null) \
+    upstream=$(__git_dir_decorator rev-parse --abbrev-ref --symbolic-full-name "@{upstream}" 2>/dev/null) \
     && upstream=" -> ${upstream}"
   fi
 
-  echo "${ZSH_THEME_GIT_PROMPT_PREFIX}${ref:gs/%/%%}${upstream:gs/%/%%}$(parse_git_dirty)${ZSH_THEME_GIT_PROMPT_SUFFIX}"
+  echo "${ZSH_THEME_GIT_PROMPT_PREFIX}${ref:gs/%/%%}${upstream:gs/%/%%}$(parse_git_dirty $dir)${ZSH_THEME_GIT_PROMPT_SUFFIX}"
 }
 
 # Checks if working tree is dirty
 function parse_git_dirty() {
+  readonly dir="$1"
+
+  # Decorates git command if a directory is passed to us.
+  function __git_dir_decorator() {
+    if [[ -n $dir ]]; then
+      __git_prompt_git -C "$dir" "$@"
+    else
+      __git_prompt_git "$@"
+    fi
+  }
+
   local STATUS
   local -a FLAGS
   FLAGS=('--porcelain')
-  if [[ "$(__git_prompt_git config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
+  if [[ "$(__git_dir_decorator config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
     if [[ "${DISABLE_UNTRACKED_FILES_DIRTY:-}" == "true" ]]; then
       FLAGS+='--untracked-files=no'
     fi
@@ -51,7 +73,7 @@ function parse_git_dirty() {
         FLAGS+="--ignore-submodules=${GIT_STATUS_IGNORE_SUBMODULES:-dirty}"
         ;;
     esac
-    STATUS=$(__git_prompt_git status ${FLAGS} 2> /dev/null | tail -n 1)
+    STATUS=$(__git_dir_decorator status ${FLAGS} 2> /dev/null | tail -n 1)
   fi
   if [[ -n $STATUS ]]; then
     echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
