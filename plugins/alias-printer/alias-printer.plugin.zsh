d737a53be34eeb9d9efffcfe8950de899925a05b
Nicholas Drone <ndrone@gmail.com>
2022-02-08 09:58:20 -0500

    fixed flow control issue while using --on and --off that is would try and find aliases for --on of --off

--- plugins/alias-printer/alias-printer.plugin.zsh
+++ plugins/alias-printer/alias-printer.plugin.zsh
@@ -1,15 +1,22 @@
 alias-printer() {
-  if [[ "$1" = "--on" ]]; then
-    autoload -U add-zsh-hook;
-    add-zsh-hook preexec alias-printer;
-    echo "alias-printer turned on";
-  fi
-  if [[ "$1" = "--off" ]]; then
-    autoload -U add-zsh-hook;
-    add-zsh-hook -d preexec alias-printer;
-    echo "alias-printer turned off";
-  fi
-  if [[ ! -z $2 ]]; then
-    echo 'alias-printer: '"$2";
-  fi
+  for arg in $@; do
+    case $arg in
+      --on )
+        autoload -U add-zsh-hook;
+        add-zsh-hook preexec alias-printer;
+        echo "alias-printer turned on";
+        ;;
+      --off )
+        autoload -U add-zsh-hook;
+        add-zsh-hook -d preexec alias-printer;
+        echo "alias-printer turned off";
+        ;;
+      * )
+        if [[ ! -z $2 ]]; then
+          echo 'alias-printer: '"$2";
+        fi
+        break
+      ;;
+    esac
+  done
 }
