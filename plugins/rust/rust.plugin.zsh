3f214329d631b3ae39d6b283262c77819a0078de
Carlo Sala <carlosalag@protonmail.com>
2022-03-28 13:38:49 +0200

    chore: removing old completion code (#10616)

--- plugins/rust/rust.plugin.zsh
+++ plugins/rust/rust.plugin.zsh
@@ -2,11 +2,6 @@ if ! (( $+commands[rustup] && $+commands[cargo] )); then
   return
 fi
 
-# Add completions folder in $ZSH_CACHE_DIR
-# TODO: 2021-12-28: remove this bit of code as it exists in oh-my-zsh.sh
-command mkdir -p "$ZSH_CACHE_DIR/completions"
-(( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
-
 # If the completion file doesn't exist yet, we need to autoload it and
 # bind it to `cargo`. Otherwise, compinit will have already done that
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_cargo" ]]; then
