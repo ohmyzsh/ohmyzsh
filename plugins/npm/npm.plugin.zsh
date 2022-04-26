99d6407d2793d0325c0f81acb692fbc42cdaaaeb
denis-rossati <denis.rossatiramos@gmail.com>
2022-04-26 12:51:39 -0300

    feat(npm): adding npmIt alias

--- plugins/npm/npm.plugin.zsh
+++ plugins/npm/npm.plugin.zsh
@@ -58,7 +58,7 @@ alias npmt="npm test"
 # Run npm scripts
 alias npmR="npm run"
 
-# Run npm publish 
+# Run npm publish
 alias npmP="npm publish"
 
 # Run npm init
@@ -69,6 +69,9 @@ alias npmi="npm info"
 
 # Run npm search
 alias npmSe="npm search"
+
+# Run npm install-test
+alias npmIt="npm install-test"
 
 npm_toggle_install_uninstall() {
   # Look up to the previous 2 history commands
