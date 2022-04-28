10f8b90d1850d96cdfdb92948cca5632c3d2a11f
Will LE <lexuandinhct@gmail.com>
2022-04-28 22:50:31 +0700

    feat(git): fix logic in `parse_git_dirty()`
    Do not output when `oh-my-zsh.hide-dirty`
    
    - Previous behavior: output `$ZSH_THEME_GIT_PROMPT_CLEAN` when `oh-my-zsh.hide-dirty`
    - New behavior: output nothing when `oh-my-zsh.hide-dirty`
      Rationale: the dirty status is unknown, so it is neither "clean" nor "dirty"
    
    Signed-off-by: Will LE <lexuandinhct@gmail.com>

--- lib/git.zsh
+++ lib/git.zsh
@@ -37,22 +37,23 @@ function parse_git_dirty() {
   local STATUS
   local -a FLAGS
   FLAGS=('--porcelain')
-  if [[ "$(__git_prompt_git config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
-    if [[ "${DISABLE_UNTRACKED_FILES_DIRTY:-}" == "true" ]]; then
-      FLAGS+='--untracked-files=no'
-    fi
-    case "${GIT_STATUS_IGNORE_SUBMODULES:-}" in
-      git)
-        # let git decide (this respects per-repo config in .gitmodules)
-        ;;
-      *)
-        # if unset: ignore dirty submodules
-        # other values are passed to --ignore-submodules
-        FLAGS+="--ignore-submodules=${GIT_STATUS_IGNORE_SUBMODULES:-dirty}"
-        ;;
-    esac
-    STATUS=$(__git_prompt_git status ${FLAGS} 2> /dev/null | tail -n 1)
+  if [[ "$(__git_prompt_git config --get oh-my-zsh.hide-dirty)" == "1" ]]; then
+    return 0
   fi
+  if [[ "${DISABLE_UNTRACKED_FILES_DIRTY:-}" == "true" ]]; then
+    FLAGS+='--untracked-files=no'
+  fi
+  case "${GIT_STATUS_IGNORE_SUBMODULES:-}" in
+    git)
+      # let git decide (this respects per-repo config in .gitmodules)
+      ;;
+    *)
+      # if unset: ignore dirty submodules
+      # other values are passed to --ignore-submodules
+      FLAGS+="--ignore-submodules=${GIT_STATUS_IGNORE_SUBMODULES:-dirty}"
+      ;;
+  esac
+  STATUS=$(__git_prompt_git status ${FLAGS} 2> /dev/null | tail -n 1)
   if [[ -n $STATUS ]]; then
     echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
   else
