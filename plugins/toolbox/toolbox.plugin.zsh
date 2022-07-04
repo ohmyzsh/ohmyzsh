5065d87df4b90638edd19de40faab3fbf4772624
Robert Detjens <github@detjens.dev>
2022-07-03 19:12:26 -0700

    feat(toolbox): add prompt function to display name alongside icon
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- plugins/toolbox/toolbox.plugin.zsh
+++ plugins/toolbox/toolbox.plugin.zsh
@@ -7,6 +7,11 @@ function toolbox_prompt_info() {
   [[ -f /run/.toolboxenv ]] && echo "⬢"
 }
 
+function toolbox_prompt_info_name () {
+  # Loads the container name from .containerenv and prints that as well
+  [[ -f /run/.toolboxenv ]] && (. /run/.containerenv && echo "⬢ $name")
+}
+
 # check if completion command exists
 # (exit status 16 for "no manual entry" (command exists), 1 for bad command)
 toolbox completion >| /dev/null
