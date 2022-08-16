15fb428ee6c6096030713d573d51b75a265cbb78
Ben Walton <bdwalton@google.com>
2022-08-16 14:40:56 +0100

    chore: Add .dir-locals.el for emacs users
    Initially, we only enable sh-mode on a few additional .zsh-* files,
    but this could be extended to incorporate the editor settings in
    .editorconfig for people not using that package.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((auto-mode-alist . (
+		     ("\\.zsh-theme\\'" . sh-mode)
+		     ("\\.zsh-template\\'" . sh-mode)
+		     )))
