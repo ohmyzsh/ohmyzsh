1b619c8bf69260f1b7bf9efe5ac57d307787be49
ox-9 <86934011+ox-9@users.noreply.github.com>
2022-10-01 10:25:20 -0700

    double space

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -36,6 +36,7 @@ fi
 #
 # Copyright (c) 2021 Kat Marchán
 #
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
