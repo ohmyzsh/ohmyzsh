d4128701703c3da486f1c2a77cd4b79834b7f79f
Kaspar Vollenweider <vok@panter.ch>
2022-10-06 16:07:16 +0200

    fix(rust): only create completion cache when needed
    Completions were generated and cached on every shell opened.
    This fix caches the version the cache was last generated for and
    only runs the completion generator in case there is a mismatch or
    no completion cache file available.

--- plugins/rust/rust.plugin.zsh
+++ plugins/rust/rust.plugin.zsh
@@ -18,9 +18,41 @@ if [[ ! -f "$ZSH_CACHE_DIR/completions/_rustup" ]]; then
   _comps[rustup]=_rustup
 fi
 
-# Generate completion files in the background
-rustup completions zsh >| "$ZSH_CACHE_DIR/completions/_rustup" &|
-cat >| "$ZSH_CACHE_DIR/completions/_cargo" <<'EOF'
-#compdef cargo
-source "$(rustc +${${(z)$(rustup default)}[1]} --print sysroot)"/share/zsh/site-functions/_cargo
-EOF
+# Cache rustup completions
+#
+# - Caches the output of `rustup completions zsh` to $ZSH_CACHE_DIR/completions/_rustup
+# - Refreshes when the version of rustc changes
+function _rustup_completions_cache() {
+  local rustup_version version_cache completion_cache
+  version_cache="$ZSH_CACHE_DIR/rustc_cached_version"
+  completion_cache="$ZSH_CACHE_DIR/completions/_rustup"
+  rustup_version=$(rustc --version)
+  if ! [ -f "$version_cache" ] || \
+     ! [ -f "$completion_cache" ] || \
+     [[ $(head -n 1 "$version_cache") != "$rustup_version" ]]
+  then
+    echo "$rustup_version" > "$version_cache"
+    rustup completions zsh >| "$completion_cache" &|
+  fi
+}
+_rustup_completions_cache
+
+# Cache cargo completions
+#
+# - Caches cargo's completions to $ZSH_CACHE_DIR/completions/_cargo
+# - Refreshes when the version of rustc or cargo changes
+function _cargo_completions_cache() {
+  local cargo_version version_cache completion_cache
+  version_cache="$ZSH_CACHE_DIR/cargo_cached_version"
+  completion_cache="$ZSH_CACHE_DIR/completions/_cargo"
+  cargo_version="$(rustc --version) $(cargo -V)"
+  if ! [ -f "$version_cache" ] || \
+     ! [ -f "$completion_cache" ] || \
+     [[ $(head -n 1 "$version_cache") != "$cargo_version" ]]
+  then
+    echo "$cargo_version" > "$version_cache"
+    echo '#compdef cargo' > "$completion_cache"
+    echo "source $(rustc +${${(z)$(rustup default)}[1]} --print sysroot)/share/zsh/site-functions/_cargo" >> "$completion_cache"
+  fi
+}
+_cargo_completions_cache
