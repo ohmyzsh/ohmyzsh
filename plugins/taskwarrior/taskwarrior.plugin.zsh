350924d4350a74c253203b89059e07fc2ee74369
Manoel Pereira de Queiroz <git@manoelpqueiroz.anonaddy.com>
2022-10-14 18:22:26 -0300

    Correcting current Taskwarrior alias
    Plugin has not been updated since 2015, with the sole alias needing
    additional ticks in order to work.

--- plugins/taskwarrior/taskwarrior.plugin.zsh
+++ plugins/taskwarrior/taskwarrior.plugin.zsh
@@ -1,7 +1,6 @@
 zstyle ':completion:*:*:task:*' verbose yes
 zstyle ':completion:*:*:task:*:descriptions' format '%U%B%d%b%u'
-
 zstyle ':completion:*:*:task:*' group-name ''
 
-alias t=task
+alias t='task'
 compdef _task t=task
