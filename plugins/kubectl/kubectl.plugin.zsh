b692ff2e1597816563cbdaeeedf0966ad9babeca
Carlo Sala <carlosalag@protonmail.com>
2022-12-07 12:18:51 +0100

    fix(kubectl): redirect stderr in completion generation
    Fixes #11364

--- plugins/kubectl/kubectl.plugin.zsh
+++ plugins/kubectl/kubectl.plugin.zsh
@@ -10,7 +10,7 @@ if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
   _comps[kubectl]=_kubectl
 fi
 
-kubectl completion zsh >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
+kubectl completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
 
 # This command is used a LOT both below and in daily life
 alias k=kubectl
