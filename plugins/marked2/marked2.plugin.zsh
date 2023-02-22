ad7c8e2b4efa94e536669238d9c984d869407524
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-02-22 18:26:30 +0800

    feat(marked): update funciton to make more sense

--- plugins/marked2/marked2.plugin.zsh
+++ plugins/marked2/marked2.plugin.zsh
@@ -3,10 +3,5 @@
 # If marked is passed a file, open it in Marked
 #
 function marked() {
-    if [ "$1" ]
-    then
-        open -a "marked 2.app" "$1"
-    else
-        open -a "marked 2.app"
-    fi
+    open -a "marked 2.app" "$1"
 }
