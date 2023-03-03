5bf7f9c83325a6cb2752e14ca01a574dbeef206e
Marc Cornellà <hello@mcornella.com>
2023-03-03 12:34:31 +0100

    fix(lib): use `$BROWSER` in `open_command` if set (#11532)
    Fixes #11098

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -30,6 +30,13 @@ function open_command() {
               ;;
   esac
 
+  # If a URL is passed, $BROWSER might be set to a local browser within SSH.
+  # See https://github.com/ohmyzsh/ohmyzsh/issues/11098
+  if [[ -n "$BROWSER" && "$1" = (http|https)://* ]]; then
+    "$BROWSER" "$@"
+    return
+  fi
+
   ${=open_cmd} "$@" &>/dev/null
 }
 
