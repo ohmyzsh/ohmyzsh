72732a224e886933df6b64a49ec6f5e94c884612
Marc Cornellà <hello@mcornella.com>
2023-03-12 15:47:58 +0100

    fix(lib): set equivalent LS_COLORS and LSCOLORS variables
    As reported by https://geoff.greer.fm/lscolors
    
    Fixes #11554

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -78,6 +78,6 @@ if [[ -z "$LS_COLORS" ]]; then
       && source <(dircolors -b "$HOME/.dircolors") \
       || source <(dircolors -b)
   else
-    export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=37;41:sg=30;43:tw=30;42:ow=34;42:"
+    export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
   fi
 fi
