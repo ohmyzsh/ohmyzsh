2ededed8d2aaa647e384b9be52fcbda566d30877
Katherine Fu <kfu@flexport.com>
2023-06-06 19:49:21 +0200

    Second PR

--- custom/plugins/example/example.plugin.zsh
+++ custom/plugins/example/example.plugin.zsh
@@ -1,4 +1,4 @@
 # Add your own custom plugins in the custom/plugins directory. Plugins placed
 # here will override ones with the same name in the main plugins directory.
 # See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization#overriding-and-adding-plugins
-# This is for my first PR
+# This is for my second PR
