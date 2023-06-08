50c678687e73d1433f278b7bb7f168e8fa817670
Marc Cornellà <hello@mcornella.com>
2023-06-08 18:47:05 +0200

    fix(termsupport): fix pwd report for Konsole (#11730)
    The Konsole terminal shows an error if the host is provided in the
    OSC 7 sequence.
    
    Fixes #11730

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -150,8 +150,11 @@ function omz_termsupport_cwd {
   URL_HOST="$(omz_urlencode -P $HOST)" || return 1
   URL_PATH="$(omz_urlencode -P $PWD)" || return 1
 
+  # Konsole errors if the HOST is provided
+  [[ -z "$KONSOLE_VERSION" ]] || URL_HOST=""
+
   # common control sequence (OSC 7) to set current host and path
-  printf "\e]7;%s\a" "file://${URL_HOST}${URL_PATH}"
+  printf "\e]7;file://%s%s\e\\" "${URL_HOST}" "${URL_PATH}"
 }
 
 # Use a precmd hook instead of a chpwd hook to avoid contaminating output
