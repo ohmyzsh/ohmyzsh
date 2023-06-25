95a20a3614c1b9abbdbff4dd08868f158141a3bb
Jalal Uddin <dgvai.hridoy@gmail.com>
2023-06-25 14:16:51 +0600

    feat(lib): update nvm library

--- lib/nvm.zsh
+++ lib/nvm.zsh
@@ -1,6 +1,14 @@
 # get the nvm-controlled node.js version
 function nvm_prompt_info() {
   which nvm &>/dev/null || return
+  local package_json="package.json"
+  test -f ${package_json} &>/dev/null || return
   local nvm_prompt=${$(nvm current)#v}
-  echo "${ZSH_THEME_NVM_PROMPT_PREFIX}${nvm_prompt:gs/%/%%}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
+  local nvm_short_version=${nvm_prompt:0:2}
+  if [ "${ZSH_THEME_NVM_PROMPT_SHORT}" = true ]; 
+  then 
+    echo "${ZSH_THEME_NVM_PROMPT_PREFIX}${nvm_short_version:gs/%/%%}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
+  else 
+    echo "${ZSH_THEME_NVM_PROMPT_PREFIX}${nvm_prompt:gs/%/%%}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
+  fi
 }
