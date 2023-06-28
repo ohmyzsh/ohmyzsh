7268805d36c6a6d1a334f138160ea7b417af5c9b
Fred Klassen <fred.klassen@broadcom.com>
2023-06-28 09:52:48 -0700

    more git aliases, less pager, iterm2 support

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,6 +2,15 @@
 
 ohmyzsh=$HOME/.oh-my-zsh
 
+gcohistory() {
+  i=0
+  while [ $? -eq 0 ]; do
+    i=$((i+1))
+    echo -n "$i. "
+    git rev-parse --symbolic-full-name @{-$i} 2> /dev/null
+  done
+}
+
 # Get operating system
 platform='unknown'
 unamestr=$(uname)
@@ -35,7 +44,7 @@ TRAPHUP() {
   source $ohmyzsh/custom/aliases.zsh
 }
 
-alias ae='vi $ohmyzsh/custom/aliases.zsh'      #alias edit
+alias ae='vi $ohmyzsh/custom/aliases.zsh'         #alias edit
 alias arl='source $oh-my-zsh/custom/aliases.zsh'  #alias reload
 alias gar="killall -HUP -u \"$USER\" zsh"         #global alias reload
 
@@ -60,3 +69,14 @@ if [ ! "$MSYSTEM" = "MINGW64" ]; then
   alias ls='exa'
   alias la='ll -a'
 fi
+
+# git
+alias gcoh="gcohistory"
+alias gld="git log --pretty=format:'%C(yellow)%h%C(reset) - %an [%C(green)%ar%C(reset)] %s'"
+alias glsi='git ls-files --other --ignored --exclude-standard'
+alias galiases='git config -l | grep alias | cut -c 7-'
+alias gls='git log --stat --decorate --source -p'
+alias glhistg="git log --graph --pretty=format:'%C(yellow)%h%C(bold white red)%d%Creset %s %C(bold green)%cr %Creset%C(white)%an' --abbrev-commit --date=relative"
+alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+alias gdtree='git diff-tree'
+
