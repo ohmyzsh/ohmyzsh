9f6e56d773692da3468dcb8cc65deb002dc9385e
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2023-07-25 22:14:37 +0100

    refactor(git): move function to alias list

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -14,14 +14,6 @@ function current_branch() {
   git_current_branch
 }
 
-# Pretty log messages
-function _git_log_prettily(){
-  if ! [ -z $1 ]; then
-    git log --pretty=$1
-  fi
-}
-compdef _git _git_log_prettily=git-log
-
 # Warn if the current branch is a WIP
 function work_in_progress() {
   command git -c log.showSignature=false log -n 1 2>/dev/null | grep -q -- "--wip--" && echo "WIP!!"
@@ -198,6 +190,15 @@ alias glol='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgre
 alias glo='git log --oneline --decorate'
 alias glog='git log --oneline --decorate --graph'
 alias gloga='git log --oneline --decorate --graph --all'
+
+# Pretty log messages
+function _git_log_prettily(){
+  if ! [ -z $1 ]; then
+    git log --pretty=$1
+  fi
+}
+compdef _git _git_log_prettily=git-log
+
 alias glp='_git_log_prettily'
 alias glg='git log --stat'
 alias glgp='git log --stat --patch'
