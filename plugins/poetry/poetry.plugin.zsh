2a9bcf35357a695545d59b8f3866e17dc0776d75
baltic-tea <97766478+baltic-tea@users.noreply.github.com>
2023-08-01 02:08:26 +0300

    Update poetry.plugin.zsh – added new aliases

--- plugins/poetry/poetry.plugin.zsh
+++ plugins/poetry/poetry.plugin.zsh
@@ -1,3 +1,24 @@
+alias pad='poetry add'
+alias pbld='poetry build'
+alias pch='poetry check'
+alias pnew='poetry new'
+alias pcmd='poetry list'
+alias pconf='poetry config --list'
+alias pexp='poetry export --without-hashes > requirements.txt'
+alias pin='poetry init'
+alias pinst='poetry install'
+alias plock='poetry lock'
+alias pplug='poetry self show plugins'
+alias ppub='poetry publish'
+alias prm='poetry remove'
+alias prun='poetry run'
+alias psad='poetry self add'
+alias psh='poetry shell'
+alias pshow='poetry show'
+alias psup='poetry self update'
+alias psync='poetry install --sync'
+alias pup='poetry update'
+
 # Return immediately if poetry is not found
 if (( ! $+commands[poetry] )); then
   return
