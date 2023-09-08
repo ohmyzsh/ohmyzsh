a792e96946bd27bb0719e3c5ea0ccccca55aa95a
Ege Kuzubasioglu <egek92@users.noreply.github.com>
2023-09-08 09:35:07 +0200

    feat(melos): add aliases for melos

---
+++ plugins/git-flow/melos/melos.plugin.zsh
@@ -0,0 +1,20 @@
+# Bootstrap
+alias mb='melos bs'
+# Clean
+alias mc='melos clean'
+# Execute
+alias me='melos exec'
+# List
+alias mll='melos list --long'
+alias mla='melos list --all'
+alias mlr='melos list --relative'
+alias mlp='melos list --parsable'
+alias mlj='melos list --json'
+alias mlg='melos list --gviz'
+alias mlc='melos list --cycles'
+# Publish
+alias mp='melos publish'
+alias mpdr='melos publish --dry-run'
+alias mpndr='melos publish --no-dry-run'
+# Run
+alias mr='melos run'
