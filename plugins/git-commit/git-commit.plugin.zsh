b07c8cfe692bc6c7702f0aef5d493b12dfb43392
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-09-10 09:28:33 +0200

    fix(git-commit): fix compatibility of alias functions (#11883)

--- plugins/git-commit/git-commit.plugin.zsh
+++ plugins/git-commit/git-commit.plugin.zsh
@@ -1,6 +1,6 @@
 function _git_commit_register {
   if ! git config --global --get-all alias.$1 >/dev/null 2>&1; then
-    git config --global alias.$1 '!a() { if [[ "$1" == "-s" || "$1" == "--scope" ]]; then git commit -m "'$1'(${2}): ${@:3}"; else git commit -m "'$1': ${@}"; fi }; a'
+    git config --global alias.$1 '!a() { if [ "$1" = "-s" ] || [ "$1" = "--scope" ]; then local scope="$2"; shift 2; git commit -m "'$1'(${scope}): ${@}"; else git commit -m "'$1': ${@}"; fi }; a'
   fi
 }
 
