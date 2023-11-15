c8e600f39779c6d2ef75b40304b5391a541c41df
Thomas Luzat <thomas@luzat.com>
2023-11-15 12:53:39 +0100

    fix(direnv): initialize properly direnv
    Closes #12038
    Closes #12039
    
    Co-authored-by: Carlo Sala <carlosalag@protonmail.com>

--- plugins/direnv/direnv.plugin.zsh
+++ plugins/direnv/direnv.plugin.zsh
@@ -3,14 +3,14 @@ command -v direnv &>/dev/null || return
 
 _direnv_hook() {
   trap -- '' SIGINT;
-  eval "$(direnv hook zsh)";
+  eval "$(direnv export zsh)";
   trap - SIGINT;
 }
 typeset -ag precmd_functions;
-if [[ -z ${precmd_functions[(r)_direnv_hook]} ]]; then
+if [[ -z "${precmd_functions[(r)_direnv_hook]+1}" ]]; then
   precmd_functions=( _direnv_hook ${precmd_functions[@]} )
 fi
 typeset -ag chpwd_functions;
-if [[ -z ${chpwd_functions[(r)_direnv_hook]} ]]; then
+if [[ -z "${chpwd_functions[(r)_direnv_hook]+1}" ]]; then
   chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
 fi
