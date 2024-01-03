521adbbdaad883fbaee6da6747f27c624bd4b7d8
Jared Allard <jaredallard@users.noreply.github.com>
2024-01-03 12:57:55 +0100

    fix(mise)!: rename `rtx` plugin to `mise`
    BREAKING CHANGE: Rename `rtx` plugin to `mise`. The project swapped name
    in `v2024.1.0`. Replace references to `rtx` with `mise` while
    maintaining backward-compatibility for `rtx` installations.
    
    Co-authored-by: Carlo Sala <carlosalag@protonmail.com>

--- plugins/rtx/rtx.plugin.zsh
+++ plugins/rtx/rtx.plugin.zsh
@@ -1,18 +1,2 @@
-# rtx needs to be in $PATH
-if (( ! ${+commands[rtx]} )); then
-  return
-fi
-
-# Load rtx hooks
-eval "$(rtx activate zsh)"
-
-# If the completion file doesn't exist yet, we need to autoload it and
-# bind it to `rtx`. Otherwise, compinit will have already done that.
-if [[ ! -f "$ZSH_CACHE_DIR/completions/_rtx" ]]; then
-  typeset -g -A _comps
-  autoload -Uz _rtx
-  _comps[rtx]=_rtx
-fi
-
-# Generate and load rtx completion
-rtx completion zsh >! "$ZSH_CACHE_DIR/completions/_rtx" &|
+# TODO: 2024-01-03 remove rtx support
+echo "[oh-my-zsh] 'rtx' plugin has been renamed to 'mise'"
