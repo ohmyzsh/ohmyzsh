530cd7f76b9e7e129ad79b67fcf222f7278fc6da
Carlo Sala <carlosalag@protonmail.com>
2024-01-16 11:01:39 +0100

    fix(history-substring-search): source properly plugin

--- plugins/history-substring-search/history-substring-search.plugin.zsh
+++ plugins/history-substring-search/history-substring-search.plugin.zsh
@@ -1,5 +1,9 @@
-0=${(%):-%N}
-source ${0:A:h}/zsh-history-substring-search.zsh
+# Handle $0 according to the standard:
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+source ${0:A:h}/history-substring-search.zsh
 
 
 # Bind terminal-specific up and down keys
