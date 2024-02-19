15ffbdd346daa58de6701d3421787012420aa6cc
Julian <489233+naydichev@users.noreply.github.com>
2024-02-19 10:29:37 +0100

    feat(zoxide): add option to override cmd (#12218)

--- plugins/zoxide/zoxide.plugin.zsh
+++ plugins/zoxide/zoxide.plugin.zsh
@@ -1,5 +1,5 @@
 if (( $+commands[zoxide] )); then
-  eval "$(zoxide init zsh)"
+  eval "$(zoxide init --cmd ${ZOXIDE_CMD_OVERRIDE:-z} zsh)"
 else
   echo '[oh-my-zsh] zoxide not found, please install it from https://github.com/ajeetdsouza/zoxide'
 fi
