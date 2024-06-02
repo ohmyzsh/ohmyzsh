27e517540f348d37f58f598d26f6e8cc6e098233
EraPartner <era.partner_0n@icloud.com>
2024-06-02 21:29:43 +0200

    testttttttttttttttttttttt

--- custom/example.zsh
+++ custom/example.zsh
@@ -1,6 +1,5 @@
 # Put files in this folder to add your own custom functionality.
 # See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
-# tt
 #
 # Files in the custom/ directory will be:
 # - loaded automatically by the init script, in alphabetical order
