0b27b15d0b08645bd995e13b8d69fcbe2ef7e8eb
Marc Cornellà <marc@mcornella.com>
2024-07-13 18:32:22 +0200

    fix(init): fix subscript flag in 11ca9e2a

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -65,7 +65,7 @@ fi
 
 # Create cache and completions dir and add to $fpath
 mkdir -p "$ZSH_CACHE_DIR/completions"
-(( ${fpath[(ie)$ZSH_CACHE_DIR/completions]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
+(( ${fpath[(Ie)$ZSH_CACHE_DIR/completions]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
 
 # Check for updates on initial load...
 source "$ZSH/tools/check_for_upgrade.sh"
