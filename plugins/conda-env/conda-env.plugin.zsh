b1400713fc9f4966e7532116f5c28f752e294f23
Marc Cornellà <marc@mcornella.com>
2024-07-15 18:53:54 +0200

    Simplify and add documentation

--- plugins/conda-env/conda-env.plugin.zsh
+++ plugins/conda-env/conda-env.plugin.zsh
@@ -1,12 +1,9 @@
 function conda_prompt_info(){
-  if command -v pyenv &>/dev/null && [[ $plugins == *"virtualenv"* ]]; then
-    return
-  else
-    [[ -n ${CONDA_DEFAULT_ENV} ]] || return
-    echo "${ZSH_THEME_CONDA_PREFIX=[}${CONDA_DEFAULT_ENV:t:gs/%/%%}${ZSH_THEME_CONDA_SUFFIX=]}"
-  fi
+  [[ -n ${CONDA_DEFAULT_ENV} ]] || return
+  echo "${ZSH_THEME_CONDA_PREFIX=[}${CONDA_DEFAULT_ENV:t:gs/%/%%}${ZSH_THEME_CONDA_SUFFIX=]}"
 }
 
-# disables display (${CONDA_DEFAULT_ENV})
+# Has the same effect as `conda config --set changeps1 false`
+# - https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#determining-your-current-environment
+# - https://conda.io/projects/conda/en/latest/user-guide/configuration/use-condarc.html#precedence
 export CONDA_CHANGEPS1=false
-
