421bf9b782f4c7fe0c3ba59da6e6311ceb3d1d62
SpeakinTelnet <gui.lac@protonmail.com>
2024-08-02 09:23:19 -0400

    feat(toolbox): add function to show toolbox name

--- plugins/toolbox/toolbox.plugin.zsh
+++ plugins/toolbox/toolbox.plugin.zsh
@@ -2,5 +2,10 @@ function toolbox_prompt_info() {
   [[ -f /run/.toolboxenv ]] && echo "⬢"
 }
 
+function toolbox_prompt_name() {
+  [[ -f /run/.containerenv ]] &&
+    echo $(cat /run/.containerenv | awk -F\" '/name/ { print$2 }')
+}
+
 alias tbe="toolbox enter"
 alias tbr="toolbox run"
