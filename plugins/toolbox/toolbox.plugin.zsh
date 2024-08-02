2cb403e89f618548b340181b5690639768305aad
SpeakinTelnet <105018871+SpeakinTelnet@users.noreply.github.com>
2024-08-02 10:37:00 -0400

    feat(toolbox): add function to show toolbox name (#12594)
    Co-authored-by: Carlo Sala <carlosalag@protonmail.com>

--- plugins/toolbox/toolbox.plugin.zsh
+++ plugins/toolbox/toolbox.plugin.zsh
@@ -2,5 +2,9 @@ function toolbox_prompt_info() {
   [[ -f /run/.toolboxenv ]] && echo "⬢"
 }
 
+function toolbox_prompt_name() {
+  [[ -f /run/.containerenv ]] && cat /run/.containerenv | awk -F\" '/name/ { print$2 }'
+}
+
 alias tbe="toolbox enter"
 alias tbr="toolbox run"
