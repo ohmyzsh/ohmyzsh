8203214664b98601cf5a4c11632625b0c4f4d215
Marc Cornellà <marc@mcornella.com>
2024-10-08 20:23:40 +0200

    Generate completion in the background

--- plugins/k9s/k9s.plugin.zsh
+++ plugins/k9s/k9s.plugin.zsh
@@ -1,11 +1,14 @@
-if (( $+commands[k9s] )); then
-  # If the completion file does not exist, generate it and then source it
-  # Otherwise, source it and regenerate in the background
-  if [[ ! -f "$ZSH_CACHE_DIR/completions/_k9s" ]]; then
-    k9s completion zsh | tee "$ZSH_CACHE_DIR/completions/_k9s" >/dev/null
-    source "$ZSH_CACHE_DIR/completions/_k9s"
-  else
-    source "$ZSH_CACHE_DIR/completions/_k9s"
-    k9s completion zsh | tee "$ZSH_CACHE_DIR/completions/_k9s" >/dev/null &|
-  fi
+if (( ! $+commands[k9s] )); then
+  return
 fi
+
+# If the completion file does not exist, fake it and load it
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_k9s" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _k9s
+  _comps[k9s]=_k9s
+fi
+
+# and then generate it in the background. On first completion,
+# the actual completion file will be loaded.
+k9s completion zsh >| "$ZSH_CACHE_DIR/completions/_k9s" &|
