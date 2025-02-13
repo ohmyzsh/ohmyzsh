8924759b125eb0e52fc60367303bbfa1986602b0
Fabricio Silva <hi@fabricio.dev>
2025-02-13 11:16:07 +0000

    feat(fnm): add autostart option to setup fnm env
    Add new setting `ZSH_FNM_AUTOSTART` to be able to automatic setup the `fnm env`.
    Default set to `false` to avoid breaking existing setup.

--- plugins/fnm/fnm.plugin.zsh
+++ plugins/fnm/fnm.plugin.zsh
@@ -2,6 +2,11 @@ if (( ! $+commands[fnm] )); then
   return
 fi
 
+# CONFIGURATION VARIABLES
+# Automatically start the fnm multishell
+: ${ZSH_FNM_AUTOSTART:=false} # opt-in to avoid duplicated sessions
+: ${ZSH_FNM_USE_ON_CD:=true} # upstream recommendation
+
 # If the completion file doesn't exist yet, we need to autoload it and
 # bind it to `fnm`. Otherwise, compinit will have already done that.
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_fnm" ]]; then
@@ -11,3 +16,9 @@ if [[ ! -f "$ZSH_CACHE_DIR/completions/_fnm" ]]; then
 fi
 
 fnm completions --shell=zsh >| "$ZSH_CACHE_DIR/completions/_fnm" &|
+
+if [[ "$ZSH_FNM_AUTOSTART" == "true" ]]; then
+  local -a fnm_env_cmd
+  [[ "$ZSH_FNM_USE_ON_CD" == "true" ]] && fnm_env_cmd+=("--use-on-cd")
+  eval "$(fnm env --shell=zsh $fnm_env_cmd)"
+fi
