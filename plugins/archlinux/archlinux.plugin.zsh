6543392358c6ad0203b0ff6578ecfd2ba0d84cb3
deimosian <deimosian@gmail.com>
2025-02-16 01:59:30 -0500

    Actually check repo version
    Actually check repo version of keyring package on first run of upgrade...

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -180,7 +180,7 @@ fi
 function upgrade() {
   echo ":: Checking Arch Linux PGP Keyring..."
   local installedver="$(LANG= sudo pacman -Qi archlinux-keyring | grep -Po '(?<=Version         : ).*')"
-  local currentver="$(LANG= sudo pacman -Si archlinux-keyring | grep -Po '(?<=Version         : ).*')"
+  local currentver="$(LANG= sudo pacman -Syi archlinux-keyring | grep -Po '(?<=Version         : ).*')"
   if [ $installedver != $currentver ]; then
     echo " Arch Linux PGP Keyring is out of date."
     echo " Updating before full system upgrade."
