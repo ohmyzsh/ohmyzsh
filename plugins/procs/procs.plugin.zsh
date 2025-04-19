de1ca65dcaebd19f5ca6626616bb79b529362458
Adam Page <adam.page@investorflow.com>
2025-04-19 15:08:03 -0400

    feat(procs): support completion for `procs<0.14` (#13053)
    Co-authored-by: Carlo Sala <carlosalag@protonmail.com>

--- plugins/procs/procs.plugin.zsh
+++ plugins/procs/procs.plugin.zsh
@@ -3,11 +3,19 @@ if (( ! $+commands[procs] )); then
 fi
 
 # If the completion file doesn't exist yet, we need to autoload it and
-# bind it to `minikube`. Otherwise, compinit will have already done that.
+# bind it to `procs`. Otherwise, compinit will have already done that.
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_procs" ]]; then
   typeset -g -A _comps
   autoload -Uz _procs
   _comps[procs]=_procs
 fi
 
-procs --gen-completion-out zsh >| "$ZSH_CACHE_DIR/completions/_procs" &|
+{
+  autoload -Uz is-at-least
+  local _version=$(procs --version)
+  if is-at-least "0.14" "${_version#procs }"; then
+    procs --gen-completion-out zsh >| "$ZSH_CACHE_DIR/completions/_procs"
+  else
+    procs --completion-out zsh >| "$ZSH_CACHE_DIR/completions/_procs"
+  fi
+} &|
