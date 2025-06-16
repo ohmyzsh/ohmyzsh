2fbdfa93f942611c56bb02657a39f577bd7f0ef0
Sergii Tkachenko <sergiitk@google.com>
2025-06-16 07:54:12 -0700

    fix(init): ensure hostname is always valid for macOS (#13171)

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -99,8 +99,8 @@ done
 
 # Figure out the SHORT hostname
 if [[ "$OSTYPE" = darwin* ]]; then
-  # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
-  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST="${HOST/.*/}"
+  # macOS's $HOST changes with dhcp, etc. Use LocalHostName if possible.
+  SHORT_HOST=$(scutil --get LocalHostName 2>/dev/null) || SHORT_HOST="${HOST/.*/}"
 else
   SHORT_HOST="${HOST/.*/}"
 fi
