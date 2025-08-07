f6f2de9b6ce51ee557c2168599cdca671a4dde9c
xusd320 <xusd320@gmail.com>
2025-08-07 14:46:00 +0800

    feat(web-search): add crates.io, docs.rs (#13250)

--- plugins/direnv/README.md
+++ plugins/direnv/README.md
@@ -1,6 +1,6 @@
 # direnv plugin
 
-This plugin creates the [Direnv](https://direnv.net/) hook.
+This plugin initializes the [Direnv](https://direnv.net/) shell hook for zsh.
 
 To use it, add `direnv` to the plugins array in your zshrc file:
 
@@ -10,6 +10,6 @@ plugins=(... direnv)
 
 ## Requirements
 
-In order to make this work, you will need to have the direnv installed.
+In order to make this work, you will need to have direnv installed and available in your PATH.
 
 More info on the usage and install: https://github.com/direnv/direnv
--- plugins/direnv/direnv.plugin.zsh
+++ plugins/direnv/direnv.plugin.zsh
@@ -4,16 +4,8 @@ if (( ! $+commands[direnv] )); then
   return
 fi
 
-_direnv_hook() {
-  trap -- '' SIGINT;
-  eval "$(direnv export zsh)";
-  trap - SIGINT;
-}
-typeset -ag precmd_functions;
-if [[ -z "${precmd_functions[(r)_direnv_hook]+1}" ]]; then
-  precmd_functions=( _direnv_hook ${precmd_functions[@]} )
-fi
-typeset -ag chpwd_functions;
-if [[ -z "${chpwd_functions[(r)_direnv_hook]+1}" ]]; then
-  chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
+# Load direnv's shell hook, which is the supported integration mechanism for
+# modern direnv versions.
+if ! eval "$(direnv hook zsh)"; then
+  echo "Warning: direnv hook failed to initialize. Please ensure direnv is installed correctly."
 fi
