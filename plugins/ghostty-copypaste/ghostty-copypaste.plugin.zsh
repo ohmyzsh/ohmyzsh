0347ab360fa77e7fcb1c222a0c81eb892ad26931
Louis Rubet <louis.rubet@hudl.com>
2025-10-07 18:38:43 +0200

    fix: Multi-line syntax after coding style

--- plugins/ghostty-copypaste/ghostty-copypaste.plugin.zsh
+++ plugins/ghostty-copypaste/ghostty-copypaste.plugin.zsh
@@ -1,22 +1,21 @@
 function zle-copy-to-clipboard {
     local selected_text start_pos end_pos
 
-    (( $MARK < $CURSOR ))
-        && { start_pos=$MARK; end_pos=$CURSOR; }
+    (( $MARK < $CURSOR )) \
+        && { start_pos=$MARK; end_pos=$CURSOR; } \
         || { start_pos=$CURSOR; end_pos=$MARK; }
 
-    (( start_pos < 0 || end_pos < 0 ))
+    (( start_pos < 0 || end_pos < 0 )) \
         && zle -R && return
 
     selected_text="${BUFFER[$start_pos+1, $end_pos]}"
 
-    command -v xclip &> /dev/null
-        && ( echo -n "$selected_text" | xclip -selection clipboard )
+    command -v xclip &> /dev/null \
+        && ( echo -n "$selected_text" | xclip -selection clipboard ) \
         && zle -R && return
 
-    command -v wl-copy &> /dev/null
-        && ( echo -n "$selected_text" | wl-copy )
-        && zle -R && return
+    command -v wl-copy &> /dev/null \
+        && ( echo -n "$selected_text" | wl-copy ) && zle -R && return
 
     zle -R
 }
@@ -24,9 +23,9 @@ function zle-copy-to-clipboard {
 function zle-paste-from-clipboard {
     local clipboard_content
 
-    command -v xclip &> /dev/null
+    command -v xclip &> /dev/null \
         && clipboard_content=$(xclip -selection clipboard -o 2>/dev/null)
-    command -v wl-paste &> /dev/null
+    command -v wl-paste &> /dev/null \
         && clipboard_content=$(wl-paste --no-newline 2>/dev/null)
 
     [[ -z "$clipboard_content" ]] && return
