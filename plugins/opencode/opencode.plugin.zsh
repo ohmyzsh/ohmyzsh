d97b7fd7f61c1669a25c109435b88037c1446cb7
Pranav Avva <pranav.avva@gmail.com>
2026-01-28 13:27:04 -0500

    style(opencode): comply with line length style rule
    Signed-off-by: Pranav Avva <pranav.avva@gmail.com>

--- plugins/opencode/opencode.plugin.zsh
+++ plugins/opencode/opencode.plugin.zsh
@@ -1,5 +1,6 @@
 if ! (( $+commands[opencode] )); then
-  print "zsh opencode plugin: opencode not found. Please install opencode before using this plugin." >&2
+  print "zsh opencode plugin: opencode not found. Please install opencode \
+    before using this plugin." >&2
   return 1
 fi
 
@@ -8,7 +9,8 @@ _opencode_yargs_completions() {
   local reply
   local si=$IFS
   IFS=$'
-' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" opencode --get-yargs-completions "${words[@]}"))
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" \
+  COMP_POINT="$CURSOR" opencode --get-yargs-completions "${words[@]}"))
   IFS=$si
   if [[ ${#reply} -gt 0 ]]; then
     _describe 'values' reply
