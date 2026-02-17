63552ef44bcd378701ebceca1f1b71bf9757548b
Pascal Kontschan <pascal.kontschan@sva.de>
2026-02-17 22:32:09 +0100

    feat(molecule): added aliases for molecule

--- plugins/molecule/molecule.plugin.zsh
+++ plugins/molecule/molecule.plugin.zsh
@@ -12,3 +12,26 @@ if [[ ! -f "$ZSH_CACHE_DIR/completions/_molecule" ]]; then
 fi
 
 _MOLECULE_COMPLETE=zsh_source molecule >| "$ZSH_CACHE_DIR/completions/_molecule" &|
+
+# Alias
+# molecule: https://docs.ansible.com/projects/molecule/
+alias mol='molecule '
+alias mhlp='molecule --help '
+alias mchk='molecule check '
+alias mcln='molecule cleanup '
+alias mcnv='molecule converge '
+alias mcrt='molecule create '
+alias mdep='molecule dependency '
+alias mdes='molecule destroy '
+alias mdrv='molecule drivers '
+alias midm='molecule idempotence '
+alias minit='molecule init scenario '
+alias mlst='molecule list '
+alias mlgn='molecule login '
+alias mtrx='molecule matrix '
+alias mprp='molecule prepare '
+alias mrst='molecule reset '
+alias msef='molecule side-effect '
+alias msyn='molecule syntax '
+alias mtest='molecule test '
+alias mvrf='molecule verify '
