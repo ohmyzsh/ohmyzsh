d6fa1686be03225f3e6a14e34f2ab05052ac0680
Alexander Schaber <a.schaber@dachs-it.de>
2026-04-02 23:43:41 +0200

    refactor(plugins): use cached background completion generation

--- plugins/svcat/svcat.plugin.zsh
+++ plugins/svcat/svcat.plugin.zsh
@@ -1,6 +1,14 @@
-# Autocompletion for svcat.
-#
-
-if [ $commands[svcat] ]; then
-  source <(svcat completion zsh)
+# Autocompletion for the Service Catalog CLI (svcat).
+if (( ! $+commands[svcat] )); then
+  return
 fi
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `svcat`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_svcat" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _svcat
+  _comps[svcat]=_svcat
+fi
+
+svcat completion zsh >| "$ZSH_CACHE_DIR/completions/_svcat" &|
