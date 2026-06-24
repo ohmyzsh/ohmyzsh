bfe6c129684d146f9f019269ce429d57d18156f2
Sri Harsha Ponukumati <hponukumati@gmail.com>
2026-06-24 11:42:07 -0700

    chore(key-bindings): remove dead commented-out emacs keybindings
    These ~18 lines have been commented out and unreferenced for a long
    time with no active emacs keybindings code path; remove for clarity.

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -125,21 +125,3 @@ bindkey '\C-x\C-e' edit-command-line
 # file rename magick
 bindkey "^[m" copy-prev-shell-word
 
-# consider emacs keybindings:
-
-#bindkey -e  ## emacs key bindings
-#
-#bindkey '^[[A' up-line-or-search
-#bindkey '^[[B' down-line-or-search
-#bindkey '^[^[[C' emacs-forward-word
-#bindkey '^[^[[D' emacs-backward-word
-#
-#bindkey -s '^X^Z' '%-^M'
-#bindkey '^[e' expand-cmd-path
-#bindkey '^[^I' reverse-menu-complete
-#bindkey '^X^N' accept-and-infer-next-history
-#bindkey '^W' kill-region
-#bindkey '^I' complete-word
-## Fix weird sequence that rxvt produces
-#bindkey -s '^[[Z' '\t'
-#
