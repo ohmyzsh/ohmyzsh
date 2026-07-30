f9d07dc1a7ae300976d40655e7232d3241966db0
codeDude <codeDude@disroot.org>
2026-07-30 15:59:12 -0600

    Add a autocompletation plugin for forgejo-cli Fix #13909

---
+++ plugins/fj/fj.plugin.zsh
@@ -0,0 +1,14 @@
+# Autocompletion for the Forgejo CLI (fj).
+if (( ! $+commands[fj] )); then
+  return
+fi
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `fj`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_fj" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _fj
+  _comps[fj]=_fj
+fi
+
+fj completion --shell zsh >| "$ZSH_CACHE_DIR/completions/_fj" &|
