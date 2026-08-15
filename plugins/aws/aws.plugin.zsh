a0f3fe81da789138602ca7ecd67bbbbe244b89db
deepshekhardas <deepshekharadas1234@gmail.com>
2026-08-15 07:58:15 +0530

    fix(aws): asr() prints error to stderr and resets terminal color

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -69,7 +69,8 @@ function asr() {
   local -a available_regions
   available_regions=($(aws_regions))
   if [[ -z "${available_regions[(r)$1]}" ]]; then
-    echo "${fg[red]}Available regions: \n$(aws_regions)"
+    echo "${fg[red]}Region '$1' not found" >&2
+    echo "Available regions: ${(j:, :)available_regions:-no regions found}${reset_color}" >&2
     return 1
   fi
 
