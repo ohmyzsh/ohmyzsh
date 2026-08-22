553a2c387a1f4e5585ae74ee9cba27cc8139907b
deepshekhardas <deepshekharadas1234@gmail.com>
2026-08-22 07:23:33 +0530

    fix(jj): avoid racing compinit with async completion generation

--- plugins/jj/jj.plugin.zsh
+++ plugins/jj/jj.plugin.zsh
@@ -11,7 +11,7 @@ if [[ ! -f "$ZSH_CACHE_DIR/completions/_jj" ]]; then
   _comps[jj]=_jj
 fi
 
-COMPLETE=zsh jj >| "$ZSH_CACHE_DIR/completions/_jj" &|
+COMPLETE=zsh jj >| "$ZSH_CACHE_DIR/completions/_jj.tmp.$$" && mv -f "$ZSH_CACHE_DIR/completions/_jj.tmp.$$" "$ZSH_CACHE_DIR/completions/_jj" &|
 
 function __jj_prompt_jj() {
   local -a flags
