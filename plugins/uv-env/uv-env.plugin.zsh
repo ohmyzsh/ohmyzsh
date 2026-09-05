aad7d091e71a30055c7fd4f017d075751ce6c4ca
Fazle Arefin <fazlearefin@yahoo.com>
2026-09-05 18:24:37 +1000

    fix(uv-env): address review feedback from robbyrussell
    - Add `local` to venv_dir to prevent leaking into user's shell
    - Replace `export` with `typeset -g` for uv_active and uv_dir
    - Use `autoload -Uz` to match the style guide

--- plugins/uv-env/uv-env.plugin.zsh
+++ plugins/uv-env/uv-env.plugin.zsh
@@ -5,22 +5,22 @@ _toggleUvShell() {
   fi
 
   if [[ $uv_active -eq 1 ]] && { [[ $in_uv_dir -eq 0 ]] || [[ "$PWD" != "$uv_dir" && "$PWD" != "$uv_dir"/* ]]; }; then
-    export uv_active=0
+    typeset -g uv_active=0
     unset uv_dir
     (( $+functions[deactivate] )) && deactivate
   fi
 
   if [[ $in_uv_dir -eq 1 ]] && [[ $uv_active -ne 1 ]]; then
-    venv_dir="${UV_PROJECT_ENVIRONMENT:-$PWD/.venv}"
+    local venv_dir="${UV_PROJECT_ENVIRONMENT:-$PWD/.venv}"
     [[ "$venv_dir" != /* ]] && venv_dir="$PWD/$venv_dir"
 
     if [[ -f "${venv_dir}/bin/activate" ]]; then
-      export uv_active=1
-      export uv_dir="$PWD"
+      typeset -g uv_active=1
+      typeset -g uv_dir="$PWD"
       source "${venv_dir}/bin/activate"
     fi
   fi
 }
-autoload -U add-zsh-hook
+autoload -Uz add-zsh-hook
 add-zsh-hook chpwd _toggleUvShell
 _toggleUvShell
